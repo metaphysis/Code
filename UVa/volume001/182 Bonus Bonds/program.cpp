1c02216d34897d3909673bec88827d5ddccdc90a
metaphysis <metaphysis@yeah.net>
2016-03-11 13:25:41 +0800

    Bonus Bonds

--- UVa/volume001/182 Bonus Bonds/program.cpp
+++ UVa/volume001/182 Bonus Bonds/program.cpp
@@ -1,8 +1,8 @@
 // Bonus Bonds
 // UVa IDs: 182
-// Verdict: 
-// Submission Date: 
-// UVa Run Time: s
+// Verdict: Accepted
+// Submission Date: 2016-03-11
+// UVa Run Time: 0.000s
 //
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
@@ -15,26 +15,22 @@
 using namespace std;
 
 string numberText, positionText;
-long long number, position;
 
-bool calculate()
+void calculate()
 {
     int region = numberText[2] - '0';
+
     numberText.erase(numberText.begin() + 2);
-    number = stoll(numberText);
-    position = stoll(positionText);
+    long long number = stoll(numberText);
+    long long position = stoll(positionText);
 
     if (number == 0 || (number == 1 && region == 0))
     {
         for (int i = 1; i <= 10; i++)
             cout << right << setw(11) << 0 << endl;
-        return true;
+        return;
     }
-        
-    cout << "region: " << region << endl;
-    cout << "position: " << position << endl;
-    cout << "numberText: " << numberText << endl;
-    
+
     vector < int > digits;
     long long temp = number - 1;
     while (temp)
@@ -42,27 +38,21 @@ bool calculate()
         digits.push_back(temp % 10);
         temp /= 10;
     }
-    while (digits.size() < numberText)
+    while (digits.size() < numberText.length())
         digits.push_back(0);
-    reverse(digits.bgin(), digits.end());
-    
-    else if (position == 3)
+    reverse(digits.begin(), digits.end());
+
+    for (int i = 0; i <= 9; i++)
     {
-        for (int i = 0; i <= 9; i++)
+        long long times = 0;
+
+        if (position == 3)
         {
-            long long times = 0;
             if (i == region)
                 times = number;
-            if (i == 0 && region == 0)
-                times--;
-            cout << right << setw(11) << times << endl;
         }
-    }
-    else if (position == 1)
-    {
-        for (int i = 0; i <= 9; i++)
+        else if (position == 1)
         {
-            long long times = 0;
             if (i < digits[0])
                 times = pow(10, digits.size() - 1);
             else if (i == digits[0])
@@ -71,31 +61,41 @@ bool calculate()
                     times = times * 10 + digits[j];
                 times += 1;
             }
-            if (i == 0 && region == 0)
-                times--;
-            cout << right << setw(11) << times << endl;
         }
-    }
-    else if (position == 10)
-    {
-        for (int i = 0; i <= 9; i++)
+        else if (position == 10)
         {
-            long long times = 0;
             for (int j = 0; j <= 7; j++)
                 times += digits[j] * pow(10, 7 - j);
             if (i <= digits[8])
                 times++;
-            if (i == 0 && region == 0)
-                times--;
-            cout << right << setw(11) << times << endl;
         }
+        else
+        {
+            int tempPosition = position;
+            if (tempPosition > 3)
+                tempPosition--;
+            tempPosition--;
+
+            for (int j = 0; j < tempPosition; j++)
+                times += digits[j] * pow(10, 7 - j);
+            if (i < digits[tempPosition])
+            {
+                times += pow(10, digits.size() - (tempPosition + 1));
+            }
+            else if (i == digits[tempPosition])
+            {
+                long long temp = 0;
+                for (int j = tempPosition + 1; j < digits.size(); j++)
+                    temp = temp * 10 + digits[j];
+                times += temp;
+                times++;
+            }
+        }
+
+        if (i == 0 && region == 0)
+            times--;
+        cout << right << setw(11) << times << endl;
     }
-    else
-    {
-        
-    }
-    
-    return true;
 }
 
 int main(int argc, char *argv[])
@@ -105,15 +105,15 @@ int main(int argc, char *argv[])
     {
         if (positionText == "0")
             break;
-            
+
         if (printBlankLine)
             cout << "\n";
-            
+
         if (printBlankLine == false)
             printBlankLine = true;
-  
+
         calculate();
     }
-    
-	return 0;
+
+    return 0;
 }
