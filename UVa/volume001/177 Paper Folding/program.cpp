e84e30f535c17bb1f2e75d2e0e22352ca59abb0d
metaphysis <metaphysis@yeah.net>
2016-03-11 21:24:19 +0800

    Paper Folding

--- UVa/volume001/177 Paper Folding/program.cpp
+++ UVa/volume001/177 Paper Folding/program.cpp
@@ -32,58 +32,62 @@ string unfolding(int n)
 void display(int n)
 {
     string pattern = unfolding(n);
-    memset(matrix, 0, sizeof(matrix));
-    
+    pattern += '0';
+
+    memset(matrix, ' ', sizeof(matrix));
     int x = 512, y = 512;
-    for (int i = 0; i < pattern.length(); i++)
+    int minx = 512, maxx = 512, miny = 512, maxy = 512;
+    for (int i = 0; i < pattern.length() - 1; i++)
     {
         if (pattern[i] == 'L')
         {
             matrix[x][y] = '_';
+            y -= 1;
         }
         else if (pattern[i] == 'R')
         {
             matrix[x][y] = '_';
+            y += 1;
         }
         else if (pattern[i] == 'U')
         {
             matrix[x][y] = '|';
+            x -= 1;
+            if (pattern[i + 1] == 'L')
+                y -= 1;
+            if (pattern[i + 1] == 'R')
+                y += 1;
         }
-        else
+        else if (pattern[i] == 'D')
         {
+            x += 1;
             matrix[x][y] = '|';
+            if (pattern[i + 1] == 'L')
+                y -= 1;
+            if (pattern[i + 1] == 'R')
+                y += 1;
+        }
+
+        minx = min(x, minx);
+        maxx = max(x, maxx);
+        miny = min(y, miny);
+        maxy = max(y, maxy);
+    }
+
+    for (int i = minx; i <= maxx; i++)
+    {
+        int lasty = maxy;
+        while (matrix[i][lasty] == ' ')
+            lasty--;
+
+        if (lasty >= 0)
+        {
+            for (int j = miny; j <= lasty; j++)
+                cout << matrix[i][j];
+            cout << "\n";
         }
     }
 
-    // left margin
-    int left = 1023;
-    for (int i = 0; i < 1024; i++)
-        for (int j = 0; j < 1024; j++)
-        {
-            if (matrix[i][j] != 0)
-            {
-                if (j < left)
-                    left = j;
-                break;
-            }
-        }
-    
-    for (int i = 0; i < 1024; i++)
-    {
-        int end = -1;
-        for (int j = 1023; j >= 0; j--)
-            if (matrix[i][j] != 0)
-            {
-                end = j;
-                break;
-            }
-        if (end == -1)
-            continue;
-            
-        for (int j = left; j <= end; j++)
-            if (matrxi
-    }
-    
     cout << "^\n";
 }
 
@@ -91,15 +95,15 @@ int main(int argc, char *argv[])
 {
     cin.tie(0);
     cout.sync_with_stdio(false);
-    
+
     mapped.insert(make_pair('R', 'U'));
     mapped.insert(make_pair('U', 'L'));
     mapped.insert(make_pair('L', 'D'));
     mapped.insert(make_pair('D', 'R'));
-    
+
     int n;
     while (cin >> n, n)
         display(n);
-    
-	return 0;
+
+    return 0;
 }
