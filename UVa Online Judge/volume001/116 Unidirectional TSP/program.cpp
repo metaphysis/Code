905392d17f5cf8b57d5399983def2f0cd8f47607
metaphysis <metaphysis@yeah.net>
2016-03-12 08:43:19 +0800

    Dole Queue

---
+++ UVa Online Judge/volume001/116 Unidirectional TSP/program.cpp
@@ -0,0 +1,94 @@
+// Unidirectional TSP
+// UVa IDs: 116
+// Verdict: Accepted
+// Submission Date: 2011-10-12
+// UVa Run Time: 0.252s
+//
+// 版权所有（C）2011，邱秋。metaphysis # yeah dot net
+
+#include <iostream>
+
+using namespace std;
+
+#define MAXLINE 11
+#define MAXROW 101
+#define MAXINT (1 << 30 - 1)
+
+int main(int ac, char *av[])
+{
+	int line, row, tag, tmp;
+	int successor[MAXLINE][MAXROW];
+	long long matrix[MAXLINE][MAXROW];
+	long long min;
+	
+	while (cin >> line >> row)
+	{
+		for (int i = 1; i <= line; i++)
+			for (int j = 1; j <= row; j++)
+				cin >> matrix[i][j];
+
+		// 列优先处理，从倒数第二列开始处理，因为需要字典序最小的最小权和路径，如果从左
+		// 往右处理，可以得到行号最小的权和方案，但是不一定是字典序最小的。
+		for (int j = row - 1; j >= 1; j--)
+		{
+			for (int i = 1; i <= line; i++)
+			{
+				min = MAXINT;
+
+				tmp = (i == 1 ? line : i - 1);
+				if (min > matrix[tmp][j + 1])
+				{
+					min = matrix[tmp][j + 1];
+					tag = tmp;
+				}
+				else if (min == matrix[tmp][j + 1] && tag > tmp)
+					tag = tmp;
+
+				if (min > matrix[i][j + 1])
+				{
+					min = matrix[i][j + 1];
+					tag = i;
+				}
+				else if (min == matrix[i][j + 1] && tag > i)
+					tag = i;
+
+				tmp = (i == line ? 1 : i + 1);
+				if (min > matrix[tmp][j + 1])
+				{
+					min = matrix[tmp][j + 1];
+					tag = tmp;
+				}
+				else if (min == matrix[tmp][j + 1] && tag > tmp)
+					tag = tmp;
+
+				matrix[i][j] += min;
+				successor[i][j] = tag;
+			}
+		}
+
+		// 找权和最小的单元格。
+		min = MAXINT;
+		for (int i = 1; i <= line; i++)
+			if (matrix[i][1] < min)
+			{
+				min = matrix[i][1];
+				tag = i;
+			}
+
+		// 输出。
+		cout << tag;
+		int next = 1;
+		int tmp = tag;
+		while (next < row)
+		{
+			cout << " " << successor[tmp][next];
+			tmp = successor[tmp][next];
+			next++;
+		}
+
+		cout << endl;
+		cout << matrix[tag][1] << endl;
+	}
+
+	return 0;
+}
---
+++ UVa Online Judge/volume001/120 Stacks of Flapjacks/program.cpp
@@ -0,0 +1,96 @@
+// Stacks of Flapjacks
+// UVa IDs: 120
+// Verdict: Accepted
+// Submission Date: 2011-05-22
+// UVa Run Time: 0.028s
+//
+// 版权所有（C）2011，邱秋。metaphysis # yeah dot net
+	
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+	
+using namespace std;
+	
+#define MAXSIZE 30
+	
+struct pancake
+{
+	int diameter;
+	int index;
+};
+	
+pancake pancakes[MAXSIZE];
+pancake original[MAXSIZE];
+	
+bool cmp(pancake x, pancake y)
+{
+	return x.diameter < y.diameter;
+}
+	
+void flip(int pos, int size)
+{
+	pancake tmp;
+	int i = 0, j = size - pos;
+	for (; i < j; i++, j--)
+		if (original[i].diameter != original[j].diameter)
+		{
+			tmp = original[i];
+			original[i] = original[j];
+			original[j] = tmp;
+		}
+}
+	
+int main(int ac, char *av[])
+{
+	string line;
+	while (getline(cin, line))
+	{
+		// 回显。
+		cout << line << endl;
+	
+		// 读入数据。
+		int capacity = 0;
+		istringstream iss(line);
+		while (iss >> pancakes[capacity].diameter)
+		{
+			pancakes[capacity].index = capacity;
+			original[capacity] = pancakes[capacity];
+			capacity++;
+		}
+	
+		// 排序。
+		sort(pancakes, pancakes + capacity, cmp);
+	
+		// 执行翻转操作，若第 i 大元素未在第i位上，则先找到其序号，然后
+		// 先将其翻转到顶端，然后再翻转到位置i。
+		for (int i = capacity - 1; i >= 0; i--)
+		{
+			// 在当前序列中找到该元素。
+			// 假如数原来的序号与当前的序号不等，需要翻转操作。
+			int marker;
+			for (int j = 0; j < capacity; j++)
+				if (original[j].index == pancakes[i].index)
+				{
+					marker = j;
+					break;
+				}
+	
+			if (marker != i)
+			{
+				if (marker != 0)
+				{
+					cout << (capacity - marker) << " ";
+					flip(capacity - marker, capacity);
+				}
+
+				cout << (capacity - i) << " ";
+				flip(capacity - i, capacity);
+			}
+		}
+	
+		cout << "0" << endl;
+	}
+	
+	return 0;
+}
