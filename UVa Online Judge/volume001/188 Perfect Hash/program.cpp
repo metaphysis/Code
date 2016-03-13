f2b73b0d7a1585a95b765df0031e9fcf7c1e2161
metaphysis <metaphysis@yeah.net>
2016-03-13 22:57:06 +0800

    Perfect Hash

---
+++ UVa Online Judge/volume001/188 Perfect Hash/program.cpp
@@ -0,0 +1,19 @@
+// Perfect Hash
+// UVa IDs: 188
+// Verdict: 
+// Submission Date: 
+// UVa Run Time: s
+//
+// 版权所有（C）2016，邱秋。metaphysis # yeah dot net
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
