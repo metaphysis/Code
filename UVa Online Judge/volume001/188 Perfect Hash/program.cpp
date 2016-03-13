bb6a27cbdaf454b89947706b0ddfb659ce634cd5
metaphysis <metaphysis@yeah.net>
2016-03-13 23:34:34 +0800

    Perfect Hash

--- UVa Online Judge/volume001/188 Perfect Hash/program.cpp
+++ UVa Online Judge/volume001/188 Perfect Hash/program.cpp
@@ -12,8 +12,65 @@
 
 using namespace std;
 
+vector < int > words;
+
+bool test(long long C)
+{
+    vector < int > used;
+    for (int i = 0; i < words.size(); i++)
+        used.push_back(true);
+      
+    for (int i = 0; i < words.size(); i++)
+    {
+        int remainder = C / words[i] % words.size();
+        if (used[remainder] == false)
+            return false;
+        else
+            used[remainder] = true;
+    }
+            
+        
+    return true;
+}
+
+long long find()
+{
+    long long C = 1;
+    int n = words.size();
+    
+    do
+    {
+        long long oldC = C;
+        for (int i = 0; i < words.size(); i++)
+        {
+            
+            C = max(
+        }
+        
+    } while (test() == false);
+    
+    return C;
+}
+
 int main(int argc, char *argv[])
 {
-
+    string line, block;
+    while (getline(cin, line))
+    {
+        istringstream iss(line);
+        while (iss >> block)
+        {
+            int number = 0;
+            for (int i = 0; i < block.length(); i++)
+                number = number * 32 + block[i] - 'a' + 1;
+            words.push_back(number);
+            
+            sort(words.begin(), words.end());
+            
+            cout << line << "\n";
+            cout << find() << "\n";
+        }
+    }
+    
 	return 0;
 }
