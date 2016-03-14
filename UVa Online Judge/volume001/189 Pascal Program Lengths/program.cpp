75d4161d59a11352b0aef9d3c99414970a6dd8b6
metaphysis <metaphysis@yeah.net>
2016-03-14 08:27:12 +0800

    Pascal Program Lengths

---
+++ UVa Online Judge/volume001/189 Pascal Program Lengths/program.cpp
@@ -0,0 +1,26 @@
+// Pascal Program Lengths
+// UVa IDs: 189
+// Verdict: 
+// Submission Date: 
+// UVa Run Time: s
+//
+// 版权所有（C）2016，邱秋。metaphysis # yeah dot net
+
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    string line;
+    while (getline(cin, line))
+    {
+        
+    }
+    
+	return 0;
+}
