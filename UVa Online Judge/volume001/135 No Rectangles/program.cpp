ca04d92e81c6741d5e95b87924242e6efc5bff0d
metaphysis <metaphysis@yeah.net>
2016-04-05 17:04:26 +0800

    2016-04-05

--- UVa Online Judge/volume001/135 No Rectangles/program.cpp
+++ UVa Online Judge/volume001/135 No Rectangles/program.cpp
@@ -1,8 +1,8 @@
 // No Rectangles
 // UVa IDs: 135
-// Verdict: 
-// Submission Date: 
-// UVa Run Time: s
+// Verdict: Accepted
+// Submission Date: 2016-04-05
+// UVa Run Time: 0.003s
 //
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
@@ -12,6 +12,37 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+    cin.tie(0);
+    cout.sync_with_stdio(false);
+    
+    int k;
+    bool printBlank = false;
+    while (cin >> k)
+    {
+        if (printBlank)
+            cout << "\n";
+        else
+            printBlank = true;
 
-	return 0;
+        int m = k - 1;
+        for (int i = 0; i < k; i++)
+        {
+            cout << 1;
+            for (int j = 1; j < k; j++)
+                cout << ' ' << i * m + j + 1;
+            cout << "\n";
+
+        }
+
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < m; j++)
+            {
+                cout << i + 2;
+                for (int s = 0; s < m; s++)
+                    cout << ' ' << (j + (s * i)) % m + s * m + k + 1;
+                cout << "\n";
+            }
+    }
+
+    return 0;
 }
