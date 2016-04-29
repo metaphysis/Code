8da7607e25af9c9cc34b628b023b9345bb7d937d
metaphysis <metaphysis@yeah.net>
2016-04-29 22:05:41 +0800

    2016-04-29

--- UVa Online Judge/volume002/214 Code Generation/program.cpp
+++ UVa Online Judge/volume002/214 Code Generation/program.cpp
@@ -1,8 +1,8 @@
 // Code Generation
 // UVa IDs: 214
-// Verdict: 
-// Submission Date: 
-// UVa Run Time: s
+// Verdict: Accepted
+// Submission Date: 2016-04-29
+// UVa Run Time: 0.000s
 //
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
