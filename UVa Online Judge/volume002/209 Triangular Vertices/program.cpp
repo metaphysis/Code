845b341f82b3ce684196a974b397e44ea660bc75
metaphysis <metaphysis@yeah.net>
2016-04-30 17:11:18 +0800

    2016-04-30

--- UVa Online Judge/volume002/209 Triangular Vertices/program.cpp
+++ UVa Online Judge/volume002/209 Triangular Vertices/program.cpp
@@ -7,66 +7,57 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <iostream>
-#include <iomanip>
 #include <sstream>
 #include <vector>
 #include <algorithm>
-#include <limits>
 
 using namespace std;
 
-vector < int >rightmost, vertices, level;
-int leftParent[33000], rightParent[33000];
+vector < int > vertices, levels;
+int secondary[33000], principal[33000], level[33000];
 
-int findLeft(int x)
-{
-    return x == leftParent[x] ? x : findLeft(leftParent[x]);
-}
-
-int findRight(int x)
-{
-    return x == rightParent[x] ? x : findRight(rightParent[x]);
-}
-
-void initialize()
+void makeSet()
 {
     for (int n = 0, step = 1; (n + step) < 33000; n += step, step++)
     {
         int parent = n + step;
-        rightmost.push_back(parent);
-        leftParent[parent] = parent;
+        secondary[parent] = parent;
         for (int i = parent, j = step; (i + j) < 33000; i += j, j++)
-            leftParent[i + j] = parent;
+            secondary[i + j] = parent;
     }
 
     for (int n = 1, step = 0; (n + step) < 33000; n += step, step++)
     {
         int parent = n + step;
-        rightParent[parent] = parent;
+        principal[parent] = parent;
         for (int i = parent, j = step + 2; (i + j) < 33000; i += j, j++)
-            rightParent[i + j] = parent;
+            principal[i + j] = parent;
     }
+    
+    for (int n = 1, step = 1; n < 33000; step++)
+        for (int i = 1; i <= step && n < 33000; i++)
+            level[n++] = step;
 }
 
 bool triangle()
 {
-    if (level[1] == level[2])
+    if (levels[1] == levels[2])
     {
-        if ((level[1] - level[0]) != (vertices[2] - vertices[1]))
+        if ((levels[1] - levels[0]) != (vertices[2] - vertices[1]))
             return false;
-        if (findLeft(vertices[0]) != findLeft(vertices[1]))
+        if (secondary[vertices[0]] != secondary[vertices[1]])
             return false;
-        if (findRight(vertices[0]) != findRight(vertices[2]))
+        if (principal[vertices[0]] != principal[vertices[2]])
             return false;
         return true;
     }
-    else if (level[0] == level[1])
+    else if (levels[0] == levels[1])
     {
-        if ((level[2] - level[0]) != (vertices[1] - vertices[0]))
+        if ((levels[2] - levels[0]) != (vertices[1] - vertices[0]))
             return false;
-        if (findRight(vertices[0]) != findRight(vertices[2]))
+        if (principal[vertices[0]] != principal[vertices[2]])
             return false;
-        if (findLeft(vertices[1]) != findLeft(vertices[2]))
+        if (secondary[vertices[1]] != secondary[vertices[2]])
             return false;
         return true;
     }
@@ -76,27 +67,27 @@ bool triangle()
 
 bool parallelogram()
 {
-    if (level[0] == level[1] && level[2] == level[3])
+    if (levels[0] == levels[1] && levels[2] == levels[3])
     {
         if ((vertices[1] - vertices[0]) != (vertices[3] - vertices[2]))
             return false;
-        if ((vertices[1] - vertices[0]) != (level[2] - level[0]))
+        if ((vertices[1] - vertices[0]) != (levels[2] - levels[0]))
             return false;
-        if (findLeft(vertices[0]) == findLeft(vertices[2]))
+        if (secondary[vertices[0]] == secondary[vertices[2]])
             return true;
-        if (findRight(vertices[0]) == findRight(vertices[2]))
+        if (principal[vertices[0]] == principal[vertices[2]])
             return true;
         return false;
     }
-    else if (level[1] == level[2])
+    else if (levels[1] == levels[2])
     {
-        if ((level[1] - level[0]) != (vertices[2] - vertices[1]))
+        if ((levels[1] - levels[0]) != (vertices[2] - vertices[1]))
             return false;
-        if ((level[3] - level[1]) != (vertices[2] - vertices[1]))
+        if ((levels[3] - levels[1]) != (vertices[2] - vertices[1]))
             return false;
-        if (findLeft(vertices[0]) != findLeft(vertices[1]))
+        if (secondary[vertices[0]] != secondary[vertices[1]])
             return false;
-        if (findRight(vertices[1]) != findRight(vertices[3]))
+        if (principal[vertices[1]] != principal[vertices[3]])
             return false;
         return true;
     }
@@ -105,19 +96,19 @@ bool parallelogram()
 
 bool hexagon()
 {
-    if (level[0] != level[1] || level[2] != level[3] || level[4] != level[5])
+    if (levels[0] != levels[1] || levels[2] != levels[3] || levels[4] != levels[5])
         return false;
     if ((vertices[1] - vertices[0]) != (vertices[5] - vertices[4]))
         return false;
-    if ((vertices[1] - vertices[0]) != (level[2] - level[0]))
+    if ((vertices[1] - vertices[0]) != (levels[2] - levels[0]))
         return false;
-    if ((vertices[1] - vertices[0]) != (level[4] - level[2]))
+    if ((vertices[1] - vertices[0]) != (levels[4] - levels[2]))
         return false;
     if ((vertices[3] - vertices[2]) != 2 * (vertices[1] - vertices[0]))
         return false;
-    if (findLeft(vertices[0]) != findLeft(vertices[2]))
+    if (secondary[vertices[0]] != secondary[vertices[2]])
         return false;
-    if (findRight(vertices[2]) != findRight(vertices[4]))
+    if (principal[vertices[2]] != principal[vertices[4]])
         return false;
     return true;
 }
@@ -126,10 +117,9 @@ void analyze(string line)
 {
     cout << line;
 
-    level.clear();
+    levels.clear();
     for (int i = 0; i < vertices.size(); i++)
-        level.push_back(lower_bound(rightmost.begin(), rightmost.end(),
-                vertices[i]) - rightmost.begin() + 1);
+        levels.push_back(level[vertices[i]]);
 
     if (vertices.size() == 3 && triangle())
         cout << " are the vertices of a triangle\n";
@@ -143,10 +133,7 @@ void analyze(string line)
 
 int main(int argc, char *argv[])
 {
-    cin.tie(0);
-    cin.sync_with_stdio(false);
-
-    initialize();
+    makeSet();
 
     string line;
     while (getline(cin, line))
