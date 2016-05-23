a6eccaef18a88975c08baf49fcc3dd8034125771
metaphysis <metaphysis@yeah.net>
2016-05-23 19:50:56 +0800

    2016-05-23

--- UVa Online Judge/volume002/221 Urban Elevations/program.cpp
+++ UVa Online Judge/volume002/221 Urban Elevations/program.cpp
@@ -1,36 +1,28 @@
 // Urban Elevations
 // UVa IDs: 221
-// Verdict: 
-// Submission Date: 2016-05-15
-// UVa Run Time: s
+// Verdict: Accepted
+// Submission Date: 2016-05-23
+// UVa Run Time: 0.000s
 //
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <iostream>
-#include <iomanip>
-#include <sstream>
 #include <vector>
 #include <algorithm>
-#include <limits>
-#include <cstring>
-#include <stack>
-#include <map>
-#include <queue>
-#include <set>
 
 using namespace std;
 
 struct building
 {
     int index;
-    double x, y, width, depth, height;
-    
+    double left, bottom, width, right, depth, height;
+
     bool operator<(building another) const
     {
-        if (x != another.x)
-            return x < another.x;
+        if (left != another.left)
+            return left < another.left;
         else
-            return y < another.y;
+            return bottom < another.bottom;
     }
 };
 
@@ -38,33 +30,102 @@ vector < building > buildings;
 
 void findVisibleBuilding()
 {
-    
+    sort(buildings.begin(), buildings.end());
+
+    cout << buildings.front().index;
+
+    for (int i = 1; i < buildings.size(); i++)
+    {
+        vector < pair < double, double >>interval;
+        interval.push_back(make_pair(buildings[i].left, buildings[i].right));
+
+        for (int j = 0; j < buildings.size(); j++)
+        {
+            if (buildings[i].index == buildings[j].index)
+                continue;
+
+            if (buildings[i].bottom + buildings[i].depth <= buildings[j].bottom)
+                continue;
+
+            vector < pair < double, double >>more;
+            for (int k = interval.size() - 1; k >= 0; k--)
+            {
+                if (buildings[i].height <= buildings[j].height)
+                {
+                    if (interval[k].first >= buildings[j].left &&
+                        interval[k].second <= buildings[j].right)
+                        interval.erase(interval.begin() + k);
+
+                    if (interval[k].first < buildings[j].left &&
+                        interval[k].second > buildings[j].right)
+                    {
+                        interval.erase(interval.begin() + k);
+                        more.push_back(make_pair(interval[k].first,
+                                buildings[j].left));
+                        more.push_back(make_pair(buildings[j].right,
+                                interval[k].second));
+                    }
+                    else if (interval[k].first < buildings[j].left &&
+                        interval[k].second > buildings[j].left &&
+                        interval[k].second <= buildings[j].right)
+                    {
+                        interval.erase(interval.begin() + k);
+                        more.push_back(make_pair(interval[k].first,
+                                buildings[j].left));
+                    }
+                    else if (interval[k].first >= buildings[j].left &&
+                        interval[k].first < buildings[j].right &&
+                        interval[k].second > buildings[j].right)
+                    {
+                        interval.erase(interval.begin() + k);
+                        more.push_back(make_pair(buildings[j].right,
+                                interval[k].second));
+                    }
+                }
+
+            }
+
+            interval.insert(interval.begin(), more.begin(), more.end());
+
+            if (interval.size() == 0)
+                break;
+        }
+
+        if (interval.size())
+            cout << " " << buildings[i].index;
+    }
+
+    cout << endl;
 }
 
 int main(int argc, char *argv[])
 {
-    cin.tie(0);
-    cout.sync_with_stdio(false);
-    
-    int n, first = 1;
+    int n, cases = 0, first = true;
     while (cin >> n, n)
     {
-        building.clear();
-        
-        double x, y, width, depth, height;
+        buildings.clear();
+
+        double left, bottom, width, depth, height;
         for (int i = 1; i <= n; i++)
         {
-            cin >> x >> y >> width >> depth >> heigth;
-            buildings.push_back((building){i, x, y, width, depth, height});
+            cin >> left >> bottom >> width >> depth >> height;
+            buildings.push_back((building)
+                {
+                i, left, bottom, width, left + width, depth, height});
         }
-        
+
         if (first)
-            first = 0;
+            first = false;
         else
             cout << endl;
-            
+
+        cases++;
+
+        cout << "For map #" << cases;
+        cout << ", the visible buildings are numbered as follows:" << endl;
+
         findVisibleBuilding();
     }
-    
-	return 0;
+
+    return 0;
 }
