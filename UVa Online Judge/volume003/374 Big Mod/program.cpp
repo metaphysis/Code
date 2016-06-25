2efa8f02df4410ab8c1ebde3c5960276da54f499
metaphysis <metaphysis@yeah.net>
2016-06-25 10:07:26 +0800

    2016-06-25

---
+++ UVa Online Judge/volume003/374 Big Mod/program.cpp
@@ -0,0 +1,64 @@
+// Big Mod
+// UVa IDs: 374
+// Verdict: Accetped
+// Submission Date: 2016-06-25
+// UVa Run Time: 0.000s
+//
+// 版权所有（C）2016，邱秋。metaphysis # yeah dot net
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+long long int mod(long long int B, long long int P, long long int M)
+{
+    if (P == 1)
+        return B % M;
+    
+    if (P == 2)
+        return (B * B) % M;
+    
+    long long int m = mod(B, P / 2, M);
+    m *= m;
+    if (P % 2 == 1) m *= B;
+    
+    return m % M;
+}
+
+int main(int argc, char *argv[])
+{
+    long long int B, P, M;
+    
+    while (cin >> B >> P >> M)
+    {
+        if (P == 0)
+        {
+            cout << 1 % M << endl;
+            continue;
+        }
+        
+        if (B % M == 0)
+        {
+            cout << "0" << endl;
+            continue;
+        }
+        
+        cout << mod(B % M, P, M) << endl;
+    }
+    
+	return 0;
+}
---
+++ UVa Online Judge/volume003/382 Perfection/program.cpp
@@ -0,0 +1,51 @@
+// Perfection
+// UVa IDs: 382
+// Verdict: Accepted
+// Submission Date: 2016-06-25
+// UVa Run Time: 0.000s
+//
+// 版权所有（C）2016，邱秋。metaphysis # yeah dot net
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    ios::sync_with_stdio(false);
+    
+    cout << "PERFECTION OUTPUT" << endl;
+    
+    int n;
+    while (cin >> n, n)
+    {
+        int sum = 0;
+        for (int i = 1; i < n; i++)
+            if (n % i == 0)
+                sum += i;
+                
+        cout << setw(5) << right << n;
+        cout << "  ";
+        if (sum == n) cout << "PERFECT" << endl;
+        else if (sum < n) cout << "DEFICIENT" << endl;
+        else cout << "ABUNDANT" << endl;
+    }
+    
+    cout << "END OF OUTPUT" << endl;
+    
+	return 0;
+}
