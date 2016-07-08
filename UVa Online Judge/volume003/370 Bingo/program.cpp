842ac53047b59f1bf89f04088cd7d229ef665929
metaphysis <metaphysis@yeah.net>
2016-07-08 23:49:18 +0800

    2016-07-08

--- UVa Online Judge/volume003/370 Bingo/program.cpp
+++ UVa Online Judge/volume003/370 Bingo/program.cpp
@@ -1,8 +1,8 @@
 // Bingo
 // UVa IDs: 370
-// Verdict: 
-// Submission Date: 
-// UVa Run Time: s
+// Verdict: Accepted
+// Submission Date: 2016-07-08
+// UVa Run Time: 0.210s
 //
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
@@ -24,40 +24,111 @@
 
 using namespace std;
 
-pair<int, int> location[100][30];
-int original[5][5], backup[5][5], counter[100] = { };
+vector<int> sum(13), operation(25), backup(25);
+map<int, vector<int>> mapping;
+map<int, vector<int>> indexer;
+map<int, vector<int>> location;
+
+void display()
+{
+    for (int i = 0; i < 13; i++)
+    {
+        if (sum[i] == 0)
+        {
+            if (i <= 4) cout << "BINGO #1" << endl;
+            else if (i <= 9) cout << "BINGO #2" << endl;
+            else if (i == 10)  cout << "BINGO #3" << endl;
+            else cout << "BINGO #4" << endl;
+
+            for (auto position : indexer[i])
+            {
+                cout << (position / 5 + 1) << "," << (position % 5 + 1) << ",";
+                if (backup[position] == 0) cout << "FREE";
+                else cout << backup[position];
+                cout << endl;
+            }
+        }
+    }
+}
 
 bool isBingo()
 {
+    for (auto element : sum)
+        if (element == 0)
+        {
+            display();
+            return true;
+        }
+    return false;
 }
 
 int main(int argc, char *argv[])
 {
     ios::sync_with_stdio(false);
 
-    int number;
-    for (int i = 0; i < 5; i++)
-        for (int j = 0; j < 5; j++)
-        {
-            cin >> number;
-            ogriginal[i][j] = number;
-            backup[i][j] = original[i][j];
-            location[number][counter[number]++] = make_pair(i, j);
-        }
-        
-    if (isBingo() == false)
+    for (int position = 0; position < 25; position++)
     {
-        int called;
-        while (cin >> called, called)
+        int i = position / 5, j = position % 5;
+        mapping[position].push_back(i);
+        indexer[i].push_back(position);
+
+        mapping[position].push_back(j + 5);
+        indexer[j + 5].push_back(position);
+
+        if (i == j)
         {
-            for (int i = 0; i < counter[called]; i++)
-            {
-                if (isBingo()) return 0;
-            }
+            mapping[position].push_back(11);
+            indexer[11].push_back(position);
         }
-        
-        cout << "No BINGO on this card." << endl;
+
+        if (i + j == 4)
+        {
+            mapping[position].push_back(12);
+            indexer[12].push_back(position);
+        }
+
+        if ((i == 0 && j == 0) || (i == 0 && j == 4) ||
+            (i == 4 && j == 0) || (i == 4 && j == 4))
+        {
+            mapping[position].push_back(10);
+            indexer[10].push_back(position);
+        }
     }
     
+    int number, called;
+    while (cin >> number)
+    {
+        fill(sum.begin(), sum.end(), 0);
+
+        location.clear();
+        int readedNumber = 0, row, column;
+        do
+        {
+            operation[readedNumber] = number;
+            backup[readedNumber] = operation[readedNumber];
+            location[number].push_back(readedNumber);
+
+            for (auto position : mapping[readedNumber])
+                sum[position] += number;
+            readedNumber++;
+        } while (readedNumber < 25 && (cin >> number));
+
+        bool ignore = isBingo();
+        while (cin >> called, called)
+        {
+            if (ignore) continue;
+            for (auto located : location[called])
+            {
+                operation[located] = 0;
+                for (auto position : mapping[located])
+                    sum[position] -= called;
+            }
+            ignore = isBingo();
+        }
+
+        if (!ignore) cout << "No BINGO on this card." << endl << endl;
+        else cout << endl;
+    }
+
 	return 0;
 }
---
+++ UVa Online Judge/volume003/396 Top Dog/program.cpp
@@ -0,0 +1,121 @@
+// Top Dog
+// UVa IDs: 396
+// Verdict: Accepted
+// Submission Date: 2016-07-08
+// UVa Run Time: 0.000s
+//
+// 版权所有（C）2016，邱秋。metaphysis # yeah dot net
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+vector<string> names;
+map<string, vector<vector<string>>> tables;
+
+int main(int argc, char *argv[])
+{
+    ios::sync_with_stdio(false);
+
+    string line;
+    while (getline(cin, line))
+    {
+        int n = stoi(line);
+        cout << n << endl;
+        
+        names.clear();
+        tables.clear();
+
+        while (getline(cin, line), line.length() > 0)
+            names.push_back(line);
+        
+        while (getline(cin, line), line.length() > 0)
+        {
+            vector<vector<string>> fields;
+            string name = line;
+            while (getline(cin, line), line != "#")
+            {
+                istringstream iss(line);
+                vector<string> property;
+                string block;
+                while (iss >> block) property.push_back(block);
+                fields.push_back(property);
+            }
+            tables[name] = fields;
+        }
+        
+        for (auto name : names)
+        {
+            if (tables.find(name) == tables.end())
+                cout << "<TABLE NOT FOUND>" << endl;
+            else
+            {
+                cout << "SELECT ";
+                bool first_field = true;
+                for (auto field : tables[name])
+                {
+                    if (first_field) first_field = false;
+                    else cout << ", ";
+                    
+                    if (field[1] == "INT" || field[1] == "LONGINT") cout << "(NUM) ";
+                    else cout << "(CHAR) ";
+                    
+                    cout << field[0];
+                }
+                cout << " FROM " << name << ";" << endl;
+            }
+        }
+        
+        cout << endl;
+        
+        while (getline(cin, line), line.length() > 0)
+        {
+            string name = line;
+            vector<vector<string>> records;
+            vector<string> record;
+            
+            while (getline(cin, line), line != "#")
+            {
+                if (line == "<NULL>")
+                    record.push_back("");
+                else
+                    record.push_back(line);
+                if (record.size() == tables[name].size())
+                {
+                    records.push_back(record);
+                    record.clear();
+                }
+            }
+            
+            cout << name << "(" << tables[name].size() << ")(" << records.size() << ")" << endl;
+            for (int i = 0; i < records.size(); i++)
+            {
+                for (int j = 0; j < records[i].size(); j++)
+                {
+                    cout << tables[name][j].front();
+                    if (tables[name][j][1] == "VARCHAR" && stoi(tables[name][j][2]) > 64)
+                        cout << "(" << tables[name][j][2] << ")";
+                    cout << " ";
+                    cout << "\"" << records[i][j] << "\"";
+                    cout << endl;
+                }
+            }
+        }
+    }
+    
+	return 0;
+}
