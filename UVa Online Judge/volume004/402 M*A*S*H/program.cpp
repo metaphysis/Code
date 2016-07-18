9d9f4171cac04af66d6138cc484aaff49b28f994
metaphysis <metaphysis@yeah.net>
2016-07-18 13:38:41 +0800

    2016-07-18

---
+++ UVa Online Judge/volume004/402 M*A*S*H/program.cpp
@@ -0,0 +1,97 @@
+// M*A*S*H
+// UVa ID: 402
+// Verdict: Accepted
+// Submission Date: 2016-07-18
+// UVa Run Time: 0.050s
+//
+// 版权所有（C）2016，邱秋。metaphysis # yeah dot net
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
+
+    int cases = 0, n, x;
+    
+    string line;
+    while (getline(cin, line))
+    {
+        istringstream iss(line);
+        iss >> n >> x;
+        
+        queue<int> cards;
+        int number;
+        while (iss >> number)
+            cards.push(number);
+            
+        vector<int> person(n);
+        iota(person.begin(), person.end(), 1);
+        
+        int remaining = n;
+        
+        while (remaining > x && cards.empty() == false)
+        {
+            int token = cards.front();
+            cards.pop();
+            
+            int current = 0, counter = 0;
+            while (current < n)
+            {
+                if (person[current] > 0)
+                    counter++;
+
+                if (counter == token)
+                {
+                    person[current] = 0;
+                    remaining--;
+                    
+                    if (remaining == x)
+                        goto skip;
+                        
+                    counter = 0;
+                }
+
+                current++;
+            }
+        }
+        
+        skip:
+        cout << "Selection #" << ++cases << '\n';
+        
+        bool first = true;
+        for (int i = 0; i < person.size(); i++)
+        {
+            if (person[i] == 0)
+                continue;
+            
+            if (first)
+                first = false;
+            else
+                cout << ' ';
+
+            cout << person[i];
+        }
+
+        cout << "\n\n";
+    }
+    
+	return 0;
+}
---
+++ UVa Online Judge/volume004/448 OOPS!/program.cpp
@@ -0,0 +1,107 @@
+// OOPS!
+// UVa ID: 448
+// Verdict: Accepted
+// Submission Date: 2016-07-18
+// UVa Run Time: 0.000s
+//
+// 版权所有（C）2016，邱秋。metaphysis # yeah dot net
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+map<char, string> hex_value = {
+    {'0', "0000"}, {'1', "0001"}, {'2', "0010"}, {'3', "0011"}, {'4', "0100"}, 
+    {'5', "0101"}, {'6', "0110"}, {'7', "0111"}, {'8', "1000"}, {'9', "1001"}, 
+    {'A', "1010"}, {'B', "1011"}, {'C', "1100"}, {'D', "1101"}, {'E', "1110"}, 
+    {'F', "1111"}
+};
+
+map<char, string> instruction = {
+    {'0', "ADD"}, {'1', "SUB"}, {'2', "MUL"}, {'3', "DIV"}, {'4', "MOV"},
+    {'5', "BREQ"}, {'6', "BRLE"}, {'7', "BRLS"}, {'8', "BRGE"}, {'9', "BRGR"},
+    {'A', "BRNE"}, {'B', "BR"}, {'C', "AND"}, {'D', "OR"}, {'E', "XOR"},
+    {'F', "NOT"}
+};
+
+string source_code;
+int indexer = 0;
+
+string getOperand(string& opcode)
+{
+    bitset<14> number(opcode.substr(2));
+    string operand = to_string(number.to_ulong());
+    if (opcode[0] == '0' && opcode[1] == '0') return "R" + operand;
+    else if (opcode[0] == '0' && opcode[1] == '1') return "$" + operand;
+    else if (opcode[0] == '1' && opcode[1] == '0') return "PC+" + operand;
+    else return operand;
+}
+
+void arithmetic(char letter)
+{
+    string op1, op2;
+    for (int i = 0; i < 4; i++) op1 += hex_value[source_code[indexer++]];
+    for (int i = 0; i < 4; i++) op2 += hex_value[source_code[indexer++]];
+    cout << instruction[letter] << ' ' << getOperand(op1) << ',' << getOperand(op2) << '\n';
+}
+
+void compare(char letter)
+{
+    string op1;
+    for (int i = 0; i < 4; i++) op1 += hex_value[source_code[indexer++]];
+    cout << instruction[letter] << ' ' << getOperand(op1) << '\n';    
+}
+
+void boolean(char letter)
+{
+    string op1, op2, op3;
+    for (int i = 0; i < 4; i++) op1 += hex_value[source_code[indexer++]];
+    for (int i = 0; i < 4; i++) op2 += hex_value[source_code[indexer++]];
+    for (int i = 0; i < 4; i++) op3 += hex_value[source_code[indexer++]];
+    cout << instruction[letter] << ' ';
+    cout << getOperand(op1) << ',' << getOperand(op2) << ',' << getOperand(op3) << '\n';
+}
+
+int main(int argc, char *argv[])
+{
+    cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
+    
+    string line;
+    while (getline(cin, line))
+        source_code += line;
+
+    for (int i = source_code.length(); i >= 0; i--)
+        if (isblank(source_code[i]))
+            source_code.erase(source_code.begin() + i);
+
+    while (indexer < source_code.length())
+    {
+        char last_letter = source_code[indexer++];
+        if (last_letter == '0' || last_letter == '1' || last_letter == '2' ||
+            last_letter == '3' || last_letter == '4')
+            arithmetic(last_letter);
+        else if (last_letter == '5' || last_letter == '6' || last_letter == '7' ||
+            last_letter == '8' || last_letter == '9' || last_letter == 'A' ||
+            last_letter == 'B' || last_letter == 'F')
+            compare(last_letter);
+        else
+            boolean(last_letter);
+    }
+    
+	return 0;
+}
