853d07db9cc67a7264e519c4f2ad4176d99b1b2d
metaphysis <metaphysis@yeah.net>
2016-07-24 23:57:02 +0800

    2016-07-24

--- UVa Online Judge/volume004/453 Intersecting Circles/program.cpp
+++ UVa Online Judge/volume004/453 Intersecting Circles/program.cpp
@@ -25,46 +25,82 @@
 
 using namespace std;
 
-const double EPSILON = 1e-7;
+const double epsilon = 1e-6, epsilon2 = 5.14e-5;
 
 int main(int argc, char *argv[])
 {
-    cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(false);
+    cout.precision(3);
 
     double x1, y1, r1, x2, y2, r2;
-    
+
     while (cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2)
     {
-        double distances1 = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
-        double distances2 = (r1 + r2) * (r1 + r2);
-        
-        // same center
-        if (distances1 <= EPSILON)
+        if (r2 > r1)
+            swap(x1, x2), swap(y1, y2), swap(r1, r2);
+
+        double distance1 = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+        double distance2 = r1 + r2;
+
+        // same circle
+        if (distance1 <= epsilon)
         {
-            if (fabs(r1 - r2) <= EPSILON)
-                cout << "THE CIRCLES ARE THE SAME\n";
+            if (fabs(r1 - r2) <= epsilon)
+            {
+                if (r1 > epsilon)
+                    cout << "THE CIRCLES ARE THE SAME\n";
+                else
+                    cout << '(' << fixed << x1 + epsilon2 << ',' << fixed << y1 + epsilon2 << ")\n";
+            }
             else
                 cout << "NO INTERSECTION\n";
             continue;
         }
-        
-        // the distance between two circle center bigger than sum of radius
-        if (distances1 > distances2 + EPSILON)
+
+        // no intersection, one circle contains within another circle
+        if (distance1 > distance2 + epsilon || distance1 + r2 + epsilon < r1)
         {
             cout << "NO INTERSECTION\n";
             continue;
         }
-        
-        if (fabs(distance1 - distance2) <= EPSION)
+
+        // one intersecion
+        if (fabs(distance1 - distance2) <= epsilon)
         {
+            x1 += r1 / (r1 + r2) * (x2 - x1);
+            y1 += r1 / (r1 + r2) * (y2 - y1);
+            cout << '(' << fixed << x1 + epsilon2 << ',' << fixed << y1 + epsilon2 << ")\n";
+            continue;
         }
-        
-        double distances3 = (r1 - r2) * (r1 - r2);
-        if (r1 > r2 + EPSILON && distance1 < distances3)
+
+        if (fabs(distance1 + r2 - r1) <= epsilon)
         {
+            x1 += distance1 / r1 * (x2 - x1);
+            y1 += distance1 / r1 * (y2 - y1);
+            cout << '(' << fixed << x1 + epsilon2 << ',' << fixed << y1 + epsilon2 << ")\n";
+            continue;
         }
+
+        // two intersection
+        double a, h;
+        a = (r1 * r1 - r2 * r2 + distance1 * distance1) / (2 * distance1);
+        h = sqrt(r1 * r1 - a * a);
+        double x3 = (x2 - x1) * (a / distance1) + x1, y3 = (y2 - y1) * (a / distance1) + y1;
         
+        double x4, y4, x5, y5;
+        x4 = x3 + h * (y2 - y1) / distance1;
+        y4 = y3 - h * (x2 - x1) / distance1;
+        x5 = x3 - h * (y2 - y1) / distance1;
+        y5 = y3 + h * (x2 - x1) / distance1;
+
+        if (fabs(x4 - x5) <= epsilon && y4 > y5) swap(y4, y5);
+        else if (x4 > x5) swap(x4, x5), swap(y4, y5);
+        
+        cout << '(' << fixed << x4 << ',' << fixed << y4 << ")(";
+        cout << fixed << x5 << ',' << fixed << y5 << ")\n";
     }
-    
-	return 0;
+
+    return 0;
 }
