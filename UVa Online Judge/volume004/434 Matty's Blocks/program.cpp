f44dac6bbe0484bc94b009c576b630e23225abd3
metaphysis <metaphysis@yeah.net>
2016-07-29 00:16:03 +0800

    2016-07-29

--- UVa Online Judge/volume004/434 Matty's Blocks/program.cpp
+++ UVa Online Judge/volume004/434 Matty's Blocks/program.cpp
@@ -1,8 +1,8 @@
 // Matty's Blocks
 // UVa ID: 434
-// Verdict: 
-// Submission Date: 
-// UVa Run Time: s
+// Verdict: Accepted
+// Submission Date: 2016-07-29
+// UVa Run Time: 0.000s
 //
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
@@ -38,67 +38,31 @@ int main(int argc, char *argv[])
         cin >> row;
 
         vector<int> front_view(row), right_view(row);
-        vector<vector<int>> matrix(row, vector<int>());
         
         for (int j = 0; j < row; j++)
             cin >> front_view[j];
         for (int j = 0; j < row; j++)
             cin >> right_view[j];
-
-        reverse(right_view.begin(), right_view.end());
   
         int max_blocks = 0;
         for (int j = 0; j < row; j++)
             for (int k = 0; k < row; k++)
-            {
                 max_blocks += min(right_view[j], front_view[k]);
-                matrix[j].push_back(min(right_view[j], front_view[k]));
-            }
 
-        for (int j = 0; j < row; j++)
-        {
-            for (int k = 0; k < row; k++)
-                cout << setw(3) << matrix[j][k];
-            cout << endl;
-        }
-
-        for (int j = 0; j < row; j++)
-            for (int k = 0; k < row; k++)
-            {
-                while (matrix[j][k] > 0)
-                {
-                    matrix[j][k]--;
-                    
-                    bool same = true;
-                    for (int x = 0; x < row; x++)
-                    {
-                        int max_blocks_row = 0, max_blocks_column = 0;
-                        for (int y = 0; y < row; y++)
-                        {
-                            max_blocks_row = max(max_blocks_row, matrix[x][y]);
-                            max_blocks_column = max(max_blocks_column, matrix[y][x]);
-                        }
-                            
-                        if (max_blocks_row < right_view[x] || max_blocks_column < front_view[x])
-                        {
-                            same = false;
-                            break;
-                        }
-                    }
-                            
-                    if (!same)
-                    {
-                        matrix[j][k]++;
-                        break;
-                    }
-                }
-            }
-        
         int min_blocks = 0;
         for (int j = 0; j < row; j++)
             for (int k = 0; k < row; k++)
-                min_blocks += matrix[j][k];
-                    
+                if (front_view[j] == right_view[k])
+                {
+                    min_blocks += front_view[j];
+                    front_view[j] = 0;
+                    right_view[k] = 0;
+                    break;
+                }
+
+        for (int j = 0; j < row; j++)
+            min_blocks += front_view[j] + right_view[j];
+                   
         cout << "Matty needs at least " << min_blocks;
         cout << " blocks, and can add at most ";
         cout << (max_blocks - min_blocks) << " extra blocks.\n";
