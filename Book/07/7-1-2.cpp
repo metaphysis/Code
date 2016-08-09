cb2e2c4c87b503daa57ddae405281cc9dd6c1c7e
metaphysis <metaphysis@yeah.net>
2016-08-09 13:39:43 +0800

    2016-08-09

---
+++ Book/07/7-1-2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAX_N = 1000000;
+
+int primes[MAX_N + 1] = { 0 }, prime_count = 0;
+
+void filter()
+{
+    primes[prime_count++] = 2;
+
+    for (int i = 3; i <= MAX_N; i += 2)
+        if (primes[i] == 0)
+        {
+            for (int j = 2 * i; j <= MAX_N; j += i)
+                primes[j] = -1;
+            primes[prime_count++] = i;
+        }
+}
+
+int main(int argc, char *argv[])
+{
+    filter();
+
+    for (int i = 0; i < prime_count; i++)
+    {
+        cout << primes[i] << " ";
+        if ((i + 1) % 100 == 0)
+            cout << "\n";
+    }
+
+    return 0;
+}
