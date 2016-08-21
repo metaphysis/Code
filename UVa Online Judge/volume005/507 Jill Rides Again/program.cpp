34fe825a487375697b315252c5ee85588fcc9b64
metaphysis <metaphysis@yeah.net>
2016-08-22 00:23:06 +0800

    2016-08-22

--- UVa Online Judge/volume005/507 Jill Rides Again/program.cpp
+++ UVa Online Judge/volume005/507 Jill Rides Again/program.cpp
@@ -1,27 +1,12 @@
 // Jill Rides Again
 // UVa ID: 507
-// Verdict: 
-// Submission Date: 
-// UVa Run Time: s
+// Verdict: Accepted
+// Submission Date: 2016-08-21
+// UVa Run Time: 0.040s
 //
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
-#include <algorithm>
-#include <bitset>
-#include <cassert>
-#include <cmath>
-#include <cstring>
-#include <iomanip>
 #include <iostream>
-#include <limits>
-#include <list>
-#include <map>
-#include <numeric>
-#include <queue>
-#include <set>
-#include <sstream>
-#include <stack>
-#include <vector>
 
 using namespace std;
 
@@ -29,5 +14,46 @@ int main(int argc, char *argv[])
 {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
 
+    int cases;
+    cin >> cases;
+    
+    for (int r = 1; r <= cases; r++)
+    {
+        int s, niceness;
+        cin >> s >> niceness;
+        
+        int max = niceness, start = 1, end = 2;
+        int maxCurrent = niceness, startCurrent = 1, endCurrent = 2;
+        
+        s--;
+        for (int i = 2; i <= s; i++)
+        {
+            cin >> niceness;
+
+            endCurrent = i + 1;
+            maxCurrent += niceness;
+            
+            if (niceness > maxCurrent)
+            {
+                startCurrent = i, endCurrent = i + 1;
+                maxCurrent = niceness;
+            }
+
+            if (maxCurrent > max || (maxCurrent == max && abs(endCurrent - startCurrent) > abs(end - start)))
+            {
+                max = maxCurrent;
+                start = startCurrent, end = endCurrent;
+            }
+        }
+        
+        if (max > 0)
+        {
+            cout << "The nicest part of route " << r << " is between stops ";
+            cout << start << " and " << end << '\n';
+        }
+        else
+            cout << "Route " << r << " has no nice parts\n";
+    }
+    
 	return 0;
 }
---
+++ UVa Online Judge/volume005/557 Burger/program.cpp
@@ -0,0 +1,46 @@
+// Burger
+// UVa ID: 557
+// Verdict: Accepted
+// Submission Date: 2016-08-21
+// UVa Run Time: 0.000s
+//
+// 版权所有（C）2016，邱秋。metaphysis # yeah dot net
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
+
+    // Bernoulli trial
+    double probability[50001] = { 1 };
+    for (int i = 1; i <= 50000; i++)
+        probability[i] = probability[i - 1] * (long double)(2 * i - 1) / (long double)(2 * i);
+    
+    int cases, n;
+    cin >> cases;
+    for (int c = 1; c <= cases; c++)
+    {
+        cin >> n;
+        cout << fixed << setprecision(4) << (1 - probability[n / 2 - 1]) << '\n';
+    }
+    
+	return 0;
+}
