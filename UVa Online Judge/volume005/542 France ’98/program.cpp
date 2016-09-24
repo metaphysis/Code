dfa99c96617a2351ff60be8b9d033f8e9f8a185a
metaphysis <metaphysis@yeah.net>
2016-09-24 10:52:58 +0800

    2016-09-24

---
+++ UVa Online Judge/volume005/542 France ’98/program.cpp
@@ -0,0 +1,71 @@
+// France '98
+// UVa ID: 542
+// Verdict: Accepted
+// Submission Date: 2016-09-24
+// UVa Run Time: s
+//
+// 版权所有（C）2016，邱秋。metaphysis # yeah dot net
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
+
+    string names[16];
+    double possibility[16][16] = {0}, win[16][5] = {0};
+
+    for (int i = 0; i < 16; i++)
+        cin >> names[i];
+    for (int i = 0; i < 16; i++)
+    {
+        for (int j = 0; j < 16; j++)
+            cin >> possibility[i][j];
+        win[i][0] = 100.0;
+        for (int j = 1; j <= 4; j++)
+            win[i][j] = 0.0;
+    }
+    
+    int group = 2, last_group = 1;
+    for (int i = 1; i <= 4; i++)
+    {
+        for (int j = 0; j < 16; j++)
+        {
+            for (int k = 0; k < 16; k++)
+                if (((j / group) == (k / group)) && ((j / last_group) != (k / last_group)))
+                    win[j][i] += win[k][i - 1] * possibility[j][k] / 10000.0;
+            
+            win[j][i] *= win[j][i - 1];
+        }
+        
+        last_group = group;
+        group *= 2;
+    }
+    
+    for (int i = 0; i < 16; i++)
+    {
+        cout << setw(10) << left << names[i];
+        cout << " p=";
+        cout << fixed << setprecision(2) << win[i][4];
+        cout << "%\n";
+    }
+    
+	return 0;
+}
--- UVa Online Judge/volume005/580 Critical Mass/program.cpp
+++ UVa Online Judge/volume005/580 Critical Mass/program.cpp
@@ -1,8 +1,8 @@
 // Critical Mass
 // UVa ID: 580
-// Verdict: 
-// Submission Date: 
-// UVa Run Time: s
+// Verdict: Accepted
+// Submission Date: 2016-09-21
+// UVa Run Time: 0.000s
 //
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
@@ -25,64 +25,18 @@
 
 using namespace std;
 
-const int MAX_N = 31;
-
-int dp[MAX_N][8][2] = {0}, number[MAX_N] = {0};
-
-void initialize()
-{
-    for (int i = 0; i < 8; i++)
-        dp[3][i][0] = 0, dp[3][i][1] = 1;
-    dp[3][7][0] = 1, dp[3][7][1] = 0;
-
-    for (int i = 4; i <= 30; i++)
-    {
-        dp[i][0][0] = dp[i - 1][0][0] + dp[i - 1][4][0];
-        dp[i][0][1] = dp[i - 1][0][1] + dp[i - 1][4][1];
-        
-        dp[i][1][0] = dp[i - 1][0][0] + dp[i - 1][4][0];
-        dp[i][1][1] = dp[i - 1][0][1] + dp[i - 1][4][1];
-        
-        dp[i][2][0] = dp[i - 1][1][0] + dp[i - 1][5][0];
-        dp[i][2][1] = dp[i - 1][1][1] + dp[i - 1][5][1];
-        
-        dp[i][3][0] = dp[i - 1][1][0] + dp[i - 1][5][0];
-        dp[i][3][1] = dp[i - 1][1][1] + dp[i - 1][5][1];
-        
-        dp[i][4][0] = dp[i - 1][2][0] + dp[i - 1][6][0];
-        dp[i][4][1] = dp[i - 1][2][1] + dp[i - 1][6][1];
-        
-        dp[i][5][0] = dp[i - 1][2][0] + dp[i - 1][6][0];
-        dp[i][5][1] = dp[i - 1][2][1] + dp[i - 1][6][1];
-        
-        dp[i][6][0] = dp[i - 1][3][0] + dp[i - 1][7][0];
-        dp[i][6][1] = dp[i - 1][3][1] + dp[i - 1][7][1];
-        
-        dp[i][7][0] = dp[i - 1][3][0] + dp[i - 1][7][0];
-        dp[i][7][1] = dp[i - 1][3][1] + dp[i - 1][7][1];
-    }
-    
-    
-    for (int i = 3; i <= 30; i++)
-    {
-        for (int j = 0; j < 8; j++)
-        {
-            cout << dp[i][j][0] << ' ' << dp[i][j][1] << ' ';
-            number[i] += dp[i][j][0];
-        }
-        cout << '\n';
-    }
-}
-
 int main(int argc, char *argv[])
 {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
 
-    initialize();
+    int solutions[31] = {0};
+    solutions[3] = 1, solutions[4] = 3;
+    for (int i = 5; i <= 30; i++)
+        solutions[i] = 2 * solutions[i - 1] - solutions[i - 4] + (1 << (i - 4));
     
     int n;
     while (cin >> n, n)
-        cout << number[n] << '\n';
+        cout << solutions[n] << '\n';
         
 	return 0;
 }
