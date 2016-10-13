6ea700ae10e64a6b91fd6f643eab108ff32d1cd3
metaphysis <metaphysis@yeah.net>
2016-10-13 11:48:15 +0800

    2016-10-13

--- UVa Online Judge/volume005/563 Crimewave/program.cpp
+++ UVa Online Judge/volume005/563 Crimewave/program.cpp
@@ -25,26 +25,22 @@
 
 using namespace std;
 
-const int MAXV = 2610;
+const int MAXV = 5100;
 
-struct arc
-{
-	int capacity, flow, residual;
-};
-
-map<int, arc> arcs[MAXV];
-int parents[MAXV], discovered[MAXV], occupied[MAXV];
+map<int, int> arcs[MAXV];
+int path[MAXV], visited[MAXV];
 int offset[4][2] = {{1, 0}, {0, -1}, {-1, 0}, {0, 1}};
+int problem, streets, avenues, banks;
 
-void bfs(int source, int sink)
+bool bfs(int source, int sink)
 {
-    memset(discovered, 0, sizeof(discovered));
-	memset(parents, -1, sizeof(parents));
+    memset(visited, 0, sizeof(visited));
+	memset(path, -1, sizeof(path));
 
 	queue<int> vertices;
 
 	vertices.push(source);
-	discovered[source] = 1;
+	visited[source] = 1;
 
 	while (!vertices.empty())
 	{
@@ -52,92 +48,81 @@ void bfs(int source, int sink)
 		vertices.pop();
 		for (auto a : arcs[v])
 			// 沿着残留容量为正的弧进行图遍历，检查是否为饱和边。
-			if (a.second.residual > 0)
-				if (!discovered[a.first])
+			if (a.second > 0)
+				if (!visited[a.first])
 				{
 					vertices.push(a.first);
-					discovered[a.first] = 1;
-					parents[a.first] = v;
+					visited[a.first] = 1;
+					path[a.first] = v;
 
 					// 遍历到汇点后说明已经找到一条增广路，可以退出。
 					if (a.first == sink)
-						return;
+						return true;
 				}
 	}
+
+	return false;
 }
 
-void augmentPath(int source, int sink, int volume)
+void augmentPath(int source, int sink)
 {
 	if (source == sink) return;
-
-	arcs[parents[sink]][sink].flow += volume;
-	arcs[parents[sink]][sink].residual -= volume;
-
-	arcs[sink][parents[sink]].residual += volume;
-
-	augmentPath(source, parents[sink], volume);
+	arcs[path[sink]][sink] = 0;
+	arcs[sink][path[sink]] = 1;
+	augmentPath(source, path[sink]);
 }
 
-int pathVolume(int source, int sink)
+int edmondsKarp()
 {
-	if (parents[sink] == -1) return 0;
-
-    int volume = arcs[parents[sink]][sink].residual;
-	if (source == parents[sink])
-		return volume;
-	else
-		return (min(pathVolume(source, parents[sink]), volume));
-}
-
-bool edmondsKarp(int streets, int avenues, int banks)
-{
-	int maxFlow = 0, volume, source = 1, sink = 2600;
-
-	bfs(source, sink);
-	volume = pathVolume(source, sink);
-	while (volume > 0)
+	int maxFlow = 0, source = 0, sink = 5010;
+	while (bfs(source, sink))
 	{
-		maxFlow += volume;
-		augmentPath(source, sink, volume);
-		bfs(source, sink);
-		volume = pathVolume(source, sink);
+		maxFlow++;
+		augmentPath(source, sink);
 	}
-
-	return maxFlow == banks;
+	return maxFlow;
 }
 
-void buildGraph(int streets, int avenues)
+void buildGraph()
 {
-    int edged[MAXV][4];
-    memset(edged, 0, sizeof(edged));
+    for (int i = 0; i < 5100; i++)
+        arcs[i].clear();
+
+    // 在源点和银行之间建立有向弧。
+    for (int b = 1, x, y; b <= banks; b++)
+    {
+        cin >> x >> y;
+        arcs[0][(x - 1) * avenues + y] = 1;
+        arcs[(x - 1) * avenues + y][0] = 0;
+    }
     
+    int base = streets * avenues;
     for (int s = 1; s <= streets; s++)
         for (int a = 1; a <= avenues; a++)
         {
-            // 源点到每家银行所处交叉路口的弧。
-            if (occupied[s * avenues + a])
-            {
-                arcs[1][s * avenues + a] = (arc){1, 0, 1};
-                arcs[s * avenues + a][1] = (arc){1, 0, 0};
-            }
-                
-            // 城镇边界上的交叉路口到汇点的弧。
+            int index = (s - 1) * avenues + a;
+            
+            // 在城镇边界上的交叉路口和汇点之间建立有向弧。
             if (s == 1 || s == streets || a == 1 || a == avenues)
             {
-                arcs[s * avenues + a][2600] = (arc){1, 0, 1};
-                arcs[2600][s * avenues + a] = (arc){1, 0, 0};
+                arcs[base + index][5010] = 1;
+                arcs[5010][base + index] = 0;
             }
                 
-            // 在其余顶点间构建残留网络。
+            // 将交叉路口拆分为两个顶点，在顶点间构建残留网络。
+            arcs[index][base + index] = 1;
+            arcs[base + index][index] = 0;
+                    
             for (int f = 0; f < 4; f++)
             {
                 int ss = s + offset[f][0], aa = a + offset[f][1];
                 if (ss >= 1 && ss <= streets && aa >= 1 && aa <= avenues)
                 {
-                    if (!occupied[ss * avenues + aa])
-                        arcs[s * avenues + a][ss * avenues + aa] = (arc){1, 0, 1};
-                    if (!occupied[s * avenues + a])
-                        arcs[ss * avenues + aa][s * avenues + a] = (arc){1, 0, 0};
+                    arcs[base + index][(ss - 1) * avenues + aa] = 1;
+                    arcs[(ss - 1) * avenues + aa][base + index] = 0;
+                    
+                    //arcs[base + (ss - 1) * avenues + aa][index] = 1;
+                    //arcs[index][base + (ss - 1) * avenues + aa] = 0;
                 }
             }
         }
@@ -146,23 +131,13 @@ void buildGraph(int streets, int avenues)
 int main(int argc, char *argv[])
 {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
-    int problem, streets, avenues, banks, x, y;
-    cin >> problem;
     
+    cin >> problem;
     for (int p = 1; p <= problem; p++)
     {
         cin >> streets >> avenues >> banks;
-        
-        memset(occupied, 0, sizeof(occupied));
-        for (int b = 1; b <= banks; b++)
-        {
-            cin >> x >> y;
-            occupied[x * avenues + y] = 1;
-        }
-        
-        buildGraph(streets, avenues);
-        bool possible = edmondsKarp(streets, avenues, banks);
+        buildGraph();
+        bool possible = edmondsKarp() == banks;
         cout << (possible ? "possible" : "not possible") << '\n';
     }
     
