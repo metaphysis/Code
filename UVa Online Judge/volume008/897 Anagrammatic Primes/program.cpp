43b7369847663bf57d53fe3e823c268217571c41
metaphysis <metaphysis@yeah.net>
2016-12-06 23:18:25 +0800

    2016-12-06

--- UVa Online Judge/volume008/897 Anagrammatic Primes/program.cpp
+++ UVa Online Judge/volume008/897 Anagrammatic Primes/program.cpp
@@ -1,8 +1,8 @@
 // Anagrammatic Primes
 // UVa ID: 897
-// Verdict: 
-// Submission Date: 
-// UVa Run Time: s
+// Verdict: Accepted
+// Submission Date: 2016-12-06
+// UVa Run Time: 0.060s
 //
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
@@ -29,6 +29,7 @@ const int MAXN = 10000000;
 
 int level[7] = {10, 100, 1000, 10000, 100000, 1000000, 10000000};
 int primes[MAXN], counter = 0;
+int anagrammatic[10000];
 
 void sieve()
 {
@@ -56,6 +57,75 @@ int main(int argc, char *argv[])
 
     sieve();
     
+    // 剔除包含数字0，2，4，5，6，8的素数，它们不是乱序素数。
+    int next_counter = 0;
+    for (int i = 0, t = primes[i]; i < counter; i++, t = primes[i])
+    {
+        if (t < 10)
+        {
+            primes[next_counter++] = primes[i];
+            continue;
+        }
+        
+        bool added = true;
+        while (t)
+        {
+            int d = t % 10;
+            if (d % 2 == 0 || d == 5)
+            {
+                added = false;
+                break;
+            }
+            t /= 10;
+        }
+        
+        if (added) primes[next_counter++] = primes[i];
+    }
+    counter = next_counter;
+    
+    // 逐个检查剩余的素数是否为乱序素数。
+    next_counter = 0;
+    for (int i = 0, t = primes[i]; i < counter; i++, t = primes[i])
+    {
+        if (t < 10)
+        {
+            anagrammatic[next_counter++] = primes[i];
+            continue;
+        }
+        
+        vector<int> digits;
+        while (t)
+        {
+            int d = t % 10;
+            if (d % 2 == 0 || d == 5)
+            {
+                digits.clear();
+                break;
+            }
+            digits.push_back(d);
+            t /= 10;
+        }
+
+        if (digits.size() > 0)
+        {
+            bool flag = true;
+            sort(digits.begin(), digits.end());
+            do
+            {
+                int next = 0;
+                for (int j = 0; j < digits.size(); j++)
+                    next = next * 10 + digits[j];
+                if (!binary_search(primes, primes + counter, next))
+                {
+                    flag = false;
+                    break;
+                }
+            } while (next_permutation(digits.begin(), digits.end()));
+            
+            if (flag) anagrammatic[next_counter++] = primes[i];
+        }
+    }
+    
     int n, upper;
     while (cin >> n, n > 0)
     {
@@ -66,54 +136,9 @@ int main(int argc, char *argv[])
                 break;
             }
                 
-        int k = upper_bound(primes, primes + counter, n) - primes;
-        
-        bool successed = false;
-        if (k < counter)
-        {
-            vector<int> digits;
-            for (int i = k, t = primes[i]; t < upper; i++, t = primes[i])
-            {
-                cout << n << ' ' << t << '\n';
-                while (t)
-                {
-                    int d = t % 10;
-                    if (d % 2 == 0 || d == 5)
-                    {
-                        digits.clear();
-                        break;
-                    }
-                    digits.push_back(d);
-                    t /= 10;
-                }
-
-                if (digits.size() > 0)
-                {
-                    bool flag = true;
-                    sort(digits.begin(), digits.end());
-                    do
-                    {
-                        int next = 0;
-                        for (int j = 0; j < digits.size(); j++)
-                            next = next * 10 + digits[j];
-                        if (!binary_search(primes, primes + counter, next))
-                        {
-                            flag = false;
-                            break;
-                        }
-                    } while (next_permutation(digits.begin(), digits.end()));
-                    
-                    if (flag)
-                    {
-                        cout << primes[i] << '\n';
-                        successed = true;
-                        break;
-                    }
-                }
-            }
-        }
-        
-        if (!successed) cout << "0\n";
+        int k = upper_bound(anagrammatic, anagrammatic + next_counter, n) - anagrammatic;
+        if (k < counter && anagrammatic[k] < upper) cout << anagrammatic[k] << '\n';
+        else cout << "0\n";
     }
     
 	return 0;
