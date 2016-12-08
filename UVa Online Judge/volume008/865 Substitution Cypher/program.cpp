09eff716146f677f6ddc4840e138854a73de69a4
metaphysis <metaphysis@yeah.net>
2016-12-08 17:50:52 +0800

    2016-12-08

--- UVa Online Judge/volume008/865 Substitution Cypher/program.cpp
+++ UVa Online Judge/volume008/865 Substitution Cypher/program.cpp
@@ -1,8 +1,8 @@
 // Substitution Cypher
 // UVa ID: 865
-// Verdict: 
-// Submission Date: 
-// UVa Run Time: s
+// Verdict: Accepted
+// Submission Date: 2016-12-08
+// UVa Run Time: 0.000s
 //
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
