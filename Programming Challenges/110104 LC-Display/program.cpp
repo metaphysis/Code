3c1bb1257aa19b19674ba453f838eed2fe55a7d5
metaphysis <metaphysis@yeah.net>
2016-12-09 18:49:03 +0800

    2016-12-09

--- Programming Challenges/110104 LC-Display/program.cpp
+++ Programming Challenges/110104 LC-Display/program.cpp
@@ -21,33 +21,26 @@ void lcdDisplay(int size, string number)
         " - ", "   ", " - ", " - ", "   ", " - ", " - ", "   ", " - ", " - "
     };
 
-    // 根据指定 size 将关键笔划扩大显示，实际上就是将关键笔划重复 size 次。
+    // 根据指定size将关键笔划扩大显示，实际上就是将关键笔划重复size次。
     for (int row = 1; row <= (2 * size + 3); row++)
     {
         for (int i = 0; i < number.length(); i++)
         {
             string line;
             int column = number[i] - '0';
-            if (row == 1)
-                line = outline[0][column];
-            if (2 <= row && row < (size + 2))
-                line = outline[1][column];
-            if (row == (size + 2))
-                line = outline[2][column];
-            if ((size + 3) <= row && row <= (2 * size + 2))
-                line = outline[3][column];
-            if (row == (2 * size + 3))
-                line = outline[4][column];
+            if (row == 1) line = outline[0][column];
+            if (2 <= row && row < (size + 2)) line = outline[1][column];
+            if (row == (size + 2)) line = outline[2][column];
+            if ((size + 3) <= row && row <= (2 * size + 2)) line = outline[3][column];
+            if (row == (2 * size + 3)) line = outline[4][column];
 
             // 输出关键笔划。
             cout << line[0];
-            for (int j = 0; j < size; j++)
-                cout << line[1];
+            for (int j = 0; j < size; j++) cout << line[1];
             cout << line[2];
 
             // 在两个数字间要有一列空行。
-            if (i < 7)
-                cout << " ";
+            if (i < 7) cout << " ";
         }
 
         cout << '\n';
@@ -58,6 +51,7 @@ int main(int argc, char *argv[])
 {
     int size;
     string number;
+
     while (cin >> size >> number, size)
     {
         lcdDisplay(size, number);
---
+++ UVa Online Judge/volume008/852 Deciding victory in Go/program.cpp
@@ -0,0 +1,93 @@
+// Deciding victory in Go
+// UVa ID: 852
+// Verdict: Accepted
+// Submission Date: 2016-12-09
+// UVa Run Time: 0.000s
+//
+// 版权所有（C）2016，邱秋。metaphysis # yeah dot net
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+const int MAX_SIZE = 9;
+char grid[MAX_SIZE][MAX_SIZE];
+int offset[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+int black_stone, white_stone, points;
+
+void flood_fill(int i, int j, char old, char replaced)
+{
+    if (i >= 0 && i < 9 && j >= 0 && j < 9)
+    {
+        if (grid[i][j] == old)
+        {
+            points++;
+            grid[i][j] = replaced;
+            for (int k = 0; k < 4; k++)
+                flood_fill(i + offset[k][0], j + offset[k][1], old, replaced);
+        }
+        else
+        {
+            if (grid[i][j] == 'X') black_stone++;
+            else if (grid[i][j] == 'O') white_stone++;
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
+
+    int cases;
+    cin >> cases;
+
+    for (int c = 1; c <= cases; c++)
+    {
+        int black = 0, white = 0;
+        
+        for (int i = 0; i < 9; i++)
+            for (int j = 0; j < 9; j++)
+            {
+                cin >> grid[i][j];
+                if (grid[i][j] == 'X') black++;
+                if (grid[i][j] == 'O') white++;
+            }
+
+        if (black == 0 && white == 0)
+        {
+            cout << "Black 0 White 0\n";
+            continue;
+        }
+        
+        for (int i = 0; i < 9; i++)
+            for (int j = 0; j < 9; j++)
+                if (grid[i][j] == '.')
+                {
+                    black_stone = 0, white_stone = 0, points = 0;
+                    
+                    flood_fill(i, j, '.', 'F');
+                    
+                    if (black_stone == 0) white += points;
+                    if (white_stone == 0) black += points;
+                }
+            
+        cout << "Black " << black << " White " << white << '\n';
+    }
+    
+	return 0;
+}
