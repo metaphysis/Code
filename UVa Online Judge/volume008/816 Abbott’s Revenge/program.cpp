a61fd1877339a71bfee8f4aa11416b5df3434fef
metaphysis <metaphysis@yeah.net>
2016-12-13 11:33:45 +0800

    2016-12-13

---
+++ UVa Online Judge/volume008/816 Abbott’s Revenge/program.cpp
@@ -0,0 +1,119 @@
+// Abbott’s Revenge
+// UVa ID: 816
+// Verdict: Accepted
+// Submission Date: 2016-12-13
+// UVa Run Time: 0.000s
+//
+// 版权所有（C）2016，邱秋。metaphysis # yeah dot net
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+map<char, int> direction = {{'N', 0}, {'E', 1}, {'S', 2}, {'W', 3}};
+map<char, int> turn = {{'L', 0}, {'F', 1}, {'R', 2}};
+
+struct pack
+{
+    int r, c, d;
+    vector<pair<int, int>> walk;
+};
+
+bool flag = false;
+char startd;
+string maze, sign;
+int visited[10][10][4], successor[10][10][4][4], record[1000][2];
+int bias[4][2] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+int offset[4][3][3] = {
+    {{0, -1, 3}, {-1, 0, 0}, {0, 1, 1}}, 
+    {{-1, 0, 0}, {0, 1, 1}, {1, 0, 2}},
+    {{0, 1, 1}, {1, 0, 2}, {0, -1, 3}},
+    {{1, 0, 2}, {0, -1, 3}, {-1, 0, 0}}
+};
+int startr, startc, endr, endc, somer, somec;
+
+bool bfs()
+{
+    vector<pair<int, int>> startwalk = {{startr, startc}};
+    startr += bias[direction[startd]][0], startc += bias[direction[startd]][1];
+    pack start = (pack){startr, startc, direction[startd], startwalk};
+
+    memset(visited, 0, sizeof(visited));
+    queue<pack> unvisited; unvisited.push(start);
+    
+    while (unvisited.empty() == false)
+    {
+        pack current = unvisited.front();
+        unvisited.pop();
+
+        int r = current.r, c = current.c, d = current.d;
+        current.walk.push_back(make_pair(r, c));
+
+        if (r == endr && c == endc)
+        {
+            for (int i = 0; i < current.walk.size(); i++)
+            {
+                if (i % 10 == 0) cout << ' ';
+                cout << " (" << current.walk[i].first << ',' << current.walk[i].second << ')';
+                if ((i + 1) % 10 == 0) cout << '\n';
+            }
+            if (current.walk.size() % 10 != 0) cout << '\n';
+
+            return true;
+        }
+        
+        visited[r][c][d] = 1;
+        for (int i = 0; i < 3; i++)
+            if (successor[r][c][d][i])
+            {
+                int nextr = r + offset[d][i][0], nextc = c + offset[d][i][1], nextd = offset[d][i][2];
+                if (visited[nextr][nextc][nextd] == 0)
+                    unvisited.push((pack){nextr, nextc, nextd, current.walk});
+            }
+    }
+    
+    return false;
+}
+
+int main(int argc, char *argv[])
+{
+    cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
+
+    while (cin >> maze, maze != "END")
+    {
+        memset(successor, 0, sizeof(successor));
+
+        cin >> startr >> startc >> startd >> endr >> endc;
+ 
+        while (cin >> somer, somer > 0)
+        {
+            cin >> somec;
+            while (cin >> sign, sign != "*")
+            {
+                for (int i = 1; i < sign.length(); i++)
+                    successor[somer][somec][direction[sign.front()]][turn[sign[i]]] = 1;
+            }
+        }
+
+        cout << maze << '\n';
+
+        if (!bfs()) cout << "  No Solution Possible\n";
+    }
+    
+	return 0;
+}
