99b14ccdcbc3647ac6c6232363fd437a7e1f0bd6
metaphysis <metaphysis@yeah.net>
2016-12-14 10:58:23 +0800

    2016-12-14

--- Book/01/1-1-1.cpp
+++ Book/01/1-1-1.cpp
@@ -5,13 +5,13 @@ using namespace std;
 union
 {
     unsigned int word;
-    unsigned char byte;           
+    unsigned char byte;
 } block;
 
 int main(int argc, char *argv[])
 {
-    // 利用联合的特性，将联合的第一个成员赋值为1，然后获取内存低位字节的值，
-    // 如果是小端序，则低位字节存储的值为1，若为大端序，则值为0。
+    // 利用union的特性，将block的第一个成员赋值为1，然后获取内存低位字节的值，如果是
+    // 小端序，则低位字节存储的值为1，若为大端序，则值为0。
     block.word = 1;
     cout << (block.byte ? "little-endian\n" : "big-endian\n");
 
---
+++ UVa Online Judge/volume008/824 Coast Tracker/program.cpp
@@ -0,0 +1,61 @@
+// Coast Tracker
+// UVa ID: 824
+// Verdict: Accepted
+// Submission Date: 2016-12-14
+// UVa Run Time: 0.000s
+//
+// 版权所有（C）2016，邱秋。metaphysis # yeah dot net
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
+
+    int offset[8][2] = {{0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}, {1, 0}, {1, 1}};
+    int next[8]= {6, 6, 0, 0, 2, 2, 4, 4};
+    int x, y, d, xi, yi, si;
+    int surface[3][3];
+
+    while (cin >> x, x > 0)
+    {
+        cin >> y >> d;
+        for (int i = 0; i < 8; i++)
+        {
+            cin >> xi >> yi >> si;
+            surface[xi - x + 1][yi - y + 1] = si;
+        }
+        
+        for (int i = 0; i < 8; i++)
+        {
+            int nextd = (next[d] + i) % 8;
+            int nextx = 1 + offset[nextd][0];
+            int nexty = 1 + offset[nextd][1];
+            
+            if (surface[nextx][nexty])
+            {
+                cout << nextd << '\n';
+                break;
+            }
+        }
+    }
+    
+	return 0;
+}
