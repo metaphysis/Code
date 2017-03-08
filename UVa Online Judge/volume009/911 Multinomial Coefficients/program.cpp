64ad2a6ea3a715eed59b0178836c78951d64ae31
metaphysis <metaphysis@yeah.net>
2017-03-08 21:30:04 +0800

    2017-03-08

---
+++ UVa Online Judge/volume009/911 Multinomial Coefficients/program.cpp
@@ -0,0 +1,85 @@
+// Multinomial Coefficients
+// UVa ID: 911
+// Verdict: Accepted
+// Submission Date: 2017-03-08
+// UVa Run Time: 0.000s
+//
+// 版权所有（C）2017，邱秋。metaphysis # yeah dot net
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+int gcd(int a, int b)
+{
+    while (a != b) if (a > b) a -= b; else b -= a;
+    return a;
+}
+
+int main(int argc, char *argv[])
+{
+    cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
+
+    int n, k, z;
+    
+    while (cin >> n)
+    {
+        vector<int> upper;
+        for (int i = 2; i <= n; i++) upper.push_back(i);
+            
+        cin >> k;
+        for (int i = 1; i <= k; i++)
+        {
+            cin >> z;
+            if (z >= 2)
+            {
+                for (int j = 2; j <= z; j++)
+                {
+                    int remainder = j;
+                    
+                    while (remainder > 1)
+                    {
+                        for (int l = upper.size() - 1; l >= 0; l--)
+                        {
+                            int quotient = gcd(upper[l], remainder);
+                            if (quotient > 1)
+                            {
+                                upper[l] /= quotient;
+                                remainder /= quotient;
+                            }
+                            if (upper[l] == 1)
+                            {
+                                upper.erase(upper.begin() + l);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        int r = 1;
+        for (int i = 0; i < upper.size(); i++)
+            r *= upper[i];
+        cout << r << '\n';
+    }
+    
+    return 0;
+}
---
+++ UVa Online Judge/volume009/962 Taxicab Numbers/program.cpp
@@ -0,0 +1,67 @@
+// TaxicabNumbers Numbers
+// UVa ID: 962
+// Verdict: Accepted
+// Submission Date: 2017-03-08
+// UVa Run Time: 0.210s
+//
+// 版权所有（C）2017，邱秋。metaphysis # yeah dot net
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
+
+    long long cubes[2000];
+    
+    for (int i = 0; i < 1010; i++) cubes[i] = i * i * i;
+
+    unordered_map<long long, int> cabNumbers;
+    
+    for (int i = 1; i < 1010; i++)
+        for (int j = i + 1; j < 1010; j++)
+            cabNumbers[cubes[i] + cubes[j]]++;
+    
+    int counter = 0;
+    for (auto cab : cabNumbers)
+        if (cab.second >= 2)
+            cubes[counter++] = cab.first;
+
+    long long nl, rg;
+    while (cin >> nl >> rg)
+    {
+        rg += nl;
+
+        int printed = 0;
+        for (int i = 0; i < counter; i++)
+        {
+            if (cubes[i] >= nl && cubes[i] <= rg)
+            {
+                cout << cubes[i] << '\n';
+                printed++;
+            }
+        }
+        if (printed == 0) cout << "None\n";
+    }
+    
+    return 0;
+}
