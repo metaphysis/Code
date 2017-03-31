b5b248778119691f38dd6a495c82c981f8908273
metaphysis <metaphysis@yeah.net>
2017-03-31 11:28:49 +0800

    2017-03-31

--- UVa Online Judge/miscellaneous/11151 Longest Palindrome/program.cpp
+++ UVa Online Judge/miscellaneous/11151 Longest Palindrome/program.cpp
@@ -2,7 +2,7 @@
 // UVa ID: 11151
 // Verdict: Accepted
 // Submission Date: 2017-03-31
-// UVa Run Time: 0.010s
+// UVa Run Time: 0.000s
 //
 // 版权所有（C）2017，邱秋。metaphysis # yeah dot net
 
@@ -32,31 +32,29 @@ int main(int argc, char *argv[])
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
 
     int cases = 0;
+    string s;
 
     cin >> cases;
     cin.ignore(1024, '\n');
     
     for (int c = 1; c <= cases; c++)
     {
-        string s, t;
-        
         getline(cin, s);
         
-        t.assign(s);
-        reverse(t.begin(), t.end());
-        
-        int memo[s.length() + 1][t.length() + 1];
+        int memo[s.length() + 1][s.length() + 1];
 
         memset(memo, 0, sizeof(memo));
 
         for (int i = 1; i <= s.length(); i++)
-            for (int j = 1; j <= t.length(); j++)
-                if (s[i - 1] == t[j - 1])
+            for (int j = 1; j <= s.length(); j++)
+            {
+                if (s[i - 1] == s[s.length() - j])
                     memo[i][j] = max(memo[i][j], memo[i - 1][j - 1] + 1);
                 else
                     memo[i][j] = max(memo[i - 1][j], memo[i][j - 1]);
-
-        cout << memo[s.length()][t.length()] << '\n';
+            }
+            
+        cout << memo[s.length()][s.length()] << '\n';
     }
     
     return 0;
