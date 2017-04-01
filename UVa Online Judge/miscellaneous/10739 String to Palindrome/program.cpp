8872650eef92dae62668b3e6b6aded2ae216b53b
metaphysis <metaphysis@yeah.net>
2017-04-01 08:39:15 +0800

    2017-04-01

---
+++ UVa Online Judge/miscellaneous/10739 String to Palindrome/program.cpp
@@ -0,0 +1,66 @@
+// String to Palindrome
+// UVa ID: 10739
+// Verdict: Accepted
+// Submission Date: 2017-04-01
+// UVa Run Time: 0.000s
+//
+// 版权所有（C）2017，邱秋。metaphysis # yeah dot net
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+int dp[1010][1010];
+string line;
+
+int find(int i, int j)
+{
+    if (i >= j) return 0;
+    if (dp[i][j] >= 0) return dp[i][j];
+
+    if (line[i] == line[j])
+        dp[i][j] = find(i + 1, j - 1);
+    else
+        dp[i][j] = 1 + min(find(i + 1, j - 1), min(find(i + 1, j), find(i, j - 1)));
+
+    return dp[i][j];
+}
+
+int main(int argc, char *argv[])
+{
+    cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
+
+    int cases;
+    cin >> cases;
+    
+    cin.ignore(1024, '\n');
+
+    for (int c = 1; c <= cases; c++)
+    {
+        getline(cin, line);
+
+        memset(dp, -1, sizeof(dp));
+        
+        cout << "Case " << c << ": ";
+        cout << find(0, line.length() - 1) << '\n';
+    }
+    
+    return 0;
+}
---
+++ UVa Online Judge/miscellaneous/11221 Magic square palindromes/program.cpp
@@ -0,0 +1,113 @@
+// Magic square palindromes
+// UVa ID: 11221
+// Verdict: Accepted
+// Submission Date: 2017-04-01
+// UVa Run Time: 0.000s
+//
+// 版权所有（C）2017，邱秋。metaphysis # yeah dot net
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
+
+    int cases = 0;
+    cin >> cases;
+    
+    cin.ignore(1024, '\n');
+    
+    string line;
+    char grid[110][110];
+    
+    for (int c = 1;  c <= cases; c++)
+    {
+        cout << "Case #" << c << ":\n";
+        getline(cin, line);
+        
+        string letters;
+        for (auto c : line)
+            if (isalpha(c))
+                letters.push_back(c);
+        
+        int root = sqrt((int)letters.size());
+        int K = 0;
+        bool isSquare = false;
+        
+        for (int i = root - 1; i <= root + 1; i++)
+            if (i * i == letters.size())
+            {
+                isSquare = true;
+                K = i;
+                break;
+            }
+            
+        if (!isSquare)
+        {
+            cout << "No magic :(\n";
+            continue;
+        }
+        
+        memset(grid, 0, sizeof(grid));
+        
+        for (int i = 0; i < K; i++)
+            for (int j = 0; j < K; j++)
+                grid[i][j] = letters[i * K + j];
+                
+        string reading;
+        for (int i = 0; i < K; i++)
+            for (int j = 0; j < K; j++)
+                reading += grid[j][i];
+                
+        if (reading != letters)
+        {
+            cout << "No magic :(\n";
+            continue;
+        }
+        
+        reading.clear();
+        for (int i = K - 1; i >= 0; i--)
+            for (int j = K - 1; j >= 0; j--)
+                reading += grid[i][j];
+                
+        if (reading != letters)
+        {
+            cout << "No magic :(\n";
+            continue;
+        }
+        
+        reading.clear();
+        for (int i = K - 1; i >= 0; i--)
+            for (int j = K - 1; j >= 0; j--)
+                reading += grid[j][i];
+                
+        if (reading != letters)
+        {
+            cout << "No magic :(\n";
+            continue;
+        }
+        
+        cout << K << '\n';
+    }
+    
+    return 0;
+}
