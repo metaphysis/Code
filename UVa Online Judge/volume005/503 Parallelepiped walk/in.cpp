735bb81030b8dc4d19f0ba92eea5b76929e363be
metaphysis <metaphysis@yeah.net>
2017-04-18 11:51:21 +0800

    2017-04-18

---
+++ UVa Online Judge/volume005/503 Parallelepiped walk/in.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    srand(time(NULL));
+    
+    for (int c = 1; c <= 1000; c++)
+    {
+        int L = rand() % 1001, W = rand() % 1001, H = rand() % 1001;
+        int x1, y1, z1, x2, y2, z2;
+        
+        int plane1 = rand() % 6;
+        switch (plane1)
+        {
+            // top
+            case 0:
+            x1 = rand() % (L + 1), y1 = rand() % (W + 1), z1 = H;
+            break;
+            
+            // left
+            case 1:
+            x1 = rand() % (L + 1), y1 = 0, z1 = rand() % (H + 1);
+            break;
+            
+            // right
+            case 2:
+            x1 = rand() % (L + 1), y1 = W, z1 = rand() % (H + 1);
+            break;
+            
+            // front
+            case 3:
+            x1 = L, y1 = rand() % (W + 1), z1 = rand() % (H + 1);
+            break;
+            
+            // back
+            case 4:
+            x1 = 0, y1 = rand() % (W + 1), z1 = rand() % (H + 1);
+            break;
+            
+            // bottom
+            case 5:
+            x1 = rand() % (L + 1), y1 = rand() % (W + 1), z1 = 0;
+            break;
+        }
+        
+        int plane2 = rand() % 6;
+        switch (plane2)
+        {
+            // top
+            case 0:
+            x2 = rand() % (L + 1), y2 = rand() % (W + 1), z2 = H;
+            break;
+            
+            // left
+            case 1:
+            x2 = rand() % (L + 1), y2 = 0, z2 = rand() % (H + 1);
+            break;
+            
+            // right
+            case 2:
+            x2 = rand() % (L + 1), y2 = W, z2 = rand() % (H + 1);
+            break;
+            
+            // front
+            case 3:
+            x2 = L, y2 = rand() % (W + 1), z2 = rand() % (H + 1);
+            break;
+            
+            // back
+            case 4:
+            x2 = 0, y2 = rand() % (W + 1), z2 = rand() % (H + 1);
+            break;
+            
+            // bottom
+            case 5:
+            x2 = rand() % (L + 1), y2 = rand() % (W + 1), z2 = 0;
+            break;
+        }
+        
+        cout << L << ' ' << W << ' ' << H << ' ';
+        cout << x1 << ' ' << y1 << ' ' << z1 << ' ';
+        cout << x2 << ' ' << y2 << ' ' << z2 << '\n';
+    }
+    
+    return 0;
+}
+
+
--- UVa Online Judge/volume005/503 Parallelepiped walk/program.cpp
+++ UVa Online Judge/volume005/503 Parallelepiped walk/program.cpp
@@ -27,15 +27,55 @@
 
 using namespace std;
 
+bool debug = true;
+int xx1, yy1, zz1, xx2, yy2, zz2;
+int dist = 100000000;
+
+void copy(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    xx1 = x1, yy1 = y1, zz1 = z1, xx2 = x2, yy2 = y2, zz2 = z2;
+}
+
+void swap()
+{
+    swap(xx1, xx2), swap(yy1, yy2), swap(zz1, zz2);
+}
+
+int curve(int L, int W, int H, int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    // top plane, back plane
+    if (z1 == H && x2 == 0)
+    {
+        dist = min(dist, (y1 - y2) * (y1 - y2) + (H + x1 - z2) * (H + x1 - z2));
+        if (debug) cout << "1 = " << dist << '\n';
+        dist = min(dist, (x1 + y2) * (x1 + y2) + (H + y1 - z2) * (H + y1 - z2));
+        if (debug) cout << "2 = " << dist << '\n';
+        dist = min(dist, (x1 + (W - y2)) * (x1 + (W - y2)) + (H + (W - y1) - z2) * (H + (W - y1) - z2));
+        if (debug) cout << "3 = " << dist << '\n';
+    }
+    
+    // top plane, bottom plane
+    if (z1 == H && z2 == 0)
+    {
+        dist = min(dist, (x1 - x2) * (x1 - x2) + (W + H + (W - y1) - y2) * (W + H + (W - y1) - y2));
+        dist = min(dist, (x1 - x2) * (x1 - x2) + (-(H + y1) - y2) * (-(H + y1) - y2));
+        dist = min(dist, (L + H + (L - x1) - x2) * (L + H + (L - x1) - x2) + (y1 - y2) * (y1 - y2));
+        dist = min(dist, (-(H + x1) - x2) * (-(H + x1) - x2) + (y1 - y2) * (y1 - y2));
+    }
+    
+    return dist;
+}
+
 int main(int argc, char *argv[])
 {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
 
-    int L, W, H, x1, y1, z1, x2, y2, z2;
+    int L, W, H;
+    int x1, y1, z1, x2, y2, z2;
     
     while (cin >> L >> W >> H >> x1 >> y1 >> z1 >> x2 >> y2 >> z2)
     {
-        int dist = 100000000;
+        dist = 100000000;
         
         if (L == 0 || W == 0 || H == 0 ||
             (x1 == x2 && (x1 == L || x1 == 0)) ||
@@ -50,260 +90,136 @@ int main(int argc, char *argv[])
         // top plane, left plane
         if ((z1 == H && y2 == 0) || (y1 == 0 && z2 == H))
         {
-            int zz1 = z1, zz2 = z2;
-            if (z1 == H)
-                zz1 = H + y1;
-            else
-                zz2 = H + y2;
-            dist = min(dist, (x1 - x2) * (x1 - x2) + (zz1 - zz2) * (zz1 - zz2));
-            
-            cout << "Top Left = " << dist << endl;
+            if (debug) cout << "top plane, left plane" << '\n';
+            copy(x1, y1, z1, x2, y2, z2);
+            if (y1 == 0) swap();
+            dist = min(dist, curve(W, L, H, yy1, L - xx1, H, 0, L - xx2, zz2));
         }
         
         // top plane, right plane
         if ((z1 == H && y2 == W) || (y1 == W && z2 == H))
         {
-            int yy1 = y1, yy2 = y2;
-            if (yy1 == W)
-                yy1 = W + (H - z1);
-            else
-                yy2 = W + (H - z2);
-            dist = min(dist, (x1 - x2) * (x1 - x2) + (yy1 - yy2) * (yy1 - yy2));
-            
-            cout << "Top Right = " << dist << endl;
+            if (debug) cout << "top plane, right plane" << '\n';
+            copy(x1, y1, z1, x2, y2, z2);
+            if (y1 == W) swap();
+            dist = min(dist, curve(W, L, H, W - yy1, xx1, H, 0, xx2, zz2));
         }
         
         // top plane, front plane
         if ((z1 == H && x2 == L) || (x1 == L && z2 == H))
         {
-            int xx1 = x1, xx2 = x2;
-            if (xx1 == L)
-                xx1 = L + (H - z1);
-            else
-                xx2 = L + (H - z1);
-            dist = min(dist, (xx1 - xx2) * (xx1 - xx2) + (y1 - y2) * (y2 - y2));
-            
-            cout << "Top Front = " << dist << endl;
+            if (debug) cout << "top plane, front plane" << '\n';
+            copy(x1, y1, z1, x2, y2, z2);
+            if (x1 == L) swap();
+            dist = min(dist, curve(L, W, H, L - xx1, W - yy1, H, 0, W - yy2, zz2));
         }
         
         // top plane, back plane
-        if ((z1 == H && x2 == 0) || (z2 == H && x1 == 0))
+        if ((z1 == H && x2 == 0) || (x1 == 0 && z2 == H))
         {
-            int zz1 = z2, zz2 = z2;
-            if (z1 == H)
-                zz1 = H + x1;
-            else
-                zz2 = H + x2;
-            dist = min(dist, (y1 - y2) * (y1 - y2) + (zz1 - zz2) * (zz1 - zz2));
-            
-            cout << "Top Back = " << dist << endl;
+            if (debug) cout << "top plane, back plane" << '\n';
+            copy(x1, y1, z1, x2, y2, z2);
+            if (x1 == 0) swap();
+            dist = min(dist, curve(L, W, H, xx1, yy1, H, 0, yy2, zz2));
         }
         
         // top plane, bottom plane
-        if ((z1 == H && z2 == 0) || (z2 == H && z1 == 0))
+        if ((z1 == H && z2 == 0) || (z1 == 0 && z2 == H))
         {
-            int yy1 = y1, yy2 = y2;
-            if (z1 == H)
-                yy1 = W + H + (W - y1);
-            else
-                yy2 = W + H + (W - y2);
-            dist = min(dist, (x1 - x2) * (x1 - x2) + (yy1 - yy2) * (yy1 - yy2));
-            
-            yy1 = y1, yy2 = y2;
-            if (z1 == H)
-                yy1 = -(H + y1);
-            else
-                yy2 = -(H + y2);
-            dist = min(dist, (x1 - x2) * (x1 - x2) + (yy1 - yy2) * (yy1 - yy2));
-            
-            int xx1 = x1, xx2 = x2;
-            if (z1 == H)
-                xx1 = L + H + (L - x1);
-            else
-                xx2 = L + H + (L - x2);
-            dist = min(dist, (xx1 - xx2) * (xx1 - xx2) + (y1 - y2) * (y1 - y2));
-            
-            xx1 = x1, xx2 = x2;
-            if (z1 == H)
-                xx1 = -(H + x1);
-            else
-                xx2 = -(H + x2);
-            dist = min(dist, (xx1 - xx2) * (xx1 - xx2) + (y1 - y2) * (y1 - y2));
-            
-            cout << "Top Bottom = " << dist << endl;
+            if (debug) cout << "top plane, bottom plane" << '\n';
+            copy(x1, y1, z1, x2, y2, z2);
+            if (z1 == 0) swap();
+            dist = min(dist, curve(L, W, H, xx1, yy1, H, xx2, yy2, 0));
         }
         
         // right plane, front plane
-        if ((y1 == W && x2 == L) || (y2 == W && x1 == L))
+        if ((x1 == L && y2 == W) || (y1 == W && x2 == L))
         {
-            int yy1 = y1, yy2 = y2;
-            if (y1 == W)
-                yy1 = W + (L - x1);
-            else
-                yy2 = W + (L - x2);
-            dist = min(dist, (yy1 - yy2) * (yy1 - yy2) + (z1 - z2) * (z1 - z2));
-            
-            cout << "Right Front = " << dist << endl;
+            if (debug) cout << "right plane, front plane" << '\n';
+            copy(x1, y1, z1, x2, y2, z2);
+            if (y1 == W) swap();
+            dist = min(dist, curve(W, H, L, W - yy1, H - zz1, L, 0, H - zz2, xx2));
         }
         
         // right plane, back plane
-        if ((y1 == W && x2 == 0) || (y2 == W && x1 == 0))
+        if ((y1 == W && x2 == 0) || (x1 == 0 && y2 == W))
         {
-            int yy1 = y1, yy2 = y2;
-            if (y1 == W)
-                yy1 = W + x1;
-            else
-                yy2 = W + x2;
-            dist = min(dist, (yy1 - yy2) * (yy1 - yy2) + (z1 - z2) * (z1 - z2));
-            
-            cout << "Right Back = " << dist << endl;
+            if (debug) cout << "right plane, back plane" << '\n';
+            copy(x1, y1, z1, x2, y2, z2);
+            if (x1 == 0) swap();
+            dist = min(dist, curve(L, H, W, xx1, H - zz1, W, 0, H - zz2, yy2));
         }
         
         // right plane, bottom plane
-        if ((y1 == W && z2 == 0) || (y2 == W && z1 == 0))
+        if ((y1 == W && z2 == 0) || (z1 == 0 && y2 == W))
         {
-            int yy1 = y1, yy2 = y2;
-            if (y1 == W)
-                yy1 = W + z1;
-            else
-                yy2 = W + z2;
-            dist = min(dist, (x1 - x2) * (x1 - x2) + (yy1 - yy2) * (yy1 - yy2));
-            
-            cout << "Right Bottom = " << dist << endl;
+            if (debug) cout << "right plane, bottom plane" << '\n';
+            copy(x1, y1, z1, x2, y2, z2);
+            if (z1 == 0) swap();
+            dist = min(dist, curve(H, L, W, zz1, xx1, W, 0, xx2, yy2));
         }
         
         // right plane, left plane
-        if ((y1 == W && y2 == 0) || (y2 == W && y1 == 0))
+        if ((y1 == W && y2 == 0) || (y1 == 0 && y2 == W))
         {
-            int zz1 = z1, zz2 = z2;
-            if (y1 == W)
-                zz1 = H + W + (H - z1);
-            else
-                zz2 = H + W + (H - z2);
-            dist = min(dist, (x1 - x2) * (x1 - x2) + (zz1 - zz2) * (zz1 - zz2));
-            
-            zz1 = z1, zz2 = z2;
-            if (y1 == W)
-                zz1 = -(W + z1);
-            else 
-                zz2 = -(W + z2);
-                
-            dist = min(dist, (x1 - x2) * (x1 - x2) + (zz1 - zz2) * (zz1 - zz2));
-            
-            int xx1 = x1, xx2 = x2;
-            if (y1 == W)
-                xx1 = L + W + (L - x1);
-            else
-                xx2 = L + W + (L - x2);
-            dist = min(dist, (xx1 - xx2) * (xx1 - xx2) + (z1 - z2) * (z1 - z2));
-            
-            xx1 = x1, xx2 = x2;
-            if (y1 == W)
-                xx1 = -(W + x1);
-            else
-                xx2 = -(W + x2);
-            dist = min(dist, (xx1 - xx2) * (xx1 - xx2) + (z1 - z2) * (z1 - z2));
-            
-            cout << "Right Left = " << dist << endl;
+            if (debug) cout << "right plane, left plane" << '\n';
+            copy(x1, y1, z1, x2, y2, z2);
+            if (y1 == 0) swap();
+            dist = min(dist, curve(H, L, W, zz1, xx1, W, zz2, xx2, 0));
         }
         
         // front plane, left plane
-        if ((x1 == L && y2 == 0) || (x2 == L && y1 == 0))
+        if ((x1 == L && y2 == 0) || (y1 == 0 && x2 == L))
         {
-            int xx1 = x1, xx2 = x2;
-            if (x1 == L)
-                xx1 = L + y1;
-            else
-                xx2 = L + y2;
-            dist = min(dist, (xx1 - xx2) * (xx1 - xx2) + (z1 - z2) * (z1 - z2));
-            
-            cout << "Front Left = " << dist << endl;
+            if (debug) cout << "front plane, left plane" << '\n';
+            copy(x1, y1, z1, x2, y2, z2);
+            if (y1 == 0) swap();
+            dist = min(dist, curve(W, H, L, yy1, zz1, L, 0, zz2, xx2));
         }
         
         // front plane, bottom plane
-        if ((x1 == L && z2 == 0) && (x2 == L && z1 == 0))
+        if ((x1 == L && z2 == 0) || (z1 == 0 && x2 == L))
         {
-            int xx1 = x1, xx2 = x2;
-            if (x1 == L)
-                xx1 = L + z1;
-            else
-                xx2 = L + z2;
-            dist = min(dist, (xx1 - xx2) * (xx1 - xx2) + (y1 - y2) * (y1 - y2));
-            
-            cout << "Front Bottom = " << dist << endl;
+            if (debug) cout << "front plane, bottom plane" << '\n';
+            copy(x1, y1, z1, x2, y2, z2);
+            if (z1 == 0) swap();
+            dist = min(dist, curve(H, W, L, zz1, W - yy1, L, 0, W - yy2, xx2));
         }
         
         // front plane, back plane
-        if ((x1 == L && x2 == 0) || (x2 == L && x1 == 0))
+        if ((x1 == L && x2 == 0) || (x1 == 0 && x2 == L))
         {
-            int zz1 = z1, zz2 = z2;
-            if (x1 == L)
-                zz1 = H + L + (H - x1);
-            else
-                zz2 = H + L + (H - x2);
-            dist = min(dist, (y1 - y2) * (y1 - y2) + (zz1 - zz2) * (zz1 - zz2));
-            
-            zz1 = z1, zz2 = z2;
-            if (x1 == L)
-                zz1 = -(L + z1);
-            else
-                zz2 = -(L + z2);
-            dist = min(dist, (y1 - y2) * (y1 - y2) + (zz1 - zz2) * (zz1 - zz2));
-            
-            int yy1 = y1, yy2 = y2;
-            if (x1 == L)
-                yy1 = W + L + (W - y1);
-            else
-                yy2 = W + L + (W - y2);
-            dist = min(dist, (yy1 - yy2) * (yy2 - yy2) + (z1 - z2) * (z1 - z2));
-            
-            yy1 = y1, yy2 = y2;
-            if (x1 == L)
-                yy1 = -(L + y1);
-            else
-                yy2 = -(L + y2);
-            dist = min(dist, (yy1 - yy2) * (yy2 - yy2) + (z1 - z2) * (z1 - z2));
-            
-            cout << "Front Back = " << dist << endl;
+            if (debug) cout << "front plane, back plane" << '\n';
+            copy(x1, y1, z1, x2, y2, z2);
+            if (x1 == 0) swap();
+            dist = min(dist, curve(W, H, L, yy1, zz1, L, yy2, zz2, 0));
         }
         
         // left plane, back plane
-        if ((y1 == 0 && x2 == 0) || (y2 == 0 && x1 == 0))
+        if ((y1 == 0 && x2 == 0) || (x1 == 0 && y2 == 0))
         {
-            int yy1 = y1, yy2 = y2;
-            if (y1 == 0)
-                yy1 = -x1;
-            else
-                yy2 = -x2;
-            dist = min(dist, (yy1 - yy2) * (yy1 - yy2) + (z1 - z2) * (z1 - z2));
-            
-            cout << "Left Back = " << dist << endl;
+            if (debug) cout << "left plane, back plane" << '\n';
+            copy(x1, y1, z1, x2, y2, z2);
+            if (x1 == 0) swap();
+            dist = min(dist, curve(L, H, W, xx1, zz1, W, 0, zz2, W - yy2));
         }
         
-        // left plane, bottom plaen
-        if ((y1 == 0 && z2 == 0) || (y2 == 0 && z1 == 0))
+        // left plane, bottom plane
+        if ((y1 == 0 && z2 == 0) || (z1 == 0 && y2 == 0))
         {
-            int yy1 = y1, yy2 = y2;
-            if (y1 == 0)
-                yy1 = -z1;
-            else
-                yy2 = -z2;
-            dist = min(dist, (x1 - x2) * (x1 - x2) + (yy1 - yy2) * (yy1 - yy2));
-            
-            cout << "Left Bottom = " << dist << endl;  
+            if (debug) cout << "left plane, bottom plane" << '\n';
+            copy(x1, y1, z1, x2, y2, z2);
+            if (z1 == 0) swap();
+            dist = min(dist, curve(H, L, W, zz1, L - xx1, W, 0, L - xx2, W - yy2));
         }
         
         // back plane, bottom plane
-        if ((x1 == 0 && z2 == 0) && (x2 == 0 && z1 == 0))
+        if ((x1 == 0 && z2 == 0) || (z1 == 0 && x2 == 0))
         {
-            int xx1 = x1, xx2 = x2;
-            if (x1 == 0)
-                xx1 = -z1;
-            else
-                xx2 = -z2;
-            dist = min(dist, (xx1 - xx2) * (xx1 - xx2) + (y1 - y2) * (y1 - y2));
-            
-            cout << "Back Bottom = " << dist << endl;
+            if (debug) cout << "back plane, bottom plane" << '\n';
+            copy(x1, y1, z1, x2, y2, z2);
+            if (z1 == 0) swap();
+            dist = min(dist, curve(H, W, L, zz1, yy1, L, 0, yy2, L - xx2));
         }
         
         cout << dist << '\n';
