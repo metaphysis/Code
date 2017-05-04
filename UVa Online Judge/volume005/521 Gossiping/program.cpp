c2b0c8dcb71dbbc6bf8b4627d090c877e532572b
metaphysis <metaphysis@yeah.net>
2017-05-04 11:48:12 +0800

    2017-05-04

--- UVa Online Judge/volume005/521 Gossiping/program.cpp
+++ UVa Online Judge/volume005/521 Gossiping/program.cpp
@@ -1,8 +1,8 @@
 // Gossiping
 // UVa ID: 521
-// Verdict: 
-// Submission Date: 
-// UVa Run Time: s
+// Verdict: Accepted
+// Submission Date: 2017-05-04
+// UVa Run Time: 0.000s
 //
 // 版权所有（C）2017，邱秋。metaphysis # yeah dot net
 
@@ -32,47 +32,38 @@ struct driver
     int line, start;
 };
 
-vector<vector<int>> busLines(40, vector<int>());
-vector<set<int>> busStops(40, set<int>());
+vector<vector<int>> busLines(40, vector <int>());
 driver drivers[40];
 bool connected[40][40];
-int n, d, s;
-int from, to, stop;
-int si, di;
-istringstream iss;
-string line;
+int n, d, s, si, di;
 
 bool exchanged(int a, int b)
 {
     if (a == b) return true;
 
     int aline = drivers[a].line, bline = drivers[b].line;
-    int total = 4 * busLines[aline].size() * busLines[bline].size();
-    
+    int total = busLines[aline].size() * busLines[bline].size();
+
     vector<int> s1;
     while (s1.size() < total)
     {
         for (int i = drivers[a].start; i < busLines[aline].size(); i++)
             s1.push_back(busLines[aline][i]);
-        for (int i = busLines[aline].size() - 2; i > 0; i--)
-            s1.push_back(busLines[aline][i]);
-        for (int i = 0; i <= drivers[a].start; i++)
+        for (int i = 0; i < drivers[a].start; i++)
             s1.push_back(busLines[aline][i]);
     }
-    
+
     vector<int> s2;
     while (s2.size() < total)
     {
         for (int i = drivers[b].start; i < busLines[bline].size(); i++)
             s2.push_back(busLines[bline][i]);
-        for (int i = busLines[bline].size() - 2; i > 0; i--)
-            s2.push_back(busLines[bline][i]);
-        for (int i = 0; i <= drivers[b].start; i++)
+        for (int i = 0; i < drivers[b].start; i++)
             s2.push_back(busLines[bline][i]);
     }
-    
+
     for (int i = 0; i < total; i++)
-        if (s1[i] == s2[i] && busStops[aline].count(s1[i]) && busStops[bline].count(s2[i]))
+        if (s1[i] == s2[i])
             return true;
 
     return false;
@@ -81,6 +72,9 @@ bool exchanged(int a, int b)
 int main(int argc, char *argv[])
 {
     cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
+
+    istringstream iss;
+    string line;
 
     while (getline(cin, line))
     {
@@ -95,19 +89,15 @@ int main(int argc, char *argv[])
         {
             busLines[i].clear();
 
-            getline(cin, line);
-            iss.clear(); iss.str(line);
-            while (iss >> stop)
-                busLines[i].push_back(stop), busStops[i].insert(stop);
+            getline(cin, line); iss.clear(); iss.str(line);
+            while (iss >> si) busLines[i].push_back(si);
 
-            getline(cin, line);
-            iss.clear(); iss.str(line);
+            getline(cin, line); iss.clear(); iss.str(line);
             while (iss >> si >> di)
             {
                 for (int j = 0; j < busLines[i].size(); j++)
-                    if (busLines[i][j] == si)
+                    if (si == busLines[i][j])
                     {
-                    
                         drivers[di] = driver{i, j};
                         break;
                     }
@@ -121,7 +111,8 @@ int main(int argc, char *argv[])
         for (int k = 1; k <= d; k++)
             for (int i = 1; i <= d; i++)
                 for (int j = 1; j <= d; j++)
-                    connected[i][j] = connected[i][k] && connected[k][j];
+                    if (connected[i][k] && connected[k][j])
+                        connected[i][j] = connected[j][i] = true;
 
         bool allConnected = true;
         for (int i = 1; i <= d; i++)
@@ -133,6 +124,6 @@ int main(int argc, char *argv[])
 
         cout << (allConnected ? "Yes" : "No") << '\n';
     }
-    
+
     return 0;
 }
