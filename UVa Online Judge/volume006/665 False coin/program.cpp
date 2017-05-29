f3c7101f08cd9c8955ac006cbe9f33a3fb63eebf
metaphysis <metaphysis@yeah.net>
2017-05-29 21:17:22 +0800

    2017-05-29

---
+++ UVa Online Judge/volume006/665 False coin/program.cpp
@@ -0,0 +1,98 @@
+// False coin
+// UVa ID: 665
+// Verdict: Accepted
+// Submission Date: 2017-05-29
+// UVa Run Time: 0.000s
+//
+// 版权所有（C）2017，邱秋。metaphysis # yeah dot net
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
+
+    int M, N, K, Pi;
+    unordered_set<int> leftPan[128], rightPan[128];
+    char result[128];
+    
+    cin >> M;
+    for (int c = 1; c <= M; c++)
+    {
+        if (c > 1) cout << '\n';
+        
+        cin >> N >> K;
+        
+        int identifiers;
+        for (int i = 1; i <= K; i++)
+        {
+            leftPan[i].clear(), rightPan[i].clear();
+
+            cin >> Pi;
+            for (int j = 1; j <= Pi; j++)
+            {
+                cin >> identifiers;
+                leftPan[i].insert(identifiers);
+            }
+            
+            for (int j = 1; j <= Pi; j++)
+            {
+                cin >> identifiers;
+                rightPan[i].insert(identifiers);                
+            }
+            
+            cin >> result[i];
+        }
+        
+        int possibility = 0, falseCoin = -1;
+        for (int i = 1; i <= N; i++)
+        {
+            for (int w = 0; w <= 2; w += 2)
+            {
+                bool conformed = true;
+                for (int j = 1; j <= K; j++)
+                {
+                    int leftPanWeight = leftPan[j].find(i) != leftPan[j].end() ? w : 1;
+                    int rightPanWeight = rightPan[j].find(i) != rightPan[j].end() ? w : 1;
+                    
+                    if (leftPanWeight < rightPanWeight && result[j] == '<') continue;
+                    if (leftPanWeight > rightPanWeight && result[j] == '>') continue;
+                    if (leftPanWeight == rightPanWeight && result[j] == '=') continue;
+                    
+                    conformed = false;
+                    break;
+                }
+                
+                if (conformed)
+                {
+                    possibility += 1, falseCoin = i;
+                    break;
+                }
+            }
+        }
+        
+        if (possibility == 1) cout << falseCoin << '\n';
+        else cout << "0\n";
+    }
+    
+    return 0;
+}
