21f0e532581657a988efb034b90003c814e4c587
metaphysis <metaphysis@yeah.net>
2017-05-30 23:23:11 +0800

    2017-05-30

---
+++ UVa Online Judge/volume006/651 Deck/program.cpp
@@ -0,0 +1,52 @@
+// Deck
+// UVa ID: 651
+// Verdict: Accepted
+// Submission Date: 2017-05-30
+// UVa Run Time: 0.000s
+//
+// 版权所有（C）2017，邱秋。metaphysis # yeah dot net
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
+
+    cout.setf(ios::fixed);
+    cout.precision(3);
+
+    cout << "# Cards Overhang\n";
+    
+    double overhang[100000] = {0.0};
+    
+    for (int i = 1; i <= 99999; i++)
+        overhang[i] = overhang[i - 1] + 1.0 / (double)(2 * i);
+    
+    int cards;
+    while (cin >> cards)
+    {
+        cout << setw(5) << right << cards;
+        cout << setw(10) << right << overhang[cards] << '\n';
+    }
+    
+    return 0;
+}
