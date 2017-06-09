7a85a0a596cc8c45a3d88eca696dc11418bce123
metaphysis <metaphysis@yeah.net>
2017-06-09 21:48:04 +0800

    2017-06-09

--- Book/14/14-4-1.cpp
+++ Book/14/14-4-1.cpp
@@ -1,7 +1,7 @@
 // Useless Tile Packers （没用的瓷砖打包公司）
 // PC/UVa IDs: 111405/10065, Popularity: C, Success rate: average Level: 3
 // Verdict: Accepted
-// Submission Date: 2016-08-13
+// Submission Date: 2017-06-09
 // UVa Run Time: 0.000s
 //
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
@@ -10,48 +10,43 @@
 #include <iomanip>
 #include <cmath>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-const int MAXV = 105;
 const int EPSILON = 0;
 
 struct point
 {
 	int x, y;
 	
-	bool operator<(const point& another) const
+	bool operator<(const point &p) const
 	{
-	    if (fabs(x - another.x) > EPSILON)
-	        return x < another.x;
-	    else
-	        return y < another.y;
+	    if (fabs(x - p.x) > EPSILON) return x < p.x;
+	    return y < p.y;
 	}
 	
-	bool operator==(const point& another) const
+	bool operator==(const point &p) const
 	{
-	    return fabs(x - another.x) <= EPSILON && fabs(y - another.y) <= EPSILON;
+	    return fabs(x - p.x) <= EPSILON && fabs(y - p.y) <= EPSILON;
 	}
 };
 
-struct polygon
-{
-	int number;
-	point vertex[MAXV];
-};
+typedef vector<point> polygon;
 
 point lowerLeftPoint;
 
 // 利用有向面积计算多边形的面积，注意最后结果取绝对值，因为顶点顺序可能并不是按
 // 逆时针方向给出。
-double area(point vertex[], int number)
+double area(polygon pg)
 {
 	double areaOfPolygon = 0.0;
 
-	for (int i = 0; i < number; i++)
+    int n = pg.size();
+	for (int i = 0; i < n; i++)
 	{
-		int j = (i + 1) % number;
-		areaOfPolygon += (vertex[i].x * vertex[j].y - vertex[j].x * vertex[i].y);
+		int j = (i + 1) % n;
+		areaOfPolygon += (pg[i].x * pg[j].y - pg[j].x * pg[i].y);
 	}
 
 	return fabs(areaOfPolygon / 2.0);
@@ -68,6 +63,7 @@ bool cw(point a, point b, point c)
 {
 	return crossProduct(a, b, c) < -EPSILON;
 }
+
 // 从点a向点b望去，点c位于线段ab的左侧时，返回true。
 bool ccw(point a, point b, point c)
 {
@@ -87,89 +83,65 @@ bool ccwOrCollinear(point a, point b, point c)
 }
 
 // 两点距离的平方值。
-double distanceToLowerLeftPoint(point p)
+double distanceToLowerLeftPoint(point &p)
 {
     return pow(lowerLeftPoint.x - p.x, 2) + pow(lowerLeftPoint.y - p.y, 2);
 }
 
 // 按相对于参考点的极角大小进行排序。
-bool smallerAngle(point first, point second)
+bool smallerAngle(point &p1, point &p2)
 {
-    if (collinear(lowerLeftPoint, first, second))
-        return distanceToLowerLeftPoint(first) <= distanceToLowerLeftPoint(second);
-    return ccw(lowerLeftPoint, first, second);
+    if (collinear(lowerLeftPoint, p1, p2))
+        return distanceToLowerLeftPoint(p1) <= distanceToLowerLeftPoint(p2);
+    return ccw(lowerLeftPoint, p1, p2);
 }
 
 // Graham凸包扫描算法。
-polygon grahamConvexHull(point vertex[], int number)
+polygon grahamConvexHull(polygon &vertices)
 {
-    polygon pg;
+    polygon ch(vertices);
 
-    // 点数小于3个，不构成凸包。
-	if (number < 3)
+	sort(ch.begin(), ch.end());
+    ch.erase(unique(ch.begin(), ch.end()), ch.end());
+
+    lowerLeftPoint = ch.front();
+    sort(ch.begin() + 1, ch.end(), smallerAngle);
+
+	ch.push_back(lowerLeftPoint);
+
+	int top = 2, candidate = 2, total = ch.size() - 1;
+	while (candidate <= total)
 	{
-		for (int i = 0; i < number; i++)
-			pg.vertex[i] = vertex[i];
-		pg.number = number;
-		return pg;
-	}
-
-	// 按横坐标和纵坐标排序，移除重复点。
-	sort(vertex, vertex + number);
-    number = unique(vertex, vertex + number) - vertex;
-
-    // 按极角排序。
-    lowerLeftPoint = vertex[0];
-    sort(vertex + 1, vertex + number, smallerAngle);
-
-    // 将初始的两点放入凸包。
-	pg.vertex[0] = vertex[0];
-	pg.vertex[1] = vertex[1];
-	
-	// 设置哨兵元素，将最左最低点设置为最后一个元素以便扫描时能回到参考点。
-	vertex[number] = lowerLeftPoint;
-	
-	int i = 2, top = 1;
-	while (i <= number)
-	{
-	    if (cw(pg.vertex[top - 1], pg.vertex[top], vertex[i]))
+	    if (cw(ch[top - 2], ch[top - 1], ch[candidate]))
 	        top--;
-	    else if (collinear(pg.vertex[top - 1], pg.vertex[top], vertex[i]))
-            pg.vertex[top] = vertex[i++];
-	    else
-	        pg.vertex[++top] = vertex[i++];
+	    else 
+	    {
+	        if (collinear(ch[top - 2], ch[top - 1], ch[candidate]))
+	            ch[top - 1] = ch[candidate++];
+	        else
+	            ch[top++] = ch[candidate++];
+        }
 	}
-	
-    pg.number = top;
-    
-    return pg;
+	ch.erase(ch.begin() + top, ch.end());
+
+	return ch;
 }
 
 int main(int ac, char *av[])
 {
-	point tile[MAXV];
-	int number, currentCase = 1;
-
 	cout.precision(2);
 	cout.setf(ios::fixed | ios::showpoint);
 
+	int number, cases = 1;
 	while (cin >> number, number)
 	{
+	    polygon tile(number);
 		for (int i = 0; i < number; i++)
-		{
-			cin >> tile[i].x;
-			cin >> tile[i].y;
-		}
-
-		double used = area(tile, number);
-
-		polygon container = grahamConvexHull(tile, number);
-
-		cout << "Tile #" << currentCase++ << endl;
-		double all = area(container.vertex, container.number);
-		double rate = (1.0 - used / all) * 100.0;
-		cout << "Wasted Space = " << rate << " %" << endl;
-		cout << endl;
+			cin >> tile[i].x >> tile[i].y;
+		double used = area(tile);
+		double all = area(grahamConvexHull(tile));
+		cout << "Tile #" << cases++ << '\n';
+		cout << "Wasted Space = " << (1.0 - used / all) * 100.0 << " %\n\n";
 	}
 
 	return 0;
