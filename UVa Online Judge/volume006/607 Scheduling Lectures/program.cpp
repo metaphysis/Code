476685ec1d95d9d51dc352901c575d37c754fb98
metaphysis <metaphysis@yeah.net>
2017-06-25 14:13:32 +0800

    2017-06-25

--- UVa Online Judge/volume006/607 Scheduling Lectures/program.cpp
+++ UVa Online Judge/volume006/607 Scheduling Lectures/program.cpp
@@ -1,8 +1,8 @@
 // Scheduling Lectures
 // UVa ID: 607
-// Verdict: 
-// Submission Date: 
-// UVa Run Time: s
+// Verdict: Accepted
+// Submission Date: 2017-06-25
+// UVa Run Time: 0.260s
 //
 // 版权所有（C）2017，邱秋。metaphysis # yeah dot net
 
@@ -27,54 +27,70 @@
 
 using namespace std;
 
-const int INF = 250000000;
+const int INF = 1 << 30, MAXN = 1024;
+
+int N, L, C, T, topics[MAXN], DI[MAXN][MAXN], visited[MAXN][MAXN];
+int cases = 0;
+
+int cost(int m)
+{
+    int r = L - m;
+    if (r == 0) return 0;
+    if (1 <= r && r <= 10) return -C;
+    return (r - 10) * (r - 10);
+}
+
+int dp(int x, int y)
+{
+    if (visited[x][y]) return DI[x][y];
+
+    int *di = &DI[x][y];
+    if (x == 0) return y ? INF : 0;
+    else
+    {
+        *di = INF;
+        int m = 0;
+        for (int i = y; i >= 1; i--)
+        {
+            m += topics[i];
+            if (m > L) break;
+            int best = dp(x - 1, i - 1);
+            if (best != INF) *di = min(best + cost(m), *di);
+        }
+        visited[x][y] = 1;
+        return *di;
+    }
+}
 
 int main(int argc, char *argv[])
 {
     cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
 
-    int n, L, C, topics[1024], DI[1024][1024];
-    int cases = 0;
-    
-    while (cin >> n, n > 0)
+    while (cin >> N, N > 0)
     {
         cin >> L >> C;
         
-        int lectures = 0, minutes = 0;
-        
-        for (int i = 1; i <= n; i++)
+        memset(visited, 0, sizeof(visited));
+
+        int minutes = 0;
+        T = 1;
+
+        for (int i = 1; i <= N; i++)
         {
             cin >> topics[i];
-            if (minutes + topics[i] >= L)
-            {
-                lectures++;
-                if (minutes + topics[i] > L)
-                    minutes = topics[i];
-                else
-                    minutes = 0;
-            }
-            else
-                minutes += topics[i];
-        }
-        if (minutes > 0) lectures++;
-        
-        for (int i = 0; i <= lectures; i++)
-            for (int j = 0; j <= n; j++)
-                DI[i][j] = INF;
-        
-        for (int i = 1; i <= lectures; i++)
-            for (int j = 1; j <= n; j++)
-            {
-            }
             
-        int minDI = INF;
-        for (int i = 1; i <= lectures; i++)
-            minDI = min(minDI, DI[n][i]);
+            minutes += topics[i];
+            if (minutes > L)
+            {
+                T++;
+                minutes = topics[i];
+            }
+        }
 
         if (cases++ > 0) cout << '\n';
         cout << "Case " << cases << ":\n";
-        cout << "Minimum number of lectures: " << lectures << '\n';
-        cout << "Total dissatisfaction index: " << minDI << '\n';
+        cout << "Minimum number of lectures: " << T << '\n';
+        cout << "Total dissatisfaction index: " << dp(T, N) << '\n';
     }
     
     return 0;
