39d104755d4ea871b7fc187d92c11cce4cab6bea
metaphysis <metaphysis@yeah.net>
2017-06-27 12:40:29 +0800

    2017-06-27

---
+++ Book/11/11-2-6.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+const int MAXN = 1024;
+
+unsigned long long dp[MAXN][MAXN];
+
+unsigned long long ip(int n, int m)
+{
+    if (dp[n][m]) return dp[n][m];
+    if (n == 1 || m == 1) dp[n][m] = 1;
+    else if (n < m) dp[n][m] = ip(n, n);
+    else if (n == m) dp[n][m] = 1 + ip(n, m - 1);
+    else dp[n][m] = ip(n, m - 1) + ip(n - m, m);
+    return dp[n][m];
+}
+
+int main(int argc, char *argv[])
+{
+    int n, m;
+
+    while (cin >> n >> m)
+    {
+        memset(dp, 0, sizeof(dp));
+        cout << ip(n, m) << '\n';
+    }
+
+    return 0;
+}
