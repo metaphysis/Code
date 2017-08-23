6dade660a02f40e1469088d958651df3f4e0530a
metaphysis <metaphysis@yeah.net>
2017-08-23 12:49:33 +0800

    2017-08-23

--- UVa Online Judge/miscellaneous/12118 Inspector’s Dilemma/program.cpp
+++ UVa Online Judge/miscellaneous/12118 Inspector’s Dilemma/program.cpp
@@ -1,8 +1,8 @@
 // Inspector’s Dilemma
 // UVa ID: 12118
-// Verdict: 
-// Submission Date: 
-// UVa Run Time: s
+// Verdict: Accepted
+// Submission Date: 2017-08-23
+// UVa Run Time: 0.030s
 //
 // 版权所有（C）2017，邱秋。metaphysis # yeah dot net
 
@@ -28,14 +28,14 @@
 using namespace std;
 
 vector<int> edges[1010];
-int V, E, T, visited[1010];
+int V, E, T, visited[1010], appeared[1010];
 
 int dfs(int u)
 {
     visited[u] = 1;
     int oddDegreeVertices = (edges[u].size() & 1 ? 1 : 0);
     for (auto v : edges[u])
-        if (edges[v].size() && !visited[v])
+        if (!visited[v])
             oddDegreeVertices += dfs(v);
     return oddDegreeVertices;
 }
@@ -52,19 +52,21 @@ int main(int argc, char *argv[])
         for (int i = 1; i <= V; i++)
             edges[i].clear();
 
+        memset(visited, 0, sizeof(visited));
+        memset(appeared, 0, sizeof(appeared));
+        
         int a, b;
         for (int i = 1; i <= E; i++)
         {
             cin >> a >> b;
             edges[a].push_back(b);
             edges[b].push_back(a);
+            appeared[a] = appeared[b] = 1;
         }
         
-        memset(visited, 0, sizeof(visited));
-
         int additional = 0, components = 0;
         for (int i = 1; i <= V; i++)
-            if (edges[i].size() && !visited[i])
+            if (appeared[i] && !visited[i])
             {
                 components++;
                 int oddDegreeVertices = dfs(i);
@@ -72,7 +74,7 @@ int main(int argc, char *argv[])
             }
 
         cout << "Case " << ++cases << ": ";
-        cout << T * ((components - 1) + E + additional) << '\n';
+        cout << T * (max(components - 1, 0) + E + additional) << '\n';
     }
 
     return 0;
