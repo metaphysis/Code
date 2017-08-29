381245897bf67ec0bc26a37e727fb1ad07df5d48
metaphysis <metaphysis@yeah.net>
2017-08-29 22:21:39 +0800

    2017-08-29

---
+++ UVa Online Judge/miscellaneous/10071 Back to High School Physics/program.cpp
@@ -0,0 +1,38 @@
+// Back to High School Physics
+// UVa ID: 10071
+// Verdict: Accepted
+// Submission Date: 2017-08-29
+// UVa Run Time: 0.000s
+//
+// 版权所有（C）2017，邱秋。metaphysis # yeah dot net
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
+
+    long long v, t;
+    while (cin >> v >> t) cout << 2 * v * t << '\n';
+    
+    return 0;
+}
---
+++ UVa Online Judge/miscellaneous/11172 Relational Operators/program.cpp
@@ -0,0 +1,52 @@
+// Relational Operators
+// UVa ID: 11172
+// Verdict: Accepted
+// Submission Date: 2017-08-29
+// UVa Run Time: 0.000s
+//
+// 版权所有（C）2017，邱秋。metaphysis # yeah dot net
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
+
+    int cases;
+    
+    while (cin >> cases)
+    {
+        long long a, b;
+        for (int c = 1; c <= cases; c++)
+        {
+            cin >> a >> b;
+            
+            if (a > b) cout << '>';
+            else if (a < b) cout << '<';
+            else cout << '=';
+            
+            cout << '\n';
+        }
+    }
+    
+    return 0;
+}
