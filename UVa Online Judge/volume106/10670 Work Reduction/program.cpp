8eac22d4546ec8b26dccc4f6a610b60557237923
metaphysis <metaphysis@yeah.net>
2017-10-14 23:22:37 +0800

    2017-10-14

---
+++ UVa Online Judge/volume106/10670 Work Reduction/program.cpp
@@ -0,0 +1,112 @@
+// Work Reduction
+// UVa ID: 10670
+// Verdict: Accepted
+// Submission Date: 2017-10-14
+// UVa Run Time: 0.030s
+//
+// 版权所有（C）2017，邱秋。metaphysis # yeah dot net
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+struct agent
+{
+    string name;
+    long long cost;
+
+    bool operator<(const agent &a) const
+    {
+        if (cost == a.cost) return name < a.name;
+        else return cost < a.cost;
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
+
+    int cases, L;
+    long long N, M;
+
+    cin >> cases;
+    for (int c = 1; c <= cases; c++)
+    {
+        vector<agent> agencies;
+
+        cin >> N >> M >> L;
+        cin.ignore(1024, '\n');
+        for (int i = 1; i <= L; i++)
+        {
+            string line;
+            getline(cin, line);
+            
+            string next;
+            for (int i = 0; i < line.length(); i++)
+            {
+                if (isblank(line[i])) continue;
+                if (line[i] == ':')
+                    next += ',';
+                else
+                    next += line[i];
+            }
+            
+            istringstream iss(next);
+
+            string name, A, B;
+            getline(iss, name, ',');
+            getline(iss, A, ',');
+            getline(iss, B, ',');
+            
+            long long feeA = stoi(A), feeB = stoi(B);
+            long long cost = 0;
+
+            // greed algorithm.
+            long long backupN = N;
+            while (N > M)
+            {
+                long long nextN = N / 2;
+                if (nextN < M)
+                {
+                    N--;
+                    cost += feeA;
+                }
+                else
+                {
+                    if ((N - nextN) * feeA > feeB)
+                        cost += feeB;
+                    else
+                        cost += (N - nextN) * feeA;
+                    N = nextN;
+                }
+            }
+            N = backupN;
+            agencies.push_back(agent{name, cost});
+        }
+
+        sort(agencies.begin(), agencies.end());
+
+        cout << "Case " << c << '\n';
+        for (auto a : agencies)
+            cout << a.name << ' ' << a.cost << '\n';
+    }
+
+    return 0;
+}
---
+++ UVa Online Judge/volume107/10763 Foreign Exchange/program.cpp
@@ -0,0 +1,79 @@
+// Foreign Exchange
+// UVa ID: 10763
+// Verdict: Accepted
+// Submission Date: 2017-10-14
+// UVa Run Time: 0.090s
+//
+// 版权所有（C）2017，邱秋。metaphysis # yeah dot net
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+struct exchange
+{
+    int x, y;
+    
+    bool operator<(const exchange &e) const
+    {
+        if (x != e.x) return x < e.x;
+        else return y < e.y;
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
+
+    int n, x, y;
+    while (cin >> n, n > 0)
+    {
+        vector<exchange> a, b;
+        for (int i = 1; i <= n; i++)
+        {
+            cin >> x >> y;
+            if (x < y) a.push_back(exchange{x, y});
+            else b.push_back(exchange{y, x});
+        }
+        
+        if ((n % 2) == 1) cout << "NO\n";
+        else
+        {
+            sort(a.begin(), a.end());
+            sort(b.begin(), b.end());
+            
+            if (a.size() != b.size()) cout << "NO\n";
+            else
+            {
+                bool same = true;
+                for (int i = 0; i < a.size(); i++)
+                    if (a[i].x != b[i].x || a[i].y != b[i].y)
+                    {
+                        same = false;
+                        break;
+                    }
+                    
+                cout << (same ? "YES" : "NO") << '\n';
+            }
+        }
+    }
+    
+    return 0;
+}
