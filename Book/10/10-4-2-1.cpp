29870ff226fede8d9179ef009e7c6acd2319e19b
metaphysis <metaphysis@yeah.net>
2017-10-24 21:06:35 +0800

    2017-10-24

--- Book/10/10-4-2-1.cpp
+++ Book/10/10-4-2-1.cpp
@@ -1,36 +1,14 @@
-// Wormholes
-// UVa ID: 558
-// Verdict: Accepted
-// Submission Date: 2016-08-17
-// UVa Run Time: 0.000s
-//
-// 版权所有（C）2016，邱秋。metaphysis # yeah dot net
+const int MAXV = 1100, MAXE = 2100, INF = 100000000;
 
-#include <iostream>
-
-using namespace std;
-
-const int NODES = 1100, EDGES = 2100, INFINITY = 100000000;
-
-struct edge
-{
-    int from, to, weight;
-};
-
-edge edges[EDGES];
-int dist[NODES], predecessor[NODES];
+struct edge { int from, to, weight; } edge edges[MAXE];
+int dist[MAXV], parent[MAXV], n, m;
 
 // edges为边列表，predecessor保存各顶点的前驱，n为顶点数目，m为边的数目，
 // source为起始顶点的序号。
-bool bellmanFord(edge edges[], int predecessor[], int n, int m, int source)
+bool bellmanFord(int source)
 {
     // 初始化距离为无限大。各顶点的前驱顶点为未定义。
-    for (int i = 0; i <= n; i++)
-    {
-        dist[i] = INFINITY; predecessor[i] = -1;
-    }
-
-    // 起始顶点的距离为0。
+    for (int i = 0; i <= n; i++) dist[i] = INF, parent[i] = -1;
     dist[source] = 0;
     
     // 松弛。迭代次数为顶点数目减一。
@@ -41,7 +19,7 @@ bool bellmanFord(edge edges[], int predecessor[], int n, int m, int source)
             if (dist[edges[i].to] > dist[edges[i].from] + edges[i].weight)
             {
                 dist[edges[i].to] = dist[edges[i].from] + edges[i].weight;
-                predecessor[edges[i].to] = edges[i].from;
+                parent[edges[i].to] = edges[i].from;
                 updated = true;
             }
             
@@ -60,27 +38,4 @@ bool bellmanFord(edge edges[], int predecessor[], int n, int m, int source)
     
     // 返回是否存在权和为负值的环。
     return cycled;
-}
-
-int main(int argc, char *argv[])
-{
-    cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
-    int cases, n, m, x, y, t;
-    cin >> cases;
-    for (int c = 1; c <= cases; c++)
-    {
-        cin >> n >> m;
-        for (int i = 1; i <= m; i++)
-        {
-            cin >> x >> y >> t;
-            edges[i] = (edge){x, y, t};
-        }
-        
-        bool cycled = bellmanFord(edges, predecessor, n, m, 0);
-        
-        cout << (cycled ? "possible\n" : "not possible\n");
-    }
-    
-	return 0;
 }
---
+++ UVa Online Judge/volume104/10449 Traffic/program.cpp
@@ -0,0 +1,91 @@
+// Traffic
+// UVa ID: 10449
+// Verdict: Accepted
+// Submission Date: 2017-10-24
+// UVa Run Time: 0.000s
+//
+// 版权所有（C）2017，邱秋。metaphysis # yeah dot net
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+const int MAXV = 210, MAXE = 100010, MAXW = 0xfffff, INF = 0x3fffffff;
+
+struct edge { int from, to, weight; } edges[MAXE];
+
+int main(int argc, char *argv[])
+{
+    cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
+
+    int cases = 0, n, r, x, y, q, d;
+    int busyness[MAXV], dist[MAXV], old[MAXV], cycled[MAXV];
+
+    while (cin >> n)
+    {
+        cout << "Set #" << ++cases << '\n';
+
+        for (int i = 1; i <= n; i++)
+            cin >> busyness[i];
+            
+        cin >> r;
+        for (int i = 1; i <= r; i++)
+        {
+            cin >> x >> y;
+            int t = pow(busyness[y] - busyness[x], 3);
+            edges[i] = edge{x, y, t};
+        }
+
+        for (int i = 0; i <= n; i++) dist[i] = INF;
+ 
+        dist[1] = 0;
+        for (int k = 1; k <= n; k++)
+        {
+            bool updated = false;
+            for (int i = 1; i <= r; i++)
+                if (dist[edges[i].to] > dist[edges[i].from] + edges[i].weight)
+                {
+                    dist[edges[i].to] = dist[edges[i].from] + edges[i].weight;
+                    updated = true;
+                }
+            if (!updated) break;
+        }
+
+        memcpy(old, dist, sizeof(dist));
+        for (int i = 1; i <= r; i++)
+            if (dist[edges[i].to] > dist[edges[i].from] + edges[i].weight)
+                dist[edges[i].to] = dist[edges[i].from] + edges[i].weight;
+        
+        memset(cycled, 0, sizeof(cycled));
+        for (int i = 1; i <= n; i++)
+            if (dist[i] < old[i] || dist[i] > MAXW)
+                cycled[i] = 1;
+
+        cin >> q;
+        for (int i = 1; i <= q; i++)
+        {
+            cin >> d;
+            if (cycled[d] || dist[d] < 3) cout << "?\n";
+            else cout << dist[d] << '\n';
+        }
+    }
+    
+	return 0;
+}
