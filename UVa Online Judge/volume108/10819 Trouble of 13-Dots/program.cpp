7beba316d034149a5e23c19b1ffdac9d5cdac27e
metaphysis <metaphysis@yeah.net>
2017-11-20 21:53:44 +0800

    2017-11-20

--- UVa Online Judge/volume108/10819 Trouble of 13-Dots/program.cpp
+++ UVa Online Judge/volume108/10819 Trouble of 13-Dots/program.cpp
@@ -1,8 +1,8 @@
 // Trouble of 13-Dots
 // UVa ID: 10819
-// Verdict: 
-// Submission Date: 
-// UVa Run Time: s
+// Verdict: Accepted
+// Submission Date: 2017-11-20
+// UVa Run Time: 0.040s
 //
 // 版权所有（C）2017，邱秋。metaphysis # yeah dot net
 
@@ -31,33 +31,32 @@ int main(int argc, char *argv[])
 {
     cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
 
-    int m, n, value[10300][2], prices[110], favour[110];
+    int m, n, value[10300], prices[110], favour[110];
     
     while (cin >> m >> n)
     {
         for (int i = 1; i <= n; i++)
             cin >> prices[i] >> favour[i];
 
-        int money = m + 200;
-        for (int j = 0; j <= money; j++)
-            value[j][0] = value[j][1] = 0;
-
+        memset(value, 0, sizeof(value));
         for (int i = 1; i <= n; i++)
-            for (int j = money; j >= prices[i]; j--)
-                if (value[j - prices[i]][0] + favour[i] > value[j][0])
-                {
-                    value[j][0] = value[j - prices[i]][0] + favour[i];
-                    value[j][1] = value[j - prices[i]][1] + prices[i];
-                }
+            for (int j = m; j >= prices[i]; j--)
+                value[j] = max(value[j], value[j - prices[i]] + favour[i]);
 
-        if (m < 1800)
-            cout << value[m][0] << '\n';
+        if (m <= 1800) cout << value[m] << '\n';
         else
         {
-            if (value[money][1] > 2000)
-                cout << value[money][0] << '\n';
-            else
-                cout << value[m][0] << '\n';
+            int r = value[m];
+            memset(value, -1, sizeof(value));
+            value[0] = 0;
+            m += 200;
+            for (int i = 1; i <= n; i++)
+                for (int j = m; j >= prices[i]; j--)
+                    if (value[j - prices[i]] >= 0)
+                        value[j] = max(value[j], value[j - prices[i]] + favour[i]);
+            for (int i = 2001; i <= m; i++)
+                r = max(r, value[i]);
+            cout << r << '\n';
         }
     }
     
