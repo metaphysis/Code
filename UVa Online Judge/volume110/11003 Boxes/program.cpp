9b6dc3698fb559754857f105b99e65a7abf0dc0b
metaphysis <metaphysis@yeah.net>
2017-11-22 12:41:34 +0800

    2017-11-22

---
+++ UVa Online Judge/volume110/11003 Boxes/program.cpp
@@ -0,0 +1,59 @@
+// Boxes
+// UVa ID: 11003
+// Verdict: Accepted
+// Submission Date: 2017-11-21
+// UVa Run Time: 0.000s
+//
+// 版权所有（C）2017，邱秋。metaphysis # yeah dot net
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+struct box
+{
+    int weight, load;
+} boxes[1100];
+
+int main(int argc, char *argv[])
+{
+    cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
+
+    int n;
+    
+    while (cin >> n, n > 0)
+    {
+        for (int i = 1; i <= n; i++)
+            cin >> boxes[i].weight >> boxes[i].load;
+            
+        vector<int> m;            
+        m.push_back(0);
+        for (int i = n; i >= 1; i--)
+        {
+            if (boxes[i].load >= m.back()) m.push_back(m.back() + boxes[i].weight);
+            for (int j = m.size() - 2; j >= 0; j--)
+                if (boxes[i].load >= m[j] && m[j] + boxes[i].weight < m[j + 1])
+                    m[j + 1] = m[j] + boxes[i].weight;
+        }
+        cout << m.size() - 1 << '\n';
+    }
+
+    return 0;
+}
---
+++ UVa Online Judge/volume113/11341 Term Strategy/program.cpp
@@ -0,0 +1,72 @@
+// Term Strategy
+// UVa ID: 11341
+// Verdict: Accepted
+// Submission Date: 2017-11-22
+// UVa Run Time: 0.000s
+//
+// 版权所有（C）2017，邱秋。metaphysis # yeah dot net
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
+
+    int cases, n, m, grade[11][110], sum[11][110];
+    cin >> cases;
+
+    for (int c = 1; c <= cases; c++)
+    {
+        cin >> n >> m;
+
+        int t = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            bool flag = false;
+            for (int j = 1; j <= m; j++)
+            {
+                cin >> grade[i][j];
+                if (t >= 0 && !flag && grade[i][j] >= 5) t += j, flag = true;
+            }
+            if (!flag) t = -1;
+        }
+
+        if (t < 0 || t > m)
+        {
+            cout << "Peter, you shouldn't have played billiard that much.\n";
+            continue;
+        }
+
+        memset(sum, -1, sizeof(sum));
+        sum[0][0] = 0;
+        for (int i = 1; i <= n; i++)
+            for (int k = i; k <= i + m - n; k++)
+                for (int j = 1; j <= k - i + 1; j++)
+                    if (grade[i][j] >= 5 && sum[i - 1][k - j] >= 0)
+                        sum[i][k] = max(sum[i][k], sum[i - 1][k - j] + grade[i][j]);
+
+        cout << "Maximal possible average mark - ";
+        cout << fixed << setprecision(2) << (1.0 * sum[n][m] / n + 1e-9) << ".\n";
+    }
+
+    return 0;
+}
