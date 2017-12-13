41ac4f7321897c90158382e8b94685fbddce7e3f
metaphysis <metaphysis@yeah.net>
2017-12-14 00:20:41 +0800

    2017-12-14

--- Book/14/14-2-1.cpp
+++ Book/14/14-2-1.cpp
@@ -1,3 +1,22 @@
+struct point {
+    double x, y;
+
+    point (double x = 0, double y = 0): x(x), y(y) {}
+
+    point operator + (point p) { return point(x + p.x, y + p.y); };
+    point operator - (point p) { return point(x - p.x, y - p.y); };
+    point operator * (double a) { return point(a * x, a * y); };
+    point operator / (double a) { return point(x / a, y / a); };
+
+    bool operator < (const point &p) const {
+        return x != p.x ? x < p.x : y < p.y;
+    }
+
+    bool operator == (const point &p) const {
+        return fabs(x - p.x) < EPSILON && fabs(y - p.y) < EPSILON;
+    }
+};
+
 double nrom(point a)
 {
 	return a.x * a.x + a.y * a.y;
@@ -17,22 +36,3 @@ double cross(point a, point b)
 {
     return a.x * b.y - a.y * b.x;
 }
-
-struct point {
-    double x, y;
-
-    point (double x = 0, double y = 0): x(x), y(y) {}
-
-    point operator + (point p) { return point(x + p.x, y + p.y); };
-    point operator - (point p) { return point(x - p.x, y - p.y); };
-    point operator * (double a) { return point(a * x, a * y); };
-    point operator / (double a) { return point(x / a, y / a); };
-
-    bool operator < (const point &p) const {
-        return x != p.x ? x < p.x : y < p.y;
-    }
-
-    bool operator == (const point &p) const {
-        return fabs(x - p.x) < EPSILON && fabs(y - p.y) < EPSILON;
-    }
-};
---
+++ Book/14/14-8.cpp
@@ -0,0 +1,76 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+const double EPSILON = 1e-7;
+
+struct point {
+    double x, y;
+
+    point (double x = 0, double y = 0): x(x), y(y) {}
+
+    point operator + (point p) { return point(x + p.x, y + p.y); };
+    point operator - (point p) { return point(x - p.x, y - p.y); };
+    point operator * (double a) { return point(a * x, a * y); };
+    point operator / (double a) { return point(x / a, y / a); };
+
+    bool operator < (const point &p) const {
+        return x != p.x ? x < p.x : y < p.y;
+    }
+
+    bool operator == (const point &p) const {
+        return fabs(x - p.x) < EPSILON && fabs(y - p.y) < EPSILON;
+    }
+    
+    double distTo(point a)
+    {
+        return sqrt(pow(x - a.x, 2) + pow(y - a.y, 2));
+    }
+};
+
+typedef vector<point> polygon;
+
+double cross(point a, point b)
+{
+    return a.x * b.y - a.y * b.x;
+}
+
+double cp(point a, point b, point c)
+{
+    return cross(b - a, c - a);
+}
+
+double rotatingCalipers(polygon pg)
+{
+    double dist = 0.0;
+    pg.push_back(pg.front());
+    for (int i = 0, q = 1, n = pg.size() - 1; i < n; i++)
+    {
+        while (cp(pg[i], pg[i + 1], pg[q + 1]) > cp(pg[i], pg[i + 1], pg[q]))
+            q = (q + 1) % pg.size();
+        dist = max(dist, max(pg[i].distTo(pg[q]), pg[i + 1].distTo(pg[q + 1])));
+    }
+    return dist;
+}
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
