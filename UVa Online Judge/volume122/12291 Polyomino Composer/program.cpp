f54dca50c04c5b4d01498d8fc14b0e452cd262cd
metaphysis <metaphysis@yeah.net>
2018-01-11 11:12:46 +0800

    2018-01-11

--- UVa Online Judge/volume122/12291 Polyomino Composer/program.cpp
+++ UVa Online Judge/volume122/12291 Polyomino Composer/program.cpp
@@ -1,8 +1,8 @@
 // Polyomino Composer
 // UVa ID: 12291
-// Verdict: 
-// Submission Date: 
-// UVa Run Time: s
+// Verdict: Accepted
+// Submission Date: 2018-01-11
+// UVa Run Time: 0.000s
 //
 // 版权所有（C）2018，邱秋。metaphysis # yeah dot net
 
@@ -30,6 +30,100 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
+
+    string line;
+    char large[32][32], small[16][16], temp[32][32];
+    int n, m;
+
+    while (cin >> n >> m, n > 0)
+    {
+        memset(large, '.', sizeof(large));
+        memset(small, '.', sizeof(small));
+
+        int cnt1 = 0, cnt2 = 0;
+        
+        // WARNING:
+        // Some lines of input may contain less character than n or m.
+        cin.ignore(1024, '\n');
+        for (int i = 0; i < n; i++)
+        {
+            getline(cin, line);
+            for (int j = 0; j < line.length(); j++)
+            {
+                large[i][j] = line[j];
+                if (large[i][j] == '*') cnt1 += 1;
+            }
+        }
+
+        int leftx = m, rightx = 0, upy = m, bottomy = 0;
+        for (int i = 0; i < m; i++)
+        {
+            getline(cin, line);
+            for (int j = 0; j < line.length(); j++)
+            {
+                small[i][j] = line[j];
+                if (small[i][j] == '*')
+                {
+                    cnt2 += 1;
+                    leftx = min(leftx, j), rightx = max(rightx, j);
+                    upy = min(upy, i), bottomy = max(bottomy, i);
+                }
+            }
+        }
+
+        if (cnt1 != 2 * cnt2)
+        {
+            cout << "0\n";
+            continue;
+        }
+
+        // First match.
+        bool matched = false;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+            {
+                bool same = true;
+                for (int k = upy, y = 0; k <= bottomy; k++, y++)
+                    for (int l = leftx, x = 0; l <= rightx; l++, x++)
+                        if (large[i + y][j + x] == '.' && small[k][l] == '*')
+                        {
+                            same = false;
+                            break;
+                        }
+                if (same)
+                {
+                    // Copy and set.
+                    memcpy(temp, large, sizeof(large));
+
+                    for (int k = upy, y = 0; k <= bottomy; k++, y++)
+                        for (int l = leftx, x = 0; l <= rightx; l++, x++)
+                            if (small[k][l] == '*')
+                                temp[i + y][j + x] = '.';
+
+                    // Second match.
+                    for (int ii = 0; ii < n; ii++)
+                        for (int jj = 0; jj < n; jj++)
+                        {
+                            bool stillSame = true;
+                            for (int k = upy, y = 0; k <= bottomy; k++, y++)
+                                for (int l = leftx, x = 0; l <= rightx; l++, x++)
+                                    if (temp[ii + y][jj + x] == '.' && small[k][l] == '*')
+                                    {
+                                        stillSame = false;
+                                        break;
+                                    }
+                            if (stillSame)
+                            {
+                                matched = true;
+                                goto print;
+                            }
+                        }
+                }
+            }
+            
+        print:
+        cout << (matched ? 1 : 0) << '\n';
+    }
 
     return 0;
 }
---
+++ UVa Online Judge/volume123/12398 NumPuzz I/program.cpp
@@ -0,0 +1,98 @@
+// NumPuzz I
+// UVa ID: 12398
+// Verdict: Accepted
+// Submission Date: 2018-01-11
+// UVa Run Time: 0.000s
+//
+// 版权所有（C）2018，邱秋。metaphysis # yeah dot net
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
+
+    string line;
+    int cases = 0, grid[3][3];
+
+    while (getline(cin, line))
+    {
+        cout << "Case #" << ++cases << ":\n";
+        memset(grid, 0, sizeof(grid));
+        
+        for (int i = line.length() - 1; i >= 0; i--)
+        {
+            if (line[i] == 'a')
+            {
+                grid[0][0]++, grid[0][1]++, grid[1][0]++;
+            }
+            else if (line[i] == 'b')
+            {
+                grid[0][0]++, grid[0][1]++, grid[0][2]++, grid[1][1]++;
+            }
+            else if (line[i] == 'c')
+            {
+                grid[0][1]++, grid[0][2]++, grid[1][2]++;
+            }
+            else if (line[i] == 'd')
+            {
+                grid[0][0]++, grid[1][0]++, grid[1][1]++, grid[2][0]++;
+            }
+            else if (line[i] == 'e')
+            {
+                grid[0][1]++, grid[1][0]++, grid[1][1]++, grid[1][2]++, grid[2][1]++;
+            }
+            else if (line[i] == 'f')
+            {
+                grid[0][2]++, grid[1][1]++, grid[1][2]++, grid[2][2]++;
+            }
+            else if (line[i] == 'g')
+            {
+                grid[1][0]++, grid[2][0]++, grid[2][1]++;
+            }
+            else if (line[i] == 'h')
+            {
+                grid[1][1]++, grid[2][0]++, grid[2][1]++, grid[2][2]++;
+            }
+            else if (line[i] == 'i')
+            {
+                grid[1][2]++, grid[2][1]++, grid[2][2]++;
+            }
+            
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    grid[i][j] %= 10;
+        }
+        
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (j) cout << ' ';
+                cout << grid[i][j];
+            }
+            cout << '\n';
+        }
+    }
+
+    return 0;
+}
