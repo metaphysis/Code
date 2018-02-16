eabe467f4d667e230a1e504e89f438495964767f
metaphysis <metaphysis@yeah.net>
2018-02-16 13:00:39 +0800

    2018-02-16

---
+++ UVa Online Judge/volume103/10330 Power Transmission/program.cpp
@@ -0,0 +1,158 @@
+// Power Transmission
+// UVa ID: 10330
+// Verdict: Accepted
+// Submission Date: 2018-02-16
+// UVa Run Time: 0.010s
+//
+// 版权所有（C）2018，邱秋。metaphysis # yeah dot net
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+const int INF = 0x7fffffff;
+
+struct arc
+{
+    int u, v, capacity, residual, next;
+};
+
+class Dinic
+{
+private:
+    arc *arcs;
+    int vertices, idx, source, sink, *head, *dist;
+
+    bool bfs()
+    {
+        memset(dist, 0xff, vertices * sizeof(int));
+
+        queue<int> q; q.push(source);
+        dist[source] = 1;
+
+        while (!q.empty())
+        {
+            int u = q.front(); q.pop();
+            for (int i = head[u]; ~i; i = arcs[i].next)
+                if (arcs[i].residual > 0 && dist[arcs[i].v] < 0)
+                {
+                    dist[arcs[i].v] = dist[u] + 1;
+                    q.push(arcs[i].v);
+                }
+        }
+
+        return dist[sink] > 0;
+    }
+
+    int dfs(int u, int flow)
+    {
+        if (u == sink) return flow;
+
+        for (int i = head[u]; ~i; i = arcs[i].next)
+        {
+            int v = arcs[i].v, r = arcs[i].residual;
+            if (dist[v] == (dist[u] + 1) && r > 0)
+            {
+                int volume = dfs(v, min(r, flow));
+                if (volume > 0)
+                {
+                    arcs[i].residual -= volume;
+                    arcs[i ^ 1].residual += volume;
+                    return volume;
+                }
+            }
+        }
+
+        return 0;
+    }
+
+public:
+    Dinic(int v, int e, int s, int t)
+    {
+        vertices = v;
+        head = new int[v], dist = new int[v], arcs = new arc[e];
+        idx = 0, source = s, sink = t;
+        memset(head, -1, vertices * sizeof(int));
+    }
+
+    ~Dinic()
+    {
+        delete [] head, dist, arcs;
+    }
+
+    int maxFlow()
+    {
+        int flow = 0;
+
+        while (bfs())
+            while (int delta = dfs(0, INF))
+                flow += delta;
+
+        return flow;
+    }
+
+    void addArc(int u, int v, int capacity)
+    {
+        arcs[idx] = (arc){u, v, capacity, capacity, head[u]};
+        head[u] = idx++;
+        arcs[idx] = (arc){v, u, capacity, 0, head[v]};
+        head[v] = idx++;
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
+
+    int n, m, u, v, w, B, D, capacity[110];
+    while (cin >> n)
+    {
+        Dinic dinic(256, 256 * 1024, 0, 2 * n + 1);
+        
+        for (int i = 1; i <= n; i++)
+        {
+            cin >> capacity[i];
+            dinic.addArc(i, n + i, capacity[i]);
+        }
+        
+        cin >> m;
+        for (int i = 1; i <= m; i++)
+        {
+            cin >> u >> v >> w;
+            dinic.addArc(n + u, v, w);
+        }
+
+        cin >> B >> D;
+        for (int i = 1; i <= B; i++)
+        {
+            cin >> u;
+            dinic.addArc(0, u, capacity[u]);
+            
+        }
+        for (int i = 1; i <= D; i++)
+        {
+            cin >> u;
+            dinic.addArc(n + u, 2 * n + 1, capacity[u]);
+        }
+        cout << dinic.maxFlow() << '\n';
+    }
+
+    return 0;
+}
---
+++ UVa Online Judge/volume103/10354 Avoiding Your Boss/program.cpp
@@ -0,0 +1,126 @@
+// Avoiding Your Boss
+// UVa ID: 10354
+// Verdict: Accepted
+// Submission Date: 2018-02-16
+// UVa Run Time: 0.000s
+//
+// 版权所有（C）2018，邱秋。metaphysis # yeah dot net
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+const int MAXV = 110, INF = 0x3f3f3f3f;
+
+struct edge
+{
+    int v, w;
+    edge (int v = 0, int w = 0): v(v), w(w) {}
+    bool operator<(const edge &e) const { return w > e.w; }
+};
+
+list<edge> edges[MAXV];
+list<int> parent[MAXV];
+
+int P, R, BH, OF, YH, M;
+int dist[MAXV], disabled[MAXV];
+
+void dfs(int u)
+{
+    disabled[u] = 1;
+    for (auto v : parent[u])
+        if (!disabled[v])
+            dfs(v);
+}
+
+void mooreDijkstra()
+{
+    memset(disabled, 0, sizeof(disabled));
+
+    for (int i = 1; i <= P; i++) parent[i].clear();
+    memset(dist, 0x3f, sizeof(dist));
+    priority_queue<edge> q;
+    q.push(edge(BH, 0));
+    dist[BH] = 0;
+
+    while (!q.empty())
+    {
+        edge e1 = q.top(); q.pop();
+        for (auto e2 : edges[e1.v])
+        {
+            if (dist[e2.v] > dist[e1.v] + e2.w)
+            {
+                dist[e2.v] = dist[e1.v] + e2.w;
+                parent[e2.v].clear();
+                parent[e2.v].push_back(e1.v);
+                q.push(edge(e2.v, dist[e2.v]));
+            }
+            else if (dist[e2.v] == dist[e1.v] + e2.w)
+            {
+                parent[e2.v].push_back(e1.v);
+            }
+        }
+    }
+
+    dfs(OF);
+    
+    memset(dist, 0x3f, sizeof(dist));
+    q.push(edge(YH, 0));
+    if (!disabled[YH]) dist[YH] = 0;
+
+    while (!q.empty())
+    {
+        edge e1 = q.top(); q.pop();
+        for (auto e2 : edges[e1.v])
+        {
+            if (disabled[e2.v]) continue;
+            if (dist[e2.v] > dist[e1.v] + e2.w)
+            {
+                dist[e2.v] = dist[e1.v] + e2.w;
+                q.push(edge(e2.v, dist[e2.v]));
+            }
+        }
+    }
+    
+    if (dist[M] != INF) cout << dist[M] << '\n';
+    else cout << "MISSION IMPOSSIBLE.\n";
+}
+
+int main(int argc, char *argv[])
+{
+    cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
+
+    int u, v, w;
+
+    while (cin >> P >> R >> BH >> OF >> YH >> M)
+    {
+        for (int i = 1; i <= P; i++) edges[i].clear();
+        for (int i = 1; i <= R; i++)
+        {
+            cin >> u >> v >> w;
+            edges[u].push_back(edge(v, w));
+            edges[v].push_back(edge(u, w));
+        }
+
+        mooreDijkstra();
+    }
+
+    return 0;
+}
