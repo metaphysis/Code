ad0bc6ca08aa03d8e2dc47c8da08c87b38e6b32d
metaphysis <metaphysis@yeah.net>
2018-02-19 09:19:03 +0800

    2018-02-19

---
+++ UVa Online Judge/volume100/10008 What's Cryptanalysis?/program.cpp
@@ -0,0 +1,70 @@
+// What's Cryptanalysis?
+// UVa ID: 10008
+// Verdict: Accepted
+// Submission Date: 2018-02-19
+// UVa Run Time: 0.000s
+//
+// 版权所有（C）2018，邱秋。metaphysis # yeah dot net
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+struct frequency
+{
+    char letter;
+    int cnt;
+    bool operator<(const frequency &f) const
+    {
+        if (cnt != f.cnt) return cnt > f.cnt;
+        return letter < f.letter;
+    }
+} fs[26];
+
+int main(int argc, char *argv[])
+{
+    cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
+
+    int n;
+    string line;
+
+    cin >> n;
+    cin.ignore(1024, '\n');
+    for (int i = 0; i < 26; i++)
+    {
+        fs[i].letter = 'A' + i;
+        fs[i].cnt = 0;
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        getline(cin, line);
+        for (auto c : line)
+            if (isalpha(c))
+                fs[toupper(c) - 'A'].cnt++;
+    }
+
+    sort(fs, fs + 26);
+    for (int i = 0; i < 26; i++)
+        if (fs[i].cnt)
+            cout << fs[i].letter << ' ' << fs[i].cnt << '\n';
+        else break;
+
+    return 0;
+}
---
+++ UVa Online Judge/volume107/10701 Pre, In and Post/program.cpp
@@ -0,0 +1,67 @@
+// Pre, In and Post
+// UVa ID: 10701
+// Verdict: Accepted
+// Submission Date: 2018-02-19
+// UVa Run Time: 0.000s
+//
+// 版权所有（C）2018，邱秋。metaphysis # yeah dot net
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+void postorder(string preorder, string inorder)
+{
+    if (preorder.length() == 0)
+        return;
+
+    // 找到根结点。
+    int root = 0;
+    for (; root < inorder.length(); root++)
+        if (inorder[root] == preorder.front())
+            break;
+
+    // 在左子树中递归。
+    postorder(preorder.substr(1, root), inorder.substr(0, root));
+
+    // 在右子树中递归。
+    postorder(preorder.substr(root + 1), inorder.substr(root + 1));
+
+    // 输出根。
+    cout << preorder.front();
+}
+
+int main(int argc, char *argv[])
+{
+    cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
+
+    int cases, n;
+    string s1, s2;
+
+    cin >> cases;
+    for (int c = 1; c <= cases; c++)
+    {
+        cin >> n >> s1 >> s2;
+        postorder(s1, s2);
+        cout << '\n';
+    }
+
+    return 0;
+}
