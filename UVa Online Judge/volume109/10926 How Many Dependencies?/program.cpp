35ac62dc6535b587049f089352919492363a64a2
metaphysis <metaphysis@yeah.net>
2018-02-19 22:45:20 +0800

    2018-02-19

---
+++ UVa Online Judge/volume109/10926 How Many Dependencies?/program.cpp
@@ -0,0 +1,82 @@
+// How Many Dependencies?
+// UVa ID: 10926
+// Verdict: Accepted
+// Submission Date: 2018-02-19
+// UVa Run Time: 0.000s
+//
+// 版权所有（C）2018，邱秋。metaphysis # yeah dot net
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+const int MAXV = 110;
+
+list<int> edges[MAXV];
+int n, m, v, d, ds[MAXV], visited[MAXV], colored[MAXV];
+
+void dfs(int u)
+{
+    d++;
+    visited[u] = 1;
+    colored[u] = 1;
+    for (auto v : edges[u])
+        if (!colored[v])
+            dfs(v);
+}
+
+int main(int argc, char *argv[])
+{
+    cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
+
+    while (cin >> n, n > 0)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            edges[i].clear();
+            cin >> m;
+            for (int j = 1; j <= m; j++)
+            {
+                cin >> v;
+                edges[i].push_back(v);
+            }
+        }
+        memset(ds, 0, sizeof(ds));
+        memset(visited, 0, sizeof(visited));
+        for (int i = 1; i <= n; i++)
+            if (!visited[i])
+            {
+                memset(colored, 0, sizeof(colored));
+                d = 0;
+                dfs(i);
+                ds[i] = d;
+            }
+        
+        int maxD = *max_element(ds + 1, ds + n + 1);
+        for (int i = 1; i <= n; i++)
+            if (ds[i] == maxD)
+            {
+                cout << i << '\n';
+                break;
+            }
+    }
+
+    return 0;
+}
