bb599a9c43b2d7d8218c5036cb7d51542e64369c
metaphysis <metaphysis@yeah.net>
2018-02-28 12:10:27 +0800

    2018-02-28

--- UVa Online Judge/volume102/10257 Dick and Jane/program.cpp
+++ UVa Online Judge/volume102/10257 Dick and Jane/program.cpp
@@ -31,5 +31,14 @@ int main(int argc, char *argv[])
 {
     cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
 
+    int s, p, y, j;
+    while (cin >> s >> p >> y >> j)
+    {
+        int turtle = (j + 12 - y - p) / 3;
+        int cat = (j + 12 - s - turtle) / 2;
+        int dog = (j + 12 - cat - turtle);
+        cout << dog << ' ' << cat << ' ' << turtle << '\n';
+    }
+
     return 0;
 }
--- UVa Online Judge/volume106/10624 Super Number/program1.cpp
+++ UVa Online Judge/volume106/10624 Super Number/program1.cpp
@@ -2,7 +2,7 @@
 // UVa ID: 10624
 // Verdict: Accepted
 // Submission Date: 2018-02-28
-// UVa Run Time: 1.560s
+// UVa Run Time: 0.420s
 //
 // 版权所有（C）2018，邱秋。metaphysis # yeah dot net
 
@@ -27,7 +27,7 @@
 
 using namespace std;
 
-int number[32], n, m, finished;
+int number[32], n, m, finished, lastDigit[32][10];
 
 void dfs(int d)
 {
@@ -38,17 +38,20 @@ void dfs(int d)
         return;
     }
 
-    for (int i = (d ? 0 : 1); i <= 9; i++)
+    for (int i = (d ? 0 : 1); i < 10; i++)
     {
+        if (((d + 1) >= n) && !lastDigit[d + 1][i]) continue;
         number[d] = i;
-        int r = 0;
+        long long r = 0;
         if ((d + 1) >= n)
         {
-            for (int j = 0; j <= d; j++)
+            for (int j = 0, cnt = 1; j <= d; j++, cnt++)
             {
-                r = r * 10 + number[j];
-                r %= (d + 1);
+                
+                r = (r << 3) + (r << 1) + number[j];
+                if (cnt >= 18) cnt = 1, r %= (d + 1);
             }
+            r %= (d + 1);
         }
         if (r == 0) dfs(d + 1);
         if (finished) break;
@@ -58,6 +61,11 @@ void dfs(int d)
 int main(int argc, char *argv[])
 {
     cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
+
+    memset(lastDigit, 0, sizeof(lastDigit));
+    for (int i = 1; i < 32; i++)
+        for (int j = 0; j < 10; j++)
+            lastDigit[i][(i * j) % 10] = 1;
 
     int cases;
     cin >> cases;
