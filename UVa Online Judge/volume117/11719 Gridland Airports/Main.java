eea530ef743c372cb3aa73b49a99153ed59ec949
metaphysis <metaphysis@yeah.net>
2018-04-05 14:00:00 +0800

    2018-04-05

--- UVa Online Judge/volume117/11719 Gridland Airports/Main.java
+++ UVa Online Judge/volume117/11719 Gridland Airports/Main.java
@@ -1,8 +1,8 @@
 // Gridland Airports
 // UVa ID: 11719
-// Verdict: 
-// Submission Date: 
-// UVa Run Time: s
+// Verdict: Accepted
+// Submission Date: 2018-04-05
+// UVa Run Time: 0.720s
 //
 // 版权所有（C）2018，邱秋。metaphysis # yeah dot net
 
@@ -14,7 +14,7 @@ public class Main
 {
     public static void main(String[] args) throws IOException
     {
-        BigInteger R, C, G;
+        BigInteger R, C;
         BigInteger r = new BigInteger("10000000000000007");
 
         Scanner scan = new Scanner(System.in);
@@ -22,19 +22,24 @@ public class Main
         int cases = scan.nextInt();
         for (int c = 1; c <= cases; c++)
         {
-            int n = scan.nextInt();
-            int m = scan.nextInt();
-            R = BigInteger.valueOf(n);
-            C = BigInteger.valueOf(m);
-            R = modPow(R, m - 1, r);
-            C = modPow(C, n - 1, r);
-            G = R.multiply(C);
-            System.out.println(G.remainder(r));
+            long n = scan.nextInt();
+            long m = scan.nextInt();
+    
+            long left = ((n + 1) / 2) * ((m + 1) / 2) + (n / 2) * (m / 2);
+            long right = n * m - left;
+
+            R = BigInteger.valueOf(left);
+            C = BigInteger.valueOf(right);
+
+            R = modPow(R, right - 1, r);
+            C = modPow(C, left - 1, r);
+
+            System.out.println(R.multiply(C).remainder(r));
         }
 
     }
     
-    private static BigInteger modPow(BigInteger n, int k, BigInteger mod)
+    private static BigInteger modPow(BigInteger n, long k, BigInteger mod)
     {
         if (k == 0) return BigInteger.ONE;
         BigInteger r = modPow(n.multiply(n).remainder(mod), k >> 1, mod);
