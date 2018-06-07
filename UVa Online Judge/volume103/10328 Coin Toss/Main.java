33ce1c7c25fc7a2cf574b6c4da03802826db7674
metaphysis <metaphysis@yeah.net>
2018-06-07 16:44:54 +0800

    2018-06-07

---
+++ UVa Online Judge/volume103/10328 Coin Toss/Main.java
@@ -0,0 +1,38 @@
+// Coin Toss
+// UVa ID: 10328
+// Verdict: Accepted
+// Submission Date: 2018-06-07
+// UVa Run Time: 0.640s
+//
+// 版权所有（C）2018，邱秋。metaphysis # yeah dot net
+
+import java.io.*;
+import java.util.*;
+import java.math.BigInteger;
+
+public class Main
+{
+    static BigInteger cache[][];
+
+    public static BigInteger dp(int n, int k, int s)
+    {
+        if (n < 0 || s < 0) return BigInteger.ZERO;
+        if (n < s) return BigInteger.ZERO;
+        if (cache[n][s] != null) return cache[n][s];
+        if (s == 0) return cache[n][s] = new BigInteger("2").pow(n);
+        return cache[n][s] = dp(n - 1, k, k).add(dp(n - 1, k, s - 1));
+    }
+
+    public static void main(String[]args) throws IOException
+    {
+        Scanner scan = new Scanner(System.in);
+        int n, k;
+        while (scan.hasNextInt())
+        {
+            n = scan.nextInt();
+            k = scan.nextInt();
+            cache = new BigInteger[n + 1][k + 1];
+            System.out.println(dp(n, k, k));
+        }
+    }
+}
