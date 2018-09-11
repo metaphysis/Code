8416b620f855a4a922d39af5e95861abf332f196
metaphysis <metaphysis@yeah.net>
2018-09-12 00:37:48 +0800

    2018-09-12

--- UVa Online Judge/volume105/10536 Game of Euler/program.cpp
+++ UVa Online Judge/volume105/10536 Game of Euler/program.cpp
@@ -1,8 +1,8 @@
 // Game of Euler
 // UVa ID: 10536
-// Verdict: 
-// Submission Date: 
-// UVa Run Time: s
+// Verdict: Accepted
+// Submission Date: 2018-09-12
+// UVa Run Time: 0.020s
 //
 // 版权所有（C）2018，邱秋。metaphysis # yeah dot net
 
@@ -10,24 +10,34 @@
 
 using namespace std;
 
-int cache[1 << 16], offset[4][2] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+int cache[1 << 16], offset[4][2] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 
 int dfs(int x)
 {
     if (~cache[x]) return cache[x];
     for (int i = 0; i < 16; i++)
-        for (int j = 0; j < 4; j++)
+    {
+        if (x & (1 << i))
         {
-            int r = i / 4, c = i % 4, bit = 0;
-            for (int k = 0; k <= 2; k++)
+            if (!dfs(x ^ (1 << i))) return cache[x] = 1;
+            int r = i / 4, c = i % 4;
+            int available[4] = {r == 0, c == 0, r == 3, c == 3};
+            for (int j = 0; j < 4; j++)
             {
-                int rr = r + offset[j][0] * k, cc = c + offset[j][1] * k;
-                if (rr < 0 || rr > 3 || cc < 0 || cc > 3) break;
-                if (!(x & (1 << (rr * 4 + cc)))) break;
-                bit |= ((1 << (rr * 4 + cc)));
-                if (!dfs(x ^ bit)) return cache[x] = 1;
+                if (!available[j]) continue;
+                int bit = (1 << i);
+                for (int k = 1; k <= 2; k++)
+                {
+                    int rr = r + offset[j][0] * k, cc = c + offset[j][1] * k;
+                    if (rr < 0 || rr > 3 || cc < 0 || cc > 3) break;
+                    if (!(x & (1 << (rr * 4 + cc)))) break;
+                    bit |= ((1 << (rr * 4 + cc)));
+                    if (!dfs(x ^ bit)) return cache[x] = 1;
+                }
             }
         }
+        
+    }
     return cache[x] = 0;
 }
 
