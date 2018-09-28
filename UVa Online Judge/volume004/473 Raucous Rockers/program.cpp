7de4218e0684362ace4cce8cbb10f60c4eccac3a
metaphysis <metaphysis@yeah.net>
2018-09-28 10:56:12 +0800

    2018-09-28

--- UVa Online Judge/volume004/473 Raucous Rockers/program.cpp
+++ UVa Online Judge/volume004/473 Raucous Rockers/program.cpp
@@ -1,42 +1,60 @@
 // Raucous Rockers
 // UVa ID: 473
-// Verdict: 
-// Submission Date: 
-// UVa Run Time: s
+// Verdict: Accepted
+// Submission Date: 2018-09-28
+// UVa Run Time: 1.160s
 //
-// 版权所有（C）2016，邱秋。metaphysis # yeah dot net
+// 版权所有（C）2018，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
+vector<int> songs;
+int cache[1024][128][128];
+int N, T, M;
+
+int dfs(int n, int t, int m)
+{
+    if (n >= N || m >= M) return 0;
+    if (~cache[n][t][m]) return cache[n][t][m];
+    // Skip the song.
+    int r = max(dfs(n + 1, t, m), dfs(n + 1, 0, m + 1));
+    // Try to record the song on current disk.
+    if (t + songs[n] < T)
+        r = max(r, 1 + dfs(n + 1, t + songs[n], m));
+    else if (t + songs[n] == T)
+        r = max(r, 1 + dfs(n + 1, 0, m + 1));
+    // Try to record the song on next disk.
+    r = max(r, dfs(n, 0, m + 1));
+    return cache[n][t][m] = r;
+}
+
 int main(int argc, char *argv[])
 {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
 
-    int cases = 0;
-    
+    int cases, ti;
+    char comma;
+
     cin >> cases;
-    for (int i = 1; i <= cases; i++)
-    {
-        if (i > 1) cout << '\n';
-        
-        int n, t, m;
-        cin >> n >> t >> m;
-        cin.ignore(1024, '\n');
-        
-        string line;
-        getline(cin, line);
-        
-        istringstream iss(line);
-        vector<int> songs;
-        string minutes;
-        while (getline(iss, minutes, ','))
-            songs.push_back(stoi(minutes));
-            
-        
-        
+    for (int cs = 0; cs < cases; cs++)
+    {  
+        cin >> N >> T >> M;
+        songs.clear();
+        for (int i = 0; i < N; i++)
+        {
+            if (i) cin >> comma;
+            cin >> ti;
+            songs.push_back(ti);
+        }
+        if (cs) cout << '\n';
+        for (int i = 0; i <= N; i++)
+            for (int j = 0; j <= T; j++)
+                for (int k = 0; k <= M; k++)
+                    cache[i][j][k] = -1;
+        cout << dfs(0, 0, 0) << '\n';
     }
-    
+
 	return 0;
 }
