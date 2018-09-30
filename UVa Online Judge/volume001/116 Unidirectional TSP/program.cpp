1f417abb80fca445903dc3be57dc90af4b260fab
metaphysis <metaphysis@yeah.net>
2018-09-30 17:00:28 +0800

    2018-09-30

--- UVa Online Judge/volume001/116 Unidirectional TSP/program.cpp
+++ UVa Online Judge/volume001/116 Unidirectional TSP/program.cpp
@@ -17,71 +17,71 @@ int main(int ac, char *av[])
 {
     cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
 
-	while (cin >> m >> n)
-	{
-		for (int i = 1; i <= m; i++)
-			for (int j = 1; j <= n; j++)
-				cin >> matrix[i][j];
+    while (cin >> m >> n)
+    {
+        for (int i = 1; i <= m; i++)
+            for (int j = 1; j <= n; j++)
+                cin >> matrix[i][j];
 
-		// 从右往左进行动态规划，即列优先处理，从倒数第二列开始，因为需要字典序最小
-	    // 的最小权和路径，如果从左往右处理，可以得到行号最小的权和方案，但是不一定
-	    // 是字典序最小的。
-	    for (int j = n - 1; j >= 1; j--)
-		    for (int i = 1; i <= m; i++)
-		    {
-			    minWeight = INT_MAX;
+        // 从右往左进行动态规划，即列优先处理，从倒数第二列开始，因为需要字典序最小
+        // 的最小权和路径，如果从左往右处理，可以得到行号最小的权和方案，但是不一定
+        // 是字典序最小的。
+        for (int j = n - 1; j >= 1; j--)
+            for (int i = 1; i <= m; i++)
+            {
+                minWeight = INT_MAX;
 
-			    preRow = (i == 1 ? m : i - 1);
-			    if (minWeight > matrix[preRow][j + 1])
-			    {
-				    minWeight = matrix[preRow][j + 1];
-				    minRow = preRow;
-			    }
-			    else if (minWeight == matrix[preRow][j + 1] && minRow > preRow)
-				    minRow = preRow;
+                preRow = (i == 1 ? m : i - 1);
+                if (minWeight > matrix[preRow][j + 1])
+                {
+                    minWeight = matrix[preRow][j + 1];
+                    minRow = preRow;
+                }
+                else if (minWeight == matrix[preRow][j + 1] && minRow > preRow)
+                    minRow = preRow;
 
-			    if (minWeight > matrix[i][j + 1])
-			    {
-				    minWeight = matrix[i][j + 1];
-				    minRow = i;
-			    }
-			    else if (minWeight == matrix[i][j + 1] && minRow > i)
-				    minRow = i;
+                if (minWeight > matrix[i][j + 1])
+                {
+                    minWeight = matrix[i][j + 1];
+                    minRow = i;
+                }
+                else if (minWeight == matrix[i][j + 1] && minRow > i)
+                    minRow = i;
 
-			    preRow = (i == m ? 1 : i + 1);
-			    if (minWeight > matrix[preRow][j + 1])
-			    {
-				    minWeight = matrix[preRow][j + 1];
-				    minRow = preRow;
-			    }
-			    else if (minWeight == matrix[preRow][j + 1] && minRow > preRow)
-				    minRow = preRow;
+                preRow = (i == m ? 1 : i + 1);
+                if (minWeight > matrix[preRow][j + 1])
+                {
+                    minWeight = matrix[preRow][j + 1];
+                    minRow = preRow;
+                }
+                else if (minWeight == matrix[preRow][j + 1] && minRow > preRow)
+                    minRow = preRow;
 
-			    matrix[i][j] += minWeight;
-			    parent[i][j] = minRow;
-		    }
+                matrix[i][j] += minWeight;
+                parent[i][j] = minRow;
+            }
 
-	    // 找第一列权和最小的单元格。
-	    minWeight = INT_MAX;
-	    for (int i = 1; i <= m; i++)
-		    if (matrix[i][1] < minWeight)
-		    {
-			    minWeight = matrix[i][1];
-			    minRow = i;
-		    }
+        // 找第一列权和最小的单元格。
+        minWeight = INT_MAX;
+        for (int i = 1; i <= m; i++)
+            if (matrix[i][1] < minWeight)
+            {
+                minWeight = matrix[i][1];
+                minRow = i;
+            }
 
-	    // 输出路径和最小权和。
-	    cout << minRow;
-	    int next = 1, preRow = minRow;
-	    while (next < n)
-	    {
-		    cout << " " << parent[preRow][next];
-		    preRow = parent[preRow][next];
-		    next++;
-	    }
-	    cout << "\n";
-	    cout << matrix[minRow][1] << "\n";
-	}
+        // 输出路径和最小权和。
+        cout << minRow;
+        int next = 1, preRow = minRow;
+        while (next < n)
+        {
+            cout << " " << parent[preRow][next];
+            preRow = parent[preRow][next];
+            next++;
+        }
+        cout << "\n";
+        cout << matrix[minRow][1] << "\n";
+    }
 
-	return 0;
+    return 0;
 }
