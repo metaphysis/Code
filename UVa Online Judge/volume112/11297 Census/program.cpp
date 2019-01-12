cb92fd571270b5398529dda554e98378588f1271
metaphysis <metaphysis@yeah.net>
2019-01-12 11:11:42 +0800

    2019-01-12

--- UVa Online Judge/volume112/11297 Census/program.cpp
+++ UVa Online Judge/volume112/11297 Census/program.cpp
@@ -1,79 +1,114 @@
 // Census
 // UVa ID: 11297
 // Verdict: Accepted
-// Submission Date: 2017-10-30
-// UVa Run Time: 0.240s
+// Submission Date: 2019-01-12
+// UVa Run Time: 0.400s
 //
-// 版权所有（C）2017，邱秋。metaphysis # yeah dot net
+// 版权所有（C）2019，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-const int MAXV = 250000, INF = 0x3fffffff;
+const int INF = 0x3f3f3f3f;
 
-struct node { int high, low; } qt[4 * MAXV], city[512][512];
-
-void build(int p, int r1, int c1, int r2, int c2)
+struct NODE
 {
-    if (r2 < r1 || c2 < c1) return;
-    if (r1 == r2 && c1 == c2) qt[p].high = city[r1][c1].high, qt[p].low = city[r1][c1].low;
-    else {
-        int mr = (r1 + r2) >> 1, mc = (c1 + c2) >> 1;
+    int high, low;
+    NODE* children[4];
+};
 
-        build((p << 2) + 1, r1, c1, mr, mc);
-        build((p << 2) + 2, r1, mc + 1, mr, c2);
-        build((p << 2) + 3, mr + 1, c1, r2, mc);
-        build((p << 2) + 4, mr + 1, mc + 1, r2, c2);
+struct QUERY
+{
+    int high, low;
+};
 
-        qt[p].high = qt[(p << 2) + 1].high, qt[p].low = qt[(p << 2) + 1].low;
-        for (int i = 2; i <= 4; i++)
-        {
-            qt[p].high = max(qt[p].high, qt[(p << 2) + i].high);
-            qt[p].low = min(qt[p].low, qt[(p << 2) + i].low);
-        }
-    }
+int city[512][512];
+
+NODE* getNode()
+{
+    NODE *nd = new NODE;
+    nd->high = -INF, nd->low = INF;
+    for (int i = 0; i < 4; i++) nd->children[i] = NULL;
+    return nd;
 }
 
-pair<int, int> query(int p, int r1, int c1, int r2, int c2, int qr1, int qc1, int qr2, int qc2)
+NODE* build(int r1, int c1, int r2, int c2)
 {
-    if (r2 < r1 || c2 < c1) return make_pair(-INF, INF);
-    if (r2 < qr1 || c2 < qc1 || r1 > qr2 || c1 > qc2) return make_pair(-INF, INF);
-    if (qr1 <= r1 && r2 <= qr2 && qc1 <= c1 && c2 <= qc2) return make_pair(qt[p].high, qt[p].low);
+    if (r1 > r2 || c1 > c2) return NULL;
+    if (r1 == r2 && c1 == c2)
+    {
+        NODE *nd = getNode();
+        nd->high = nd->low = city[r1][c1];
+        return nd;
+    }
 
     int mr = (r1 + r2) >> 1, mc = (c1 + c2) >> 1;
-    pair<int, int> q1 = query((p << 2) + 1, r1, c1, mr, mc, qr1, qc1, qr2, qc2);
-    pair<int, int> q2 = query((p << 2) + 2, r1, mc + 1, mr, c2, qr1, qc1, qr2, qc2);
-    pair<int, int> q3 = query((p << 2) + 3, mr + 1, c1, r2, mc, qr1, qc1, qr2, qc2);
-    pair<int, int> q4 = query((p << 2) + 4, mr + 1, mc + 1, r2, c2, qr1, qc1, qr2, qc2);
-    
-    int high = max(max(q1.first, q2.first), max(q3.first, q4.first));
-    int low = min(min(q1.second, q2.second), min(q3.second, q4.second));
-    
-    return make_pair(high, low);
+
+    NODE *nd = getNode();
+
+    nd->children[0] = build(r1, c1, mr, mc);
+    nd->children[1] = build(r1, mc + 1, mr, c2);
+    nd->children[2] = build(mr + 1, c1, r2, mc);
+    nd->children[3] = build(mr + 1, mc + 1, r2, c2);
+
+    int high = -INF, low = INF;
+    for (int i = 0; i < 4; i++)
+    {
+        if (nd->children[i] == NULL) continue;
+        high = max(high, nd->children[i]->high);
+        low = min(low, nd->children[i]->low);
+    }
+    nd->high = high, nd->low = low;
+
+    return nd;
 }
 
-void update(int p, int r1, int c1, int r2, int c2, int ur1, int uc1, int v)
+QUERY query(NODE *nd, int r1, int c1, int r2, int c2, int qr1, int qc1, int qr2, int qc2)
 {
-    if (r2 < r1 || c2 < c1) return;
-    if (r1 == r2 && c1 == c2 && r1 == ur1 && c1 == uc1) qt[p].high = qt[p].low = v;
-    else {
-        int mr = (r1 + r2) >> 1, mc = (c1 + c2) >> 1;
-        if (ur1 >= r1 && ur1 <= mr && uc1 >= c1 && uc1 <= mc)
-            update((p << 2) + 1, r1, c1, mr, mc, ur1, uc1, v);
-        else if (ur1 >= r1 && ur1 <= mr && uc1 >= mc + 1 && uc1 <= c2)
-            update((p << 2) + 2, r1, mc + 1, mr, c2, ur1, uc1, v);
-        else if (ur1 >= mr + 1 && ur1 <= r2 && uc1 >= c1 && uc1 <= mc)
-            update((p << 2) + 3, mr + 1, c1, r2, mc, ur1, uc1, v);
-        else if (ur1 >= mr + 1 && ur1 <= r2 && uc1 >= mc + 1 && uc1 <= c2)
-            update((p << 2) + 4, mr + 1, mc + 1, r2, c2, ur1, uc1, v);
+    if (r1 > r2 || c1 > c2) return QUERY{-INF, INF};
+    if (r2 < qr1 || c2 < qc1 || r1 > qr2 || c1 > qc2) return QUERY{-INF, INF};
+    if (qr1 <= r1 && r2 <= qr2 && qc1 <= c1 && c2 <= qc2) return QUERY{nd->high, nd->low};
 
-        qt[p].high = qt[(p << 2) + 1].high, qt[p].low = qt[(p << 2) + 1].low;
-        for (int i = 2; i <= 4; i++)
+    int mr = (r1 + r2) >> 1, mc = (c1 + c2) >> 1;
+    QUERY q1 = query(nd->children[0], r1, c1, mr, mc, qr1, qc1, qr2, qc2);
+    QUERY q2 = query(nd->children[1], r1, mc + 1, mr, c2, qr1, qc1, qr2, qc2);
+    QUERY q3 = query(nd->children[2], mr + 1, c1, r2, mc, qr1, qc1, qr2, qc2);
+    QUERY q4 = query(nd->children[3], mr + 1, mc + 1, r2, c2, qr1, qc1, qr2, qc2);
+    int high = max(max(q1.high, q2.high), max(q3.high, q4.high));
+    int low = min(min(q1.low, q2.low), min(q3.low, q4.low));
+    
+    return QUERY{high, low};
+}
+
+void update(NODE *nd, int r1, int c1, int r2, int c2, int ur1, int uc1, int v)
+{
+    if (r1 > r2 || c1 > c2) return;
+    if (r1 == r2 && c1 == c2 && r1 == ur1 && c1 == uc1) 
+    {
+        nd->high = v, nd->low = v;
+    }
+    else
+    {
+        int mr = (r1 + r2) >> 1, mc = (c1 + c2) >> 1;
+
+        if (ur1 >= r1 && ur1 <= mr && uc1 >= c1 && uc1 <= mc)
+            update(nd->children[0], r1, c1, mr, mc, ur1, uc1, v);
+        else if (ur1 >= r1 && ur1 <= mr && uc1 >= mc + 1 && uc1 <= c2)
+            update(nd->children[1], r1, mc + 1, mr, c2, ur1, uc1, v);
+        else if (ur1 >= mr + 1 && ur1 <= r2 && uc1 >= c1 && uc1 <= mc)
+            update(nd->children[2], mr + 1, c1, r2, mc, ur1, uc1, v);
+        else if (ur1 >= mr + 1 && ur1 <= r2 && uc1 >= mc + 1 && uc1 <= c2)
+            update(nd->children[3], mr + 1, mc + 1, r2, c2, ur1, uc1, v);
+
+        int high = -INF, low = INF;
+        for (int i = 0; i < 4; i++)
         {
-            qt[p].high = max(qt[p].high, qt[(p << 2) + i].high);
-            qt[p].low = min(qt[p].low, qt[(p << 2) + i].low);
+            if (nd->children[i] == NULL) continue;
+            high = max(high, nd->children[i]->high);
+            low = min(low, nd->children[i]->low);
         }
+        nd->high = high, nd->low = low;
     }
 }
 
@@ -81,43 +116,33 @@ int main(int argc, char *argv[])
 {
     cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
 
-    int n, nn;
-    while (cin >> n) {
-        for (int i = 0; i < 512; i++)
-            for (int j = 0; j < 512; j++)
-                city[i][j].high = -INF, city[i][j].low = INF;
-
-        int k;
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-            {
-                cin >> k;
-                city[i][j].high = city[i][j].low = k;
-            }
-
-        n = 512;
+    int n;
+    cin >> n;
         
-        build(0, 0, 0, n - 1, n - 1);
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cin >> city[i][j];
 
-        int q, x1, y1, x2, y2, v;
-        char action;
+    NODE *root = build(0, 0, n - 1, n - 1);
 
-        cin >> q;
-        for (int i = 1; i <= q; i++)
+    int q, x1, y1, x2, y2, v;
+    char action;
+
+    cin >> q;
+    for (int i = 1; i <= q; i++)
+    {
+        cin >> action;
+        if (action == 'c')
         {
-            cin >> action;
-            if (action == 'c') {
-                cin >> x1 >> y1 >> v;
-                update(0, 0, 0, n - 1, n - 1, x1 - 1, y1 - 1, v);
-            }
-            else {
-                //assert(x1 <= nn && y1 <= nn && x2 <= nn && y2 <= nn);
-                //assert(x1 <= nn && y1 <= nn);
-                cin >> x1 >> y1 >> x2 >> y2;
-                x2 = min(x2, nn), y2 = min(y2, nn);
-                pair<int, int> r = query(0, 0, 0, n - 1, n - 1, x1 - 1, y1 - 1, x2 - 1, y2 - 1);
-                cout << r.first << ' ' << r.second << '\n';
-            }
+            cin >> x1 >> y1 >> v;
+            update(root, 0, 0, n - 1, n - 1, x1 - 1, y1 - 1, v);
+        }
+        else
+        {
+            cin >> x1 >> y1 >> x2 >> y2;
+            x2 = min(x2, n), y2 = min(y2, n);
+            QUERY r = query(root, 0, 0, n - 1, n - 1, x1 - 1, y1 - 1, x2 - 1, y2 - 1);
+            cout << r.high << ' ' << r.low << '\n';
         }
     }
 
--- UVa Online Judge/volume119/11992 Fast Matrix Operations/program.cpp
+++ UVa Online Judge/volume119/11992 Fast Matrix Operations/program.cpp
@@ -10,91 +10,109 @@
 
 using namespace std;
 
-const int MAXV = 1000010, INF = 0x3f3f3f3f;
+const int INF = 0x3f3f3f3f;
 
-struct node { int sum, high, low; } qt[4 * MAXV];
-
-void build(int p, int r1, int c1, int r2, int c2)
+struct NODE
 {
-    if (r2 < r1 || c2 < c1) return;
-    if (r1 == r2 && c1 == c2) qt[p].sum = qt[p].high = qt[p].low = 0;
-    else {
-        int mr = (r1 + r2) >> 1, mc = (c1 + c2) >> 1;
+    int sum, high, low;
+    NODE* children[4];
+};
 
-        build((p << 2) + 1, r1, c1, mr, mc);
-        build((p << 2) + 2, r1, mc + 1, mr, c2);
-        build((p << 2) + 3, mr + 1, c1, r2, mc);
-        build((p << 2) + 4, mr + 1, mc + 1, r2, c2);
+struct QUERY
+{
+    int sum, high, low;
+};
 
-        qt[p].sum = qt[(p << 2) + 1].sum;
-        qt[p].high = qt[(p << 2) + 1].high;
-        qt[p].low = qt[(p << 2) + 1].low;
-        for (int i = 2; i <= 4; i++)
-        {
-            qt[p].sum += qt[(p << 2) + i].sum;
-            qt[p].high = max(qt[p].high, qt[(p << 2) + i].high);
-            qt[p].low = min(qt[p].low, qt[(p << 2) + i].low);
-        }
-    }
+NODE* getNode()
+{
+    NODE *nd = new NODE;
+    nd->sum = 0, nd->high = -INF, nd->low = INF;
+    for (int i = 0; i < 4; i++) nd->children[i] = NULL;
+    return nd;
 }
 
-node query(int p, int r1, int c1, int r2, int c2, int qr1, int qc1, int qr2, int qc2)
+NODE* build(int r1, int c1, int r2, int c2)
 {
-    if (r1 > r2 || c1 > c2) return node{0, -INF, INF};
-    if (r2 < qr1 || c2 < qc1 || r1 > qr2 || c1 > qc2) return node{0, -INF, INF};
-    if (qr1 <= r1 && r2 <= qr2 && qc1 <= c1 && c2 <= qc2) return node{qt[p].sum, qt[p].high, qt[p].low};
+    if (r1 > r2 || c1 > c2) return NULL;
+    if (r1 == r2 && c1 == c2)
+    {
+        NODE *nd = getNode();
+        nd->sum = nd->high = nd->low = 0;
+        return nd;
+    }
 
     int mr = (r1 + r2) >> 1, mc = (c1 + c2) >> 1;
-    node q1 = query((p << 2) + 1, r1, c1, mr, mc, qr1, qc1, qr2, qc2);
-    node q2 = query((p << 2) + 2, r1, mc + 1, mr, c2, qr1, qc1, qr2, qc2);
-    node q3 = query((p << 2) + 3, mr + 1, c1, r2, mc, qr1, qc1, qr2, qc2);
-    node q4 = query((p << 2) + 4, mr + 1, mc + 1, r2, c2, qr1, qc1, qr2, qc2);
 
+    NODE *nd = getNode();
+
+    nd->children[0] = build(r1, c1, mr, mc);
+    nd->children[1] = build(r1, mc + 1, mr, c2);
+    nd->children[2] = build(mr + 1, c1, r2, mc);
+    nd->children[3] = build(mr + 1, mc + 1, r2, c2);
+
+    int sum = 0, high = -INF, low = INF;
+    for (int i = 0; i < 4; i++)
+    {
+        if (nd->children[i] == NULL) continue;
+        sum += nd->children[i]->sum;
+        high = max(high, nd->children[i]->high);
+        low = min(low, nd->children[i]->low);
+    }
+    nd->sum = sum, nd->high = high, nd->low = low;
+
+    return nd;
+}
+
+QUERY query(NODE *nd, int r1, int c1, int r2, int c2, int qr1, int qc1, int qr2, int qc2)
+{
+    if (r1 > r2 || c1 > c2) return QUERY{0, -INF, INF};
+    if (r2 < qr1 || c2 < qc1 || r1 > qr2 || c1 > qc2) return QUERY{0, -INF, INF};
+    if (qr1 <= r1 && r2 <= qr2 && qc1 <= c1 && c2 <= qc2) return QUERY{nd->sum, nd->high, nd->low};
+
+    int mr = (r1 + r2) >> 1, mc = (c1 + c2) >> 1;
+    QUERY q1 = query(nd->children[0], r1, c1, mr, mc, qr1, qc1, qr2, qc2);
+    QUERY q2 = query(nd->children[1], r1, mc + 1, mr, c2, qr1, qc1, qr2, qc2);
+    QUERY q3 = query(nd->children[2], mr + 1, c1, r2, mc, qr1, qc1, qr2, qc2);
+    QUERY q4 = query(nd->children[3], mr + 1, mc + 1, r2, c2, qr1, qc1, qr2, qc2);
     int sum = q1.sum + q2.sum + q3.sum + q4.sum;
     int high = max(max(q1.high, q2.high), max(q3.high, q4.high));
     int low = min(min(q1.low, q2.low), min(q3.low, q4.low));
     
-    return node{sum, high, low};
+    return QUERY{sum, high, low};
 }
 
-void update(int p, int r1, int c1, int r2, int c2, int ur1, int uc1, int v, int add)
+void update(NODE *nd, int r1, int c1, int r2, int c2, int ur1, int uc1, int v, int add)
 {
-    if (r2 < r1 || c2 < c1) return;
+    if (r1 > r2 || c1 > c2) return;
     if (r1 == r2 && c1 == c2 && r1 == ur1 && c1 == uc1) 
     {
         if (add)
-        {
-            qt[p].sum += v;
-            qt[p].high += v;
-            qt[p].low += v;
-        }
+            nd->sum += v, nd->high += v, nd->low += v;
         else
-        {
-            qt[p].sum = v;
-            qt[p].high = v;
-            qt[p].low = v;
-        }
+            nd->sum = v, nd->high = v, nd->low = v;
     }
-    else {
+    else
+    {
         int mr = (r1 + r2) >> 1, mc = (c1 + c2) >> 1;
-        if (ur1 >= r1 && ur1 <= mr && uc1 >= c1 && uc1 <= mc)
-            update((p << 2) + 1, r1, c1, mr, mc, ur1, uc1, v, add);
-        else if (ur1 >= r1 && ur1 <= mr && uc1 >= mc + 1 && uc1 <= c2)
-            update((p << 2) + 2, r1, mc + 1, mr, c2, ur1, uc1, v, add);
-        else if (ur1 >= mr + 1 && ur1 <= r2 && uc1 >= c1 && uc1 <= mc)
-            update((p << 2) + 3, mr + 1, c1, r2, mc, ur1, uc1, v, add);
-        else if (ur1 >= mr + 1 && ur1 <= r2 && uc1 >= mc + 1 && uc1 <= c2)
-            update((p << 2) + 4, mr + 1, mc + 1, r2, c2, ur1, uc1, v, add);
 
-        qt[p].sum = qt[(p << 2) + 1].sum;
-        qt[p].high = qt[(p << 2) + 1].high;
-        qt[p].low = qt[(p << 2) + 1].low;
-        for (int i = 2; i <= 4; i++)
+        if (ur1 >= r1 && ur1 <= mr && uc1 >= c1 && uc1 <= mc)
+            update(nd->children[0], r1, c1, mr, mc, ur1, uc1, v, add);
+        else if (ur1 >= r1 && ur1 <= mr && uc1 >= mc + 1 && uc1 <= c2)
+            update(nd->children[1], r1, mc + 1, mr, c2, ur1, uc1, v, add);
+        else if (ur1 >= mr + 1 && ur1 <= r2 && uc1 >= c1 && uc1 <= mc)
+            update(nd->children[2], mr + 1, c1, r2, mc, ur1, uc1, v, add);
+        else if (ur1 >= mr + 1 && ur1 <= r2 && uc1 >= mc + 1 && uc1 <= c2)
+            update(nd->children[3], mr + 1, mc + 1, r2, c2, ur1, uc1, v, add);
+
+        int sum = 0, high = -INF, low = INF;
+        for (int i = 0; i < 4; i++)
         {
-            qt[p].sum += qt[(p << 2) + i].sum;
-            qt[p].high = max(qt[p].high, qt[(p << 2) + i].high);
-            qt[p].low = min(qt[p].low, qt[(p << 2) + i].low);
+            if (nd->children[i] == NULL) continue;
+            sum += nd->children[i]->sum;
+            high = max(high, nd->children[i]->high);
+            low = min(low, nd->children[i]->low);
         }
+        nd->sum = sum, nd->high = high, nd->low = low;
     }
 }
 
@@ -105,8 +123,7 @@ int main(int argc, char *argv[])
     int r, c, m;
     while (cin >> r >> c >> m)
     {
-        r = c = 32;
-        build(0, 0, 0, r - 1, c - 1);
+        NODE *root = build(0, 0, r - 1, c - 1);
         int q, x1, y1, x2, y2, v;
         for (int z = 1; z <= m; z++)
         {
@@ -116,23 +133,22 @@ int main(int argc, char *argv[])
                 cin >> x1 >> y1 >> x2 >> y2 >> v;
                 for (int i = x1; i <= x2; i++)
                     for (int j = y1; j <= y2; j++)
-                        update(0, 0, 0, r - 1, c - 1, i - 1, j - 1, v, 1);
+                        update(root, 0, 0, r - 1, c - 1, i - 1, j - 1, v, 1);
             }
             else if (q == 2)
             {
                 cin >> x1 >> y1 >> x2 >> y2 >> v;
                 for (int i = x1; i <= x2; i++)
                     for (int j = y1; j <= y2; j++)
-                        update(0, 0, 0, r - 1, c - 1, i - 1, j - 1, v, 0);
+                        update(root, 0, 0, r - 1, c - 1, i - 1, j - 1, v, 0);
             }
             else
             {
                 cin >> x1 >> y1 >> x2 >> y2;
-                node nd = query(0, 0, 0, r - 1, c - 1, x1 - 1, y1 - 1, x2 - 1, y2 - 1);
+                QUERY nd = query(root, 0, 0, r - 1, c - 1, x1 - 1, y1 - 1, x2 - 1, y2 - 1);
                 cout << nd.sum << ' ' << nd.low << ' ' << nd.high << '\n';
             }
         }
-        break;
     }
 
     return 0;
