27b907142533d8365a531d43cefb2c48bf53deea
metaphysis <metaphysis@yeah.net>
2019-01-21 22:19:30 +0800

    2019-01-21

---
+++ UVa Online Judge/volume004/475 Wild Thing/program.cpp
@@ -0,0 +1,75 @@
+// Wild Thing
+// UVa ID: 475
+// Verdict: Accepted
+// Submission Date: 2019-01-21
+// UVa Run Time: 0.010s
+//
+// 版权所有（C）2019，邱秋。metaphysis # yeah dot net
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool done;
+string pattern, filename;
+
+void dfs(int pl, int pr, int fl, int fr)
+{
+    if (pl == pr && fl == fr)
+    {
+        done = true;
+        return;
+    }
+    if (pl < pr && pattern[pl] == '*')
+    {
+        int x = 0;
+        for (x = pl + 1; x < pr && pattern[x] != '*'; x++);
+        int L = x - pl - 1;
+        for (int i = fl; i + L <= fr; i++)
+        {
+            if (pattern.substr(pl + 1, L) == filename.substr(i, L))
+                dfs(pl + L + 1, pr, i + L, fr);
+            if (done) return;
+        }
+    }
+    else if (pl < pr && pattern[pl] != '*')
+    {
+        int x = 0;
+        for (x = pl + 1; x < pr && pattern[x] != '*'; x++);
+        int L = x - pl;
+        if (fl + L <= fr && pattern.substr(pl, L) == filename.substr(fl, L))
+            dfs(pl + L, pr, fl + L, fr);
+        if (done) return;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
+
+    int cases = 0;
+    while (getline(cin, pattern))
+    {
+        string backup = pattern;
+        for (int i = pattern.size() - 1; i > 0; i--)
+            if (pattern[i] == '*' && pattern[i - 1] == '*')
+                pattern.erase(pattern.begin() + i);
+
+        vector<string> matched;
+        while (getline(cin, filename), filename.length() > 0)
+        {
+            done = false;
+            if (pattern == "*") done = true;
+            if (!done) dfs(0, pattern.size(), 0, filename.size());
+            if (done) matched.push_back(filename);
+        }
+        if (matched.size() > 0)
+        {
+            if (cases++) cout << '\n';
+            cout << "MATCHES FOR THE PATTERN: " << backup << '\n';
+            for (auto m : matched) cout << m << '\n';
+        }
+    }
+
+    return 0;
+}
--- UVa Online Judge/volume106/10679 I Love String/program1.cpp
+++ UVa Online Judge/volume106/10679 I Love String/program1.cpp
@@ -12,44 +12,45 @@ using namespace std;
 
 const int MAXN = 1024;
 
-int pi[MAXN] = {};
-
-void getNext(string &p)
+bool kmp1(string &s, string &p)
 {
-    int n = p.length();
-    pi[0] = pi[1] = 0;
-    for (int i = 1, j = 0; i < n; i++) {
-        while (j && p[j] != p[i]) j = pi[j];
-        if (p[j] == p[i]) j++;
-        pi[i + 1] = j;
+    static int fail[MAXN] = {};
+
+    fail[0] = fail[1] = 0;
+    int i = 1, j = 0;
+    while (i < p.length()) {
+        if (j == 0 || p[i] == p[j]) {
+            i++, j++;
+            if (p[i] != p[j]) fail[i] = j;
+            else fail[i] = fail[j];
+        } else j = fail[j];
     }
-    //int i = 1, j = 0, n = p.length();
-    //while (i < n) {
-    //    if (j == 0 || p[i] == p[j]) {
-    //        i++, j++;
-    //        if (p[i] != p[j]) pi[i] = j;
-    //        else pi[i] = pi[j];
-    //   }
-    //    else j = pi[j];
-    //}
+
+    i = 0, j = 1;
+    while (i < s.length() && j < p.length()) {
+        if (j == 0 || s[i] == p[j - 1]) i++, j++;
+        else j = fail[j];
+    }
+    return j >= p.length();
 }
 
-bool kmp(string &s, string &p)
+bool kmp2(string &s, string &p)
 {
-    int m = s.length(), n = p.length();
-    getNext(p);
-    for (int i = 0, j = 0; i < m; i++) {
-        while (j && p[j] != s[i]) j = pi[j];
+    static int fail[MAXN] = {};
+
+    fail[0] = fail[1] = 0;
+    for (int i = 1, j = 0; i < p.length(); i++) {
+        while (j && p[j] != p[i]) j = fail[j];
+        if (p[j] == p[i]) j++;
+        fail[i + 1] = j;
+    }
+
+    for (int i = 0, j = 0; i < s.length(); i++) {
+        while (j && p[j] != s[i]) j = fail[j];
         if (p[j] == s[i]) j++;
-        if (j == n) return true;
+        if (j == p.length()) return true;
     }
     return false;
-    //int i = 0, j = 1;
-    //while (i < m && j < n) {
-    //    if (j == 0 || s[i] == p[j]) i++, j++;
-    //    else j = pi[j];
-    //}
-    //return j >= n;
 }
 
 int main(int argc, char *argv[])
@@ -65,7 +66,7 @@ int main(int argc, char *argv[])
         for (int i = 0; i < q; i++)
         {
             cin >> T;
-            if (S.length() >= T.length()) cout << (kmp(S, T) ? 'y' : 'n');
+            if (S.length() >= T.length()) cout << (kmp1(S, T) ? 'y' : 'n');
             cout << '\n';
         }
     }
