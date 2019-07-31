4c45e2589187d316d8b0ec0c26f1cd058dc8bfea
metaphysis <metaphysis@yeah.net>
2019-07-31 13:45:17 +0800

    2019-07-31

--- Books/PCC1/11/11.5.5.cpp
+++ Books/PCC1/11/11.5.5.cpp
@@ -1,8 +1,8 @@
-// Best Cow Fences
-// ID: USACO 2003 March Green
-// Verdict: 
-// Submission Date: 
-// UVa Run Time: s
+// USACO 2003 March Green - Best Cow Fences
+// POJ ID: 2018
+// Verdict: Accepted
+// Submission Date: 2019-07-31
+// UVa Run Time: 0.688s
 //
 // 版权所有（C）2019，邱秋。metaphysis # yeah dot net
 
@@ -23,54 +23,47 @@ long long cp(point &a, point &b, point &c)
 	return (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
 }
 
-// 从点a向点b望去，点c位于线段ab的顺时针方向。
+// 如果叉积小于0，则从点a向点b望去，点c位于线段ab的右侧，即两条连续线段ab和bc构成右转。
 bool cw(point &a, point &b, point &c)
 {
 	return cp(a, b, c) < 0;
-}
-
-// 从点a向点b望去，点c位于线段ab的逆时针方向。
-bool ccw(point &a, point &b, point &c)
-{
-	return cp(a, b, c) >= 0;
 }
 
 int main(int argc, char *argv[])
 {
     cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
 
-    int N, F, si, head, rear, best, Q[MAXN];
+    int N, F, ai, si, head, rear, best, Q[MAXN];
     while (cin >> N >> F)
     {
         point maxK(1, 0);
         // 初始化单调队列，将参考点放入队列。
         si = head = rear = best = 0;
         P[0] = point(0, 0), Q[rear++] = 0;
-        for (int i = 1, ai; i <= N; i++)
+        for (int i = 1; i <= N; i++)
         {
             cin >> ai;
-            P[i] = point(i, si + ai);
+            si += ai;
+            P[i] = point(i, si);
             // 选择单调队列中的某个点，该点与P[i]构成的直线具有最大的斜率。
-            while (best + 2 <= rear && Q[best] <= i - F && ccw(P[Q[best]], P[Q[best + 1]], P[i])) best++;
+            if (best >= rear) best = rear - 1;
+            while (best && i - Q[best] < F) best--;
+            while (best && i - Q[best] >= F && cw(P[Q[best - 1]], P[Q[best]], P[i])) best--;
+            while (best + 2 <= rear && i - Q[best + 1] >= F && !cw(P[Q[best]], P[Q[best + 1]], P[i])) best++;
             // 最优决策点需要满足长度的约束。
-            if (Q[best] <= i - F)
+            if (i - Q[best] >= F)
             {
                 point k = point(i - Q[best], P[i].y - P[Q[best]].y);
                 if (maxK.y * k.x < maxK.x * k.y) maxK = k;
             }
-            // 将P[i]放入队列，保持相邻决策点连线斜率单调不降。
-            // 既往的最佳决策点可能已经被删除，需要予以调整。由下凸包折线斜率的单调
-            // 不降可知队列末端的决策点一定比位于左侧的决策点更优，因此直接将最优决
-            // 策点的指针指向队列最末尾一个元素即可。
-            while ((rear - head) >= max(F, 2) && cw(P[Q[rear - 2]], P[Q[rear - 1]], P[i]))
+            // 将符合长度约束的决策点送入队列并求下凸包。
+            while ((i + 1) - Q[rear - 1] > F)
             {
-                rear--;
-                if (best >= rear) best = rear - 1;
+                int j = Q[rear - 1] + 1;
+                while (rear - head >= 2 && cw(P[Q[rear - 2]], P[Q[rear - 1]], P[j])) rear--;
+                Q[rear++] = j;
             }
-            Q[rear++] = i;
-            si += ai;
         }
-        cout << maxK.y << ' ' << maxK.x << '\n';
         cout << 1000 * maxK.y / maxK.x << '\n';
     }
     return 0;
