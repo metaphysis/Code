ab1ff06b2695927cff0597f026a596b69c3a1208
metaphysis <metaphysis@yeah.net>
2019-08-07 18:50:16 +0800

    2019-08-07

--- UVa Online Judge/volume120/12002 Happy Birthday/program.cpp
+++ UVa Online Judge/volume120/12002 Happy Birthday/program.cpp
@@ -1,8 +1,8 @@
 // Happy Birthday
 // UVa ID: 12002
-// Verdict: 
-// Submission Date: 
-// UVa Run Time: s
+// Verdict: Accepted
+// Submission Date: 2019-08-07
+// UVa Run Time: 0.150s
 //
 // 版权所有（C）2018，邱秋。metaphysis # yeah dot net
 
@@ -31,15 +31,23 @@ int main(int argc, char *argv[])
         for (int i = n - 1; i >= 0; i--)
             for (int j = n - 1; j > i; j--)
             {
-                if (weight[i] >= weight[j])
-                    dp1[i] = max(dp1[i], dp1[j] + 1);
                 if (weight[i] <= weight[j])
+                    dp1[i] = max(dp1[i], dp1[j] + 1);
+                if (weight[i] >= weight[j])
                     dp2[i] = max(dp2[i], dp2[j] + 1);
             }
             
         int longest = 0;
         for (int i = 0; i < n; i++)
-            longest = max(longest, dp1[i] + dp2[i] - 1);
+        {
+            longest = max(longest, dp1[i]);
+            longest = max(longest, dp2[i]);
+            for (int j = i + 1; j < n; j++)
+            {
+                if (weight[i] > weight[j]) longest = max(longest, dp1[i] + dp2[j]);
+                if (weight[i] < weight[j]) longest = max(longest, dp2[i] + dp1[j]);
+            }
+        }
         cout << longest << '\n';
     }
 
