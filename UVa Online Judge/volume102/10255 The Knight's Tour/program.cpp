161d7259283ef3fc27264181b88b0ac8ccad7567
metaphysis <metaphysis@yeah.net>
2019-08-19 08:42:09 +0800

    2019-08-19

--- UVa Online Judge/volume102/10255 The Knight's Tour/program.cpp
+++ UVa Online Judge/volume102/10255 The Knight's Tour/program.cpp
@@ -1,7 +1,7 @@
 // The Knight's Tour
 // UVa ID: 10255
 // Verdict: TLE
-// Submission Date: 2019-08-16
+// Submission Date: 2019-08-19
 // UVa Run Time: 3.000s
 //
 // 版权所有（C）2019，邱秋。metaphysis # yeah dot net
@@ -10,7 +10,7 @@
 
 using namespace std;
 
-const int MAXN = 64;
+const int MAXN = 64, INF = 0x3f3f3f3f;
 
 struct point {
     int x, y;
@@ -28,6 +28,7 @@ vector<point> path;
 
 bool cmp(point &a, point &b) { return cnt[a.x][b.y] < cnt[b.x][b.y]; }
 
+// 使用“过时”的“后继方格”数量，效率较低，会超时。
 bool dfs(int r, int c, int depth)
 {
     if (depth == NT) {
@@ -56,6 +57,46 @@ bool dfs(int r, int c, int depth)
     return false;
 }
 
+// 实时更新“后继方格”数量。
+bool dfs1(int r, int c, int depth)
+{
+    if (depth == NT) {
+        path.push_back(point(r, c));
+        return true;
+    }
+
+    srand(time(NULL));
+    int minIdx = -1, degree = INF;
+    for (int i = 0, j = rand() % 8, k; i < 8; i++) {
+        k = (i + j) % 8;
+        int nr = r + offset[k][0], nc = c + offset[k][1];
+        if (nr < 0 || nr >= NR || nc < 0 || nc >= NC) continue;
+        if (visited[nr][nc]) continue;
+        cnt[nr][nc]--;
+        if (degree > cnt[nr][nc])
+        {
+            degree = cnt[nr][nc];
+            minIdx = k;
+        }
+    }
+    if (minIdx == -1) return false;
+
+    visited[r + offset[minIdx][0]][c + offset[minIdx][1]] = 1;
+    if (dfs1(r + offset[minIdx][0], c + offset[minIdx][1], depth + 1)) {
+        path.push_back(point(r, c));
+        return true;
+    }
+    visited[r + offset[minIdx][0]][c + offset[minIdx][1]] = 0;
+    for (int i = 0; i < 8; i++) {
+        int nr = r + offset[i][0], nc = c + offset[i][1];
+        if (nr < 0 || nr >= NR || nc < 0 || nc >= NC) continue;
+        if (visited[nr][nc]) continue;
+        cnt[nr][nc]++;
+    }
+
+    return false;
+}
+
 bool knightTour(int W, int H, int SR, int SC)
 {
     NC = W, NR = H, NT = W * H;
@@ -73,7 +114,7 @@ bool knightTour(int W, int H, int SR, int SC)
     path.clear();
     memset(visited, 0, sizeof(visited));
     visited[SR - 1][SC - 1] = 1;
-    bool r = dfs(SR - 1, SC - 1, 1);
+    bool r = dfs1(SR - 1, SC - 1, 1);
     if (path.size()) reverse(path.begin(), path.end());
     return r;
 }
