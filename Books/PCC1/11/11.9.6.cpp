acc861fda2981509a35841047f7583997092a771
metaphysis <metaphysis@yeah.net>
2020-04-03 20:01:34 +0800

    2020-04-03

--- Books/PCC1/11/11.9.6.cpp
+++ Books/PCC1/11/11.9.6.cpp
@@ -1,32 +1,20 @@
-// Optimal Binary Search Tree
-// UVa ID: 10304
-// Verdict: Accepted
-// Submission Date: 2018-09-21
-// UVa Run Time: 0.070s
-//
-// 版权所有（C）2018，邱秋。metaphysis # yeah dot net
-
 #include <bits/stdc++.h>
 
 using namespace std;
 
-const int INF = 0x7f7f7f7f;
+const int MAXN = 310, INF = 0x7f7f7f7f;
 
-int n, e[256], dp[256][256], sum[256], K[256][256];
+int dp[MAXN][MAXN], K[MAXN][MAXN], n, a[MAXN], sum[MAXN] = {0};
 
 int main(int argc, char *argv[])
 {
     cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
 
-    while (cin >> n)
-    {
-        sum[0] = 0;
-        for (int i = 1; i <= n; i++)
-        {
-            cin >> e[i];
-            sum[i] = e[i] + sum[i - 1];
+    while (cin >> n) {
+        for (int i = 1; i <= n; i++) {
+            cin >> a[i];
+            sum[i] = sum[i - 1] + a[i];
         }
-
         memset(dp, 0, sizeof(dp));
         for (int i = 1; i <= n; i++) K[i][i] = i;
         for (int L = 2; L <= n; L++)
@@ -35,7 +23,7 @@ int main(int argc, char *argv[])
                 dp[i][j] = INF;
                 for (int k = K[i][j - 1]; k <= K[i + 1][j]; k++)
                 {
-                    int next = dp[i][k - 1] + dp[k + 1][j] + sum[j] - sum[i - 1] - e[k];
+                    int next = dp[i][k] + dp[k + 1][j] + sum[j] - sum[i - 1];
                     if (next < dp[i][j])
                     {
                         dp[i][j] = next;
