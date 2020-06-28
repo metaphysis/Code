3eee99642f3bafad0fa7ba2cd0a0549f7a6b9c8c
metaphysis <metaphysis@yeah.net>
2020-06-28 16:01:05 +0800

    12311 All-Pair Farthest Points

--- UVa Online Judge/volume012/1254 Top 10/program2.cpp
+++ UVa Online Judge/volume012/1254 Top 10/program2.cpp
@@ -14,137 +14,168 @@
 using namespace std;
 string word[20000];
 vector<int> Top10[100000];
-struct Node {
+
+struct Node
+{
     Node *next[26], *fail;
     vector<int> label;
     int matched;
-    Node() {
+    Node()
+    {
         fail = NULL;
         matched = -1;
         label.clear();
         memset(next, 0, sizeof(next));
     }
 };
-void insertTrie(const char str[], Node *root, int label) {
+
+void insertTrie(const char str[], Node * root, int label)
+{
     static Node *p;
     static int i, idx;
     p = root;
-    for(i = 0; str[i]; i++) {
-        idx = str[i]-'a';
-        if(p->next[idx] == NULL) {
+    for (i = 0; str[i]; i++)
+    {
+        idx = str[i] - 'a';
+        if (p->next[idx] == NULL)
+        {
             p->next[idx] = new Node();
         }
         p = p->next[idx];
     }
     p->label.push_back(label);
 }
-void freeTrie(Node *root) {
+
+void freeTrie(Node * root)
+{
     queue<Node*> Q;
     Node *nd;
-    while(!Q.empty()) {
+    while (!Q.empty())
+    {
         nd = Q.front(), Q.pop();
-        for(int i = 0; i < 26; i++)
-            if(nd->next[i] != NULL)
+        for (int i = 0; i < 26; i++)
+            if (nd->next[i] != NULL)
                 Q.push(nd->next[i]);
         nd->label.clear();
         delete nd;
     }
 }
-void buildACautomation(Node *root) {
+
+void buildACautomation(Node * root)
+{
     queue<Node*> Q;
     Node *nd, *p;
     root->fail = NULL;
     Q.push(root);
-    while(!Q.empty()) {
+    while (!Q.empty())
+    {
         nd = Q.front(), Q.pop();
-        for(int i = 0; i < 26; i++) {
-            if(nd->next[i] == NULL)
+        for (int i = 0; i < 26; i++)
+        {
+            if (nd->next[i] == NULL)
                 continue;
             Q.push(nd->next[i]);
             p = nd->fail;
-            while(p != NULL && p->next[i] == NULL)
+            while (p != NULL && p->next[i] == NULL)
                 p = p->fail;
-            if(p == NULL)
+            if (p == NULL)
                 nd->next[i]->fail = root;
             else
                 nd->next[i]->fail = p->next[i];
         }
     }
 }
-void quertACautomaiton(const char* str, Node *root, int number) {
+
+void queryACautomaiton(const char *str, Node * root, int number)
+{
     static Node *p, *q;
     static int i, idx;
     p = root;
-    for(i = 0; str[i]; i++) {
-        idx = str[i]-'a';
-        while(p != root && p->next[idx] == NULL)
+    for (i = 0; str[i]; i++)
+    {
+        idx = str[i] - 'a';
+        while (p != root && p->next[idx] == NULL)
             p = p->fail;
         p = p->next[idx];
         p = (p == NULL) ? root : p;
         q = p;
-        while(q != root && q->matched != number) {
+        while (q != root && q->matched != number)
+        {
             int bflag = 0;
-            for(vector<int>::iterator it = q->label.begin();
-                it != q->label.end(); it++) {
-                if(Top10[*it].size() == 10) {
+            for (vector < int >::iterator it = q->label.begin(); it != q->label.end(); it++)
+            {
+                if (Top10[*it].size() == 10)
+                {
                     bflag = 1;
                     break;
                 }
                 Top10[*it].push_back(number);
             }
-            if(bflag)   break;
+            if (bflag)
+                break;
             q->matched = number;
             q = q->fail;
         }
     }
 }
-void sortTrie(Node *root, Node *qroot) {
+
+void sortTrie(Node * root, Node * qroot)
+{
     // 1.shorter length 2. lexicographically 3. label small
     Node *tn;
     queue<Node*> Q;
     Q.push(root);
-    while(!Q.empty()) {
+    while (!Q.empty())
+    {
         tn = Q.front(), Q.pop();
-        for(int i = 0; i < 26; i++) {
-            if(tn->next[i] == NULL)
+        for (int i = 0; i < 26; i++)
+        {
+            if (tn->next[i] == NULL)
                 continue;
             Q.push(tn->next[i]);
         }
-        for(vector<int>::iterator it = tn->label.begin();
-            it != tn->label.end(); it++) {
-            quertACautomaiton(word[*it].c_str(), qroot, *it);
+        for (vector < int >::iterator it = tn->label.begin(); it != tn->label.end(); it++)
+        {
+            queryACautomaiton(word[*it].c_str(), qroot, *it);
             //cout << word[*it] << endl;
         }
     }
 }
-int main() {
+
+int main()
+{
     int n, m;
     int i, j;
     char s[100005];
-    while(scanf("%d", &n) == 1) {
-        while(getchar() != '\n');
+    while (scanf("%d", &n) == 1)
+    {
+        while (getchar() != '\n');
         Node *root = new Node();
-        for(i = 0; i < n; i++) {
+        for (i = 0; i < n; i++)
+        {
             gets(s);
             word[i] = s;
             insertTrie(s, root, i);
         }
         scanf("%d", &m);
-        while(getchar() != '\n');
+        while (getchar() != '\n');
         Node *qroot = new Node();
-        for(i = 0; i < m; i++) {
+        for (i = 0; i < m; i++)
+        {
             gets(s);
             insertTrie(s, qroot, i);
         }
         buildACautomation(qroot);
         sortTrie(root, qroot);
-        for(i = 0; i < m; i++) {
-            if(Top10[i].size() == 0)
+        for (i = 0; i < m; i++)
+        {
+            if (Top10[i].size() == 0)
                 puts("-1");
-            else {
-                printf("%d", Top10[i][0]+1);
-                for(j = 1; j < Top10[i].size(); j++)
-                    printf(" %d", Top10[i][j]+1);
+            else
+            {
+                printf("%d", Top10[i][0] + 1);
+                for (j = 1; j < Top10[i].size(); j++)
+                    printf(" %d", Top10[i][j] + 1);
                 puts("");
             }
             Top10[i].clear();
--- UVa Online Judge/volume123/12311 All-Pair Farthest Points/program.cpp
+++ UVa Online Judge/volume123/12311 All-Pair Farthest Points/program.cpp
@@ -10,63 +10,40 @@
 
 using namespace std;
 
-struct point {
-    double x, y;
-    point (double x = 0, double y = 0): x(x), y(y) {}
-    point operator+(point p) { return point(x + p.x, y + p.y); };
-    point operator-(point p) { return point(x - p.x, y - p.y); };
-    point operator*(double k) { return point(x * k, y * k); };
-    point operator/(double k) { return point(x / k, y / k); };
-    double distTo(point p) { return pow(x - p.x, 2) + pow(y - p.y, 2); }
-};
-
-typedef vector<point> polygon;
-
-double cross(point a, point b)
+int n;
+double px[100000], py[100000];
+    
+double f(int i, int j)
 {
-    return a.x * b.y - a.y * b.x;
-}
-
-double cp(point a, point b, point c)
-{
-    return cross(b - a, c - a);
-}
-
-void rotatingCalipers(polygon pg)
-{
-    pg.push_back(pg.front());
-    for (int i = 0, j = 1, n = pg.size() - 1; i < n; i++)
-    {
-        while (cross(pg[i + 1] - pg[i], pg[j + 1] - pg[j]) > 0)
-            j = (j + 1) % n;
-        double dist1 = pg[i].distTo(pg[j - 1]);
-        double dist2 = pg[i].distTo(pg[j]);
-        double dist3 = pg[i].distTo(pg[j + 1]);
-        int r1 = j + 1, r2 = j + 2;
-        cout << (j - 1) << '=' << dist1 << ' ' << j << '=' << dist2 << ' ' << (j + 1) << '=' << dist3 << '\n';
-        if (r1 >= n) r1 = 1;
-        if (r2 >= n) r2 = 1;
-        if (dist2 > dist3) cout << r1 << '\n';
-        else if (dist3 > dist2) cout << r2 << '\n';
-        else cout << min(r1, r2) << '\n';
-    }
+    return pow(px[i] - px[j], 2) + pow(py[i] - py[j], 2);
 }
 
 int main(int argc, char *argv[])
 {
     cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
 
-    int n;
     while (cin >> n, n > 0)
     {
-        polygon pg;
-        double x, y;
         for (int i = 0; i < n; i++)
         {
-            cin >> x >> y;
-            pg.push_back(point(x, y));            
+            cin >> px[i] >> py[i];
+            px[n + i] = px[i], py[n + i] = py[i];          
         }
-        rotatingCalipers(pg);
+        for (int i = 0; i < n; i++)
+        {
+            // ternary search.
+            int left = i + 1, right = i + n - 1, r;
+            while (left <= right)
+            {
+                int leftThird = left + (right - left) / 3;
+                int rightThird = right - (right - left) / 3;
+                if (f(i, leftThird) >= f(i, rightThird)) r = leftThird, right = rightThird - 1;
+                else r = rightThird, left = leftThird + 1;
+            }
+            r %= n;
+            if (r && f(i, r - 1) >= f(i, r)) r -= 1;
+            cout << (r + 1) << '\n';
+        }
     }
 
     return 0;
