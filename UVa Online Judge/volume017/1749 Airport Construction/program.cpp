a34f93641a50b697514c7bdb782f544d3011c575
metaphysis <metaphysis@yeah.net>
2022-11-20 22:44:33 +0800

    1748 Airport Construction

--- UVa Online Judge/volume017/1749 Airport Construction/program.cpp
+++ UVa Online Judge/volume017/1749 Airport Construction/program.cpp
@@ -27,11 +27,6 @@ struct point
 	    return y < p.y;
 	}
 	
-	bool operator==(const point &p) const
-	{
-	    return fabs(x - p.x) <= EPSILON && fabs(y - p.y) <= EPSILON;
-	}
-	
 	double distTo(const point &p)
 	{
 	    return sqrt(pow(x - p.x, 2) + pow(y - p.y, 2));
@@ -51,12 +46,11 @@ double cp(point a, point b, point c)
 	return cross(b - a, c - a);
 }
 
-bool pointInBox(point a, point b, point p, bool includeEndPoint)
+bool pointInBox(point a, point b, point p)
 {
     double minx = min(a.x, b.x), maxx = max(a.x, b.x);
     double miny = min(a.y, b.y), maxy = max(a.y, b.y);
-    if (includeEndPoint) return p.x >= minx && p.x <= maxx && p.y >= miny && p.y <= maxy;
-    else return p.x > minx && p.x < maxx && p.y > miny && p.y < maxy;
+    return p.x >= minx && p.x <= maxx && p.y >= miny && p.y <= maxy;
 }
 
 struct line
@@ -85,47 +79,21 @@ point getIntersection(line p, line q)
 
 const int OUT = 0, ON = 1, IN = 2;
 
-int isPointInPolygon(point p, polygon &pg)
+int isPointInPolygon(point p)
 {
     bool in = false;
     for (int i = 0; i < pg.size(); i++)
     {
         point a = pg[i] - p, b = pg[(i + 1) % pg.size()] - p;
-        if (fabs(cross(a, b)) < EPSILON && dot(a, b) < EPSILON) return ON;
+        if (abs(cross(a, b)) < EPSILON && dot(a, b) < EPSILON) return ON;
         if (a.y > b.y) swap(a, b);
         if (a.y < EPSILON && EPSILON < b.y && cross(a, b) > EPSILON) in = !in;
     }
     return in ? IN : OUT;
 }
 
-bool isInPolygon(int i, int j)
-{
-    if (j == i + 1) return true;
-    polygon pg1;
-    for (int k1 = 0, k2 = 1; k1 < n; k1++, k2++)
-    {
-        if (k2 >= n) k2 %= n;
-        double cp1 = cp(pg[i], pg[j], pg[k1]);
-        double cp2 = cp(pg[i], pg[j], pg[k2]);
-        if (cp1 * cp2 < -EPSILON)
-        {
-            point p1 = getIntersection(getLine(pg[i], pg[j]), getLine(pg[k1], pg[k2]));
-            if (pointInBox(pg[i], pg[j], p1, false))
-                return false;
-        }
-        if (fabs(cp1) < EPSILON && pointInBox(pg[i], pg[j], pg[k1], true)) pg1.push_back(pg[k1]);
-        if (fabs(cp2) < EPSILON && pointInBox(pg[i], pg[j], pg[k2], true)) pg1.push_back(pg[k2]);
-    }
-    sort(pg1.begin(), pg1.end());
-    for (int k = 0; k < pg1.size() - 1; k++)
-        if (isPointInPolygon((pg1[k] + pg1[k + 1]) / 2.0, pg) == OUT)
-            return false;
-    return true;
-}
-
 double getDist(int i, int j)
 {
-    if (pg[i].x > pg[j].x) swap(i, j);
     polygon pg1;
     for (int k1 = 0, k2 = 1; k1 < n; k1++, k2++)
     {
@@ -135,37 +103,25 @@ double getDist(int i, int j)
         if (cp1 * cp2 < -EPSILON)
         {
             point p1 = getIntersection(getLine(pg[i], pg[j]), getLine(pg[k1], pg[k2]));
-            if (pointInBox(pg[i], pg[j], p1, true))
-                pg1.push_back(p1);
+            if (pointInBox(pg[i], pg[j], p1)) pg1.push_back(p1);
         }
-        if (fabs(cp1) < EPSILON && pointInBox(pg[i], pg[j], pg[k1], true)) pg1.push_back(pg[k1]);
-        if (fabs(cp2) < EPSILON && pointInBox(pg[i], pg[j], pg[k2], true)) pg1.push_back(pg[k2]);
+        if (fabs(cp1) < EPSILON) pg1.push_back(pg[k1]);
     }
     sort(pg1.begin(), pg1.end());
-    int s = -1, e = -1;
-    for (int k = 0; k < pg1.size(); k++)
-        if (pg[i] == pg1[k])
-        {
-            s = k;
-            break;
-        }
-    for (int k = 0; k < pg1.size(); k++)
-        if (pg[j] == pg1[k])
-        {
-            e = k;
-            break;
-        }
-    while (true)
+    
+    double d = 0, cd = 0;
+    for (int i = 1; i < pg1.size(); i++)
     {
-        if (!s || isPointInPolygon((pg1[s] + pg1[s - 1]) / 2, pg) == OUT) break;
-        s--;
+        point m = (pg1[i] + pg[i - 1]) / 2.0;
+        if (isPointInPolygon(m) != OUT) cd += pg1[i].distTo(pg1[i - 1]);
+        else
+        {
+            d = max(d, cd);
+            cd = 0;
+        }
     }
-    while (true)
-    {
-        if (e + 1 == pg1.size() || isPointInPolygon((pg1[e] + pg1[e + 1]) / 2, pg) == OUT) break;
-        e++;
-    }
-    return pg1[s].distTo(pg1[e]);
+
+    return d;
 }
 
 int main(int argc, char *argv[])
@@ -183,11 +139,14 @@ int main(int argc, char *argv[])
         }
         double longest = 0.0;
         for (int i = 0; i < n; i++)
-            for (int j = i + 1; j < n; j++)
+        {
+            longest = max(longest, pg[i].distTo(pg[(i + 1) % n]));
+            for (int j = i + 2; j < n; j++)
             {
                 if (fabs(pg[i].distTo(pg[j]) - longest) < EPSILON) continue;
-                if (isInPolygon(i, j)) longest = max(longest, getDist(i, j));
+                longest = max(longest, getDist(i, j));
             }
+        }
         cout << fixed << setprecision(9) << longest << '\n';
     }
     
