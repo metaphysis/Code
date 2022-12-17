f66066e44d3b6b19b571058c3440bba631692d48
metaphysis <metaphysis@yeah.net>
2022-12-17 13:00:00 +0800

    11890 Calculus Simplified

--- UVa Online Judge/volume118/11890 Calculus Simplified/program.cpp
+++ UVa Online Judge/volume118/11890 Calculus Simplified/program.cpp
@@ -1,8 +1,8 @@
 // Calculus Simplified
 // UVa ID: 11890
-// Verdict: 
-// Submission Date: 
-// UVa Run Time: s
+// Verdict: Accepted
+// Submission Date: 2022-11-23
+// UVa Run Time: 0.190s
 //
 // 版权所有（C）2022，邱秋。metaphysis # yeah dot net
 
@@ -10,9 +10,36 @@
 
 using namespace std;
 
+string E;
+int T, N, a[100010], symbols[100010], cnt = 0;
 int main(int argc, char *argv[])
 {
     cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
-
-    return 0;
+    cin >> T;
+	while (T--) {
+		cin >> E >> N;
+		for (int i = 0; i < N; i++)
+			cin >> a[i];
+		sort(a, a + N);
+		int lp = 0, rp = N - 1;
+		int sign = 1, answer = 0;
+		for (int i = 0; i < E.length(); i++) {
+			if (E[i] == '(') {
+				symbols[cnt] = i ? (E[i - 1] == '-' ? -1 : 1) : 1;
+				sign *= symbols[cnt++];
+			} else if (E[i] == ')') {
+				sign /= symbols[--cnt];
+			} else if (E[i] == 'x') {
+				int plus = sign;
+				if (i && (E[i - 1] == '+' || E[i - 1] == '-'))
+					plus *= (E[i - 1] == '+' ? 1 : -1);
+				if (plus == 1)
+					answer += a[rp--];
+				else
+					answer -= a[lp++];
+			}
+		}
+		cout << answer << '\n';
+	}
+	return 0;
 }
