59f83e58bc38a7af0c51f64d3ab6c03f58c5eeb5
metaphysis <metaphysis@yeah.net>
2023-06-10 11:11:15 +0800

    1577 Low Power

--- UVa Online Judge/volume015/1577 Low Power/program.cpp
+++ UVa Online Judge/volume015/1577 Low Power/program.cpp
@@ -1,8 +1,8 @@
 // Low Power
 // UVa ID: 1577
-// Verdict: 
-// Submission Date: 
-// UVa Run Time: s
+// Verdict: Accepted
+// Submission Date: 2023-06-10
+// UVa Run Time: 0.000s
 //
 // 版权所有（C）2023，邱秋。metaphysis # yeah dot net
 
@@ -17,15 +17,16 @@ int main(int argc, char *argv[]) {
         int t = 2 * n * k;
         for (int i = 0; i < t; i++) cin >> b[i];
         sort(b, b + t);
-        int low = 0, high = b[t - 1] - b[0], r, middle;
+        int low = b[1] - b[0], high = b[t - 1] - b[0], r, middle;
         while (low <= high) {
             middle = (low + high) >> 1;
-            int cnt = 0, idx = 0;
+            int cnt = 0, idx = 0, used = 0;
             while (idx < t - 1) {
-                if (b[idx + 1] <= b[idx] + middle) idx += 2, cnt++;
-                else idx++;
+                if (b[idx + 1] <= b[idx] + middle) idx += 2, cnt++, used += 2;
+                else idx++, used++;
+                if (used > 2 * cnt * k || cnt == n) break;
             }
-            if (cnt >= k) { r = middle; high = middle - 1; }
+            if (cnt == n) { r = middle; high = middle - 1; }
             else low = middle + 1;
         }
         cout << r << '\n';
