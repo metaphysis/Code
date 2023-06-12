403d253f87921d22a23678373ae192b9b2aa27b9
metaphysis <metaphysis@yeah.net>
2023-06-12 21:39:32 +0800

    11670 Physics Experiment

--- UVa Online Judge/volume116/11670 Physics Experiment/program.cpp
+++ UVa Online Judge/volume116/11670 Physics Experiment/program.cpp
@@ -9,28 +9,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 double d[10010], D;
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
-	int n, cases = 1;
+    int n, cases = 1;
     cout << fixed << setprecision(3);
-	while (cin >> n, n) {
-		for (int i = 0; i < n; i++) cin >> d[i];
+    while (cin >> n, n)
+    {
+        for (int i = 0; i < n; i++)
+            cin >> d[i];
         cin >> D;
-		sort(d, d + n);
-		double t = 0;
-		for (int i = 1; i < n; i++) {
-			if (d[i] - d[i - 1] > D) {
-				if (d[i] - t - d[i - 1] > D) {
-					double needed = (d[i] - t - d[i - 1] - D) / 2;
-					d[i] = d[i] - needed - t;
-					t += needed;
-				} else d[i] = d[i - 1] + D;
-			} else {
-				if (d[i] + t < d[i - 1] + D) d[i] += t;
-				else d[i] = d[i - 1] + D;
-			}
-		}
+        sort(d, d + n);
+        double t = 0;
+        for (int i = 1; i < n; i++)
+        {
+            if (d[i] - d[i - 1] > D)
+            {
+                if (d[i] - t - d[i - 1] > D)
+                {
+                    double needed = (d[i] - t - d[i - 1] - D) / 2;
+                    d[i] = d[i] - needed - t;
+                    t += needed;
+                }
+                else
+                    d[i] = d[i - 1] + D;
+            }
+            else
+            {
+                if (d[i] + t < d[i - 1] + D)
+                    d[i] += t;
+                else
+                    d[i] = d[i - 1] + D;
+            }
+        }
         cout << "Case " << cases++ << ": " << t << '\n';
-	}	 
-	return 0;
+    }
+    return 0;
 }
--- UVa Online Judge/volume116/11670 Physics Experiment/program1.cpp
+++ UVa Online Judge/volume116/11670 Physics Experiment/program1.cpp
@@ -9,27 +9,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 double d[10010], D;
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
-	int n, cases = 1;
+    int n, cases = 1;
     cout << fixed << setprecision(3);
-	while (cin >> n, n) {
-		for (int i = 0; i < n; i++) cin >> d[i];
+    while (cin >> n, n)
+    {
+        for (int i = 0; i < n; i++)
+            cin >> d[i];
         cin >> D;
-		sort(d, d + n);
-		double low = 0, high = d[n - 1], mid;
-		while (fabs(low - high) >= 1e-6) {
-		    mid = (low + high) / 2;
-		    int ok = 1;
-		    double last = d[0] + mid;
-		    for (int i = 1; i < n; i++) {
-			    if (last + D < d[i] - mid) { ok = 0; break; }
-			    last = min(last + D, d[i] + mid);
-		    }
-		    if (ok) high = mid;
-		    else low = mid;
-		}
+        sort(d, d + n);
+        double low = 0, high = d[n - 1], mid;
+        while (fabs(low - high) >= 1e-6)
+        {
+            mid = (low + high) / 2;
+            int ok = 1;
+            double last = d[0] + mid;
+            for (int i = 1; i < n; i++)
+            {
+                if (last + D < d[i] - mid)
+                {
+                    ok = 0;
+                    break;
+                }
+                last = min(last + D, d[i] + mid);
+            }
+            if (ok)
+                high = mid;
+            else
+                low = mid;
+        }
         cout << "Case " << cases++ << ": " << mid << '\n';
-	}	 
-	return 0;
+    }
+    return 0;
 }
