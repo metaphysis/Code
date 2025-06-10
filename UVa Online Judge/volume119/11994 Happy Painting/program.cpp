69d262353471db1f21221c3a972bd72bd92fae25
metaphysis <metaphysis@yeah.net>
2025-06-10 21:19:35 +0800

    11994 Happy Painting

--- UVa Online Judge/volume119/11994 Happy Painting/program.cpp
+++ UVa Online Judge/volume119/11994 Happy Painting/program.cpp
@@ -1,5 +1,10 @@
-// [LUOGU P3369](https://www.luogu.com.cn/problem/P3690)
-// Link-Cut Tree
+// Happy Painting
+// UVa ID: 11994
+// Verdict: 
+// Submission Date: 
+// UVa Run Time: s
+//
+// 版权所有（C）2025，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
 
@@ -10,14 +15,14 @@ const int MAXN = 100010;
 #define LC(x) tr[x].s[0]
 #define RC(x) tr[x].s[1]
 
-struct NODE { int s[2], p, v, color, size, lazy, reversed; } tr[MAXN];
+struct NODE { int s[2], p, v, bit, size, lazy, reversed; } tr[MAXN];
 
 bool isRoot(int x) { return tr[tr[x].p].s[0] != x && tr[tr[x].p].s[1] != x; }
 
 void pushUp(int x) {
-    tr[x].color = v, tr[x].size = 1;
-    if (LC(x)) tr[x].size += LC(x).size, tr[x].color |= LC(x).color;
-    if (RC(x)) tr[x].size += RC(x).size, tr[x].color |= RC(x).color;
+    tr[x].bit = tr[x].v, tr[x].size = 1;
+    if (LC(x)) tr[x].size += tr[LC(x)].size, tr[x].bit |= tr[LC(x)].bit;
+    if (RC(x)) tr[x].size += tr[RC(x)].size, tr[x].bit |= tr[RC(x)].bit;
 }
 
 void commitReversed(int x) {
@@ -32,9 +37,9 @@ void pushDown(int x) {
         commitReversed(RC(x));
         tr[x].reversed = 0;
     }
-    if (tx[x].lazy) {
-        if (LC(x)) LC(x).v = LC(x).color = LC(x).lazy = lazy;
-        if (RC(x)) RC(x).v = RC(x).color = RC(x).lazy = lazy;
+    if (tr[x].lazy) {
+        if (LC(x)) tr[LC(x)].v = tr[LC(x)].bit = tr[LC(x)].lazy = tr[x].lazy;
+        if (RC(x)) tr[RC(x)].v = tr[RC(x)].bit = tr[RC(x)].lazy = tr[x].lazy;
         tr[x].lazy = 0;
     }
 }
@@ -91,12 +96,6 @@ int findRoot(int x) {
     return x;
 }
 
-void split(int x, int y) {
-    makeRoot(x);
-    access(y);
-    splay(y);
-}
-
 void link(int x, int y) {
     makeRoot(x);
     if (findRoot(y) != x) tr[x].p = y;
@@ -110,14 +109,77 @@ void cut(int x, int y) {
     }
 }
 
+int _cut(int x, int y) {
+    makeRoot(x);
+    access(x);
+    splay(x);
+    int rt = LC(x);
+    while (RC(rt)) rt = RC(rt);
+    tr[LC(x)].p = tr[x].p;
+    tr[x].p = LC(x) = 0;
+    return rt;
+}
+
+void change(int x, int y, int c) {
+    if (x == y) return;
+    int rt = findRoot(x), fx = 0;
+    if (rt != x) fx = _cut(rt, x);
+    if (x == findRoot(y)) {
+        if (fx) link(x, fx);
+    } else {
+        link(x, y);
+        tr[x].v = c;
+    }
+}
+
+void paint(int x, int y, int c) {
+    if (x == y || findRoot(x) != findRoot(y)) return;
+    access(x), splay(x);
+    for (int a = y, b = 0; a; a = tr[a].p) {
+        splay(a);
+        if (!tr[a].p) {
+            tr[RC(a)].v = tr[RC(a)].bit = tr[RC(a)].lazy = c;
+            tr[b].v = tr[b].bit = tr[b].lazy = c;
+        }
+        RC(a) = b;
+        b = a;
+        pushUp(b);
+    }
+}
+
+pair<int, int> query(int x, int y) {
+    if (x == y || findRoot(x) != findRoot(y)) return {0, 0};
+    access(x), splay(x);
+    pair<int, int > r = {0, 0};
+    for (int a = y, b = 0; a; a = tr[a].p) {
+        splay(a);
+        if (!tr[a].p) r = {tr[RC(a)].size + tr[b].size, tr[RC(a)].bit + tr[b].bit};
+        RC(a) = b;
+        b = a;
+        pushUp(b);
+    }
+    return r;
+}
+
 int main(int argc, char *argv[]) {
     cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
     int n, m, cmd, x, y, c;
+    int f[MAXN];
     cin >> n >> m;
-    for (int i = 0; i <= n; i++) {
-        tr[i].s[0] = tr[i].s[1] = tr[i].p = tr[i].reversed = tr[i].v = 0;
-        if (i) cin >> tr[i].w;
+    for (int i = 1; i <= n; i++) {
+        tr[i].p = tr[i].s[0] = tr[i].s[1] = 0;
+        tr[i].v = tr[i].bit = tr[i].reversed = 0;
+        tr[i].size = 1;
     }
+    for (int i = 1; i <= n; i++) {
+        cin >> f[i];
+        if (f[i]) tr[i].p = f[i];
+    }
+    for (int i = 1; i <= n; i++) {
+        cin >> c;
+        if (f[i]) tr[i].v = 1 << (c - 1);
+    }
+    
     for (int i = 1; i <= m; i++) {
         cin >> cmd;
         if (cmd == 1) {
