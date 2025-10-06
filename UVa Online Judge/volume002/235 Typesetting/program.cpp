84ba96727a941b846ea253c58daaea0b1638e293
metaphysis <metaphysis@yeah.net>
2025-10-06 15:43:44 +0800

    235 Typesetting

--- UVa Online Judge/volume002/235 Typesetting/program.cpp
+++ UVa Online Judge/volume002/235 Typesetting/program.cpp
@@ -1,8 +1,8 @@
 // Typesetting
 // UVa ID: 235
-// Verdict: 
-// Submission Date: 
-// UVa Run Time: s
+// Verdict: Accepted
+// Submission Date: 2025-10-06
+// UVa Run Time: 0.010s
 //
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
@@ -10,147 +10,81 @@
 
 using namespace std;
 
-int fontSize[110][10];
-map<char, int> indexer;
-int paragraph = 0, cases = 0;
+int font[110][10];
+map<char, int> idx;
+int paragraph = 1;
 int L, W;
 
-void process(string lines)
-{
-    cout << "Paragraph " << ++paragraph << endl;
-    
-    int sizes = 1, points = 10;
-    int lastSizes = 1, lastPoints = 10;
-    int currentWidth = 0;
-    int printedLines = 0;
-    bool firstWordSetted = false;
-    
+void process(string lines) {
+    int printedLines = 1;
+    int size = 1, point = 10;
+    int currentWidth = 0, spaceWidth;
     string firstWord, lastWord, word;
     istringstream iss(lines);
-    
-    while (iss >> word)
-    {
-        if (word == "*f1" || word == "*f2" || word == "*f3" || word == "*f4" || word == "*f5" || word == "*f6")
-        {
-            sizes = stoi(word.substr(2));
+    cout << "Paragraph " << paragraph++ << '\n';
+    while (iss >> word) {
+        if (word[0] == '*') {
+            if (word[1] == 'f') size = stoi(word.substr(2));
+            if (word[1] == 's') point = stoi(word.substr(2));
             continue;
         }
-
-        if ((word.length() == 3 && word[0] == '*' && word[1] == 's' & word[2] >= '1' && word[2] <= '9') ||
-            (word.length() == 4 && word[0] == '*' && word[1] == 's' & word[2] >= '1' && word[2] <= '9' && word[3] >= '0' && word[3] <= '9'))
-        {
-            points = stoi(word.substr(2));
-            continue;
-        }
-
-        double tempWidth = (int)round(lastPoints / 10.0 * fontSize[indexer[' ']][lastSizes]);
-        int singleBlankWidth = (int)round(tempWidth);
-        
-        int singleWordWidth = 0;
+        int wordWidth = 0;
         for (int i = 0; i < word.length(); i++)
-        {
-            tempWidth = points / 10.0 * fontSize[indexer[word[i]]][sizes];
-            singleWordWidth += (int)round(tempWidth);
-        }
-        
-        lastSizes = sizes;
-        lastPoints = points;
-                
-        if (singleWordWidth <= W)
-        {
-            if (firstWordSetted == false)
-            {
-                firstWord = word;
-                lastWord = word;
-                firstWordSetted = true;
-                currentWidth = singleWordWidth;
-                continue;
+            // use integer instead of round function, or you will get 'Wrong Answer'
+            wordWidth += (font[idx[word[i]]][size] * point + 5) / 10;
+        if ((currentWidth && currentWidth + spaceWidth + wordWidth >= W) || wordWidth >= W) {
+            if (currentWidth) {
+                cout << "Line " << printedLines++ << ": " << firstWord << " ... " << lastWord;
+                cout << " (" << (W - currentWidth) << " whitespace)\n";
+                currentWidth = 0;
             }
-            
-            if (currentWidth + singleBlankWidth + singleWordWidth <= W)
-            {
-                lastWord = word;
-                currentWidth += singleBlankWidth + singleWordWidth;
-                continue;
-            }
-            else
-            {
-                cout << "Line " << ++printedLines << ": ";
-                cout << firstWord << " ... " << lastWord;
-                cout << " (" << (W - currentWidth) << " whitespace)" << endl;
-                
-                firstWord = word;
-                lastWord = word;
-                firstWordSetted = true;
-                currentWidth = singleWordWidth;
+            if (wordWidth >= W) {
+                cout << "Line " << printedLines++ << ": " << word;
+                if (wordWidth == W) cout << " ... " << word;
+                cout << " (" << (W - wordWidth) << " whitespace)\n";
+                wordWidth = 0;
             }
         }
-        else
-        {
-            if (currentWidth > 0)
-            {
-                cout << "Line " << ++printedLines << ": ";
-                cout << firstWord << " ... " << lastWord;
-                cout << " (" << (W - currentWidth) << " whitespace)" << endl;
-            }
-
-            cout << "Line " << ++printedLines << ": ";
-            cout << word << " (" << (W - singleWordWidth) << " whitespace)" << endl;
-            
-            firstWordSetted = false;
-            currentWidth = 0;
+        if (currentWidth) currentWidth += spaceWidth;
+        else firstWord = word;
+        if (wordWidth) {
+            currentWidth += wordWidth;
+            lastWord = word;
         }
+        // use integer instead of round function, or you will get 'Wrong Answer'
+        spaceWidth = (font[idx[' ']][size] * point + 5) / 10;
     }
-    
-    if (currentWidth > 0)
-    {
-        cout << "Line " << ++printedLines << ": ";
-        cout << firstWord << " ... " << lastWord;
-        cout << " (" << (W - currentWidth) << " whitespace)" << endl;
+    if (currentWidth) {
+        cout << "Line " << printedLines++ << ": " << firstWord << " ... " << lastWord;
+        cout << " (" << (W - currentWidth) << " whitespace)\n";
     }
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     string line;
     getline(cin, line);
     int n = stoi(line);
-    
-    for (int i = 1; i <= n; i++)
-    {
+    for (int i = 1; i <= n; i++) {
         getline(cin, line);
-        
         istringstream iss(line);
-        if (line.front() == ' ')
-            indexer.insert(make_pair(' ', i));
-        else
-        {
+        if (line.front() == ' ') idx.insert(make_pair(' ', i));
+        else {
             char character;
             iss >> character;
-            indexer.insert(make_pair(character, i));
+            idx.insert(make_pair(character, i));
         }
-        
-        for (int j = 1; j <= 6; j++)
-            iss >> fontSize[i][j];
+        for (int j = 1; j <= 6; j++) iss >> font[i][j];
     }
-    
-    while (getline(cin, line))
-    {
+    while (getline(cin, line)) {
         istringstream iss(line);
         iss >> L >> W;
-        
-        if (L == 0)
-            break;
-        
+        if (L == 0) break;
         string allLines;    
-        for (int i = 1; i <= L; i++)
-        {
+        for (int i = 1; i <= L; i++) {
             getline(cin, line);
             allLines += line + ' ';
         }
-        
         process(allLines);
     }
-
 	return 0;
 }
