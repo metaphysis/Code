b5131feab111be5da4848667efddf8071caf5c6b
metaphysis <metaphysis@yeah.net>
2025-10-18 19:29:13 +0800

    917 Euro 2004

--- UVa Online Judge/volume009/917 Euro 2004/program.cpp
+++ UVa Online Judge/volume009/917 Euro 2004/program.cpp
@@ -2,232 +2,226 @@
 // UVa ID: 917
 // Verdict: Accepted
 // Submission Date: 2025-10-18
-// UVa Run Time: 1.270s
+// UVa Run Time: 0.010s
 //
 // 版权所有（C）2025，邱秋。metaphysis # yeah dot net
 
-#include <iostream>
-#include <string>
-#include <vector>
-#include <map>
-#include <algorithm>
-#include <set>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+// 常量定义：最大队伍数量和最大比赛数量
+const int MAX_TEAMS = 10010, MAX_GAMES = 10010;
+
+// 队伍数据结构：存储队伍的积分、进球数、失球数和胜场数
 struct Team {
-    string name;
-    int points = 0;
-    int goals_scored = 0;
-    int goals_conceded = 0;
-    int wins = 0;
-
-    Team() = default;
-    Team(const string& n) : name(n) {}
-
-    int goal_diff() const {
-        return goals_scored - goals_conceded;
-    }
+    int pts = 0, gs = 0, gc = 0, win = 0;  // 积分、进球数、失球数、胜场数
+    // 计算净胜球
+    int gd() const { return gs - gc; }
 };
 
+// 比赛数据结构：存储两支队伍的ID和各自的进球数
 struct Game {
-    string team1, team2;
-    int goals1, goals2;
+    int t1, t2, g1, g2;  // 队伍1ID, 队伍2ID, 队伍1进球, 队伍2进球
 };
 
-vector<Game> games;
+// 全局变量声明
+Game games[MAX_GAMES];           // 存储所有比赛的数组
+int gameCnt, teamCnt;            // 比赛数量和队伍数量计数器
+Team global[MAX_TEAMS];          // 存储所有队伍全局数据的数组
+map<string, int> nameToId;       // 队名到ID的映射
+string idToName[MAX_TEAMS];      // ID到队名的反向映射
 
-bool team_compare(const Team& a, const Team& b) {
-    if (a.points != b.points) return a.points > b.points;
-    if (a.goal_diff() != b.goal_diff()) return a.goal_diff() > b.goal_diff();
-    if (a.wins != b.wins) return a.wins > b.wins;
-    if (a.goals_scored != b.goals_scored) return a.goals_scored > b.goals_scored;
-    return a.name < b.name;
+// 队伍比较函数：用于排序，按积分、净胜球、胜场数、进球数、字母顺序的优先级比较
+bool teamCompare(const Team& a, const Team& b) {
+    if (a.pts != b.pts) return a.pts > b.pts;              // 积分高的排前面
+    if (a.gd() != b.gd()) return a.gd() > b.gd();          // 净胜球多的排前面
+    if (a.win != b.win) return a.win > b.win;              // 胜场数多的排前面
+    if (a.gs != b.gs) return a.gs > b.gs;                  // 进球数多的排前面
+    return false;                                          // 其他情况保持原顺序
 }
 
-bool teams_equal(const Team& a, const Team& b) {
-    return a.points == b.points &&
-           a.goal_diff() == b.goal_diff() &&
-           a.wins == b.wins &&
-           a.goals_scored == b.goals_scored;
+// 判断两个队伍在所有排名参数上是否完全相等
+bool teamsEqual(const Team& a, const Team& b) {
+    return a.pts == b.pts && a.gd() == b.gd() && a.win == b.win && a.gs == b.gs;
 }
 
-void compute_subleague(const vector<string>& team_names, map<string, Team>& sub_teams) {
-    sub_teams.clear();
-    for (const auto& name : team_names) {
-        sub_teams[name] = Team(name);
-    }
-
-    for (const auto& g : games) {
-        bool t1_in = find(team_names.begin(), team_names.end(), g.team1) != team_names.end();
-        bool t2_in = find(team_names.begin(), team_names.end(), g.team2) != team_names.end();
-        if (t1_in && t2_in) {
-            Team& t1 = sub_teams[g.team1];
-            Team& t2 = sub_teams[g.team2];
-            
-            t1.goals_scored += g.goals1;
-            t1.goals_conceded += g.goals2;
-            t2.goals_scored += g.goals2;
-            t2.goals_conceded += g.goals1;
-
-            if (g.goals1 > g.goals2) {
-                t1.points += 3;
-                t1.wins++;
-            } else if (g.goals1 < g.goals2) {
-                t2.points += 3;
-                t2.wins++;
-            } else {
-                t1.points++;
-                t2.points++;
-            }
+// 计算子联赛数据：只考虑指定队伍之间的比赛
+void calcLeague(const vector<int>& teams, Team res[]) {
+    // 初始化结果数组
+    for (int i = 0; i < teamCnt; i++) res[i] = Team();
+    
+    // 标记哪些队伍在当前子联赛中
+    bool inLeague[MAX_TEAMS] = {false};
+    for (int t : teams) inLeague[t] = true;
+    
+    // 遍历所有比赛，只处理子联赛内队伍之间的比赛
+    for (int i = 0; i < gameCnt; i++) {
+        const Game& g = games[i];
+        if (inLeague[g.t1] && inLeague[g.t2]) {
+            Team& t1 = res[g.t1], & t2 = res[g.t2];
+            // 更新进球和失球数据
+            t1.gs += g.g1;  t1.gc += g.g2;
+            t2.gs += g.g2;  t2.gc += g.g1;
+            // 根据比赛结果更新积分和胜场数
+            if (g.g1 > g.g2) { t1.pts += 3; t1.win++; }        // 队伍1获胜
+            else if (g.g1 < g.g2) { t2.pts += 3; t2.win++; }   // 队伍2获胜
+            else { t1.pts++; t2.pts++; }                       // 平局
         }
     }
 }
 
-vector<vector<string>> rank_teams_grouped(const vector<string>& team_names) {
-    if (team_names.size() == 1) {
-        return {team_names};
-    }
-
-    map<string, Team> sub_teams;
-    compute_subleague(team_names, sub_teams);
-
-    vector<Team> sub_vec;
-    for (const auto& name : team_names) {
-        sub_vec.push_back(sub_teams[name]);
-    }
-    sort(sub_vec.begin(), sub_vec.end(), team_compare);
-
-    vector<vector<string>> result;
-    for (size_t i = 0; i < sub_vec.size(); ) {
-        vector<string> group = {sub_vec[i].name};
-        int current_points = sub_vec[i].points;
+// 递归排名函数：对给定队伍组进行排名，处理并列情况
+vector<vector<int>> rankTeams(const vector<int>& teams) {
+    // 基本情况：如果只有一支队伍，直接返回
+    if (teams.size() == 1) return {teams};
+    
+    // 计算当前组的子联赛数据
+    Team sub[MAX_TEAMS];
+    calcLeague(teams, sub);
+    
+    // 将队伍数据与ID配对并排序
+    vector<pair<Team, int>> vec;
+    for (int t : teams) vec.push_back({sub[t], t});
+    sort(vec.begin(), vec.end(), [](const pair<Team, int>& a, const pair<Team, int>& b) { return teamCompare(a.first, b.first); });
+    
+    // 按积分分组
+    vector<vector<int>> res;
+    for (size_t i = 0; i < vec.size(); ) {
+        vector<int> group = {vec[i].second};
+        int curPts = vec[i].first.pts;
+        // 将积分相同的队伍分到同一组
+        while (++i < vec.size() && vec[i].first.pts == curPts) group.push_back(vec[i].second);
         
-        while (++i < sub_vec.size() && sub_vec[i].points == current_points) {
-            group.push_back(sub_vec[i].name);
-        }
-
-        if (group.size() == 1) {
-            result.push_back(group);
-        } else {
-            map<string, Team> final_sub_teams;
-            compute_subleague(group, final_sub_teams);
+        if (group.size() == 1)
+            res.push_back(group);  // 单支队伍直接加入结果
+        else {
+            // 对积分相同的队伍组进行进一步处理
+            Team finalSub[MAX_TEAMS];
+            calcLeague(group, finalSub);
             
-            bool all_same = true;
-            int first_pts = final_sub_teams[group[0]].points;
-            for (size_t j = 1; j < group.size() && all_same; j++) {
-                if (final_sub_teams[group[j]].points != first_pts) {
-                    all_same = false;
-                }
-            }
-
-            if (!all_same) {
-                auto sub_groups = rank_teams_grouped(group);
-                result.insert(result.end(), sub_groups.begin(), sub_groups.end());
+            // 检查在子联赛中这些队伍的积分是否仍然相同
+            bool samePts = true;
+            int firstPts = finalSub[group[0]].pts;
+            for (size_t j = 1; j < group.size() && samePts; j++)
+                if (finalSub[group[j]].pts != firstPts) samePts = false;
+            
+            if (!samePts) {
+                // 积分不再相同，递归处理这个组
+                auto subGroups = rankTeams(group);
+                res.insert(res.end(), subGroups.begin(), subGroups.end());
             } else {
-                vector<Team> final_group;
-                for (const auto& name : group) {
-                    final_group.push_back(final_sub_teams[name]);
-                }
-                sort(final_group.begin(), final_group.end(), team_compare);
+                // 积分仍然相同，使用其他排名标准
+                vector<pair<Team, int>> finalVec;
+                for (int t : group) finalVec.push_back({finalSub[t], t});
+                sort(finalVec.begin(), finalVec.end(), [](const pair<Team, int>& a, const pair<Team, int>& b) { return teamCompare(a.first, b.first); });
                 
-                for (size_t k = 0; k < final_group.size(); ) {
-                    vector<string> equal_teams = {final_group[k].name};
+                // 按排名参数分组，处理完全并列的情况
+                for (size_t k = 0; k < finalVec.size(); ) {
+                    vector<int> equalTeams = {finalVec[k].second};
                     size_t m = k + 1;
-                    while (m < final_group.size() && teams_equal(final_group[k], final_group[m])) {
-                        equal_teams.push_back(final_group[m].name);
-                        m++;
+                    // 找到所有参数完全相同的队伍
+                    while (m < finalVec.size() && teamsEqual(finalVec[k].first, finalVec[m].first)) {
+                        equalTeams.push_back(finalVec[m].second); m++;
                     }
-                    sort(equal_teams.begin(), equal_teams.end());
-                    result.push_back(equal_teams);
+                    // 完全并列的队伍按字母顺序排序
+                    sort(equalTeams.begin(), equalTeams.end(), [](int a, int b) { return idToName[a] < idToName[b]; });
+                    res.push_back(equalTeams);
                     k = m;
                 }
             }
         }
     }
+    
+    return res;
+}
 
-    return result;
+// 获取队伍ID：将队名映射为整数ID，如果不存在则创建新ID
+int getId(const string& name) {
+    if (nameToId.find(name) == nameToId.end()) {
+        int id = teamCnt++;
+        nameToId[name] = id;
+        idToName[id] = name;
+        return id;
+    }
+    return nameToId[name];
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int T;
+    // I/O优化
+    cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
+    
+    int T;  // 测试用例数量
     cin >> T;
-
+    
+    // 处理每个测试用例
     for (int tc = 0; tc < T; tc++) {
-        if (tc > 0) cout << '\n';
-
-        games.clear();
-        map<string, Team> global_teams;
-        set<string> all_team_names;
-
-        int G;
+        if (tc > 0) cout << '\n';  // 测试用例之间输出空行
+        
+        // 初始化全局变量
+        gameCnt = 0, teamCnt = 0;
+        nameToId.clear();
+        
+        int G;  // 比赛数量
         cin >> G;
+        
+        // 初始化全局队伍数据
+        for (int i = 0; i < MAX_TEAMS; i++) global[i] = Team();
+        
+        // 读取并处理所有比赛数据
         for (int i = 0; i < G; i++) {
             string t1, t2;
             int g1, g2;
             cin >> t1 >> t2 >> g1 >> g2;
-            games.push_back({t1, t2, g1, g2});
-
-            all_team_names.insert(t1);
-            all_team_names.insert(t2);
-
-            Team& team1 = global_teams[t1];
-            Team& team2 = global_teams[t2];
-            team1.name = t1;
-            team2.name = t2;
-
-            team1.goals_scored += g1;
-            team1.goals_conceded += g2;
-            team2.goals_scored += g2;
-            team2.goals_conceded += g1;
-
-            if (g1 > g2) {
-                team1.points += 3;
-                team1.wins++;
-            } else if (g1 < g2) {
-                team2.points += 3;
-                team2.wins++;
-            } else {
-                team1.points++;
-                team2.points++;
-            }
-        }
-
-        vector<string> all_teams(all_team_names.begin(), all_team_names.end());
-        vector<Team> global_vec;
-        for (const auto& name : all_teams) {
-            global_vec.push_back(global_teams[name]);
-        }
-        sort(global_vec.begin(), global_vec.end(), team_compare);
-
-        vector<vector<string>> final_groups;
-        for (size_t i = 0; i < global_vec.size(); ) {
-            vector<string> group = {global_vec[i].name};
-            int current_points = global_vec[i].points;
             
-            while (++i < global_vec.size() && global_vec[i].points == current_points) {
-                group.push_back(global_vec[i].name);
-            }
-
-            if (group.size() == 1) {
-                final_groups.push_back(group);
-            } else {
-                auto sub_groups = rank_teams_grouped(group);
-                final_groups.insert(final_groups.end(), sub_groups.begin(), sub_groups.end());
+            // 获取队伍ID
+            int id1 = getId(t1), id2 = getId(t2);
+            
+            // 存储比赛信息
+            games[gameCnt++] = {id1, id2, g1, g2};
+            
+            // 更新全局队伍数据
+            Team& tm1 = global[id1], & tm2 = global[id2];
+            tm1.gs += g1;  tm1.gc += g2;  // 更新队伍1的进球失球
+            tm2.gs += g2;  tm2.gc += g1;  // 更新队伍2的进球失球
+            // 根据比赛结果更新积分和胜场
+            if (g1 > g2) { tm1.pts += 3; tm1.win++; }        // 队伍1获胜
+            else if (g1 < g2) { tm2.pts += 3; tm2.win++; }   // 队伍2获胜
+            else { tm1.pts++; tm2.pts++; }                     // 平局
+        }
+        
+        // 收集所有队伍的ID
+        vector<int> allTeams;
+        for (int i = 0; i < teamCnt; i++) allTeams.push_back(i);
+        
+        // 按全局数据对队伍进行初步排序
+        sort(allTeams.begin(), allTeams.end(), [](int a, int b) {
+            return teamCompare(global[a], global[b]);
+        });
+        
+        // 构建最终排名分组
+        vector<vector<int>> finalGroups;
+        for (size_t i = 0; i < allTeams.size(); ) {
+            vector<int> group = {allTeams[i]};
+            int curPts = global[allTeams[i]].pts;
+            // 按积分分组
+            while (++i < allTeams.size() && global[allTeams[i]].pts == curPts) group.push_back(allTeams[i]);
+            
+            if (group.size() == 1) finalGroups.push_back(group);  // 单支队伍直接加入
+            else {
+                // 多支队伍积分相同，进入子联赛排名
+                auto subGroups = rankTeams(group);
+                finalGroups.insert(finalGroups.end(), subGroups.begin(), subGroups.end());
             }
         }
-
+        
+        // 输出最终排名结果
         int place = 1;
-        for (const auto& group : final_groups) {
-            for (const auto& name : group) {
-                cout << place << ' ' << name << '\n';
-            }
-            place += group.size();
+        for (const auto& group : finalGroups) {
+            for (int t : group) cout << place << ' ' << idToName[t] << '\n';
+            place += group.size();  // 更新名次
         }
     }
-
+    
     return 0;
 }
