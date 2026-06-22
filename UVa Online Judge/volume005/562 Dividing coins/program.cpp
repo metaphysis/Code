0b64e091ca5fbf6016b4068d24dbfac1dd9f283c
Qiu Qiu <metaphysis@yeah.net>
2026-06-22 09:05:34 +0800

    562 Dividing Coins

--- UVa Online Judge/volume005/562 Dividing coins/program.cpp
+++ UVa Online Judge/volume005/562 Dividing coins/program.cpp
@@ -7,48 +7,33 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     int coins[110], cents[25010], n, m;
-
     cin >> n;
-    for (int c = 1; c <= n; c++)
-    {
+    for (int c = 1; c <= n; c++) {
         int total = 0;
-
         cin >> m;
-        for (int i = 1; i <= m; i++)
-        {
+        for (int i = 1; i <= m; i++) {
             cin >> coins[i];
             total += coins[i];
         }
-
-        if (m <= 1)
-        {
+        if (m <= 1) {
             cout << total << '\n';
             continue;
         }
-
         int half = total / 2;
-
         memset(cents, 0, sizeof(cents)); 
-
         for (int i = 1; i <= m; i++)
             for (int j = half; j >= coins[i]; j--)
                 cents[j] = max(cents[j], cents[j - coins[i]] + coins[i]);
-
         for (int i = half; i >= 1; i--)
-            if (cents[i])
-            {
+            if (cents[i]) {
                 cout << abs(total - 2 * cents[i]) << '\n';
                 break;
             }
     }
-
-	return 0;
+    return 0;
 }
