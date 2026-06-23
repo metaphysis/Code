f26bf9df8319851e07032646194e1c9081931e8b
Qiu Qiu <metaphysis@yeah.net>
2026-06-23 08:45:19 +0800

    571 Jugs

--- UVa Online Judge/volume005/571 Jugs/program.cpp
+++ UVa Online Judge/volume005/571 Jugs/program.cpp
@@ -7,55 +7,37 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     int Ca, Cb, N;
-    while (cin >> Ca >> Cb >> N)
-    {
+    while (cin >> Ca >> Cb >> N) {
         int A = 0, B = 0;
-        while (true)
-        {
-            if (B == Cb)
-            {
+        while (true) {
+            if (B == Cb) {
                 cout << "empty B\n";
                 B = 0;
             }
-            
-            if (A == 0)
-            {
+            if (A == 0) {
                 cout << "fill A\n";
                 A = Ca;
             }
-
-            if (A == N)
-            {
+            if (A == N) {
                 cout << "success\n";
                 break;
             }
-            
             cout << "pour A B\n";
-            
             B += A;
-            if (B > Cb)
-            {
+            if (B > Cb) {
                 A = B - Cb;
                 B = Cb;
-            }
-            else
-                A = 0;
-            
-            if (A == N || B == N)
-            {
+            } else A = 0;
+            if (A == N || B == N) {
                 cout << "success\n";
                 break;
             }
         }
     }
-    
-	return 0;
+    return 0;
 }
