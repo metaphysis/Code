c365439c1e216a5208a85027fb28653af2cc28f7
Qiu Qiu <metaphysis@yeah.net>
2026-06-23 08:53:47 +0800

    573 The Snail

--- UVa Online Judge/volume005/573 The Snail/program.cpp
+++ UVa Online Judge/volume005/573 The Snail/program.cpp
@@ -7,43 +7,30 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     int H, U, D, F;
-    while (cin >> H >> U >> D >> F, H)
-    {
+    while (cin >> H >> U >> D >> F, H) {
         H *= 100, D *= 100;
-        
         int height = 0, day = 0, factor = 0;
-        while (true)
-        {
+        while (true) {
             day++;
-            
             int climbed = U * (100 - factor);
             if (climbed < 0) climbed = 0;
-
             height += climbed;
-            if (height > H)
-            {
+            if (height > H) {
                 cout << "success on day " << day << '\n';
                 break;
             }
-            
             height -= D;
-            if (height < 0)
-            {
+            if (height < 0) {
                 cout << "failure on day " << day << '\n';
                 break;
             }
-            
             factor += F;
         }
     }
-    
-	return 0;
+    return 0;
 }
