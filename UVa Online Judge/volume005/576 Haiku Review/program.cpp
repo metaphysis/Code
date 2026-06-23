95ed1a265fc14fcb852307967031753c9d26d1be
Qiu Qiu <metaphysis@yeah.net>
2026-06-23 10:33:05 +0800

    576 Haiku Review

--- UVa Online Judge/volume005/576 Haiku Review/program.cpp
+++ UVa Online Judge/volume005/576 Haiku Review/program.cpp
@@ -7,54 +7,34 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     vector<int> syllables = {0, 5, 7, 5};
     string vowels = "aeiouy";
-    
     string line, block;
-    while (getline(cin, line), line != "e/o/i")
-    {
+    while (getline(cin, line), line != "e/o/i") {
         istringstream iss(line);
-        
         bool correct = true;
         int indexer = 1;
-
-        while (getline(iss, block, '/'))
-        {
+        while (getline(iss, block, '/')) {
             int count = 0;
-
             int i = 0;
-            while (i < block.length())
-            {
-                if (vowels.find(block[i]) != block.npos)
-                {
+            while (i < block.length()) {
+                if (vowels.find(block[i]) != block.npos) {
                     count++;
-                    while (i < block.length() && vowels.find(block[i]) != block.npos)
-                        i++;
-                }
-                else
-                    i++;
+                    while (i < block.length() && vowels.find(block[i]) != block.npos) i++;
+                } else i++;
             }
-
-            if (count != syllables[indexer])
-            {
+            if (count != syllables[indexer]) {
                 correct = false;
                 break;
             }
             indexer++;
         }
-        
-        if (correct)
-            cout << "Y\n";
-        else
-            cout << indexer << '\n';
+        if (correct) cout << "Y\n";
+        else cout << indexer << '\n';
     }
-    
-	return 0;
+    return 0;
 }
