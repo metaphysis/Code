1aacfeb795fcd341ee9bd3ed59cd24f1930f6917
Qiu Qiu <metaphysis@yeah.net>
2026-06-23 10:51:43 +0800

    579 Clock Hands

--- UVa Online Judge/volume005/579 Clock Hands/program.cpp
+++ UVa Online Judge/volume005/579 Clock Hands/program.cpp
@@ -7,30 +7,19 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     string line, H, M;
-    while (getline(cin, line), line != "0:00")
-    {
+    while (getline(cin, line), line != "0:00") {
         istringstream iss(line);
         getline(iss, H, ':');
         getline(iss, M, ':');
-        
-        //cout << H << ':' << M << '\n';
-        
         int hours = stoi(H), minutes = stoi(M);
-
         int numerator = abs(60 * hours - 11 * minutes);
-        if (numerator > 360)
-            numerator = 720 - numerator;
-
+        if (numerator > 360) numerator = 720 - numerator;
         cout << fixed << setprecision(3) << ((double)(numerator) / 2.0) << '\n';
     }
-    
-	return 0;
+    return 0;
 }
