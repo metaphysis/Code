2c34374c7afaa23eab48b884a7e74aaf6d3c6cb6
Qiu Qiu <metaphysis@yeah.net>
2026-06-23 10:57:07 +0800

    568 Just the Facts

--- UVa Online Judge/volume005/568 Just the Facts/program.cpp
+++ UVa Online Judge/volume005/568 Just the Facts/program.cpp
@@ -7,18 +7,14 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
 const long long int MOD = 100000000000;
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     long long int last_number[10001] = { 1 };
-    for (int i = 1; i <= 10000; i++)
-    {
+    for (int i = 1; i <= 10000; i++) {
         long long int temp = i;
         while (temp % 10 == 0) temp /= 10;
         temp *= last_number[i - 1];
@@ -26,10 +22,7 @@ int main(int argc, char *argv[])
         if (temp > MOD) temp %= MOD;
         last_number[i] = temp;
     }
-    
     int n;
-    while (cin >> n)
-        cout << setw(5) << right << n << " -> " << (last_number[n] % 10) << '\n';
-    
-	return 0;
+    while (cin >> n) cout << setw(5) << right << n << " -> " << (last_number[n] % 10) << '\n';
+    return 0;
 }
