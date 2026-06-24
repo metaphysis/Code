a62df04c4cd5494381704230a40aa3341f62b94a
Qiu Qiu <metaphysis@yeah.net>
2026-06-24 12:02:24 +0800

    584 Bowling

--- UVa Online Judge/volume005/584 Bowling/program.cpp
+++ UVa Online Judge/volume005/584 Bowling/program.cpp
@@ -7,66 +7,43 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     string line;
-    while (getline(cin, line), line != "Game Over")
-    {
+    while (getline(cin, line), line != "Game Over") {
         vector<int> scores;
         char pins;
-        
         string text;
         istringstream iss(line);
-        while (iss >> pins)
-        {
-            if (pins == '/')
-                scores.push_back(10 - scores.back());
-            else if (pins == 'X')
-                scores.push_back(10);
-            else
-                scores.push_back(pins - '0');
-                
+        while (iss >> pins) {
+            if (pins == '/') scores.push_back(10 - scores.back());
+            else if (pins == 'X') scores.push_back(10);
+            else scores.push_back(pins - '0');
             text += pins;
         }
-        
         int total = 0, frames = 0;
-        for (int i = 0, rolls = 0; i < text.length() && frames < 10; i++)
-        {
+        for (int i = 0, rolls = 0; i < text.length() && frames < 10; i++) {
             total += scores[i];
-
-            if (text[i] == 'X')
-            {
+            if (text[i] == 'X') {
                 total += scores[i + 1];
                 total += scores[i + 2];
                 rolls = 0;
                 frames++;
-            }
-            else if (text[i] == '/')
-            {
+            } else if (text[i] == '/') {
                 total += scores[i + 1];
                 rolls = 0;
                 frames++;
-            }
-            else
-            {
+            } else {
                 rolls++;
-                if (rolls == 2)
-                {
+                if (rolls == 2) {
                     frames++;
                     rolls = 0;
                 }
             }
-            
-            cout << text[i] << ' ' << total << '\n';
         }
-        
         cout << total << '\n';
     }
-    
-	return 0;
+    return 0;
 }
