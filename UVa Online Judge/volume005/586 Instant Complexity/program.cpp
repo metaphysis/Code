f8b066748766a9d97e8aa37dadbdad172542c19c
Qiu Qiu <metaphysis@yeah.net>
2026-06-24 12:11:33 +0800

    586 Instant Complexity

--- UVa Online Judge/volume005/586 Instant Complexity/program.cpp
+++ UVa Online Judge/volume005/586 Instant Complexity/program.cpp
@@ -7,102 +7,64 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-struct term
-{
-    int coefficient, degree;
-};
+struct term { int coefficient, degree; };
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     int cases = 0, coefficients[11];
     cin >> cases;
-    
     string token;
-    for (int c = 1; c <= cases; c++)
-    {
+    for (int c = 1; c <= cases; c++)     {
         memset(coefficients, 0, sizeof(coefficients));
-        
         cout << "Program #" << c << '\n';
-        
         stack<term> terms;
-        while (cin >> token)
-        {
-            if (token == "END")
-            {
+        while (cin >> token) {
+            if (token == "END") {
                 terms.pop();
-                if (terms.empty())
-                    break;
-            }
-            else if (token == "BEGIN")
-            {
+                if (terms.empty()) break;
+            } else if (token == "BEGIN") {
                 term t;
                 t.coefficient = 1, t.degree = 0;
                 terms.push(t);
-            }
-            else if (token == "LOOP")
-            {
+            } else if (token == "LOOP") {
                 cin >> token;
-                if (token == "n")
-                {
+                if (token == "n") {
                     term t;
                     t.coefficient = terms.top().coefficient;
                     t.degree = terms.top().degree + 1;
                     terms.push(t);
-                }
-                else
-                {
+                } else {
                     int n = stoi(token);
                     term t;
                     t.coefficient = terms.top().coefficient * n;
                     t.degree = terms.top().degree;
                     terms.push(t);
                 }
-            }
-            else if (token == "OP")
-            {
+            } else if (token == "OP") {
                 cin >> token;
                 int n = stoi(token);
                 coefficients[terms.top().degree] += n * terms.top().coefficient;
             }
         }
-        
         cout << "Runtime = ";
         bool outputed = false;
-        for (int i = 10; i >= 0; i--)
-        {
-            if (outputed && coefficients[i] > 0)
-                cout << '+';
-
-            if (coefficients[i] > 0)
-                outputed = true;
-
-            if (coefficients[i] > 1)
-            {
+        for (int i = 10; i >= 0; i--) {
+            if (outputed && coefficients[i] > 0) cout << '+';
+            if (coefficients[i] > 0) outputed = true;
+            if (coefficients[i] > 1) {
                 cout << coefficients[i];
-                if (i > 0)
-                    cout << '*';
+                if (i > 0) cout << '*';
+            } else {
+                if (i == 0 && (!outputed || coefficients[i] == 1)) cout << coefficients[i];
             }
-            else
-            {
-                if (i == 0 && (!outputed || coefficients[i] == 1))
-                    cout << coefficients[i];
-            }
-            
-            if (coefficients[i] > 0 && i > 0)
-            {
+            if (coefficients[i] > 0 && i > 0) {
                 cout << 'n';
-                if (i > 1)
-                    cout << '^' << i;
+                if (i > 1) cout << '^' << i;
             }
         }
-
         cout << "\n\n";
     }
-    
-	return 0;
+    return 0;
 }
