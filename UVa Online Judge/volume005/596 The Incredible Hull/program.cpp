f6b8683690ceb66323424ddffc89ea9fb47161bc
Qiu Qiu <metaphysis@yeah.net>
2026-06-25 08:28:39 +0800

    596 The Incredible Hull

--- UVa Online Judge/volume005/596 The Incredible Hull/program.cpp
+++ UVa Online Judge/volume005/596 The Incredible Hull/program.cpp
@@ -7,31 +7,22 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
 const int MAX_VERTICES = 500;
 
-struct point
-{
+struct point {
 	int x, y;
-	
-	bool operator<(const point& another) const
-	{
-	    if (y == another.y)
-	        return x < another.x;
-	    else
-	        return y < another.y;
+	bool operator<(const point& another) const {
+	    if (y == another.y) return x < another.x;
+	    else return y < another.y;
 	}
-	
-	bool operator==(const point& another) const
-	{
+	bool operator==(const point& another) const {
 	    return x == another.x && y == another.y;
 	}
 };
 
-struct polygon
-{
+struct polygon {
 	int vertexNumber;
 	point vertex[MAX_VERTICES];
 };
@@ -40,112 +31,85 @@ point lowerLeftPoint, vertex[MAX_VERTICES];
 int vertexPerObject, vertexOfTotal = 0;
 
 // 叉积。
-int cp(point a, point b, point c)
-{
+int cp(point a, point b, point c) {
 	return (b.x - a.x) * (c.y - a.y) - (c.x - a.x) * (b.y - a.y);
 }
 
 // 从点a向点b望去，点c位于线段ab的右侧，返回true。
-bool cw(point a, point b, point c)
-{
+bool cw(point a, point b, point c) {
 	return cp(a, b, c) < 0;
 }
 // 从点a向点b望去，点c位于线段ab的左侧时，返回true。
-bool ccw(point a, point b, point c)
-{
+bool ccw(point a, point b, point c) {
 	return cp(a, b, c) > 0;
 }
 
 // 当三点共线时，返回true。
-bool collinear(point a, point b, point c)
-{
+bool collinear(point a, point b, point c) {
 	return cp(a, b, c) == 0;
 }
 
 // 判断是否向左转或共线。
-bool ccwOrCollinear(point a, point b, point c)
-{
+bool ccwOrCollinear(point a, point b, point c) {
 	return cp(a, b, c) >= 0;
 }
 
 // 两点距离的平方值。
-int distanceToLowerLeftPoint(point p)
-{
+int distanceToLowerLeftPoint(point p) {
     return pow(lowerLeftPoint.x - p.x, 2) + pow(lowerLeftPoint.y - p.y, 2);
 }
 
 // 按相对于参考点的极角大小进行排序。
-bool smallerAngle(point first, point second)
-{
+bool smallerAngle(point first, point second) {
     if (collinear(lowerLeftPoint, first, second))
         return distanceToLowerLeftPoint(first) <= distanceToLowerLeftPoint(second);
     return ccw(lowerLeftPoint, first, second);
 }
 
 // Graham凸包扫描算法。
-void grahamConvexHull()
-{
+void grahamConvexHull() {
     polygon pg;
-
 	sort(vertex, vertex + vertexOfTotal);
 	vertexOfTotal = unique(vertex, vertex + vertexOfTotal) - vertex;
     lowerLeftPoint = vertex[0];
     sort(vertex + 1, vertex + vertexOfTotal, smallerAngle);
-
 	pg.vertex[0] = vertex[0];
 	pg.vertex[1] = vertex[1];
 	vertex[vertexOfTotal] = lowerLeftPoint;
-	
 	int i = 2, top = 1;
-	while (i <= vertexOfTotal)
-	{
-	    if (cw(pg.vertex[top - 1], pg.vertex[top], vertex[i]))
-	        top--;
-	    else
-	        pg.vertex[++top] = vertex[i++];
+	while (i <= vertexOfTotal) 	{
+	    if (cw(pg.vertex[top - 1], pg.vertex[top], vertex[i])) top--;
+	    else pg.vertex[++top] = vertex[i++];
 	}
-	
     pg.vertexNumber = top;
-    
     int selected = 0;
     for (int i = 0; i < pg.vertexNumber; i++)
         if (pg.vertex[i].x > pg.vertex[selected].x ||
             (pg.vertex[i].x == pg.vertex[selected].x && pg.vertex[i].y < pg.vertex[selected].y))
-            selected = i;
-    
+                selected = i;
     for (int i = selected; i < pg.vertexNumber + selected; i++)
         cout << " (" << pg.vertex[i % pg.vertexNumber].x << ',' << pg.vertex[i % pg.vertexNumber].y << ')';
     cout << '\n';
 }
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     char start;
-    while (cin >> start, start != 'E')
-    {
+    while (cin >> start, start != 'E') {
         string description;
         getline(cin, description);
-        if (description.front() == ' ')
-            description.erase(description.begin());
+        if (description.front() == ' ') description.erase(description.begin());
         cout << description << " convex hull:\n";
-        
         vertexOfTotal = 0;
-        while (cin >> start, start == 'P')
-        {
+        while (cin >> start, start == 'P') {
             cin >> vertexPerObject;
-            for (int i = 0; i < vertexPerObject; i++)
-            {
+            for (int i = 0; i < vertexPerObject; i++) {
                 cin >> vertex[vertexOfTotal].x >> vertex[vertexOfTotal].y;
                 vertexOfTotal++;
             }
         }
-
         grahamConvexHull();
-
         cin.putback(start);
     }
-    
 	return 0;
 }
