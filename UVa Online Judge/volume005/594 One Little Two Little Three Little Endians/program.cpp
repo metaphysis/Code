3ea05028ef0709acc2fa25283ccd150c5d4707c1
Qiu Qiu <metaphysis@yeah.net>
2026-06-25 08:19:46 +0800

    594 One Little Two Little Three Little Endians

--- UVa Online Judge/volume005/594 One Little Two Little Three Little Endians/program.cpp
+++ UVa Online Judge/volume005/594 One Little Two Little Three Little Endians/program.cpp
@@ -1,4 +1,4 @@
-// One Little, Two Little, Three Little Endians
+// One Little Two Little Three Little Endians
 // UVa ID: 594
 // Verdict: Accepted
 // Submission Date: 2016-08-10
@@ -7,36 +7,25 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     int n;
-    while (cin >> n)
-    {
+    while (cin >> n) {
         cout << n << " converts to ";
-        
         bitset<32> little(n);
         string binary = little.to_string();
-
         string next;
         next += binary.substr(24, 8) + binary.substr(16, 8) + binary.substr(8, 8) + binary.substr(0, 8);
-        
-        if (next.front() == '0')
-        {
+        if (next.front() == '0') {
             bitset<32> big(next);
             cout << big.to_ulong() << '\n';
-        }
-        else
-        {
+        } else {
             next.front() = '0';
             bitset<32> big(next);
             cout << (int)(-pow(2, 31) + big.to_ulong()) << '\n';
         }
     }
-    
-	return 0;
+    return 0;
 }
