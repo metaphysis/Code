779fa8bc802fc04b8afefbe17483fd4582461fc1
Qiu Qiu <metaphysis@yeah.net>
2026-06-27 07:45:28 +0800

    601 The PATH

--- UVa Online Judge/volume006/601 The PATH/program.cpp
+++ UVa Online Judge/volume006/601 The PATH/program.cpp
@@ -7,23 +7,19 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
 char grid[90][90], field[90][90];
 int n;
 
-void duplicate()
-{
+void duplicate() {
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
             field[i][j] = grid[i][j];
 }
 
-void fillW(int i, int j)
-{
-    if (i >= 0 && i < n && j >= 0 && j < n && field[i][j] == 'W')
-    {
+void fillW(int i, int j) {
+    if (i >= 0 && i < n && j >= 0 && j < n && field[i][j] == 'W') {
         field[i][j] = '1';
         fillW(i + 1, j);
         fillW(i - 1, j);
@@ -32,10 +28,8 @@ void fillW(int i, int j)
     }
 }
 
-void fillB(int i, int j)
-{
-    if (i >= 0 && i < n && j >= 0 && j < n && field[i][j] == 'B')
-    {
+void fillB(int i, int j) {
+    if (i >= 0 && i < n && j >= 0 && j < n && field[i][j] == 'B') {
         field[i][j] = '1';
         fillB(i + 1, j);
         fillB(i - 1, j);
@@ -44,108 +38,75 @@ void fillB(int i, int j)
     }
 }
 
-bool checkW()
-{
-    // White has a winning path.
+bool checkW() {
     bool edged = false;
     for (int i = 0; i < n; i++)
-        if (field[i][0] == 'W')
-        {
+        if (field[i][0] == 'W') {
             edged = true;
             fillW(i, 0);
         }
-
     if (edged)
-    {
         for (int i = 0; i < n; i++)
             if (field[i][n - 1] == '1')
                 return true;
-    }
-    
     return false;
 }
 
-bool checkB()
-{
-    // Black has a winning path.
+bool checkB() {
     bool edged = false;
     for (int i = 0; i < n; i++)
-        if (field[0][i] == 'B')
-        {
+        if (field[0][i] == 'B') {
             edged = true;
             fillB(0, i);
         }
-
     if (edged)
-    {
         for (int i = 0; i < n; i++)
             if (field[n - 1][i] == '1')
                 return true;
-    }
-    
     return false;
 }
 
-void check()
-{
+void check() {
     duplicate();
-    if (checkW())
-    {
+    if (checkW()) {
         cout << "White has a winning path.\n";
         return;
     }
-    
     duplicate();
-    if (checkB())
-    {
+    if (checkB()) {
         cout << "Black has a winning path.\n";
         return;
     }
-    
-    // White can win in one move.
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
-            if (grid[i][j] == 'U')
-            {
+            if (grid[i][j] == 'U') {
                 duplicate();
                 field[i][j] = 'W';
-                
-                if (checkW())
-                {
+                if (checkW()) {
                     cout << "White can win in one move.\n";
                     return;
                 }
             }
-            
-    // Black can win in one move.
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
-            if (grid[i][j] == 'U')
-            {
+            if (grid[i][j] == 'U') {
                 duplicate();
                 field[i][j] = 'B';
-                
-                if (checkB())
-                {
+                if (checkB()) {
                     cout << "Black can win in one move.\n";
                     return;
                 }
             }
-            
     cout << "There is no winning path.\n";
 }
 
-int main(int argc, char *argv[])
-{
-    cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
-    while (cin >> n, n)
-    {
+int main() {
+    cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
+    while (cin >> n, n) {
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
                 cin >> grid[i][j];
         check();
     }
-    
-	return 0;
+    return 0;
 }
