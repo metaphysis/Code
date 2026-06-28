756e4dd2bdc717249d74eb858de27638db6618f8
Qiu Qiu <metaphysis@yeah.net>
2026-06-28 08:21:35 +0800

    610 Street Directions

--- UVa Online Judge/volume006/610 Street Directions/program.cpp
+++ UVa Online Judge/volume006/610 Street Directions/program.cpp
@@ -7,69 +7,52 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
 vector<vector<int>> way;
 int visited[1001];
 
-void dfs(int i)
-{
+void dfs(int i) {
     for (auto e : way[i])
-        if (e > 0 && !visited[e])
-        {
+        if (e > 0 && !visited[e]) {
             visited[e] = 1;
             dfs(e);
         }
 }
 
-bool connected(int i, int j)
-{
+bool connected(int i, int j) {
     memset(visited, 0, sizeof(visited));
     dfs(i);
     return visited[j];
 }
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     int n, m, from, to, cases = 0;
-    while (cin >> n >> m, n)
-    {
+    while (cin >> n >> m, n) {
         cout << ++cases << "\n\n";
-        
         way.assign(n + 1, vector<int>());
-        for (int i = 1; i <= m; i++)
-        {
+        for (int i = 1; i <= m; i++) {
             cin >> from >> to;
             way[from].push_back(to);
             way[to].push_back(from);
         }
-        
         set<int> broken;
         for (int i = 1; i <= n; i++)
-            for (int j = 0; j < way[i].size(); j++)
-            {
+            for (int j = 0; j < way[i].size(); j++) {
                 int save = way[i][j];
                 if (broken.find(i * 10000 + save) == broken.end() &&
-                    broken.find(save * 10000 + i) == broken.end())
-                {
+                    broken.find(save * 10000 + i) == broken.end()) {
                     way[i][j] = -1;
-                    if (!connected(i, save))
-                        way[i][j] = save;
-                    else
-                        broken.insert(i * 10000 + save);
+                    if (!connected(i, save)) way[i][j] = save;
+                    else broken.insert(i * 10000 + save);
                 }
             }
-        
         for (int i = 1; i <= n; i++)
             for (int j = 0; j < way[i].size(); j++)
                 if (way[i][j] > 0)
                     cout << i << ' ' << way[i][j] << '\n';
-            
         cout << "#\n";
     }
-    
-	return 0;
+    return 0;
 }
