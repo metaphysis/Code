c6370321341931958e01783e7bcf15440560a36f
Qiu Qiu <metaphysis@yeah.net>
2026-06-29 09:46:23 +0800

    553 Simply Proportion

--- UVa Online Judge/volume005/553 Simply proportion/program.cpp
+++ UVa Online Judge/volume005/553 Simply proportion/program.cpp
@@ -1,4 +1,4 @@
-// Simply proportion
+// Simply Proportion
 // UVa ID: 553
 // Verdict: Accepted
 // Submission Date: 2017-05-11
@@ -7,69 +7,49 @@
 // 版权所有（C）2017，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
-
     map<char, int> widths{{'A', 18}, {'B', 17}, {'I', 10}, {'M', 20}, {'S', 16}, {'Y', 13}};
     int N;
     string line;
-    
-    while (true)
-    {
+    while (true) {
         getline(cin, line);
         N = stoi(line);
         if (N == 0) break;
-        
         getline(cin, line);
-        
         int widthOfLetters = 0, gapsOfWord = 0, gapsOfLetter = 0;
         vector<string> words;
-        
         string word;
         istringstream iss(line);
-        while (iss >> word)
-        {
+        while (iss >> word) {
             for (auto c : word) widthOfLetters += widths[c];
             gapsOfLetter += word.length() - 1;
             words.push_back(word);
         }
-        
         gapsOfWord = words.size() - 1;
-        
         int dotBetweenLetter = 3, dotBetweenWord = 10, backup = dotBetweenLetter;
-        
-        while (true)
-        {
+        while (true) {
             dotBetweenWord = (N - widthOfLetters - gapsOfLetter * dotBetweenLetter) / gapsOfWord;
             if (dotBetweenWord < 10 || dotBetweenWord < 3 * dotBetweenLetter)
                 break;
             backup = dotBetweenLetter;
             dotBetweenLetter++;
         }
-        
         dotBetweenLetter = backup;
         dotBetweenWord = (N - widthOfLetters - gapsOfLetter * dotBetweenLetter) / gapsOfWord;
         int remainDot = N - widthOfLetters - gapsOfLetter * dotBetweenLetter - gapsOfWord * dotBetweenWord;
         int beginAddDot = gapsOfWord - remainDot;
-        
-        for (int i = 0; i < words.size(); i++)
-        {
-            for (int j = 0; j < words[i].length(); j++)
-            {
+        for (int i = 0; i < words.size(); i++) {
+            for (int j = 0; j < words[i].length(); j++) {
                 cout << words[i][j];
-                if (j < words[i].length() - 1)
-                {
+                if (j < words[i].length() - 1) {
                     cout << '/';
                     cout << '(' << dotBetweenLetter << ')';
                 }
             }
-            
-            if (i < words.size() - 1)
-            {
+            if (i < words.size() - 1) {
                 cout << '/';
                 cout << '(';
                 if (remainDot > 0 && i >= beginAddDot)
@@ -79,9 +59,7 @@ int main(int argc, char *argv[])
                 cout << ')';
             }
         }
-        
         cout << '\n';
     }
-    
     return 0;
 }
