a5b9802d00be4b18571c45180a1ef384f3d788b8
Qiu Qiu <metaphysis@yeah.net>
2026-06-29 09:28:49 +0800

    557 Burger

--- UVa Online Judge/volume005/557 Burger/program.cpp
+++ UVa Online Judge/volume005/557 Burger/program.cpp
@@ -7,25 +7,17 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
-    // Bernoulli trial
     double probability[50001] = { 1 };
-    for (int i = 1; i <= 50000; i++)
-        probability[i] = probability[i - 1] * (long double)(2 * i - 1) / (long double)(2 * i);
-    
+    for (int i = 1; i <= 50000; i++) probability[i] = probability[i - 1] * (long double)(2 * i - 1) / (long double)(2 * i);
     int cases, n;
     cin >> cases;
-    for (int c = 1; c <= cases; c++)
-    {
+    for (int c = 1; c <= cases; c++) {
         cin >> n;
         cout << fixed << setprecision(4) << (1 - probability[n / 2 - 1]) << '\n';
     }
-    
-	return 0;
+    return 0;
 }
