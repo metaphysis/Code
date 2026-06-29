aa8b721e88ee178f3181dbc589d54bd1f4d6b6b2
Qiu Qiu <metaphysis@yeah.net>
2026-06-29 09:25:31 +0800

    558 Wormholes

--- UVa Online Judge/volume005/558 Wormholes/program1.cpp
+++ UVa Online Judge/volume005/558 Wormholes/program1.cpp
@@ -7,13 +7,11 @@
 // 版权所有（C）2017，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
 const int MAXV = 1100, INF = 0x3fffffff;
 
-struct edge
-{
+struct edge {
     int from, to, weight;
     edge *next;
 };
@@ -22,79 +20,48 @@ edge *edges[MAXV];
 int n, dist[MAXV], parent[MAXV], visited[MAXV], counter[MAXV];
 
 // source为起始顶点的序号。
-bool spfa(int source)
-{
-    for (int i = 0; i < n; i++)
-    {
+bool spfa(int source) {
+    for (int i = 0; i < n; i++) {
         dist[i] = INF, parent[i] = -1;
         visited[i] = 0, counter[i] = 0;
     }
-
     dist[source] = 0, visited[source]++;
-    
     queue<int> q; q.push(source);
-    
-    while (!q.empty())
-    {
+    while (!q.empty()) {
         int u = q.front(); q.pop();
- 
-        if (counter[u] > n) return true;
-        
+         if (counter[u] > n) return true;
         edge *e = edges[u];
-        while (e != NULL)
-        {
+        while (e != NULL) {
             int v = e->to;
-            if (dist[v] > dist[u] + e->weight)
-            {
+            if (dist[v] > dist[u] + e->weight) {
                 dist[v] = dist[u] + e->weight; parent[v] = u;
-                if (!visited[v])
-                {
-                    q.push(v); visited[v]++; counter[v]++;
-                }
+                if (!visited[v]) { q.push(v); visited[v]++; counter[v]++; }
             }
             e = e->next;
         }
-        
         visited[u]--;
     }
-
     return false;
 }
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     int cases, m, x, y, t;
-
     cin >> cases;
-    for (int c = 1; c <= cases; c++)
-    {
+    for (int c = 1; c <= cases; c++) {
         cin >> n >> m;
-        for (int i = 1; i <= m; i++)
-        {
+        for (int i = 1; i <= m; i++) {
             cin >> x >> y >> t;
-            
             edge *e = new edge;
             e->to = y, e->weight = t, e->next = NULL;
-
-            if (edges[x] == NULL)
-                edges[x] = e;
-            else
-                e->next = edges[x], edges[x] = e;
+            if (edges[x] == NULL) edges[x] = e;
+            else e->next = edges[x], edges[x] = e;
         }
-
         cout << (spfa(0) ? "possible\n" : "not possible\n");
-        
-        for (int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < n; i++) {
             edge *e = edges[i];
-            while (e != NULL)
-            {
-                edges[i] = e->next; delete e; e = edges[i];
-            }
+            while (e != NULL) { edges[i] = e->next; delete e; e = edges[i]; }
         }
     }
-    
 	return 0;
 }
