b4db457126ca660c76b90208d4706481cfb6b833
Qiu Qiu <metaphysis@yeah.net>
2026-06-29 09:30:45 +0800

    556 Amazing

--- UVa Online Judge/volume005/556 Amazing/program.cpp
+++ UVa Online Judge/volume005/556 Amazing/program.cpp
@@ -7,52 +7,36 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     int b, w;
     char grid[100][100];
     int counter[100][100];
     int offset[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
     int rightside[4][2] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-    
-    while (cin >> b >> w, b)
-    {
+    while (cin >> b >> w, b) {
         memset(counter, 0, sizeof(counter));
-        
         for (int i = 1; i <= b; i++)
             for (int j = 1; j <= w; j++)
                 cin >> grid[b - i + 1][j];
-        
         int x = 1, y = 1, nextx, nexty, rightx, righty, d = 0;    
-        do
-        {
+        do {
             nextx = x + offset[d][0], nexty = y + offset[d][1];
-            if (nextx < 1 || nextx > b || nexty < 1 || nexty > w || grid[nextx][nexty] == '1')
-            {
+            if (nextx < 1 || nextx > b || nexty < 1 || nexty > w || grid[nextx][nexty] == '1') {
                 d++;
                 d %= 4;
-            }
-            else
-            {
-                //cout << nextx << ' ' << nexty << '\n';
-                
+            } else {
                 counter[nextx][nexty]++;
                 x = nextx, y = nexty;
-
                 rightx = nextx + rightside[d][0], righty = nexty + rightside[d][1];
-                if (rightx >= 1 && rightx <= b && righty >= 1 && righty <= w && grid[rightx][righty] == '0')
-                {
+                if (rightx >= 1 && rightx <= b && righty >= 1 && righty <= w && grid[rightx][righty] == '0') {
                     d += 3;
                     d %= 4;
                 }
             }
         } while (x != 1 || y != 1);
-        
         int times[5] = {0, 0, 0, 0, 0};
         for (int i = 1; i <= b; i++)
             for (int j = 1; j <= w; j++)
@@ -62,6 +46,5 @@ int main(int argc, char *argv[])
             cout << setw(3) << times[i];
         cout << '\n';
     }
-    
-	return 0;
+    return 0;
 }
