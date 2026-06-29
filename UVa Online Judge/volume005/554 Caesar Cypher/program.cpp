b67eaae431040aedf541ea7ffa574249e491cd55
Qiu Qiu <metaphysis@yeah.net>
2026-06-29 09:34:46 +0800

    554 Caesar Cypher

--- UVa Online Judge/volume005/554 Caesar Cypher/program.cpp
+++ UVa Online Judge/volume005/554 Caesar Cypher/program.cpp
@@ -7,82 +7,57 @@
 // 版权所有（C）2017，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     set<string> dictionary;
     string word;
-    
     while (cin >> word, word != "#") dictionary.insert(word);
-        
     string sentence;
     cin.ignore(1024, '\n');
     getline(cin, sentence);
-    
     int max_matches = 0, K = 0;
     string plain;
-
-    for (int k = 0; k <= 26; k++)
-    {
+    for (int k = 0; k <= 26; k++) {
         int matches = 0;
         string original;
-
-        for (int i = 0; i < sentence.length(); i++)
-        {
+        for (int i = 0; i < sentence.length(); i++) {
             int letter = sentence[i] == ' ' ? 0 : (sentence[i] - 64);
             letter = (letter + 27 - k) % 27;
-
             if (letter == 0) original += ' ';
             else original += (char)(letter + 64);
         }
-
         istringstream in(original);
         while (in >> word)
             if (dictionary.count(word) == 1)
                 matches++;
-
-        if (matches > max_matches)
-        {
+        if (matches > max_matches) {
             max_matches = matches;
             K = k;
             plain = original;
         }
     }
-
     vector<string> words;
     stringstream out(plain);
     while (out >> word) words.push_back(word);
-
     int i = 0, j = 0;
-    while (i < words.size())
-    {
-        if (j > 0)
-        {
-            if ((j + 1 + words[i].length()) >= 60)
-            {
+    while (i < words.size()) {
+        if (j > 0) {
+            if ((j + 1 + words[i].length()) >= 60) {
                 cout << '\n';
                 j = 0;
-            }
-            else
-            {
+            } else {
                 cout << ' ' << words[i];
                 j += (1 + words[i].length());
                 i++;
             }
-        }
-        else
-        {
+        } else {
             cout << words[i];
             j += words[i].length();
             i++;
         }
     }
-    
     if (j > 0) cout << '\n';
-
     return 0;
 }
