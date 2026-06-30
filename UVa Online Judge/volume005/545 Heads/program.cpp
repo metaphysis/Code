1295388763b46f94c76ac12e06710c7b67cdd129
Qiu Qiu <metaphysis@yeah.net>
2026-06-30 09:19:47 +0800

    545 Heads

--- UVa Online Judge/volume005/545 Heads/program.cpp
+++ UVa Online Judge/volume005/545 Heads/program.cpp
@@ -7,27 +7,20 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     int r, n;
     cin >> r;
-    for (int i = 1; i <= r; i++)
-    {
+    for (int i = 1; i <= r; i++) {
         cin >> n;
-        
         double expoent = log10(2) * n;
         double rounded = ceil(expoent);
         double digits = pow(10, rounded - expoent);
-
         cout << "2^-" << n << " = ";
         cout << fixed << setprecision(3) << digits;
         cout << "E-" << (int)rounded << '\n';
     }
-    
-	return 0;
+    return 0;
 }
