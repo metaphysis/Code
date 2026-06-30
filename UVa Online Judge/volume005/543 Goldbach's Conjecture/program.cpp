9dd8a951b91a2c92963d90300014d2d079f68548
Qiu Qiu <metaphysis@yeah.net>
2026-06-30 09:22:41 +0800

    543 Goldbach's Conjecture

--- UVa Online Judge/volume005/543 Goldbach's Conjecture/program.cpp
+++ UVa Online Judge/volume005/543 Goldbach's Conjecture/program.cpp
@@ -7,29 +7,22 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     int primes[1000001] = { 0 };
-    
     for (int i = 3; i <= 1000000; i += 2)
         if (primes[i] == 0)
             for (int j = 2 * i; j <= 1000000; j += i)
                 primes[j] = -1;
     int n;
-    while (cin >> n, n)
-    {
+    while (cin >> n, n) {
         for (int i = 3; i <= 500000; i += 2)
-            if (primes[i] == 0 && primes[n - i] == 0)
-            {
+            if (primes[i] == 0 && primes[n - i] == 0) {
                 cout << n << " = " << i << " + " << (n - i) << '\n';
                 break;
             }
     }
-        
-	return 0;
+    return 0;
 }
