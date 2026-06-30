2b3fa76945ed672a4c4ee8e0366c051190e90c0e
Qiu Qiu <metaphysis@yeah.net>
2026-06-30 09:25:54 +0800

    541 Error Correction

--- UVa Online Judge/volume005/541 Error Correction/program.cpp
+++ UVa Online Judge/volume005/541 Error Correction/program.cpp
@@ -7,48 +7,31 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     int n, grid[110][110];
-
-    while (cin >> n, n)
-    {
+    while (cin >> n, n) {
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
                 cin >> grid[i][j];
-        
         int row, column, row_odd_number = 0, column_odd_number = 0;
-        for (int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < n; i++) {
             int row_sum = 0, column_sum = 0;
-            for (int j = 0; j < n; j++)
-                row_sum += grid[i][j], column_sum += grid[j][i];
-
-            if (row_sum % 2 == 1)
-            {
+            for (int j = 0; j < n; j++) row_sum += grid[i][j], column_sum += grid[j][i];
+            if (row_sum % 2 == 1) {
                 row_odd_number++;
                 row = i;
             }
-            
-            if (column_sum % 2 == 1)
-            {
+            if (column_sum % 2 == 1) {
                 column_odd_number++;
                 column = i;
             }
         }
-        
-        if (row_odd_number == 0 && column_odd_number == 0)
-            cout << "OK\n";
-        else if (row_odd_number == 1 && column_odd_number == 1)
-            cout << "Change bit (" << (row + 1) << "," << (column + 1) << ")\n";
-        else
-            cout << "Corrupt\n";
+        if (row_odd_number == 0 && column_odd_number == 0) cout << "OK\n";
+        else if (row_odd_number == 1 && column_odd_number == 1) cout << "Change bit (" << (row + 1) << "," << (column + 1) << ")\n";
+        else cout << "Corrupt\n";
     }
-    
-	return 0;
+    return 0;
 }
