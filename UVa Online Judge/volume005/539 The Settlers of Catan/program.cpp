81ab7c7019a4405c63fe10206b0dc6dd2bc55aec
Qiu Qiu <metaphysis@yeah.net>
2026-07-03 08:54:35 +0800

    539 The Settlers of Catan

--- UVa Online Judge/volume005/539 The Settlers of Catan/program.cpp
+++ UVa Online Judge/volume005/539 The Settlers of Catan/program.cpp
@@ -1,56 +1,43 @@
 // The Settlers of Catan
-// UVa ID: 593
-// Verdict: 
+// UVa ID: 539
+// Verdict: Accepted
 // Submission Date: 
 // UVa Run Time: s
 //
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
 const int MAX_V = 30;
 
 int n, m, maxLength = 0, visited[MAX_V][MAX_V], connected[MAX_V][MAX_V];
 
-void dfs(int u, int length)
-{
+void dfs(int u, int length) {
     maxLength = max(maxLength, length);
-
     for (int v = 0; v < n; v++)
-        if (connected[u][v] && !visited[u][v] && !visited[v][u])
-        {
+        if (connected[u][v] && !visited[u][v] && !visited[v][u]) {
             visited[u][v] = visited[v][u] = 1;
             dfs(v, length + 1);
             visited[u][v] = visited[v][u] = 0;
         }
 }
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     int from, to;
-
-    while (cin >> n >> m, n > 0)
-    {
+    while (cin >> n >> m, n > 0) {
         memset(connected, 0, sizeof(connected));
-        for (int i = 1; i <= m; i++)
-        {
+        for (int i = 1; i <= m; i++) {
             cin >> from >> to;
             connected[from][to] = connected[to][from] = 1;
         }
-
         maxLength = 0;
-        for (int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < n; i++) {
             memset(visited, 0, sizeof(visited));
             dfs(i, 0);
         }
-
         cout << maxLength << '\n';
     }
-    
-	return 0;
+    return 0;
 }
