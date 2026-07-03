d72dfe079047b4e43d938c99c6ff592a38832b9e
Qiu Qiu <metaphysis@yeah.net>
2026-07-03 08:26:12 +0800

    530 Binomial Showdown

--- UVa Online Judge/volume005/530 Binomial Showdown/program.cpp
+++ UVa Online Judge/volume005/530 Binomial Showdown/program.cpp
@@ -7,51 +7,34 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int gcd(int a, int b)
-{
+int gcd(int a, int b) {
     int t;
     while (a % b) t = a, a = b, b = t % b;
     return b;
 }
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     int n, k;
-    while (cin >> n >> k, n || k)
-    {
-        if (k > n / 2)
-            k = n - k;
-
+    while (cin >> n >> k, n || k) {
+        if (k > n / 2) k = n - k;
         vector<int> up, down;
-        for (int i = 1; i <= k; i++)
-        {
+        for (int i = 1; i <= k; i++) {
             up.push_back(n - i + 1);
             down.push_back(i);
         }
-        
         for (int i = 0; i < k; i++)
             for (int j = 0; j < k; j++)
-            {
-                if (up[i] > 1 && down[j] > 1)
-                {
+                if (up[i] > 1 && down[j] > 1) {
                     int divider = gcd(up[i], down[j]);
-                    if (divider > 1)
-                        up[i] /= divider, down[j] /= divider;
+                    if (divider > 1) up[i] /= divider, down[j] /= divider;
                 }
-            }
-            
         long long int number = 1;
         for (int i = 0; i < k; i++)
-            if (up[i] > 1)
-                number *= up[i];
-                
+            if (up[i] > 1) number *= up[i];
         cout << number << '\n';
     }
-    
-	return 0;
+    return 0;
 }
