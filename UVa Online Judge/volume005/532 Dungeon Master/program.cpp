8e8108d1ce4313c9ee3576687d4ac7deb072f57e
Qiu Qiu <metaphysis@yeah.net>
2026-07-03 08:42:02 +0800

    532 Dungeon Master

--- UVa Online Judge/volume005/532 Dungeon Master/program.cpp
+++ UVa Online Judge/volume005/532 Dungeon Master/program.cpp
@@ -7,62 +7,42 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-struct state
-{
+struct state {
     int i, j, k, minutes;
 };
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     char grid[32][32][32], visited[32][32][32];
-    
     int L, R, C;
-    while (cin >> L >> R >> C, L && R && C)
-    {
+    while (cin >> L >> R >> C, L && R && C) {
         int starti, startj, startk, endi, endj, endk;
-
         for (int i = 0; i < L; i++)
             for (int j = 0; j < R; j++)
-                for (int k = 0; k < C; k++)
-                {
+                for (int k = 0; k < C; k++) {
                     cin >> grid[i][j][k];
-
-                    if (grid[i][j][k] == 'S')
-                        starti = i, startj = j, startk = k;
-                    if (grid[i][j][k] == 'E')
-                        endi = i, endj = j, endk = k;
+                    if (grid[i][j][k] == 'S') starti = i, startj = j, startk = k;
+                    if (grid[i][j][k] == 'E') endi = i, endj = j, endk = k;
                 }
-                        
         memset(visited, 0, sizeof(visited));
-        
         queue<state> unvisited;
         unvisited.push((state){starti, startj, startk, 0});
-        
         bool escaped = false;
-        while (!unvisited.empty())
-        {
+        while (!unvisited.empty()) {
             state current = unvisited.front();
             unvisited.pop();
-            
             if (current.i < 0 || current.i >= L || current.j < 0 || current.j >= R ||
                 current.k < 0 || current.k >= C || grid[current.i][current.j][current.k] == '#' ||
                 visited[current.i][current.j][current.k])
                 continue;
-                
             visited[current.i][current.j][current.k] = 1;
-
-            if (current.i == endi && current.j == endj && current.k == endk)
-            {
+            if (current.i == endi && current.j == endj && current.k == endk) {
                 cout << "Escaped in " << current.minutes << " minute(s).\n";
                 escaped = true;
                 break;
             }
-            
             unvisited.push((state){current.i - 1, current.j, current.k, current.minutes + 1});
             unvisited.push((state){current.i + 1, current.j, current.k, current.minutes + 1});
             unvisited.push((state){current.i, current.j - 1, current.k, current.minutes + 1});
@@ -70,10 +50,7 @@ int main(int argc, char *argv[])
             unvisited.push((state){current.i, current.j, current.k - 1, current.minutes + 1});
             unvisited.push((state){current.i, current.j, current.k + 1, current.minutes + 1});
         }
-        
-        if (!escaped)
-            cout << "Trapped!\n";
+        if (!escaped) cout << "Trapped!\n";
     }
-
-	return 0;
+    return 0;
 }
