0410a9a8a5624eb656c4729b03fcac0bb41c2f8d
Qiu Qiu <metaphysis@yeah.net>
2026-07-03 08:48:50 +0800

    535 Globetrotter

--- UVa Online Judge/volume005/535 Globetrotter/program.cpp
+++ UVa Online Judge/volume005/535 Globetrotter/program.cpp
@@ -7,18 +7,15 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
 const double PI = 2 * acos(0), R = 6378.0;
 
-double gcd(double R, double plat, double plong, double qlat, double qlong)
-{
+double gcd(double R, double plat, double plong, double qlat, double qlong) {
     return R * acos(sin(plat) * sin(qlat) + cos(plat) * cos(qlat) * cos(plong - qlong));
 }
 
-double haversine(double R, double plat, double plong, double qlat, double qlong)
-{
+double haversine(double R, double plat, double plong, double qlat, double qlong) {
     double dlon = qlong - plong;
     double dlat = qlat - plat;
     double a = pow((sin(dlat / 2)), 2) + cos(plat) * cos(qlat) * pow(sin(dlon / 2), 2);
@@ -27,32 +24,22 @@ double haversine(double R, double plat, double plong, double qlat, double qlong)
     return d;
 }
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     string city, from, to;
     map<string, pair<double, double>> location;
     double latitude, longtitude;
-    
-    while (cin >> city, city != "#")
-    {
+    while (cin >> city, city != "#") {
         cin >> latitude >> longtitude;
         location[city] = make_pair(latitude * PI / 180.0, longtitude * PI / 180.0);
     }
-    
-    while (cin >> from >> to, from != "#")
-    {
+    while (cin >> from >> to, from != "#") {
         cout << from << " - " << to << '\n';
-        
-        if (location.find(from) == location.end() || location.find(to) == location.end())
-            cout << "Unknown\n";
-        else
-        {
+        if (location.find(from) == location.end() || location.find(to) == location.end()) cout << "Unknown\n";
+        else {
             double D = haversine(R, location[from].first, location[from].second, location[to].first, location[to].second);
             cout << fixed << setprecision(0) << D << " km\n";
         }
     }
-    
-	return 0;
+    return 0;
 }
