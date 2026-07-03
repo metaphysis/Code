7f29bb9f266e82eb5f0dd823fb3fc128aa24a1db
Qiu Qiu <metaphysis@yeah.net>
2026-07-03 08:53:17 +0800

    538 Balancing Bank Accounts

--- UVa Online Judge/volume005/538 Balancing Bank Accounts/program.cpp
+++ UVa Online Judge/volume005/538 Balancing Bank Accounts/program.cpp
@@ -7,46 +7,30 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     int n, t, cases = 0, amount;
     string name1, name2;
-    
-    while (cin >> n >> t, n > 0)
-    {
+    while (cin >> n >> t, n > 0) {
         map<string, int> money;
         string names[30];
-
-        for (int i = 1; i <= n; i++)
-        {
+        for (int i = 1; i <= n; i++) {
             cin >> names[i];
             money[names[i]] = 0;
         }
-
-        for (int i = 1; i <= t; i++)
-        {
+        for (int i = 1; i <= t; i++) {
             cin >> name1 >> name2 >> amount;
             money[name1] -= amount;
             money[name2] += amount;
         }
-        
         cout << "Case #" << ++cases << '\n';
-        
-        for (int i = 1; i < n; i++)
-        {
-            if (money[names[i]] > 0)
-                cout << names[i] << ' ' << names[n] << ' ' << money[names[i]] << '\n';
-            
-            if (money[names[i]] < 0)
-                cout << names[n] << ' ' << names[i] << ' ' << abs(money[names[i]]) << '\n';
+        for (int i = 1; i < n; i++) {
+            if (money[names[i]] > 0) cout << names[i] << ' ' << names[n] << ' ' << money[names[i]] << '\n';
+            if (money[names[i]] < 0) cout << names[n] << ' ' << names[i] << ' ' << abs(money[names[i]]) << '\n';
         }
         cout << '\n';
     }
-    
-	return 0;
+    return 0;
 }
