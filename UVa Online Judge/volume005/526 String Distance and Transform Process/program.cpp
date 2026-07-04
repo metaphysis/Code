0056cea7915dfa960fddc609996f3f963c4ae4fe
Qiu Qiu <metaphysis@yeah.net>
2026-07-04 09:17:29 +0800

    526 String Distance and Transform Process

--- UVa Online Judge/volume005/526 String Distance and Transform Process/program.cpp
+++ UVa Online Judge/volume005/526 String Distance and Transform Process/program.cpp
@@ -13,8 +13,7 @@ using namespace std;
 const int NONE = -1, DELETE = 0, INSERT = 1, CHANGE = 2, MATCH = 3;
 
 // 定义动态规划表格单元。
-struct cell
-{
+struct cell {
     int cost, operation;
 };
 
@@ -23,24 +22,15 @@ string S, T, operationCode[3] = {" Delete ", " Insert ", " Replace "};
 int M, N, deletions, insertions, indexer;
 
 // 显示操作步骤，注意删除操作其序号会因为已有的删除和插入操作而发生变化。
-void displayPath(int i, int j)
-{
-    if (dp[i][j].operation >= DELETE && dp[i][j].operation <= CHANGE)
-    {
+void displayPath(int i, int j) {
+    if (dp[i][j].operation >= DELETE && dp[i][j].operation <= CHANGE) {
         cout << indexer++;
         cout << operationCode[dp[i][j].operation];
-
-        if (dp[i][j].operation == CHANGE)
-        {
-            cout << j << "," << T[j] << "\n";
-        }
-        else if (dp[i][j].operation == DELETE)
-        {
+        if (dp[i][j].operation == CHANGE) cout << j << "," << T[j] << "\n";
+        else if (dp[i][j].operation == DELETE) {
             cout << (i + insertions - deletions) << "\n";
             deletions++;
-        }
-        else if (dp[i][j].operation == INSERT)
-        {
+        } else if (dp[i][j].operation == INSERT) {
             cout << j << "," << T[j] << "\n";
             insertions++;
         }
@@ -48,57 +38,36 @@ void displayPath(int i, int j)
 }
 
 // 利用递归构建操作步骤。
-void findPath(int i, int j)
-{
-    if (dp[i][j].operation != NONE)
-    {
-        if (dp[i][j].operation == DELETE)
-            findPath(i - 1, j);
-        else if (dp[i][j].operation == INSERT)
-            findPath(i, j - 1);
-        else
-            findPath(i - 1, j - 1);
+void findPath(int i, int j) {
+    if (dp[i][j].operation != NONE) {
+        if (dp[i][j].operation == DELETE) findPath(i - 1, j);
+        else if (dp[i][j].operation == INSERT) findPath(i, j - 1);
+        else findPath(i - 1, j - 1);
     }
-    
     displayPath(i, j);
 }
 
-void minimumEditDistance()
-{
+void minimumEditDistance() {
     // 为每个字符串起始位置增加一个空格，将字符串序号和表格序号对齐，方便处理。
     // 因此其长度需要相应减去 1。
     S = ' ' + S;
     T = ' ' + T;
     M = S.length() - 1;
     N = T.length() - 1;
-
     dp[0][0] = (cell){0, NONE};
-    for (int i = 1; i <= M; i++)
-        dp[i][0] = (cell){i, DELETE};
-    for (int j = 1; j <= N; j++)
-        dp[0][j] = (cell){j, INSERT};
-
+    for (int i = 1; i <= M; i++) dp[i][0] = (cell){i, DELETE};
+    for (int j = 1; j <= N; j++) dp[0][j] = (cell){j, INSERT};
     // 自底向上动态规划求解。
     for (int i = 1; i <= M; i++)
-        for (int j = 1; j <= N; j++)
-        {
+        for (int j = 1; j <= N; j++) {
             dp[i][j] = (cell){dp[i - 1][j].cost + 1, DELETE};
-
-            if (dp[i][j].cost > (dp[i][j - 1].cost + 1))
-                dp[i][j] = (cell){dp[i][j - 1].cost + 1, INSERT};
-
-            if (S[i] == T[j])
-            {
-                if (dp[i][j].cost > dp[i - 1][j - 1].cost)
-                    dp[i][j] = (cell){dp[i - 1][j - 1].cost, MATCH};
-            }
-            else
-            {
-                if (dp[i][j].cost > (dp[i - 1][j - 1].cost + 1))
-                    dp[i][j] = (cell){dp[i - 1][j - 1].cost + 1, CHANGE};
+            if (dp[i][j].cost > (dp[i][j - 1].cost + 1)) dp[i][j] = (cell){dp[i][j - 1].cost + 1, INSERT};
+            if (S[i] == T[j]) {
+                if (dp[i][j].cost > dp[i - 1][j - 1].cost) dp[i][j] = (cell){dp[i - 1][j - 1].cost, MATCH};
+            } else {
+                if (dp[i][j].cost > (dp[i - 1][j - 1].cost + 1)) dp[i][j] = (cell){dp[i - 1][j - 1].cost + 1, CHANGE};
             }
         }
-
     // 反向构建操作步骤。
     deletions = 0;
     insertions = 0;
@@ -107,20 +76,13 @@ void minimumEditDistance()
     findPath(M, N);
 }
 
-int main(int argc, char *argv[])
-{
-    cin.tie(0);
-    cout.sync_with_stdio(false);
-    
+int main() {
+    cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
     bool printBlankLine = false;
-    while (getline(cin, S) && getline(cin, T))
-    {
-        if (printBlankLine == false)
-            printBlankLine = true;
-        else
-            cout << "\n";
+    while (getline(cin, S) && getline(cin, T)) {
+        if (printBlankLine == false) printBlankLine = true;
+        else cout << "\n";
         minimumEditDistance();
     }
-    
     return 0;
 }
