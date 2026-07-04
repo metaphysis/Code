d69e4377ed0eb77a58fbb73c690c36d69dda9f93
Qiu Qiu <metaphysis@yeah.net>
2026-07-05 06:18:54 +0800

    627 The Net

--- UVa Online Judge/volume006/627 The Net/program.cpp
+++ UVa Online Judge/volume006/627 The Net/program.cpp
@@ -7,87 +7,60 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     int n, m, start, end;
-    while (cin >> n)
-    {
+    while (cin >> n) {
         cin.ignore(1024, '\n');
-        
         map<int, set<int>> edges;
-        
         string line, block;
-        for (int i = 1; i <= n; i++)
-        {
+        for (int i = 1; i <= n; i++) {
             getline(cin, line);
-            
             int hypen = line.find('-');
             start = stoi(line.substr(0, hypen));
-            
             string routers = line.substr(hypen + 1);
             istringstream iss(routers);
             while (getline(iss, block, ','))
                 edges[start].insert(stoi(block));
         }
-        
         cout << "-----\n";
         cin >> m;
-        
-        for (int j = 1; j <= m; j++)
-        {
+        for (int j = 1; j <= m; j++) {
             cin >> start >> end;
-
             map<int, int> parent;
             set<int> visited;
-            
             queue<int> unvisited;
             unvisited.push(start);
             parent[start] = -1;
             visited.insert(start);
-            
-            while (!unvisited.empty())
-            {
+            while (!unvisited.empty()) {
                 int current = unvisited.front();
                 unvisited.pop();
-                
-                if (current == end)
-                    break;
-                    
+                if (current == end) break;
                 for (auto edge : edges[current])
-                    if (visited.find(edge) == visited.end())
-                    {
+                    if (visited.find(edge) == visited.end()) {
                         parent[edge] = current;
                         visited.insert(edge);
                         unvisited.push(edge);
                     }
             }
-            
-            if (parent.find(end) == parent.end())
-                cout << "connection impossible\n";
-            else
-            {
+            if (parent.find(end) == parent.end()) cout << "connection impossible\n";
+            else {
                 vector<int> path;
-                while (parent[end] != -1)
-                {
+                while (parent[end] != -1) {
                     path.insert(path.begin(), end);
                     end = parent[end];
                 }
                 path.insert(path.begin(), end);
-                for (int k = 0; k < path.size(); k++)
-                {
-                    if (k > 0)
-                        cout << ' ';
+                for (int k = 0; k < path.size(); k++) {
+                    if (k > 0) cout << ' ';
                     cout << path[k];
                 }
                 cout << '\n';
             }
         }
     }
-    
-	return 0;
+    return 0;
 }
