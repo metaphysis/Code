35e61404204d1ae93ec6f0c58ac8d7d7a0482b0c
Qiu Qiu <metaphysis@yeah.net>
2026-07-05 06:21:08 +0800

    628 Password

--- UVa Online Judge/volume006/628 Passwords/program.cpp
+++ UVa Online Judge/volume006/628 Passwords/program.cpp
@@ -7,39 +7,28 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int n, m, password[260];
 string dictionary[110];
 
-void backtrack(string rule, int depth)
-{
-    if (depth == rule.length())
-    {
-        for (int i = 0; i < rule.length(); i++)
-        {
+void backtrack(string rule, int depth) {
+    if (depth == rule.length()) {
+        for (int i = 0; i < rule.length(); i++) {
             if (password[i] >= 0)
                 cout << dictionary[password[i]];
             else
                 cout << (char)('0' + password[i] + 10);
         }
         cout << '\n';
-    }
-    else
-    {
-        if (rule[depth] == '#')
-        {
-            for (int j = 0; j < n; j++)
-            {
+    } else {
+        if (rule[depth] == '#') {
+            for (int j = 0; j < n; j++) {
                 password[depth] = j;
                 backtrack(rule, depth + 1);
             }
-        }
-        else
-        {
-            for (int j = 0; j <= 9; j++)
-            {
+        } else {
+            for (int j = 0; j <= 9; j++) {
                 password[depth] = j - 10;
                 backtrack(rule, depth + 1);
             }
@@ -47,28 +36,21 @@ void backtrack(string rule, int depth)
     }
 }
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     string rule;
-    while (cin >> n)
-    {
+    while (cin >> n) {
         for (int i = 0; i < n; i++)
             cin >> dictionary[i];
-        
         cout << "--\n";
         cin >> m;
-        for (int i = 1; i <= m; i++)
-        {
+        for (int i = 1; i <= m; i++) {
             cin >> rule;
             for (int j = 0; j < rule.length(); j++)
                 if (rule[j] == '0')
                     rule[j] = '$';
-            
             backtrack(rule, 0);
         }
     }
-    
-	return 0;
+    return 0;
 }
