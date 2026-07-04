803944a33429e5f348eef0a1ecc346c33347d843
Qiu Qiu <metaphysis@yeah.net>
2026-07-04 09:20:06 +0800

    The Partition of a Cake

--- UVa Online Judge/volume005/527 The Partition of a Cake/program1.cpp
+++ UVa Online Judge/volume005/527 The Partition of a Cake/program1.cpp
@@ -1,4 +1,4 @@
-// The partition of a cake
+// The Partition of a Cake
 // UVa ID: 527
 // Verdict: Accepted
 // Submission Date: 2017-05-09
@@ -7,27 +7,20 @@
 // 版权所有（C）2017，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
 const double EPSILON = 1e-7;
 
-struct point
-{
+struct point {
     double x, y;
-    
-    bool operator==(const point &p) const
-    {
+    bool operator==(const point &p) const {
         return fabs(x - p.x) <= EPSILON && fabs(y - p.y) <= EPSILON;
     }
 };
 
-struct line
-{
+struct line {
     point a, b;
-
-    bool contains(point p)
-    {
+    bool contains(point p) {
         return p.x >= min(a.x, b.x) && p.x <= max(a.x, b.x) &&
             p.y >= min(a.y, b.y) && p.y <= max(a.y, b.y);
     }
@@ -35,29 +28,24 @@ struct line
 
 typedef vector<point> polygon;
 
-double crossProduct(point p1, point p2, point p3)
-{
+double crossProduct(point p1, point p2, point p3) {
     return (p3.x - p1.x) * (p2.y - p1.y) - (p2.x - p1.x) * (p3.y - p1.y);
 }
 
-bool cw(point p1, point p2, point p3)
-{
+bool cw(point p1, point p2, point p3) {
     return crossProduct(p1, p2, p3) > EPSILON;
 }
 
-bool collinear(point p1, point p2, point p3)
-{
+bool collinear(point p1, point p2, point p3) {
     return fabs(crossProduct(p1, p2, p3)) <= EPSILON;
 }
 
-bool paralle(line line1, line line2)
-{
+bool paralle(line line1, line line2) {
     return fabs((line1.a.x - line1.b.x) * (line2.a.y - line2.b.y) -
         (line2.a.x - line2.b.x) * (line1.a.y - line1.b.y)) <= EPSILON;
 }
 
-point intersection(line line1, line line2)
-{
+point intersection(line line1, line line2) {
     point p = line1.a;
     double scale = ((line1.a.x - line2.a.x) * (line2.a.y - line2.b.y) -
         (line1.a.y - line2.a.y) * (line2.a.x - line2.b.x)) /
@@ -68,94 +56,59 @@ point intersection(line line1, line line2)
     return p;
 }
 
-vector<polygon> halfPlaneIntersection(polygon pg, line cutline)
-{
+vector<polygon> halfPlaneIntersection(polygon pg, line cutline) {
     polygon cutted;
-
-    for (int i = 0; i < pg.size(); i++)
-    {
+    for (int i = 0; i < pg.size(); i++) {
         point p1 = pg[i];
         point p2 = pg[(i + 1) % pg.size()];
-
         cutted.push_back(p1);
-
         line edge = line{p1, p2};
         if (paralle(edge, cutline)) continue;
-
-        if (!collinear(cutline.a, cutline.b, p1))
-        {
+        if (!collinear(cutline.a, cutline.b, p1)) {
             point p3 = intersection(edge, cutline);
             if (edge.contains(p3)) cutted.push_back(p3);
         }
     }
-
     cutted.erase(unique(cutted.begin(), cutted.end()), cutted.end());
-
-    if (cutted.size() > 0 && cutted.front() == cutted.back())
-        cutted.pop_back();
-
+    if (cutted.size() > 0 && cutted.front() == cutted.back()) cutted.pop_back();
     polygon leftHalf, rightHalf;
-
-    for (auto v : cutted)
-    {
-        if (collinear(cutline.a, cutline.b, v))
-        {
+    for (auto v : cutted) {
+        if (collinear(cutline.a, cutline.b, v)) {
             leftHalf.push_back(v);
             rightHalf.push_back(v);
-        }
-        else
-        {
-            if (cw(cutline.a, cutline.b, v))
-                rightHalf.push_back(v);
-            else
-                leftHalf.push_back(v);
+        } else {
+            if (cw(cutline.a, cutline.b, v)) rightHalf.push_back(v);
+            else leftHalf.push_back(v);
         }
     }
-
     vector<polygon> partitions;
     if (leftHalf.size() >= 3) partitions.push_back(leftHalf);
     if (rightHalf.size() >= 3) partitions.push_back(rightHalf);
-
     return partitions;
 }
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
-
     int cases, cuts;
     double x1, y1, x2, y2;
-
-    vector<point> square
-    {
-        point{0, 0}, point{1000, 0}, point{1000, 1000}, point{0, 1000}
-    };
-
+    vector<point> square { point{0, 0}, point{1000, 0}, point{1000, 1000}, point{0, 1000} };
     cin >> cases;
-    for (int c = 1; c <= cases; c++)
-    {
+    for (int c = 1; c <= cases; c++) {
         if (c > 1) cout << '\n';
-
         vector<polygon> current, next;
         current.push_back(polygon{square});
-
         cin >> cuts;
-        for (int i = 1; i <= cuts; i++)
-        {
+        for (int i = 1; i <= cuts; i++) {
             cin >> x1 >> y1 >> x2 >> y2;
             line cutline = line{point {x1, y1}, point {x2, y2}};
-            for (auto pg : current)
-            {
+            for (auto pg : current) {
                 vector<polygon> partitions = halfPlaneIntersection(pg, cutline);
                 for (auto partition : partitions) next.push_back(partition);
             }
-
             current.swap(next);
             next.clear();
         }
-
         cout << current.size() << '\n';
     }
-
     return 0;
 }
