730822aca2d686274501675a0a51b530bf4ca304
Qiu Qiu <metaphysis@yeah.net>
2026-07-04 09:11:23 +0800

    525 Milk Bottle Data

--- UVa Online Judge/volume005/525 Milk Bottle Data/program.cpp
+++ UVa Online Judge/volume005/525 Milk Bottle Data/program.cpp
@@ -7,109 +7,69 @@
 // 版权所有（C）2017，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
 char rows[20][20];
 int used[20], R, N, chosen[20];
 
-bool backtrack(int depth)
-{
+bool backtrack(int depth) {
     if (depth >= R) return true;
-    
     for (int i = 0; i < R; i++)
-        if (!used[i])
-        {
-            // prune, cross check.
+        if (!used[i]) {
             bool matched = true;
             int start = (depth % 2 == 0 ? 1 : 0), column = depth / 2;
-            for (int j = start, k = 0; j < depth; j += 2, k++)
-            {
+            for (int j = start, k = 0; j < depth; j += 2, k++) {
                 if (rows[chosen[j]][column] == '2') continue;
                 if (rows[i][k] == '2') continue;
                 if (rows[chosen[j]][column] == rows[i][k]) continue;
                 matched = false;
                 break;
             }
-
             if (!matched) continue;
-
             used[i] = 1;
             chosen[depth] = i;
-
             if (backtrack(depth + 1)) return true;
-
             used[i] = 0;
         }
-        
     return false;
 }
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
-
     int cases;
     string row;
-    
     cin >> cases;
-    for (int c = 1; c <= cases; c++)
-    {
+    for (int c = 1; c <= cases; c++) {
         if (c > 1) cout << '\n';
-
         cin >> row;
-        for (int i = 0; i < row.length(); i++)
-            rows[0][i] = row[i];
-        
+        for (int i = 0; i < row.length(); i++) rows[0][i] = row[i];
         N = row.length(), R = 2 * N;
-
-        for (int i = 1; i < R; i++)
-        {
+        for (int i = 1; i < R; i++) {
             cin >> row;
-            for (int j = 0; j < row.length(); j++)
-                rows[i][j] = row[j];
+            for (int j = 0; j < row.length(); j++) rows[i][j] = row[j];
         }
-        
         memset(used, 0, sizeof(used));
-
         bool exist = backtrack(0);
-        
-        if (exist)
-        {
+        if (exist) {
             char grid[20][20];
-
             for (int i = 0; i < N; i++)
                 for (int j = 0; j < N; j++)
                     grid[i][j] = rows[chosen[2 * i]][j];
-
             for (int i = 0; i < N; i++)
-                for (int j = 0; j < N; j++)
-                {
-                    if (grid[j][i] == '2')
-                        grid[j][i] = rows[chosen[2 * i + 1]][j];
-                    if (grid[j][i] == '2')
-                        grid[j][i] = '0';
+                for (int j = 0; j < N; j++) {
+                    if (grid[j][i] == '2') grid[j][i] = rows[chosen[2 * i + 1]][j];
+                    if (grid[j][i] == '2') grid[j][i] = '0';
                 }
-
             cout << "     ";
-            for (int i = 0; i < N - 1; i++)
-                cout << setw(3) << left << (chosen[2 * i + 1] + 1);
+            for (int i = 0; i < N - 1; i++) cout << setw(3) << left << (chosen[2 * i + 1] + 1);
             cout << chosen[2 * (N - 1) + 1] + 1 << '\n';
             cout << '\n';
-
-            for (int i = 0; i < N; i++)
-            {
+            for (int i = 0; i < N; i++) {
                 cout << setw(3) << left << (chosen[2 * i] + 1);
-                for (int j = 0; j < N; j++)
-                    cout << setw(3) << right << grid[i][j];
+                for (int j = 0; j < N; j++) cout << setw(3) << right << grid[i][j];
                 cout << '\n';
             }
-        }
-        else
-        {
-            cout << "impossible\n";
-        }
+        } else cout << "impossible\n";
     }
-    
     return 0;
 }
