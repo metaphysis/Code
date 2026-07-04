ac36831ac7c1e044a03c60110ff558101e738b59
Qiu Qiu <metaphysis@yeah.net>
2026-07-04 09:08:44 +0800

    524 Prime Ring Problem

--- UVa Online Judge/volume005/524 Prime Ring Problem/program.cpp
+++ UVa Online Judge/volume005/524 Prime Ring Problem/program.cpp
@@ -7,7 +7,6 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int ring[20], visited[20], n;
@@ -17,22 +16,15 @@ int primes[32] = {
     0, 1, 0, 0, 0, 0, 0, 1, 0, 1
 };
     
-void backtrack(int depth)
-{
-    if (depth == n - 1)
-    {
-        if (primes[ring[depth] + 1])
-        {
-            for (int i = 0; i < n; i++)
-                cout << (i > 0 ? " " : "") << ring[i];
+void backtrack(int depth) {
+    if (depth == n - 1) {
+        if (primes[ring[depth] + 1]) {
+            for (int i = 0; i < n; i++) cout << (i > 0 ? " " : "") << ring[i];
             cout << '\n';
         }
-    }
-    else
-    {
+    } else {
         for (int i = 2; i <= n; i++)
-            if (!visited[i] && primes[ring[depth] + i])
-            {
+            if (!visited[i] && primes[ring[depth] + i]) {
                 visited[i] = true;
                 ring[depth + 1] = i;
                 backtrack(depth + 1);
@@ -41,21 +33,14 @@ void backtrack(int depth)
     }
 }
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     int cases = 0;
-    while (cin >> n)
-    {
-        if (cases)
-            cout << '\n';
-
+    while (cin >> n) {
+        if (cases) cout << '\n';
         cout << "Case " << ++cases << ":\n";
-
         ring[0] = 1;
         backtrack(0);
     }
-    
-	return 0;
+    return 0;
 }
