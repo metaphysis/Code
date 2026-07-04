63c82eeb04eba28c60fba01ac5d585dd76c783a1
Qiu Qiu <metaphysis@yeah.net>
2026-07-04 09:02:54 +0800

    521 Gossiping

--- UVa Online Judge/volume005/521 Gossiping/program.cpp
+++ UVa Online Judge/volume005/521 Gossiping/program.cpp
@@ -7,11 +7,9 @@
 // 版权所有（C）2017，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-struct driver
-{
+struct driver {
     int line, start;
 };
 
@@ -20,93 +18,67 @@ driver drivers[40];
 bool connected[40][40];
 int n, d, s, si, di;
 
-bool exchanged(int a, int b)
-{
+bool exchanged(int a, int b) {
     if (a == b) return true;
-
     int aline = drivers[a].line, bline = drivers[b].line;
     int total = busLines[aline].size() * busLines[bline].size();
-
     vector<int> s1;
-    while (s1.size() < total)
-    {
+    while (s1.size() < total) {
         for (int i = drivers[a].start; i < busLines[aline].size(); i++)
             s1.push_back(busLines[aline][i]);
         for (int i = 0; i < drivers[a].start; i++)
             s1.push_back(busLines[aline][i]);
     }
-
     vector<int> s2;
-    while (s2.size() < total)
-    {
+    while (s2.size() < total) {
         for (int i = drivers[b].start; i < busLines[bline].size(); i++)
             s2.push_back(busLines[bline][i]);
         for (int i = 0; i < drivers[b].start; i++)
             s2.push_back(busLines[bline][i]);
     }
-
     for (int i = 0; i < total; i++)
         if (s1[i] == s2[i])
             return true;
-
     return false;
 }
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
-
     istringstream iss;
     string line;
-
-    while (getline(cin, line))
-    {
+    while (getline(cin, line)) {
         iss.clear(); iss.str(line);
         iss >> n >> d >> s;
-
         if (n == 0) break;
-
         memset(connected, false, sizeof(connected));
-
-        for (int i = 1; i <= n; i++)
-        {
+        for (int i = 1; i <= n; i++) {
             busLines[i].clear();
-
             getline(cin, line); iss.clear(); iss.str(line);
             while (iss >> si) busLines[i].push_back(si);
-
             getline(cin, line); iss.clear(); iss.str(line);
-            while (iss >> si >> di)
-            {
+            while (iss >> si >> di) {
                 for (int j = 0; j < busLines[i].size(); j++)
-                    if (si == busLines[i][j])
-                    {
+                    if (si == busLines[i][j]) {
                         drivers[di] = driver{i, j};
                         break;
                     }
             }
         }
-
         for (int i = 1; i <= d; i++)
             for (int j = i; j <= d; j++)
                 connected[i][j] = connected[j][i] = exchanged(i, j);
-
         for (int k = 1; k <= d; k++)
             for (int i = 1; i <= d; i++)
                 for (int j = 1; j <= d; j++)
                     if (connected[i][k] && connected[k][j])
                         connected[i][j] = connected[j][i] = true;
-
         bool allConnected = true;
         for (int i = 1; i <= d; i++)
-            if (!connected[1][i])
-            {
+            if (!connected[1][i]) {
                 allConnected = false;
                 break;
             }
-
         cout << (allConnected ? "Yes" : "No") << '\n';
     }
-
     return 0;
 }
