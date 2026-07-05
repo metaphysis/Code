f0ead90a9e878abccb32b5f2327b7986a675e709
Qiu Qiu <metaphysis@yeah.net>
2026-07-05 15:05:37 +0800

    517 Word

--- UVa Online Judge/volume005/517 Word/program.cpp
+++ UVa Online Judge/volume005/517 Word/program.cpp
@@ -7,64 +7,43 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
 const int MAXN = 1 << 16;
-
 int rules[8], steps[MAXN], indexer[MAXN];
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-    
     int n, s;
     string word, rule;
-    
-    while (cin >> n)
-    {
+    while (cin >> n) {
         cin >> word;
-
         int w = 0;
-        for (int i = 0; i < word.length(); i++)
-        {
+        for (int i = 0; i < word.length(); i++) {
             w *= 2;
             if (word[i] == 'b') w++;
         }
-        
-        for (int i = 1; i <= 8; i++)
-        {
+        for (int i = 1; i <= 8; i++) {
             cin >> rule;
-
             int r = 0;
-            for (int j = 0; j < rule.length() - 1; j++)
-            {
+            for (int j = 0; j < rule.length() - 1; j++) {
                 r *= 2;
                 if (rule[j] == 'b') r++;
             }
             rules[r] = rule.back() - 'a';
         }
-
         cin >> s;
-
         memset(steps, -1, sizeof(steps));
-        
-        for (int i = 0; i < s; i++)
-        {
-            if (steps[w] >= 0)
-            {
+        for (int i = 0; i < s; i++) {
+            if (steps[w] >= 0) {
                 int t = (s - i) % (i - steps[w]);
                 w = indexer[steps[w] + t];
                 break;
-            }
-            else
-            {
+            } else {
                 steps[w] = i;
                 indexer[i] = w;
-                
                 int ww = 0;
-                for (int j = 0; j < n; j++)
-                {
+                for (int j = 0; j < n; j++) {
                     int k = 0;
                     if ((1 << (j + 2) % n) & w) k += 4;
                     if ((1 << j) & w) k += 2;
@@ -74,22 +53,17 @@ int main(int argc, char *argv[])
                 w = ww;
             }
         }
-        
         word = bitset<16>(w).to_string().substr(16 - n);
-
         set<string> words;
         string wordword = word + word;
         for (int i = 0; i < word.length(); i++)
             words.insert(wordword.substr(i, word.length()));
-
         word = *(words.begin());
-        for (int i = 0; i < word.length(); i++)
-        {
+        for (int i = 0; i < word.length(); i++) {
             if (word[i] == '0') cout << 'a';
             else cout << 'b';
         }
         cout << '\n';
     }
-    
-	return 0;
+    return 0;
 }
