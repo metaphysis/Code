e6cef113f059e70e839f0ca41a3eb6c1ce01e27e
Qiu Qiu <metaphysis@yeah.net>
2026-07-05 15:02:16 +0800

    515 King

--- UVa Online Judge/volume005/515 King/program.cpp
+++ UVa Online Judge/volume005/515 King/program.cpp
@@ -6,15 +6,11 @@
 //
 // 版权所有（C）2017，邱秋。metaphysis # yeah dot net
 //
-// System of difference constraints, Bellman-Ford algorithm.
-//
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-struct edge
-{
+struct edge {
     int u, v, weight;
 };
 
@@ -24,48 +20,30 @@ string oi;
 
 void addEdge(int u, int v, int weight) { edges[nedges++] = edge{ u, v, weight }; }
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
-
-    while (cin >> n, n > 0)
-    {
+    while (cin >> n, n > 0) {
         cin >> m;
-
         nedges = 0;
-        for (int i = 1; i <= m; i++)
-        {
+        for (int i = 1; i <= m; i++) {
             cin >> si >> ni >> oi >> ki;
-
-            if (oi.front() == 'g')
-                addEdge(si + ni + 1, si, -ki - 1);
-            else
-                addEdge(si, si + ni + 1, ki - 1);
+            if (oi.front() == 'g') addEdge(si + ni + 1, si, -ki - 1);
+            else addEdge(si, si + ni + 1, ki - 1);
         }
-
         n += 2;
-        for (int i = 1; i < n; i++)
-            addEdge(0, i, 0);
-
+        for (int i = 1; i < n; i++) addEdge(0, i, 0);
         dist[0] = 0;
-        for (int i = 1; i < n; i++)
-            dist[i] = (1 << 30);
-
+        for (int i = 1; i < n; i++) dist[i] = (1 << 30);
         int iterations = 0, updated = 0;
-        do
-        {
+        do {
             updated = 0;
-            for (int i = 0; i < nedges; i++)
-            {
+            for (int i = 0; i < nedges; i++) {
                 int weight = dist[edges[i].u] + edges[i].weight;
                 if (dist[edges[i].v] > weight)
                     updated++, dist[edges[i].v] = weight;
             }
-        }
-        while (updated && iterations++ < n);
-
+        } while (updated && iterations++ < n);
         cout << (updated ? "successful conspiracy\n" : "lamentable kingdom\n");
     }
-
     return 0;
 }
