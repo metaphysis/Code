2280aa87b45bb6d7304415ecd9a29609c4f6db9c
Qiu Qiu <metaphysis@yeah.net>
2026-07-06 08:19:31 +0800

    630 Anagrams (II)

--- UVa Online Judge/volume006/630 Anagrams (II)/program.cpp
+++ UVa Online Judge/volume006/630 Anagrams (II)/program.cpp
@@ -7,50 +7,35 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     string words[1100], sorted[1100];
     int word_count = 0, cases = 0;
-    
     cin >> cases;
-    
-    for (int c = 1; c <= cases; c++)
-    {
-        if (c > 1)
-            cout << '\n';
-            
+    for (int c = 1; c <= cases; c++) {
+        if (c > 1) cout << '\n';
         cin >> word_count;
-        for (int i = 0; i < word_count; i++)
-        {
+        for (int i = 0; i < word_count; i++) {
             cin >> words[i];
             sorted[i] = words[i];
             sort(sorted[i].begin(), sorted[i].end());
         }
-        
         string word, backup;
-        while (cin >> word, word != "END")
-        {
+        while (cin >> word, word != "END") {
             backup = word;
             sort(word.begin(), word.end());
             cout << "Anagrams for: " << backup << '\n';
             int count = 1;
             for (int i = 0; i < word_count; i++)
-                if (word == sorted[i])
-                {
+                if (word == sorted[i]) {
                     cout << setw(3) << right << count;
                     cout << ") " << words[i] << '\n';
                     count++;
                 }
-            if (count == 1)
-                cout << "No anagrams for: " << backup << '\n';
+            if (count == 1) cout << "No anagrams for: " << backup << '\n';
         }
-            
     }
-    
-	return 0;
+    return 0;
 }
