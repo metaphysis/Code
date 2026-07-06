e3c83d2f5694f26144bda1a180907f9498c763a7
Qiu Qiu <metaphysis@yeah.net>
2026-07-06 08:52:23 +0800

    503 Parallelepiped Walk

--- UVa Online Judge/volume005/503 Parallelepiped Walk/program.cpp
+++ UVa Online Judge/volume005/503 Parallelepiped Walk/program.cpp
@@ -5,54 +5,34 @@
 // UVa Run Time: 0.000s
 //
 // 版权所有（C）2017，邱秋。metaphysis # yeah dot net
-//
-// The solutions below is based on the standard solution which written by Pascal.
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int curve;
 
-void rotate(int i, int j, int x, int y, int z, int x0, int y0, int L, int W, int H)
-{
-    if (z == 0)
-        curve = min(curve, x * x + y * y);
-    else
-    {
-        if ((i >= 0) && (i < 2))
-            rotate(i + 1, j, x0 + L + z, y, x0 + L - x, x0 + L, y0, H, W, L);
-        if ((j >= 0) && (j < 2))
-            rotate(i, j + 1, x, y0 + W + z, y0 + W - y, x0, y0 + W, L, H, W);
-        if ((i <= 0) && (i > -2))
-            rotate(i - 1, j, x0 - z, y, x - x0, x0 - H, y0, H, W, L);
-        if ((j <= 0) && (j > -2))
-            rotate(i, j - 1, x, y0 - z, y - y0, x0, y0 - H, L, H, W);
+void rotate(int i, int j, int x, int y, int z, int x0, int y0, int L, int W, int H) {
+    if (z == 0) curve = min(curve, x * x + y * y);
+    else {
+        if ((i >= 0) && (i < 2)) rotate(i + 1, j, x0 + L + z, y, x0 + L - x, x0 + L, y0, H, W, L);
+        if ((j >= 0) && (j < 2)) rotate(i, j + 1, x, y0 + W + z, y0 + W - y, x0, y0 + W, L, H, W);
+        if ((i <= 0) && (i > -2)) rotate(i - 1, j, x0 - z, y, x - x0, x0 - H, y0, H, W, L);
+        if ((j <= 0) && (j > -2)) rotate(i, j - 1, x, y0 - z, y - y0, x0, y0 - H, L, H, W);
     }
 }
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
-
     int L, W, H, x1, y1, z1, x2, y2, z2;
-
-    while (cin >> L >> W >> H >> x1 >> y1 >> z1 >> x2 >> y2 >> z2)
-    {
-        if (z1 != 0 && z1 != H)
-        {
-            if (y1 == 0 || y1 == W)
-                swap(y1, z1), swap(y2, z2), swap(W, H);
-            else
-                swap(x1, z1), swap(x2, z2), swap(L, H);
+    while (cin >> L >> W >> H >> x1 >> y1 >> z1 >> x2 >> y2 >> z2) {
+        if (z1 != 0 && z1 != H) {
+            if (y1 == 0 || y1 == W) swap(y1, z1), swap(y2, z2), swap(W, H);
+            else swap(x1, z1), swap(x2, z2), swap(L, H);
         }
-        if (z1 == H)
-            z1 = 0, z2 = H - z2;
-
+        if (z1 == H) z1 = 0, z2 = H - z2;
         curve = numeric_limits<int>::max();
         rotate(0, 0, x2 - x1, y2 - y1, z2, -x1, -y1, L, W, H);
         cout << curve << '\n';
     }
-
     return 0;
 }
