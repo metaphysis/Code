73cd1c1575765f83dee33bdfb038dfcde5edacff
Qiu Qiu <metaphysis@yeah.net>
2026-07-06 08:48:14 +0800

    501 Black Box

--- UVa Online Judge/volume005/501 Black Box/program.cpp
+++ UVa Online Judge/volume005/501 Black Box/program.cpp
@@ -7,33 +7,22 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-    
     long long int K, M, N, number[30001];
     cin >> K;
-    
-    for (int cases = 1; cases <= K; cases++)
-    {
+    for (int cases = 1; cases <= K; cases++) {
         if (cases > 1) cout << '\n';
-        
         cin >> M >> N;
-        
-        for (int i = 0; i < M; i++)
-            cin >> number[i];
-        
+        for (int i = 0; i < M; i++) cin >> number[i];
         int u;    
-        for (int i = 0; i < N; i++)
-        {
+        for (int i = 0; i < N; i++) {
             cin >> u;
             nth_element(number, number + i, number + u);
             cout << number[i] << '\n';
         }
     }
-    
     return 0;
 }
