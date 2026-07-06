c1da5a1ee5f150442ee888f935fdd21291a56ac4
Qiu Qiu <metaphysis@yeah.net>
2026-07-06 09:01:03 +0800

    507 Jill Rides Again

--- UVa Online Judge/volume005/507 Jill Rides Again/program.cpp
+++ UVa Online Judge/volume005/507 Jill Rides Again/program.cpp
@@ -7,53 +7,35 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     int cases;
     cin >> cases;
-    
-    for (int r = 1; r <= cases; r++)
-    {
+    for (int r = 1; r <= cases; r++) {
         int s, niceness;
         cin >> s >> niceness;
-        
         int max = niceness, start = 1, end = 2;
         int maxCurrent = niceness, startCurrent = 1, endCurrent = 2;
-        
         s--;
-        for (int i = 2; i <= s; i++)
-        {
+        for (int i = 2; i <= s; i++) {
             cin >> niceness;
-
             endCurrent = i + 1;
             maxCurrent += niceness;
-            
-            if (niceness > maxCurrent)
-            {
+            if (niceness > maxCurrent) {
                 startCurrent = i, endCurrent = i + 1;
                 maxCurrent = niceness;
             }
-
-            if (maxCurrent > max || (maxCurrent == max && abs(endCurrent - startCurrent) > abs(end - start)))
-            {
+            if (maxCurrent > max || (maxCurrent == max && abs(endCurrent - startCurrent) > abs(end - start))) {
                 max = maxCurrent;
                 start = startCurrent, end = endCurrent;
             }
         }
-        
-        if (max > 0)
-        {
+        if (max > 0) {
             cout << "The nicest part of route " << r << " is between stops ";
             cout << start << " and " << end << '\n';
-        }
-        else
-            cout << "Route " << r << " has no nice parts\n";
+        } else cout << "Route " << r << " has no nice parts\n";
     }
-    
-	return 0;
+    return 0;
 }
