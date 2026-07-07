8a19ade45b8d5fca54f6687a825e790023e2f8bc
Qiu Qiu <metaphysis@yeah.net>
2026-07-07 09:46:10 +0800

    632 Compression (II)

--- UVa Online Judge/volume006/632 Compression (II)/program.cpp
+++ UVa Online Judge/volume006/632 Compression (II)/program.cpp
@@ -7,82 +7,54 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-struct cell
-{
+struct cell {
     int label;
     string s;
-    
-    bool operator<(const cell& another) const
-    {
+    bool operator<(const cell& another) const {
         return s < another.s;
     }
 };
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     int datasets;
     cin >> datasets;
-    
     string line;
-    for (int d = 1; d <= datasets; d++)
-    {
-        if (d > 1)
-            cout << '\n';
-
+    for (int d = 1; d <= datasets; d++) {
+        if (d > 1) cout << '\n';
         int length;
         cin >> length;
-        
         cin.ignore(1024, '\n');
-
         string word;
-        while (true)
-        {
+        while (true) {
             getline(cin, line);
             word += line;
-            if (word.length() >= length)
-                break;
+            if (word.length() >= length) break;
         }
-
         string s0 = word, s1 = word;
         s1.push_back(s1.front());
         s1.erase(s1.begin());
-        
         vector<cell> words;
-        for (int i = 0; i < length; i++)
-        {
-            //cout << word << '\n';
+        for (int i = 0; i < length; i++) {
             words.push_back((cell){i, word});
             word.push_back(word.front());
             word.erase(word.begin());
         }
-
         stable_sort(words.begin(), words.end());
-
         string created;
         int indexer = -1;
-        for (int i = 0; i < length; i++)
-        {
+        for (int i = 0; i < length; i++) {
             created += words[i].s.back();
-            if (indexer == -1 && words[i].label == 1)
-                indexer = i;
+            if (indexer == -1 && words[i].label == 1) indexer = i;
         }
-
         cout << indexer << '\n';
-        
-        while (created.length() > 50)
-        {
+        while (created.length() > 50) {
             cout << created.substr(0, 50) << '\n';
             created = created.substr(50);
         }
-        
-        if (created.length() > 0)
-            cout << created << '\n';
+        if (created.length() > 0) cout << created << '\n';
     }
-    
-	return 0;
+    return 0;
 }
