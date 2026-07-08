e5f247952042dd7ba4c4edc53628782d3c3910a0
Qiu Qiu <metaphysis@yeah.net>
2026-07-08 14:46:25 +0800

    490 Rotating Sentences

--- UVa Online Judge/volume004/490 Rotating Sentences/program.cpp
+++ UVa Online Judge/volume004/490 Rotating Sentences/program.cpp
@@ -7,34 +7,25 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main(int argc, char *argv[])
-{
-    ios::sync_with_stdio(false);
-
+int main() {
+    cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
     int max_length = 0;
     vector<string> sentences;
     string line;
-    while (getline(cin, line))
-    {
+    while (getline(cin, line)) {
         for (int i = line.length() - 1; i >= 0; i--)
             if (line[i] == '\t')
                 line.erase(line.begin() + i);
         max_length = max(max_length, (int)line.length());        
         sentences.push_back(line);
     }
-    
-    for (int i = 0; i < max_length; i++)
-    {
+    for (int i = 0; i < max_length; i++) {
         for (int j = sentences.size() - 1; j >= 0; j--)
-            if (i < sentences[j].length())
-                cout << sentences[j][i];
-            else
-                cout << ' ';
+            if (i < sentences[j].length()) cout << sentences[j][i];
+            else cout << ' ';
         cout << endl;
     }
-    
-	return 0;
+    return 0;
 }
