d25c71b81456bd8a3195b3ed0b84e081346a4aff
Qiu Qiu <metaphysis@yeah.net>
2026-07-08 15:03:25 +0800

    497 Strategic Defense Initiative

--- UVa Online Judge/volume004/497 Strategic Defense Initiative/program.cpp
+++ UVa Online Judge/volume004/497 Strategic Defense Initiative/program.cpp
@@ -7,74 +7,51 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
 vector<int> numbers, parent, indexer;
 
-int lis()
-{
+int lis() {
     parent.clear();
     indexer.clear();
-    
     parent.resize(numbers.size());
-    
     vector<int> m;
     m.push_back(numbers.front());
     indexer.push_back(0);
     parent[0] = -1;
-    
-    for (int i = 1; i < numbers.size(); i++)
-    { 
-        if (numbers[i] > m.back())
-        {
+    for (int i = 1; i < numbers.size(); i++) { 
+        if (numbers[i] > m.back()) {
             m.push_back(numbers[i]);
             parent[i] = indexer.back();
             indexer.push_back(i);
-        }
-        else
-        {
+        } else {
             int n = lower_bound(m.begin(), m.end(), numbers[i]) - m.begin();
             m[n] = numbers[i];
-            if (n == 0)
-                parent[i] = -1;
-            else
-                parent[i] = indexer[n - 1];
-            
+            if (n == 0) parent[i] = -1;
+            else parent[i] = indexer[n - 1];
             indexer[n] = i;
         }
     }
-    
     return m.size();
 }
 
-void findPath(int i)
-{
-    if (parent[i] != -1)
-        findPath(parent[i]);
+void findPath(int i) {
+    if (parent[i] != -1)         findPath(parent[i]);
     cout << numbers[i] << '\n';
 }
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     string line;
     getline(cin, line);
     int cases = stoi(line);
-    
     getline(cin, line);
-    for (int i = 1; i <= cases; i++)
-    {
+    for (int i = 1; i <= cases; i++) {
         if (i > 1) cout << endl;
-        
         numbers.clear();
-        while (getline(cin, line), line.length() > 0)
-            numbers.push_back(stoi(line));
-        
+        while (getline(cin, line), line.length() > 0) numbers.push_back(stoi(line));
         cout << "Max hits: " << lis() << '\n';
         findPath(indexer.back());
     }
-    
-	return 0;
+    return 0;
 }
