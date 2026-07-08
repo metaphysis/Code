07f67c0db03f5df0651669d338c1a0f0bed62bca
Qiu Qiu <metaphysis@yeah.net>
2026-07-08 15:01:39 +0800

    496 Simply Subsets

--- UVa Online Judge/volume004/496 Simply Subsets/program.cpp
+++ UVa Online Judge/volume004/496 Simply Subsets/program.cpp
@@ -7,54 +7,28 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     string line;
-    while (getline(cin, line))
-    {
+    while (getline(cin, line)) {
         set<int> A, B;
-        
         int number;
         istringstream iss(line);
-        while (iss >> number)
-            A.insert(number);
-        
+        while (iss >> number) A.insert(number);
         getline(cin, line);
         iss.clear(); iss.str(line);
-        while (iss >> number)
-            B.insert(number);
-        
+        while (iss >> number) B.insert(number);
         set<int> shared;
         for (auto a : A)
-        {
             if (B.find(a) != B.end())
                 shared.insert(a);
-        }
-        
-        if (shared.size() == A.size() && shared.size() == B.size())
-        {
-            cout << "A equals B\n";
-        }
-        else if (shared.size() == 0)
-        {
-            cout << "A and B are disjoint\n";
-        }
-        else if (shared.size() == A.size())
-        {
-            cout << "A is a proper subset of B\n";
-        }
-        else if (shared.size() == B.size())
-        {
-            cout << "B is a proper subset of A\n";
-        }
-        else
-            cout << "I'm confused!\n";
+        if (shared.size() == A.size() && shared.size() == B.size()) cout << "A equals B\n";
+        else if (shared.size() == 0) cout << "A and B are disjoint\n";
+        else if (shared.size() == A.size()) cout << "A is a proper subset of B\n";
+        else if (shared.size() == B.size()) cout << "B is a proper subset of A\n";
+        else cout << "I'm confused!\n";
     }
-    
-	return 0;
+    return 0;
 }
