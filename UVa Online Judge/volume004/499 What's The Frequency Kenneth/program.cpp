9f0e23432687e5f908957dd9f97583694e52d639
Qiu Qiu <metaphysis@yeah.net>
2026-07-08 15:06:17 +0800

    499 What's The Frequency Kenneth

--- UVa Online Judge/volume004/499 What's The Frequency Kenneth/program.cpp
+++ UVa Online Judge/volume004/499 What's The Frequency Kenneth/program.cpp
@@ -1,4 +1,4 @@
-// What's The Frequency, Kenneth?
+// What's The Frequency Kenneth
 // UVa ID: 499
 // Verdict: Accepted
 // Submission Date: 2016-07-13
@@ -7,29 +7,22 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main() {
     ios::sync_with_stdio(false);
-
     string line;
-    while (getline(cin, line))
-    {
+    while (getline(cin, line)) {
         vector<int> counter(52);
         fill(counter.begin(), counter.end(), 0);
         for (int i = 0; i < line.length(); i++)
-            if (islower(line[i]))
-                counter[line[i] - 'a' + 26]++;
-            else if (isupper(line[i]))
-                counter[line[i] - 'A']++;
+            if (islower(line[i])) counter[line[i] - 'a' + 26]++;
+            else if (isupper(line[i])) counter[line[i] - 'A']++;
         int max_frequency = *max_element(counter.begin(), counter.end());
         for (int i = 0; i < counter.size(); i++)
             if (counter[i] == max_frequency)
                 cout << (char)((i < 26 ? 'A' : 'a') + (i < 26 ? i : (i - 26)));
         cout << " " << max_frequency << endl;
     }
-    
-	return 0;
+    return 0;
 }
