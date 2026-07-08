054443322ca808a0807346e7a8ae1bf793c5674c
Qiu Qiu <metaphysis@yeah.net>
2026-07-08 14:32:08 +0800

    636 Squares (III)

--- UVa Online Judge/volume006/636 Squares(III)/program.cpp
+++ UVa Online Judge/volume006/636 Squares(III)/program.cpp
@@ -7,37 +7,28 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     string line;
-    while (getline(cin, line), line != "0")
-    {
+    while (getline(cin, line), line != "0") {
         int min_base = 2;
-        for (int i = 0; i < line.length(); i++)
-        {
+        for (int i = 0; i < line.length(); i++) {
             int digit = line[i] - '0';
             if (digit >= min_base)
                 min_base = digit + 1;
         }
-
-        for (int b = min_base; b < 100; b++)
-        {
+        for (int b = min_base; b < 100; b++) {
             long long int n = 0;
             for (int i = 0; i < line.length(); i++)
                 n = n * b + line[i] - '0';
             long long int root = sqrt(n);
-            if (root * root == n || (root - 1) * (root - 1) == n || (root + 1) * (root + 1) == n)
-            {
+            if (root * root == n || (root - 1) * (root - 1) == n || (root + 1) * (root + 1) == n) {
                 cout << b << '\n';
                 break;
             }
         }
     }
-    
-	return 0;
+    return 0;
 }
