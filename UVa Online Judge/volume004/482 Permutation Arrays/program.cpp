905fbf5d54f0c9ecec8af6d86926bdaf340a8e33
Qiu Qiu <metaphysis@yeah.net>
2026-07-09 09:28:27 +0800

    482 Permutation Arrays

--- UVa Online Judge/volume004/482 Permutation Arrays/program.cpp
+++ UVa Online Judge/volume004/482 Permutation Arrays/program.cpp
@@ -7,46 +7,30 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     string line;
-    
     getline(cin, line);
     int cases = stoi(line);
-
-    for (int i = 1; i <= cases; i++)
-    {
+    for (int i = 1; i <= cases; i++) {
         if (i > 1) cout << endl;
         vector<int> order;
         vector<string> number;
-        
         getline(cin, line);
         getline(cin, line);
         istringstream iss(line);
-        
         int index;
-        while (iss >> index)
-            order.push_back(index);
-            
+        while (iss >> index) order.push_back(index);
         getline(cin, line);
         iss.clear();
         iss.str(line);
         string block;
-        while (iss >> block)
-            number.push_back(block);
-            
+        while (iss >> block) number.push_back(block);
         vector<int> output(order.size());
-        for (int j = 0; j < order.size(); j++)
-            output[order[j] - 1] = j;
-            
-        for (int j = 0; j < output.size(); j++)
-            cout << number[output[j]] << '\n';
+        for (int j = 0; j < order.size(); j++) output[order[j] - 1] = j;
+        for (int j = 0; j < output.size(); j++) cout << number[output[j]] << '\n';
     }
-    
-	return 0;
+    return 0;
 }
