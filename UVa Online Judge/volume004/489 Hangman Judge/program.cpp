de4dfc4e21c6d1fdd812e659d1c1054163f9a341
Qiu Qiu <metaphysis@yeah.net>
2026-07-09 09:38:56 +0800

    489 Hangman Judge

--- UVa Online Judge/volume004/489 Hangman Judge/program.cpp
+++ UVa Online Judge/volume004/489 Hangman Judge/program.cpp
@@ -7,54 +7,38 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main() {
     ios::sync_with_stdio(false);
-
     int round;
     string solution, guess;
-    while (cin >> round, round != -1)
-    {
+    while (cin >> round, round != -1) {
         cout << "Round " << round << endl;
         cin >> solution >> guess;
-        
         bool flag = false;
         set<char> right, wrong, all;
-        for (int i = 0; i < solution.length(); i++)
-            all.insert(solution[i]);
-        
-        for (int i = 0; i < guess.length(); i++)
-        {
+        for (int i = 0; i < solution.length(); i++) all.insert(solution[i]);
+        for (int i = 0; i < guess.length(); i++) {
             if (right.find(guess[i]) != right.end()) continue;
-            
-            if (all.find(guess[i]) == all.end())
-            {
+            if (all.find(guess[i]) == all.end()) {
                 wrong.insert(guess[i]);
-                if (wrong.size() >= 7)
-                {
+                if (wrong.size() >= 7) {
                     cout << "You lose." << endl;
                     flag = true;
                     break;
                 }
-            }
-            else
-            {
+            } else {
                 right.insert(guess[i]);
                 all.erase(guess[i]);
-                if (all.size() == 0)
-                {
+                if (all.size() == 0) {
                     cout << "You win." << endl;
                     flag = true;
                     break;
                 }
             }
         }
-        
         if (!flag) cout << "You chickened out." << endl;
     }
-    
-	return 0;
+    return 0;
 }
