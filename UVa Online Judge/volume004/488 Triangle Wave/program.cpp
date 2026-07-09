56d2d797eac305f0186d69d440fc996ce5c26dbd
Qiu Qiu <metaphysis@yeah.net>
2026-07-09 09:40:07 +0800

    488 Triangle Wave

--- UVa Online Judge/volume004/488 Triangle Wave/program.cpp
+++ UVa Online Judge/volume004/488 Triangle Wave/program.cpp
@@ -7,34 +7,22 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main() {
     ios::sync_with_stdio(false);
-
     int amplitude, frequency, cases;
     cin >> cases;
-    for (int i = 1; i <= cases; i++)
-    {
+    for (int i = 1; i <= cases; i++) {
         cin >> amplitude >> frequency;
-        
         // it is a trick.
         if (frequency == 0) continue;
-        
         if (i > 1) cout << endl;
-        for (int j = 1; j <= frequency; j++)
-        {
+        for (int j = 1; j <= frequency; j++) {
             if (j > 1) cout << endl;
-
-            for (int k = 1; k <= amplitude; k++)
-                cout << string(k, '0' + k) << endl;
-            
-            for (int k = amplitude - 1; k >= 1; k--)
-                cout << string(k, '0' + k) << endl;
+            for (int k = 1; k <= amplitude; k++) cout << string(k, '0' + k) << endl;
+            for (int k = amplitude - 1; k >= 1; k--) cout << string(k, '0' + k) << endl;
         }
     }
-    
-	return 0;
+    return 0;
 }
