0df1e00ba8b711431fe2efec61373cf3473ecc60
Qiu Qiu <metaphysis@yeah.net>
2026-07-09 08:29:55 +0800

    642 Word Amalgamation

--- UVa Online Judge/volume006/642 Word Amalgamation/program.cpp
+++ UVa Online Judge/volume006/642 Word Amalgamation/program.cpp
@@ -7,46 +7,35 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-struct item
-{
+struct item {
     string word, sorted;
-    bool operator<(const item &another) const
-    {
+    bool operator<(const item &another) const {
         return word < another.word;
     }
 };
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     vector<item> items;
     string line;
-    while (getline(cin, line), line.front() != 'X')
-    {
+    while (getline(cin, line), line.front() != 'X') {
         string backup = line;
         sort(line.begin(), line.end());
         items.push_back((item){backup, line});
     }
     sort(items.begin(), items.end());
-    
-    while (getline(cin, line), line.front() != 'X')
-    {
+    while (getline(cin, line), line.front() != 'X') {
         sort(line.begin(), line.end());
         bool outputed = false;
         for (auto data : items)
-            if (data.sorted == line)
-            {
+            if (data.sorted == line) {
                 outputed = true;
                 cout << data.word << '\n';
             }
-        if (!outputed)
-            cout << "NOT A VALID WORD\n";
+        if (!outputed) cout << "NOT A VALID WORD\n";
         cout << "******\n";
     }
-    
-	return 0;
+    return 0;
 }
