5ac52679a178d423980030c57f210c4802b13f32
Qiu Qiu <metaphysis@yeah.net>
2026-07-09 09:26:27 +0800

    481 What Goes Up

--- UVa Online Judge/volume004/481 What Goes Up/program.cpp
+++ UVa Online Judge/volume004/481 What Goes Up/program.cpp
@@ -7,56 +7,41 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
 vector<int> numbers, m;
 vector<vector<int>> sequences;
 
-int lis()
-{
+int lis(){
     m.push_back(numbers.front());
     vector<int> sequence;
     sequence.push_back(numbers.front());
     sequences.push_back(sequence);
-    
-    for (int i = 1; i < numbers.size(); i++)
-    { 
-        if (numbers[i] > m.back())
-        {
+    for (int i = 1; i < numbers.size(); i++) { 
+        if (numbers[i] > m.back()) {
             m.push_back(numbers[i]);
             vector<int> next(sequences.back());
             next.push_back(numbers[i]);
             sequences.push_back(next);
-        }
-        else
-        {
+        } else  {
             int n = lower_bound(m.begin(), m.end(), numbers[i]) - m.begin();
             m[n] = numbers[i];
-            if (n == 0)
-                sequences[n].back() = numbers[i];
-            else
-            {
+            if (n == 0) sequences[n].back() = numbers[i];
+            else {
                 sequences[n].assign(sequences[n- 1].begin(), sequences[n - 1].end());
                 sequences[n].push_back(numbers[i]);
             }
         }
     }
-    
     return m.size();
 }
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     int number;
     while (cin >> number) numbers.push_back(number);
-    
     cout << lis() << '\n';
     cout << '-' << '\n';
-    for (auto n : sequences.back())
-        cout << n << '\n';
-    
+    for (auto n : sequences.back()) cout << n << '\n';
 	return 0;
 }
