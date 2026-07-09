98eb81f45c487a6d5fa455c6cb8c5db1b7c6d17d
Qiu Qiu <metaphysis@yeah.net>
2026-07-09 08:19:56 +0800

    640 Selft Numbers

--- UVa Online Judge/volume006/640 Self Numbers/program.cpp
+++ UVa Online Judge/volume006/640 Self Numbers/program.cpp
@@ -7,45 +7,30 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int number[1000001] = { 0 };
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     number[1] = 0;
-    for (int i = 1; i <= 1000000; i++)
-    {
-        if (number[i] == 0)
-        {
+    for (int i = 1; i <= 1000000; i++) {
+        if (number[i] == 0) {
             cout << i << '\n';
-
             int start = i;
-            while (start < 1000000)
-            {
+            while (start < 1000000) {
                 int next = start;
-                while (start > 0)
-                {
+                while (start > 0) {
                     next += start % 10;
                     start /= 10;
                 }
-                
-                if (next <= 1000000)
-                {
-                    if (number[next] == 1)
-                        break;
-
+                if (next <= 1000000) {
+                    if (number[next] == 1) break;
                     number[next] = 1;
                     start = next;
-                }
-                else
-                    break;
+                } else break;
             }
         }
     }
-    
-	return 0;
+    return 0;
 }
