36ea45f758d5d5e396e32712311f3bd6be2baa17
Qiu Qiu <metaphysis@yeah.net>
2026-07-10 08:52:40 +0800

    474 Heads Tails Probability

--- UVa Online Judge/volume004/474 Heads Tails Probability/program.cpp
+++ UVa Online Judge/volume004/474 Heads Tails Probability/program.cpp
@@ -1,4 +1,4 @@
-// Heads / Tails Probability
+// Heads Tails Probability
 // UVa ID: 474
 // Verdict: Accepted
 // Submission Date: 2016-07-17
@@ -7,31 +7,23 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     int n;
-    while (cin >> n)
-    {
+    while (cin >> n) {
         double expoent = log10(2) * n;
         double rounded = ceil(expoent);
         double digits = pow(10, rounded - expoent);
-        
         // bug, need to fix
-        if (n == 6)
-        {
+        if (n == 6) {
             cout << "2^-6 = 1.562e-2\n";
             continue;
         }
-        
         cout << "2^-" << n << " = ";
         cout << fixed << setprecision(3) << digits;
         cout << "e-" << (int)rounded << '\n';
     }
-    
-	return 0;
+    return 0;
 }
