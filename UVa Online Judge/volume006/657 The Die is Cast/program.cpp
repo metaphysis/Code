0a8d6b8344bee4f88172b154006af88bdf1f49bb
Qiu Qiu <metaphysis@yeah.net>
2026-07-12 11:30:50 +0800

    657 The Die is Cast

--- UVa Online Judge/volume006/657 The Die is Cast/program.cpp
+++ UVa Online Judge/volume006/657 The Die is Cast/program.cpp
@@ -1,4 +1,4 @@
-// The die is cast
+// The Die is Cast
 // UVa ID: 657
 // Verdict: Accepted
 // Submission Date: 2016-08-27
@@ -7,23 +7,18 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
 char grid[60][60];
 int w, h;
 
-void label(int i, int j)
-{
-    if (i >= 0 && i < h && j >= 0 && j < w)
-    {
-        if (grid[i][j] == '*' || grid[i][j] == 'X')
-        {
+void label(int i, int j) {
+    if (i >= 0 && i < h && j >= 0 && j < w) {
+        if (grid[i][j] == '*' || grid[i][j] == 'X') {
             if (grid[i][j] == '*')
                 grid[i][j] = '.';
             else
                 grid[i][j] = '1';
-                
             label(i + 1, j);
             label(i - 1, j);
             label(i, j + 1);
@@ -32,10 +27,8 @@ void label(int i, int j)
     }
 }
 
-void cover(int i, int j)
-{
-    if (i >= 0 && i < h && j >= 0 && j < w && grid[i][j] == '1')
-    {
+void cover(int i, int j) {
+    if (i >= 0 && i < h && j >= 0 && j < w && grid[i][j] == '1') {
         grid[i][j] = '.';
         cover(i + 1, j);
         cover(i - 1, j);
@@ -44,10 +37,8 @@ void cover(int i, int j)
     }
 }
 
-void flip(int i, int j)
-{
-    if (i >= 0 && i < h && j >= 0 && j < w && grid[i][j] == 'X')
-    {
+void flip(int i, int j) {
+    if (i >= 0 && i < h && j >= 0 && j < w && grid[i][j] == 'X') {
         grid[i][j] = '.';
         flip(i + 1, j);
         flip(i - 1, j);
@@ -56,58 +47,40 @@ void flip(int i, int j)
     }
 }
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     int throws = 0;
-    while (cin >> w >> h, w)
-    {
+    while (cin >> w >> h, w) {
         for (int i = 0; i < h; i++)
             for (int j = 0; j < w; j++)
                 cin >> grid[i][j];
-        
         vector<int> dices;
         for (int i = 0; i < h; i++)
             for (int j = 0; j < w; j++)
-                if (grid[i][j] == '*')
-                {
+                if (grid[i][j] == '*') {
                     label(i, j);
-                    
                     int dice = 0;
                     for (int x = 0; x < h; x++)
                         for (int y = 0; y < w; y++)
-                            if (grid[x][y] == '1')
-                            {
+                            if (grid[x][y] == '1') {
                                 dice++;
                                 cover(x, y);
                             }
-                            
                     dices.push_back(dice);
-                                
                 }
-        
         for (int i = 0; i < h; i++)
             for (int j = 0; j < w; j++)
-                if (grid[i][j] == 'X')
-                {
+                if (grid[i][j] == 'X') {
                     dices.push_back(1);
                     flip(i, j);
                 }
-                
         sort(dices.begin(), dices.end());
-        
         cout << "Throw " << ++throws << '\n';
-        
-        for (int i = 0; i < dices.size(); i++)
-        {
-            if (i > 0)
-                cout << ' ';
+        for (int i = 0; i < dices.size(); i++) {
+            if (i > 0) cout << ' ';
             cout << dices[i];
         }
         cout << "\n\n";
-                
     }
-    
-	return 0;
+    return 0;
 }
