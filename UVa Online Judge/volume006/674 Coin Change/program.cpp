4fee0b81d9f0d3bb576f62b583f95041dd4e9bc9
Qiu Qiu <metaphysis@yeah.net>
2026-07-14 10:06:03 +0800

    674 Coin Change

--- UVa Online Judge/volume006/674 Coin Change/program.cpp
+++ UVa Online Judge/volume006/674 Coin Change/program.cpp
@@ -7,30 +7,20 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     int coins[6] = {0, 1, 5, 10, 25, 50};
     long long int ways[6][7500];
-
     memset(ways, 0, sizeof(ways));
     ways[0][0] = 1;
-    
     for (int i = 1; i <= 5; i++)
-        for (int j = 0; j <= 7489; j++)
-        {
+        for (int j = 0; j <= 7489; j++) {
             ways[i][j] = ways[i - 1][j];
-            if (j - coins[i] >= 0)
-                ways[i][j] += ways[i][j - coins[i]];
+            if (j - coins[i] >= 0) ways[i][j] += ways[i][j - coins[i]];
         }
-    
-    int cents;   
-    while (cin >> cents)
-        cout << ways[5][cents] << '\n';
-    
-	return 0;
+    int cents;
+    while (cin >> cents) cout << ways[5][cents] << '\n';
+    return 0;
 }
