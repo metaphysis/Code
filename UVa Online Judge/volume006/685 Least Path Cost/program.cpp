f2b185616189a7bc5600e35e9e46079fea2926aa
Qiu Qiu <metaphysis@yeah.net>
2026-07-14 13:23:25 +0800

    685 Least Path Cost

--- UVa Online Judge/volume006/685 Least Path Cost/program.cpp
+++ UVa Online Judge/volume006/685 Least Path Cost/program.cpp
@@ -7,11 +7,9 @@
 // 版权所有（C）2017，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-struct edge
-{
+struct edge {
     int start, end, height;
 };
 
@@ -20,61 +18,42 @@ vector<vector<edge>> edges(410, vector<edge>());
 vector<int> visited(410);
 int minCost = -1;
 
-void dfs(int i, int length, int previous, int cost)
-{
-    if (minCost != -1 && length >= 2 && cost > minCost)
-        return;
-
-    if (previous != -1 && length >= 2 && edges[i].size() == 1)
-    {
+void dfs(int i, int length, int previous, int cost) {
+    if (minCost != -1 && length >= 2 && cost > minCost) return;
+    if (previous != -1 && length >= 2 && edges[i].size() == 1) {
         if (minCost == -1) minCost = cost;
         else minCost = min(minCost, cost);
-    }
-    else
-    {
-        for (auto e : edges[i])
-        {
+    } else {
+        for (auto e : edges[i]) {
             if (visited[e.end]) continue;
             visited[i] = 1;
-            if (previous == -1)
-                dfs(e.end, length + 1, e.height, cost + D);
-            else
-                dfs(e.end, length + 1, e.height, cost + D + abs(e.height - previous));
+            if (previous == -1) dfs(e.end, length + 1, e.height, cost + D);
+            else dfs(e.end, length + 1, e.height, cost + D + abs(e.height - previous));
             visited[i] = 0;
         }
     }
 }
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
-
     int cases = 0;
     int start, end, height;
-    
     cin >> cases;
-    for (int c = 1; c <= cases; c++)
-    {
+    for (int c = 1; c <= cases; c++) {
         cin >> M >> N >> D;
-        for (int i = 1; i <= N; i++)
-            edges[i].clear();
-            
-        for (int i = 1; i <= M; i++)
-        {
+        for (int i = 1; i <= N; i++) edges[i].clear();
+        for (int i = 1; i <= M; i++) {
             cin >> start >> end >> height;
             edges[start].push_back(edge{start, end, height});
             edges[end].push_back(edge{end, start, height});
         }
-
         minCost = -1;
-        for (int i = 1; i <= N; i++)
-        {
+        for (int i = 1; i <= N; i++) {
             if (edges[i].size() != 1) continue;
             fill(visited.begin(), visited.end(), 0);
             dfs(i, 0, -1, 0);
         }
         cout << minCost << '\n';
     }
-    
     return 0;
 }
