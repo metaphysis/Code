cb5b7693ca9a40396556b4ef61852a8de70650b3
Qiu Qiu <metaphysis@yeah.net>
2026-07-14 13:17:03 +0800

    677 All Walks of Length n from the First Node

--- UVa Online Judge/volume006/677 All Walks of Length n from the First Node/program.cpp
+++ UVa Online Judge/volume006/677 All Walks of Length n from the First Node/program.cpp
@@ -1,4 +1,4 @@
-// All Walks of length n from the first node
+// All Walks of Length n from the First Node
 // UVa ID: 677
 // Verdict: Accepted
 // Submission Date: 2017-05-29
@@ -7,27 +7,21 @@
 // 版权所有（C）2017，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int nodes, walks, solutions;
-int linked[20][20], used[20], selected[20], sorted[20];
+int linked[20][20], used[20], selected[20];
 
-void dfs(int depth)
-{
-    if (depth == walks)
-    {
+void dfs(int depth) {
+    if (depth == walks) {
         solutions++;
         cout << "(1";
         for (int i = 1; i < walks; i++)
             cout << ',' << selected[i];
         cout << ")\n";
-    }
-    else
-    {
+    } else {
         for (int i = 2; i <= nodes; i++)
-            if (!used[i] && linked[selected[depth - 1]][i])
-            {
+            if (!used[i] && linked[selected[depth - 1]][i]) {
                 used[i] = 1;
                 selected[depth] = i;
                 dfs(depth + 1);
@@ -36,28 +30,20 @@ void dfs(int depth)
     }
 }
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
-    
     int cases = 0;
-    while (cin >> nodes >> walks)
-    {
+    while (cin >> nodes >> walks) {
         if (cases++ > 0) cout << '\n';
-
         for (int i = 1; i <= nodes; i++)
             for (int j = 1; j <= nodes; j++)
                 cin >> linked[i][j];
-
-        cin >> solutions;
-
+        int separator;
+        cin >> separator; // 读取 -9999
         memset(used, 0, sizeof(used));
         solutions = 0, selected[0] = 1, used[1] = 1, walks += 1;
-
         dfs(1);
-
         if (solutions == 0) cout << "no walk of length " << (walks - 1) << '\n';
     }
-    
     return 0;
 }
