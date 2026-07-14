18fc4ee2713716e76211f393c40dbb89e25451d1
Qiu Qiu <metaphysis@yeah.net>
2026-07-14 13:32:04 +0800

    699 The Falling Leaves

--- UVa Online Judge/volume006/699 The Falling Leaves/program.cpp
+++ UVa Online Judge/volume006/699 The Falling Leaves/program.cpp
@@ -7,45 +7,31 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
 map<int, int> piles;
 
-void parse(int leaves, int position)
-{
-    if (leaves != -1)
-        piles[position] += leaves;
-        
+void parse(int leaves, int position) {
+    if (leaves != -1) piles[position] += leaves;
     cin >> leaves;
-    if (leaves != -1)
-        parse(leaves, position - 1);
-    
+    if (leaves != -1) parse(leaves, position - 1);
     cin >> leaves;
-    if (leaves != -1)
-        parse(leaves, position + 1);
+    if (leaves != -1) parse(leaves, position + 1);
 }
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     int leaves, cases = 0;
-    while (cin >> leaves, leaves != -1)
-    {
+    while (cin >> leaves, leaves != -1) {
         piles.clear();
         parse(leaves, 0);
-        
         cout << "Case " << ++cases << ":\n";
         int length = 0;
-        for (auto pile : piles)
-        {
-            if (length++ > 0)
-                cout << ' ';
+        for (auto pile : piles) {
+            if (length++ > 0) cout << ' ';
             cout << pile.second;
         }
         cout << "\n\n";
     }
-    
-	return 0;
+    return 0;
 }
