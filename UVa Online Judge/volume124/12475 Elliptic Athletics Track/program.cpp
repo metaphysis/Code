72c49c8fbaaa2b42caacb94cd525fe7923147c2a
Qiu Qiu <metaphysis@yeah.net>
2026-07-15 19:09:43 +0800

    12475 Elliptic Athletics Track

--- UVa Online Judge/volume124/12475 Elliptic Athletics Track/program.cpp
+++ UVa Online Judge/volume124/12475 Elliptic Athletics Track/program.cpp
@@ -32,7 +32,7 @@ double simpsonIntegral(double e, int N = 10000) {
 int main() {
     int T;
     scanf("%d", &T);
-    for (int caseNo = 1; caseNo <= T; ++caseNo) {
+    for (int cs = 1; cs <= T; ++cs) {
         int bInt, aInt;
         scanf("%d %d", &bInt, &aInt);
         double a = (double)aInt;
@@ -40,7 +40,7 @@ int main() {
         double e = sqrt(1.0 - (b * b) / (a * a));   // 离心率
         double integral = simpsonIntegral(e);       // 第二类完全椭圆积分 E(e)
         double circumference = 4.0 * a * integral;  // 椭圆周长
-        printf("Case %d: %.6f\n", caseNo, circumference);
+        printf("Case %d: %.6f\n", cs, circumference);
     }
     return 0;
 }
