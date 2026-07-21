24b90be3aa713839559e9ec4e216fe9305a357a6
Qiu Qiu <metaphysis@yeah.net>
2026-07-22 07:06:18 +0800

    Update program.cpp

--- UVa Online Judge/volume004/466 Mirror Mirror/program.cpp
+++ UVa Online Judge/volume004/466 Mirror Mirror/program.cpp
@@ -7,11 +7,9 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-string rotateCW90(string matrix, int n)
-{
+string rotateCW90(string matrix, int n) {
     string newMatrix;
     for (int i = 1; i <= n; i++)
         for (int j = 1; j <= n; j++)
@@ -19,8 +17,7 @@ string rotateCW90(string matrix, int n)
     return newMatrix;
 }
 
-string rotateCCW90(string matrix, int n)
-{
+string rotateCCW90(string matrix, int n) {
     string newMatrix;
     for (int i = 1; i <= n; i++)
         for (int j = 1; j <= n; j++)
@@ -28,98 +25,70 @@ string rotateCCW90(string matrix, int n)
     return newMatrix;
 }
 
-string rotate180(string matrix)
-{
+string rotate180(string matrix) {
     reverse(matrix.begin(), matrix.end());
     return matrix;
 }
 
-string reflect(string matrix, int n)
-{
+string reflect(string matrix, int n) {
     string newMatrix;
     for (int i = 1; i <= n; i++)
-        for (int j = 1;j <= n; j++)
+        for (int j = 1; j <= n; j++)
             newMatrix += matrix[(n - i) * n + (j - 1)];
     return newMatrix;
 }
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     int n, cases = 0;
-    while (cin >> n)
-    {
+    while (cin >> n) {
         cout << "Pattern " << ++cases << " was ";
-
         string original, transformed;
         char letter;
-        for (int i = 1; i <= n; i++)
-        {
-            for (int j = 1; j <= n; j++)
-            {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
                 cin >> letter;
                 original.push_back(letter);
             }
-
-            for (int j = 1; j <= n; j++)
-            {
+            for (int j = 1; j <= n; j++) {
                 cin >> letter;
                 transformed.push_back(letter);
             }
         }
-        
-        if (original == transformed)
-        {
+        if (original == transformed) {
             cout << "preserved.\n";
             continue;
         }
-        
-        if (rotateCW90(original, n) == transformed)
-        {
+        if (rotateCW90(original, n) == transformed) {
             cout << "rotated 90 degrees.\n";
             continue;
         }
-        
-        if (rotate180(original) == transformed)
-        {
+        if (rotate180(original) == transformed) {
             cout << "rotated 180 degrees.\n";
             continue;
         }
-        
-        if (rotateCCW90(original, n) == transformed)
-        {
+        if (rotateCCW90(original, n) == transformed) {
             cout << "rotated 270 degrees.\n";
             continue;
         }
-        
         original = reflect(original, n);
-        if (original == transformed)
-        {
+        if (original == transformed) {
             cout << "reflected vertically.\n";
             continue;
         }
-        
-        if (rotateCW90(original, n) == transformed)
-        {
+        if (rotateCW90(original, n) == transformed) {
             cout << "reflected vertically and rotated 90 degrees.\n";
             continue;
         }
-        
-        if (rotate180(original) == transformed)
-        {
+        if (rotate180(original) == transformed) {
             cout << "reflected vertically and rotated 180 degrees.\n";
             continue;
         }
-        
-        if (rotateCCW90(original, n) == transformed)
-        {
+        if (rotateCCW90(original, n) == transformed) {
             cout << "reflected vertically and rotated 270 degrees.\n";
             continue;
         }
-        
         cout << "improperly transformed.\n";
     }
-    
-	return 0;
+    return 0;
 }
