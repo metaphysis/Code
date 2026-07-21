c080621269c25fe070d5353609196ee14b7447c2
Qiu Qiu <metaphysis@yeah.net>
2026-07-22 06:59:26 +0800

    Update program.cpp

--- UVa Online Judge/volume004/468 Key to Success/program.cpp
+++ UVa Online Judge/volume004/468 Key to Success/program.cpp
@@ -7,71 +7,46 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-struct item
-{
+struct item {
     char letter;
     int frequency;
-    
-    bool operator<(const item& another) const
-    {
-        if (frequency != another.frequency)
-            return frequency > another.frequency;
-        else
-            return letter < another.letter;
+    bool operator<(const item& another) const {
+        if (frequency != another.frequency) return frequency > another.frequency;
+        else return letter < another.letter;
     }
 };
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     string line;
     getline(cin, line);
-    
     int cases = stoi(line);
-    for (int i = 1; i <= cases; i++)
-    {
-        if (i > 1)
-            cout << '\n';
-
+    for (int i = 1; i <= cases; i++) {
+        if (i > 1) cout << '\n';
         getline(cin, line);
-        
         string plain_line, encoded_line;
         map<char, int> plain, encoded;
         getline(cin, plain_line);
         for (int j = 0; j < plain_line.length(); j++)
             if (isalpha(plain_line[j]))
                 plain[plain_line[j]]++;
-                
         getline(cin, encoded_line);
         for (int j = 0; j < encoded_line.length(); j++)
             if (isalpha(encoded_line[j]))
                 encoded[encoded_line[j]]++;
-                
         vector<item> text1, text2;
-        for (auto p : plain)
-            text1.push_back((item){p.first, p.second});
-        for (auto p : encoded)
-            text2.push_back((item){p.first, p.second});
-
+        for (auto p : plain) text1.push_back((item){p.first, p.second});
+        for (auto p : encoded) text2.push_back((item){p.first, p.second});
         sort(text1.begin(), text1.end());
         sort(text2.begin(), text2.end());
-        
         map<char, char> code;
-        for (int j = 0; j < text1.size(); j++)
-            code[text2[j].letter] = text1[j].letter;
-            
+        for (int j = 0; j < text1.size(); j++) code[text2[j].letter] = text1[j].letter;
         for (auto c : encoded_line)
-            if (isalpha(c))
-                cout << code[c];
-            else
-                cout << c;
-                    
+            if (isalpha(c)) cout << code[c];
+            else cout << c;
         cout << '\n';
     }
-    
-	return 0;
+    return 0;
 }
