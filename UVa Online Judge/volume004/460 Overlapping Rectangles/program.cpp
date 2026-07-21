7de5c731271938bb0e53eda1c472bb28bffe0f23
Qiu Qiu <metaphysis@yeah.net>
2026-07-22 07:20:57 +0800

    Update program.cpp

--- UVa Online Judge/volume004/460 Overlapping Rectangles/program.cpp
+++ UVa Online Judge/volume004/460 Overlapping Rectangles/program.cpp
@@ -7,30 +7,20 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     int cases;
     cin >> cases;
-    
     int x1, y1, x2, y2, x3, y3, x4, y4;
-    for (int i = 1; i <= cases; i++)
-    {
+    for (int i = 1; i <= cases; i++) {
         if (i > 1) cout << '\n';
-        
         cin >> x1 >> y1 >> x2 >> y2;
         cin >> x3 >> y3 >> x4 >> y4;
-        
         int lowx = max(x1, x3), lowy = max(y1, y3), upx = min(x2, x4), upy = min(y2, y4);
-        if (lowx >= upx || lowy >= upy)
-            cout << "No Overlap\n";
-        else
-            cout << lowx << ' ' << lowy << ' ' << upx << ' ' << upy << '\n';
+        if (lowx >= upx || lowy >= upy) cout << "No Overlap\n";
+        else cout << lowx << ' ' << lowy << ' ' << upx << ' ' << upy << '\n';
     }
-    
-	return 0;
+    return 0;
 }
