a643f5ec9bebfabb75f2935c3736a273b8dd4982
Qiu Qiu <metaphysis@yeah.net>
2026-07-22 06:57:10 +0800

    Update program.cpp

--- UVa Online Judge/volume004/469 Wetlands of Florida/program.cpp
+++ UVa Online Judge/volume004/469 Wetlands of Florida/program.cpp
@@ -7,31 +7,25 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
 char matrix[110][110];
 int counter, row = 0, column = 0;
 
-void restore()
-{
+void restore() {
     for (int i = 1; i <= row; i++)
         for (int j = 1; j <= column; j++)
             if (matrix[i][j] == 'X')
                 matrix[i][j] = 'W';
 }
 
-void flood_fill(int r, int c)
-{
-    if (r >= 1 && r <= row && c >= 1 && c <= column)
-    {
-        if (matrix[r][c] == 'W')
-        {
+void flood_fill(int r, int c) {
+    if (r >= 1 && r <= row && c >= 1 && c <= column) {
+        if (matrix[r][c] == 'W') {
             counter++;
             matrix[r][c] = 'X';
             for (int i = -1; i <= 1; i++)
-                for (int j = -1; j <= 1; j++)
-                {
+                for (int j = -1; j <= 1; j++) {
                     if (i == 0 && j == 0) continue;
                     flood_fill(r + i, c + j);
                 }
@@ -39,35 +33,24 @@ void flood_fill(int r, int c)
     }
 }
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     string line;
     getline(cin, line);
     int cases = stoi(line);
-    
     getline(cin, line);
-    for (int i = 1; i <= cases; i++)
-    {
+    for (int i = 1; i <= cases; i++) {
         if (i > 1) cout << endl;
-        
         row = 0;
-        while (getline(cin, line), line.length() > 0)
-        {
-            if (isalpha(line.front()))
-            {
-                for (int j = 0; j < line.length(); j++)
-                    matrix[row + 1][j + 1] = line[j];
+        while (getline(cin, line), line.length() > 0) {
+            if (isalpha(line.front())) {
+                for (int j = 0; j < line.length(); j++) matrix[row + 1][j + 1] = line[j];
                 row++;
                 column = line.length();
-            }
-            else
-            {
+            } else {
                 istringstream iss(line);
                 int cell_row, cell_column;
                 iss >> cell_row >> cell_column;
-                
                 counter = 0;
                 flood_fill(cell_row, cell_column);
                 restore();
@@ -75,6 +58,5 @@ int main(int argc, char *argv[])
             }
         }
     }
-    
-	return 0;
+    return 0;
 }
