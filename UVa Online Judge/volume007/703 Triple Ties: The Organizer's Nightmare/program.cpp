462dbf144e8938c83490517c5780bd0277c76b9c
Qiu Qiu <metaphysis@yeah.net>
2026-08-18 08:22:49 +0800

    Update program.cpp

--- UVa Online Judge/volume007/703 Triple Ties: The Organizer's Nightmare/program.cpp
+++ UVa Online Judge/volume007/703 Triple Ties: The Organizer's Nightmare/program.cpp
@@ -7,47 +7,36 @@
 // 版权所有（C）2017，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-struct group
-{
+struct group {
     int a, b, c;
 };
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0), cout.tie(0), ios::sync_with_stdio(false);
-
     int n, maze[110][110];
-    while (cin >> n)
-    {
+    while (cin >> n) {
         memset(maze, 0, sizeof(maze));
         for (int i = 1; i <= n; i++)
             for (int j = 1; j <= n; j++)
                 cin >> maze[i][j];
-                
-        vector<group> groups;
+         vector<group> groups;
         for (int i = 1; i <= n; i++)
             for (int j = 1; j <= n; j++)
-                for (int k = 1; k <= n; k++)
-                {
+                for (int k = 1; k <= n; k++) {
                     if (i == j || i == k || j == k) continue;
-                    if (maze[i][j] + maze[j][i] + maze[j][k] + maze[k][j] + maze[k][i] + maze[i][k] == 0)
-                    {
+                    if (maze[i][j] + maze[j][i] + maze[j][k] + maze[k][j] + maze[k][i] + maze[i][k] == 0) {
                         if (i < j && j < k)
                             groups.push_back(group{i, j, k});
                     }
-                    if (maze[i][j] + maze[j][k] + maze[k][i] == 3)
-                    {
+                    if (maze[i][j] + maze[j][k] + maze[k][i] == 3) {
                         if ((i < j && j < k) || (i > j && j > k))
                             groups.push_back(group{i, j, k});
                     }
                 }
-
         cout << groups.size() << '\n';
         for (auto t : groups) cout << t.a << ' ' << t.b << ' ' << t.c << '\n';     
     }
-    
     return 0;
 }
