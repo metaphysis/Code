08ef23494657aaf9de3faf8704ab6ac65c5d77cc
Qiu Qiu <metaphysis@yeah.net>
2026-08-19 14:55:31 +0800

    Update program.cpp

--- UVa Online Judge/volume007/706 LC-Display/program.cpp
+++ UVa Online Judge/volume007/706 LC-Display/program.cpp
@@ -7,11 +7,9 @@
 // 版权所有（C）2011，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-void lcdDisplay(int size, string number)
-{
+void lcdDisplay(int size, string number) {
     // 定义每个数字的关键笔划。
     string outline[5][10] = {
         " - ", "   ", " - ", " - ", "   ", " - ", " - ", " - ", " - ", " - ",
@@ -20,12 +18,11 @@ void lcdDisplay(int size, string number)
         "| |", "  |", "|  ", "  |", "  |", "  |", "| |", "  |", "| |", "  |",
         " - ", "   ", " - ", " - ", "   ", " - ", " - ", "   ", " - ", " - "
     };
-
     // 根据指定size将关键笔划扩大显示，实际上就是将关键笔划重复size次。
-    for (int row = 1; row <= (2 * size + 3); row++)
-    {
-        for (int i = 0; i < number.length(); i++)
-        {
+    for (int row = 1; row <= (2 * size + 3); row++) {
+        for (int i = 0; i < number.length(); i++) {
+            // 在两个数字间要有一个空格。
+            if (i) cout << ' ';
             string line;
             int column = number[i] - '0';
             if (row == 1) line = outline[0][column];
@@ -33,30 +30,21 @@ void lcdDisplay(int size, string number)
             if (row == (size + 2)) line = outline[2][column];
             if ((size + 3) <= row && row <= (2 * size + 2)) line = outline[3][column];
             if (row == (2 * size + 3)) line = outline[4][column];
-
             // 输出关键笔划。
             cout << line[0];
             for (int j = 0; j < size; j++) cout << line[1];
             cout << line[2];
-
-            // 在两个数字间要有一列空行。
-            if (i < 7) cout << " ";
         }
-
         cout << '\n';
     }
 }
 
-int main(int argc, char *argv[])
-{
+int main() {
     int size;
     string number;
-
-    while (cin >> size >> number, size)
-    {
+    while (cin >> size >> number, size) {
         lcdDisplay(size, number);
         cout << endl;
     }
-
     return 0;
 }
