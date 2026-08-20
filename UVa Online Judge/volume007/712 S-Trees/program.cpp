53af07aeb67f3b65ae8b060b1ca8690e0186d413
Qiu Qiu <metaphysis@yeah.net>
2026-08-20 10:01:41 +0800

    Update program.cpp

--- UVa Online Judge/volume007/712 S-Trees/program.cpp
+++ UVa Online Judge/volume007/712 S-Trees/program.cpp
@@ -7,47 +7,32 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int n, m, cases = 0, level[10], base;
 string variable, terminal, vva;
 
-char travel(int i, int depth)
-{
+char travel(int i, int depth) {
     if (depth == n) return terminal[i - base];
-    if (vva[level[depth]] == '0')
-        return travel(2 * i + 1, depth + 1);
-    else
-        return travel(2 * i + 2, depth + 1);
+    if (vva[level[depth]] == '0') return travel(2 * i + 1, depth + 1);
+    else return travel(2 * i + 2, depth + 1);
 }
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-    
-    while (cin >> n, n > 0)
-    {
+    while (cin >> n, n > 0) {
         cout << "S-Tree #" << ++cases << ":\n";
-        
         base = pow(2, n) - 1;
-
-        for (int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < n; i++) {
             cin >> variable;
             level[i] = variable.back() - '0' - 1;
         }
-
         cin >> terminal >> m;
-
-        for (int i = 1; i <= m; i++)
-        {
+        for (int i = 1; i <= m; i++) {
             cin >> vva;
             cout << travel(0, 0);
         }
-        
         cout << "\n\n";
     }
-    
-	return 0;
+    return 0;
 }
