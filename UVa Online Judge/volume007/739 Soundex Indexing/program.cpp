db967e96da6ad9aa6e77919f6d12052a099a1e2c
Qiu Qiu <metaphysis@yeah.net>
2026-08-24 11:14:53 +0800

    Update program.cpp

--- UVa Online Judge/volume007/739 Soundex Indexing/program.cpp
+++ UVa Online Judge/volume007/739 Soundex Indexing/program.cpp
@@ -10,38 +10,27 @@
 
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     int digits[26] = {0, 1, 2, 3, 0, 1, 2, 0, 0, 2, 2, 4, 5, 5, 0, 1, 2, 6, 2, 3, 0, 1, 0, 2, 0, 2};
-    
     cout << string(9, ' ') << setw(25) << left << "NAME" << "SOUNDEX CODE\n";
-    
     string name;
-    while (cin >> name)
-    {
-        string code, code_next, code_last;
-        for (int i = 0; i < name.length(); i++)
-            code += (char)('0' + digits[name[i] - 'A']);
-                
-        code_next += code.front();
-        for (int i = 0; i < code.length(); i++)
-            if (code[i] != code_next.back())
-                code_next += code[i];
-
-        code_last += name.front();
-        for (int i = 1; i < code_next.length(); i++)
-            if (code_next[i] != '0')
-                code_last += code_next[i];
-                
-        while (code_last.length() < 4) code_last.push_back('0');
-        if (code_last.length() > 4) code_last = code_last.substr(0, 4);
-        
-        cout << string(9, ' ') << setw(25) << left << name << code_last << '\n';
+    while (cin >> name) {
+        string code;
+        code += name.front();
+        int last = digits[name.front() - 'A'];
+        for (int i = 1; i < name.length(); i++) {
+            int d = digits[name[i] - 'A'];
+            if (d == 0) last = 0;
+            else if (d != last) {
+                code += char('0' + d);
+                last = d;
+            }
+        }
+        while (code.length() < 4) code.push_back('0');
+        if (code.length() > 4) code = code.substr(0, 4);
+        cout << string(9, ' ') << setw(25) << left << name << code << '\n';
     }
-    
     cout << string(19, ' ') << "END OF OUTPUT\n";
-    
-	return 0;
+    return 0;
 }
