22b80abe5f4c57b31b82c476a9328b6140f738c4
Qiu Qiu <metaphysis@yeah.net>
2026-09-07 11:01:33 +0800

    Update program.cpp

--- UVa Online Judge/volume008/855 Lunch in Grid City/program.cpp
+++ UVa Online Judge/volume008/855 Lunch in Grid City/program.cpp
@@ -7,31 +7,20 @@
 // 版权所有（C）2016，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     int T, S, A, F, x[50010], y[50010], bestS, bestA;
-    
     cin >> T;
-    for (int c = 1; c <= T; c++)
-    {
+    for (int c = 1; c <= T; c++) {
         cin >> S >> A >> F;
         for (int i = 0; i < F; i++) cin >> x[i] >> y[i];
-            
         sort(x, x + F); sort(y, y + F);
-                
-        if (F % 2 == 0)
-            bestS = x[F / 2 - 1], bestA = y[F / 2 - 1];
-        else
-            bestS = x[F / 2], bestA = y[F / 2];
-        
+        if (F % 2 == 0) bestS = x[F / 2 - 1], bestA = y[F / 2 - 1];
+        else bestS = x[F / 2], bestA = y[F / 2];
         cout << "(Street: " << bestS;
         cout << ", Avenue: " << bestA << ")\n";
     }
-    
-	return 0;
+    return 0;
 }
