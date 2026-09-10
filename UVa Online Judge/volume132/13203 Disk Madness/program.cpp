e09324dd0aa02aa076ab24743fdde813ff6e3e91
Qiu Qiu <metaphysis@yeah.net>
2026-09-10 16:47:21 +0800

    Create program.cpp

---
+++ UVa Online Judge/volume132/13203 Disk Madness/program.cpp
@@ -0,0 +1,14 @@
+// Disk Madness
+// UVa ID: 13203
+// Verdict: Accepted
+// Submission Date: 2026-07-30
+// UVa Run Time: 0.000s
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long n;
+    while (cin >> n) cout << (n - 1) * (n - 2) / 2 << '\n';
+    return 0;
+}
