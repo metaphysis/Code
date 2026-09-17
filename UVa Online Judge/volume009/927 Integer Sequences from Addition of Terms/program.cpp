4ea6ebde37fa55cd1c05860ea2e96424a4eff955
Qiu Qiu <metaphysis@yeah.net>
2026-09-17 11:14:31 +0800

    Update program.cpp

--- UVa Online Judge/volume009/927 Integer Sequences from Addition of Terms/program.cpp
+++ UVa Online Judge/volume009/927 Integer Sequences from Addition of Terms/program.cpp
@@ -7,49 +7,31 @@
 // 版权所有（C）2017，邱秋。metaphysis # yeah dot net
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main() {
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-
     vector<int> total;
-    
-    for (int i = 0; i <= 2000; i++)
-        total.push_back(i * (i + 1) / 2);
-
+    for (int i = 0; i <= 2000; i++) total.push_back(i * (i + 1) / 2);
     int cases = 0;
-    
     cin >> cases;
-    for (int c = 1; c <= cases; c++)
-    {
+    for (int c = 1; c <= cases; c++) {
         int coefficients[30], degree;
-
         cin >> degree;
-        for (int i = 0; i <= degree; i++)
-            cin >> coefficients[i];
-        
+        for (int i = 0; i <= degree; i++) cin >> coefficients[i];
         int d, k;
-
         cin >> d >> k;
-        for (int i = 1; i <= 2000; i++)
-        {
-            if (total[i] * d >= k)
-            {
+        for (int i = 1; i <= 2000; i++) {
+            if (total[i] * d >= k) {
                 unsigned long long int n = coefficients[0], power = 1;
-
-                for (int k = 1; k <= degree; k++)
-                {
+                for (int k = 1; k <= degree; k++) {
                     power *= i;
                     n += coefficients[k] * power;
                 }
-                
                 cout << n << '\n';
                 break;
             }
         }
     }
-    
     return 0;
 }
